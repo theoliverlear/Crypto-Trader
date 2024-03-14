4a4a7b1d95a5e854d58720226e6bea882de23334
theoliverlear <sigwartholiver2@gmail.com>
2024-03-14 15:32:31 -0500

    Add ApiDataRetriever.java to get data from APIs through HTTP requests
    Took 28 minutes

---
+++ src/main/java/org/theoliverlear/model/http/ApiDataRetriever.java
@@ -0,0 +1,10 @@
+package org.theoliverlear.model.http;
+
+public class ApiDataRetriever {
+    String url;
+    String response;
+    public ApiDataRetriever(String url) {
+        this.url = url;
+    }
+
+}
