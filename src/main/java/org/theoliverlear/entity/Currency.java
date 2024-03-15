37c67670998e986acbdc39af61b4eb114d555c03
theoliverlear <sigwartholiver2@gmail.com>
2024-03-15 12:15:12 -0500

    Add updating methods and getters and setters for Currency.java
    Took 1 minute

--- src/main/java/org/theoliverlear/entity/Currency.java
+++ src/main/java/org/theoliverlear/entity/Currency.java
@@ -5,11 +5,26 @@ import org.theoliverlear.model.http.ApiDataRetriever;
 import java.text.DecimalFormat;
 
 public class Currency {
+    // TODO: Instead of enums, perhaps have static constants instantiated
+    //       during runtime.
+    // TODO: Make a change calculator, static or nay, that takes two
+    //       currencies, or passes one with this keyword, and returns the
+    //       difference price and percentage change.
     //============================-Variables-=================================
-    String name, currencyCode, urlPath, formattedValue;
-    double value;
-    DecimalFormat decimalFormat = new DecimalFormat("##,#00.00000000");
+    private String name;
+    private String currencyCode;
+    private String urlPath;
+    private String formattedValue;
+    private double value;
+    private DecimalFormat decimalFormat = new DecimalFormat("##,#00.00000000");
     //===========================-Constructors-===============================
+    public Currency(String name, String currencyCode, String urlPath) {
+        this.name = name;
+        this.currencyCode = currencyCode;
+        this.urlPath = urlPath;
+        this.value = this.getApiValue();
+        this.formattedValue = this.decimalFormat.format(this.value);
+    }
     public Currency(String name, String currencyCode, double value, String urlPath) {
         this.name = name;
         this.currencyCode = currencyCode;
@@ -18,25 +33,44 @@ public class Currency {
         this.formattedValue = this.decimalFormat.format(value);
     }
     //=============================-Methods-==================================
+
+    //----------------------------Update-Value--------------------------------
+    public void updateValue() {
+        this.value = this.getApiValue();
+        this.formattedValue = this.formatValue(this.value);
+    }
+    //---------------------------Get-Api-Value--------------------------------
+    public double getApiValue() {
+        String currencyApiJson = this.getCurrencyApiJson();
+        return this.getValueFromJson(currencyApiJson);
+    }
+    //----------------------------Format-Value--------------------------------
     public String formatValue(double value) {
         return this.decimalFormat.format(value);
     }
+    //-----------------------Get-Currency-Api-Json----------------------------
     public String getCurrencyApiJson() {
         ApiDataRetriever apiDataRetriever = new ApiDataRetriever(this.urlPath);
         return apiDataRetriever.getResponse();
     }
+    //------------------------Get-Value-From-Json-----------------------------
     public double getValueFromJson(String json) {
         StringBuilder currencyJson = new StringBuilder(json);
-        int indexOfAmount = currencyJson.indexOf("amount");
+        String amountKey = "amount\":\"";
+        int amountLength = amountKey.length();
+        int indexOfAmount = currencyJson.indexOf(amountKey) + amountLength;
         int endIndex = currencyJson.length();
         currencyJson.delete(0, indexOfAmount);
-        int indexOfCloseCurly = currencyJson.indexOf("}");
-        currencyJson.delete(indexOfCloseCurly, endIndex);
-        String quoteRemove = currencyJson.toString().replace("\"", "");
-        String[] keyValueSplit = quoteRemove.split(":");
-        String currencyValueString = keyValueSplit[1];
+        int indexOfCloseQuote = currencyJson.indexOf("\"");
+        currencyJson.delete(indexOfCloseQuote, endIndex);
+        String currencyValueString = currencyJson.toString();
         double currencyValue = Double.parseDouble(currencyValueString);
         return currencyValue;
+    }
+    //-------------------------Get-Updated-Value------------------------------
+    public double getUpdatedValue() {
+        this.updateValue();
+        return this.value;
     }
     //============================-Overrides-=================================
 
@@ -45,8 +79,44 @@ public class Currency {
     //------------------------------Hash-Code---------------------------------
 
     //------------------------------To-String---------------------------------
-
+    @Override
+    public String toString() {
+        String currencyString = """
+                %s - %s - $%s""".formatted(this.name, this.currencyCode,
+                                          this.formattedValue);
+        return currencyString;
+    }
     //=============================-Getters-==================================
-
+    public String getName() {
+        return this.name;
+    }
+    public String getCurrencyCode() {
+        return this.currencyCode;
+    }
+    public String getUrlPath() {
+        return this.urlPath;
+    }
+    public String getFormattedValue() {
+        return this.formattedValue;
+    }
+    public double getValue() {
+        return this.value;
+    }
     //=============================-Setters-==================================
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setCurrencyCode(String currencyCode) {
+        this.currencyCode = currencyCode;
+    }
+    public void setUrlPath(String urlPath) {
+        this.urlPath = urlPath;
+    }
+    public void setValue(double value) {
+        this.value = value;
+    }
+    public void setFormattedValue(String formattedValue) {
+        this.formattedValue = formattedValue;
+    }
+
 }
