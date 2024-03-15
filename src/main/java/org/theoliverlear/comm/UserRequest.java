054418a9f882677e93309f9faac1701fd8ae1625
theoliverlear <sigwartholiver2@gmail.com>
2024-03-15 16:10:54 -0500

    Add UserRequest.java to represent a request a user makes to log in or sign up
    Took 1 minute

---
+++ src/main/java/org/theoliverlear/comm/UserRequest.java
@@ -0,0 +1,29 @@
+package org.theoliverlear.comm;
+
+public class UserRequest {
+    private String username;
+    private String password;
+    //============================-Constructors-==============================
+    public UserRequest() {
+        this.username = "";
+        this.password = "";
+    }
+    public UserRequest(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+    //=============================-Getters-==================================
+    public String getUsername() {
+        return this.username;
+    }
+    public String getPassword() {
+        return this.password;
+    }
+    //=============================-Setters-==================================
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
