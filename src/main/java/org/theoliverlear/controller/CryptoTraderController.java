e45f3317a328e71affbd8edd58b2021281619d3c
theoliverlear <sigwartholiver2@gmail.com>
2024-03-15 16:00:18 -0500

    Add beans comment header in headers.txt
    Took 1 hour 22 minutes

---
+++ src/main/java/org/theoliverlear/controller/CryptoTraderController.java
@@ -0,0 +1,27 @@
+package org.theoliverlear.controller;
+
+import jakarta.persistence.Column;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.theoliverlear.comm.UserRequest;
+import org.theoliverlear.entity.User;
+import org.theoliverlear.service.CryptoTraderService;
+
+@Controller
+public class CryptoTraderController {
+    @Autowired
+    CryptoTraderService cryptoTraderService;
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+    @RequestMapping("/signup")
+    public ResponseEntity<String> signup(@ModelAttribute UserRequest userRequest) {
+        User newUser = new User(userRequest.getUsername(), userRequest.getPassword(), 1L);
+        this.cryptoTraderService.saveUser(newUser);
+        return ResponseEntity.ok("User " + userRequest.getUsername() + " has been signed up!");
+    }
+}
