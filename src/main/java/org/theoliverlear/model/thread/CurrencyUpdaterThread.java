bb0fc3463c04ccfe24a281f076ce32db17f005b7
theoliverlear <sigwartholiver2@gmail.com>
2024-03-15 12:12:52 -0500

    Begin functionality of CurrencyUpdaterThread.java with a run() method
    Took 1 minute

--- src/main/java/org/theoliverlear/model/thread/CurrencyUpdaterThread.java
+++ src/main/java/org/theoliverlear/model/thread/CurrencyUpdaterThread.java
@@ -1,4 +1,22 @@
 package org.theoliverlear.model.thread;
 
-public class CurrencyUpdaterThread {
+import org.theoliverlear.entity.Currency;
+
+public class CurrencyUpdaterThread extends Thread implements Runnable {
+    Currency currency;
+    public CurrencyUpdaterThread(Currency currency) {
+        this.currency = currency;
+    }
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                this.currency.updateValue();
+                System.out.println(this.currency);
+                Thread.sleep(5000);
+            } catch (InterruptedException ex) {
+                throw new RuntimeException(ex);
+            }
+        }
+    }
 }
