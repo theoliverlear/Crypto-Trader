05b7850c9fc1efe90b74e6b6e93d0caecea4a713
theoliverlear <sigwartholiver2@gmail.com>
2024-03-15 16:09:32 -0500

    Add UserRepository.java as a JPA repository to access user data
    Took 1 minute

---
+++ src/main/java/org/theoliverlear/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package org.theoliverlear.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.theoliverlear.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User getUserByUsername(String username);
+    User getUserById(Long id);
+}
