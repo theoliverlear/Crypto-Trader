89dff3e216fce2eac8ff67ee719af02d6b83645c
theoliverlear <sigwartholiver2@gmail.com>
2024-03-15 12:13:52 -0500

    Add CryptoTrader.java to be a mechanism for performing trades
    Took 1 minute

--- src/main/java/org/theoliverlear/CryptoTrader.java
+++ src/main/java/org/theoliverlear/CryptoTrader.java
@@ -1,4 +1,14 @@
 package org.theoliverlear;
 
+import org.theoliverlear.entity.Currency;
+import org.theoliverlear.model.thread.CurrencyUpdaterThread;
+
+import java.util.Scanner;
+
 public class CryptoTrader {
+    public static void main(String[] args) {
+        Currency bitcoin = new Currency("Bitcoin", "BTC", "https://api.coinbase.com/v2/prices/BTC-USD/spot");
+        CurrencyUpdaterThread currencyUpdaterThread = new CurrencyUpdaterThread(bitcoin);
+        currencyUpdaterThread.start();
+    }
 }
