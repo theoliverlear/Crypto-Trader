cf3c3072d22fb9be036876bb8e527ae39e60aa46
theoliverlear <sigwartholiver2@gmail.com>
2024-03-17 16:27:34 -0500

    Add comment headers, JPA integrations, and begin trading initialization functionality in CryptoTraderService.java
    Took 1 minute

--- src/main/java/org/theoliverlear/service/CryptoTraderService.java
+++ src/main/java/org/theoliverlear/service/CryptoTraderService.java
@@ -1,21 +1,42 @@
 package org.theoliverlear.service;
-
+//=================================-Imports-==================================
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.theoliverlear.entity.Portfolio;
 import org.theoliverlear.entity.User;
+import org.theoliverlear.model.trade.Trader;
+import org.theoliverlear.repository.PortfolioRepository;
 import org.theoliverlear.repository.UserRepository;
 
 @Service
 public class CryptoTraderService {
+    //============================-Variables-=================================
     UserRepository userRepository;
+    PortfolioRepository portfolioRepository;
+    //===========================-Constructors-===============================
     @Autowired
-    public CryptoTraderService(UserRepository userRepository) {
+    public CryptoTraderService(UserRepository userRepository,
+                               PortfolioRepository portfolioRepository) {
         this.userRepository = userRepository;
+        this.portfolioRepository = portfolioRepository;
     }
+    //=============================-Methods-==================================
     @Transactional
     public void saveUser(User user) {
         this.userRepository.save(user);
     }
-
+    @Transactional
+    public User getUser(String username) {
+        return this.userRepository.getUserByUsername(username);
+    }
+    @Transactional
+    public Portfolio getPortfolio(Long userId) {
+        return this.portfolioRepository.findPortfolioByUserId(userId);
+    }
+    public void startTrader(User user) {
+        Portfolio portfolio = this.getPortfolio(user.getId());
+        Trader trader = new Trader(portfolio);
+        trader.tradeAllAssets();
+    }
 }
