8cb4fe4055a3675c77bf54ed1edd4a062b3d399d
theoliverlear <sigwartholiver2@gmail.com>
2024-03-17 16:20:34 -0500

    Begin entity creation and add portfolio value fields in Portfolio.java
    Took 2 minutes

--- src/main/java/org/theoliverlear/entity/Portfolio.java
+++ src/main/java/org/theoliverlear/entity/Portfolio.java
@@ -1,8 +1,34 @@
 package org.theoliverlear.entity;
 
-import java.util.ArrayList;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+@Entity
+@Table(name = "portfolios")
 public class Portfolio {
+    @Id
+    private Long userId;
+    double dollarBalance;
+    double shareBalance;
+    double totalWorth;
+    LocalDateTime lastUpdated;
+    public Portfolio(Long userId) {
+        this.userId = userId;
+        this.dollarBalance = 0;
+        this.shareBalance = 0;
+        this.totalWorth = 0;
+    }
+    public Portfolio(Long userId, double dollarBalance, double shareBalance, double totalWorth) {
+        this.userId = userId;
+        this.dollarBalance = dollarBalance;
+        this.shareBalance = shareBalance;
+        this.totalWorth = totalWorth;
+    }
+    @Transient
     private ArrayList<PortfolioAsset> assets;
     public Portfolio() {
         this.assets = new ArrayList<>();
