078057cebb06c4c5c786bbfc1a9c5bdbeff8cea0
theoliverlear <sigwartholiver2@gmail.com>
2024-03-18 14:48:10 -0500

    Add CurrencyHistoryRepository.java to access a user's past portfolio performance
    Took 1 minute

---
+++ src/main/java/org/theoliverlear/repository/CurrencyHistoryRepository.java
@@ -0,0 +1,15 @@
+package org.theoliverlear.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.theoliverlear.entity.Currency;
+
+@Repository
+public interface CurrencyHistoryRepository extends JpaRepository<Currency, Long> {
+    @Query("SELECT currency FROM currencies currency WHERE currency.currencyCode = ?1")
+    Currency getCurrencyByCurrencyCode(@Param("currency_code") String currencyCode);
+
+
+}
