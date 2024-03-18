a6b697e790c1627c3986cbc119a5719a50154ba7
theoliverlear <sigwartholiver2@gmail.com>
2024-03-18 14:44:49 -0500

    Add comment headers in CryptoTraderController.java
    Took 56 seconds

--- src/main/java/org/theoliverlear/controller/CryptoTraderController.java
+++ src/main/java/org/theoliverlear/controller/CryptoTraderController.java
@@ -1,26 +1,29 @@
 package org.theoliverlear.controller;
 
-import jakarta.persistence.Column;
+//=================================-Imports-==================================
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.theoliverlear.CryptoTrader;
 import org.theoliverlear.comm.UserRequest;
 import org.theoliverlear.entity.User;
 import org.theoliverlear.service.CryptoTraderService;
 
 @Controller
 public class CryptoTraderController {
+    //============================-Variables-=================================
     User currentUser;
-    CryptoTrader cryptoTrader;
     @Autowired
     CryptoTraderService cryptoTraderService;
+    //=============================-Methods-==================================
+
+    //-------------------------------Index------------------------------------
     @RequestMapping("/")
     public String index() {
         return "index";
     }
+    //-------------------------------Login------------------------------------
     @RequestMapping("/signup")
     public ResponseEntity<String> signup(@ModelAttribute UserRequest userRequest) {
         User newUser = new User(userRequest.getUsername(), userRequest.getPassword(), 1L);
