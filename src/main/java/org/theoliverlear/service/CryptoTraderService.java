e865936611b021ad04c4ad5b161859869075549f
theoliverlear <sigwartholiver2@gmail.com>
2024-03-18 14:45:59 -0500

    Add methods integrating JPA and add a crypto trading object to CryptoTraderService.java
    Took 1 minute

--- src/main/java/org/theoliverlear/service/CryptoTraderService.java
+++ src/main/java/org/theoliverlear/service/CryptoTraderService.java
@@ -3,23 +3,39 @@ package org.theoliverlear.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.theoliverlear.CryptoTrader;
 import org.theoliverlear.entity.Portfolio;
 import org.theoliverlear.entity.User;
 import org.theoliverlear.model.trade.Trader;
 import org.theoliverlear.repository.PortfolioRepository;
 import org.theoliverlear.repository.UserRepository;
 
+import java.util.ArrayList;
+
 @Service
 public class CryptoTraderService {
     //============================-Variables-=================================
     UserRepository userRepository;
     PortfolioRepository portfolioRepository;
+    CryptoTrader cryptoTrader;
     //===========================-Constructors-===============================
     @Autowired
     public CryptoTraderService(UserRepository userRepository,
                                PortfolioRepository portfolioRepository) {
         this.userRepository = userRepository;
         this.portfolioRepository = portfolioRepository;
+        this.cryptoTrader = new CryptoTrader();
+        this.initializeCryptoTrader();
+        this.cryptoTrader.startTraders();
+    }
+
+    public void initializeCryptoTrader() {
+        ArrayList<Portfolio> portfolios = this.getPortfolios();
+        ArrayList<Trader> traders = new ArrayList<>();
+        for (Portfolio portfolio : portfolios) {
+            traders.add(new Trader(portfolio));
+        }
+        this.cryptoTrader.addTraders(traders);
     }
     //=============================-Methods-==================================
     @Transactional
@@ -33,6 +49,14 @@ public class CryptoTraderService {
     @Transactional
     public Portfolio getPortfolio(Long userId) {
         return this.portfolioRepository.findPortfolioByUserId(userId);
+    }
+    @Transactional
+    public ArrayList<Portfolio> getPortfolios() {
+        return (ArrayList<Portfolio>) this.portfolioRepository.findAll();
+    }
+    @Transactional
+    public ArrayList<User> getUsers() {
+        return (ArrayList<User>) this.userRepository.findAll();
     }
     public void startTrader(User user) {
         Portfolio portfolio = this.getPortfolio(user.getId());
