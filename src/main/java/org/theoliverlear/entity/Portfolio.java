a480d7102e51e1b67cebd36e6c6f77e928551300
theoliverlear <sigwartholiver2@gmail.com>
2024-03-18 14:56:22 -0500

    Add comment headers, constructor, and equals() method to Portfolio.java
    Took 2 minutes

--- src/main/java/org/theoliverlear/entity/Portfolio.java
+++ src/main/java/org/theoliverlear/entity/Portfolio.java
@@ -16,6 +16,12 @@ public class Portfolio {
     double shareBalance;
     double totalWorth;
     LocalDateTime lastUpdated;
+    @Transient
+    private ArrayList<PortfolioAsset> assets;
+    //===========================-Constructors-===============================
+    public Portfolio() {
+        this.assets = new ArrayList<>();
+    }
     public Portfolio(Long userId) {
         this.userId = userId;
         this.dollarBalance = 0;
@@ -28,14 +34,47 @@ public class Portfolio {
         this.shareBalance = shareBalance;
         this.totalWorth = totalWorth;
     }
-    @Transient
-    private ArrayList<PortfolioAsset> assets;
-    public Portfolio() {
-        this.assets = new ArrayList<>();
+    public Portfolio(Long userId,ArrayList<PortfolioAsset> assets) {
+        this(userId);
+        this.assets = assets;
     }
+    //=============================-Methods-==================================
+    public void updateValues() { // TODO: Make an Updatable interface
+        this.dollarBalance = 0;
+        this.shareBalance = 0;
+        for (PortfolioAsset asset : this.assets) {
+            this.dollarBalance += asset.getAssetWalletDollars();
+            this.shareBalance += asset.getSharesValueInDollars();
+        }
+        this.totalWorth = this.dollarBalance + this.shareBalance;
+        this.lastUpdated = LocalDateTime.now();
+    }
+    //============================-Overrides-=================================
+
+    //------------------------------Equals------------------------------------
+    @Override
+    public boolean equals(Object object) {
+        if (object instanceof Portfolio portfolio) {
+            boolean sameUserId = this.userId.equals(portfolio.userId);
+            boolean sameDollarBalance = this.dollarBalance == portfolio.dollarBalance;
+            boolean sameShareBalance = this.shareBalance == portfolio.shareBalance;
+            boolean sameTotalWorth = this.totalWorth == portfolio.totalWorth;
+            boolean sameLastUpdated = this.lastUpdated.equals(portfolio.lastUpdated);
+            boolean sameAssets = this.assets.equals(portfolio.assets);
+            return  sameUserId && sameDollarBalance && sameShareBalance &&
+                    sameTotalWorth && sameLastUpdated && sameAssets;
+        }
+        return false;
+    }
+    //------------------------------Hash-Code---------------------------------
+
+    //------------------------------To-String---------------------------------
+
+    //=============================-Getters-==================================
     public ArrayList<PortfolioAsset> getAssets() {
         return this.assets;
     }
+    //=============================-Setters-==================================
     public void setAssets(ArrayList<PortfolioAsset> assets) {
         this.assets = assets;
     }
