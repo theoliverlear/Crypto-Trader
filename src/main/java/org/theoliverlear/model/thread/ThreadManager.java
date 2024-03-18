596fcfc311433d386b95593b2215ece62f632da0
theoliverlear <sigwartholiver2@gmail.com>
2024-03-18 15:05:13 -0500

    Implement ThreadManager.java functionality to handle threads as batches of processes
    Took 46 seconds

--- src/main/java/org/theoliverlear/model/thread/ThreadManager.java
+++ src/main/java/org/theoliverlear/model/thread/ThreadManager.java
@@ -1,4 +1,28 @@
 package org.theoliverlear.model.thread;
 
+import java.util.ArrayList;
+
 public class ThreadManager {
+    ArrayList<Thread> threads;
+    public ThreadManager(ArrayList<Thread> threads) {
+        this.threads = threads;
+    }
+    public void startThreads() {
+        for (Thread thread : this.threads) {
+            if (!thread.isAlive()) {
+                thread.start();
+            }
+        }
+    }
+    public void stopThreads() {
+        for (Thread thread : this.threads) {
+            thread.interrupt();
+        }
+    }
+    public void addThread(Thread thread) {
+        this.threads.add(thread);
+    }
+    public void removeThread(Thread thread) {
+        this.threads.remove(thread);
+    }
 }
