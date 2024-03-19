b0fd56b5a455aaa18c2b845b4850133306eac113
theoliverlear <sigwartholiver2@gmail.com>
2024-03-18 19:31:48 -0500

    Refactor trade() to include a boolean return value if a trade was executed in AssetTrader.java
    Took 3 hours 4 minutes

--- src/main/java/org/theoliverlear/model/trade/AssetTrader.java
+++ src/main/java/org/theoliverlear/model/trade/AssetTrader.java
@@ -12,23 +12,25 @@ public class AssetTrader {
     //=============================-Methods-==================================
 
     //-------------------------------Trade------------------------------------
-    public void trade() {
+    public boolean trade() {
         double currentPrice = this.asset.getCurrency().getUpdatedValue();
         double targetPrice = this.asset.getTargetPrice();
         if (currentPrice > targetPrice) {
             if (this.asset.canSell()) {
                 this.sell();
+                return true;
             }
         } else if (currentPrice < targetPrice) {
             if (this.asset.canBuy()) {
                 this.buy();
+                return true;
             }
         }
+        return false;
     }
     //--------------------------------Sell------------------------------------
     public void sell() {
         double valueInDollars = this.asset.getSharesValueInDollars();
-        this.asset.setShares(0);
         double walletDollars = this.asset.getAssetWalletDollars() + valueInDollars;
         this.asset.setAssetWalletDollars(walletDollars);
         this.asset.updateValues();
@@ -37,18 +39,21 @@ public class AssetTrader {
                           this.asset.getShares(),
                           this.asset.getCurrency().getName(),
                           walletDollars);
+        this.asset.setShares(0);
+        this.asset.updateValues();
     }
     //---------------------------------Buy------------------------------------
     public void buy() {
         double shares = this.asset.getAssetWalletDollars() /
                         this.asset.getCurrency().getValue();
         this.asset.setShares(shares);
-        this.asset.setAssetWalletDollars(0);
         this.asset.updateValues();
         this.asset.setTargetPrice(this.asset.getCurrency().getValue());
         System.out.printf("Buying %f shares of %s for %f dollars.%n",
                           shares,
                           this.asset.getCurrency().getName(),
                           this.asset.getAssetWalletDollars());
+        this.asset.setAssetWalletDollars(0);
+        this.asset.updateValues();
     }
 }
