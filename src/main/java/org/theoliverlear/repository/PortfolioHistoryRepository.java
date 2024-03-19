daf30fee1e42c96d403441e5f3704b6abd3436e9
theoliverlear <sigwartholiver2@gmail.com>
2024-03-18 19:49:08 -0500

    Remove JPA repository bean causing runtime errors in PortfolioHistoryRepository.java
    Took 2 minutes

--- src/main/java/org/theoliverlear/repository/PortfolioHistoryRepository.java
+++ src/main/java/org/theoliverlear/repository/PortfolioHistoryRepository.java
@@ -1,9 +1,7 @@
 package org.theoliverlear.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import org.theoliverlear.entity.Portfolio;
 
-@Repository
 public interface PortfolioHistoryRepository extends JpaRepository<Portfolio, Long> {
 }
