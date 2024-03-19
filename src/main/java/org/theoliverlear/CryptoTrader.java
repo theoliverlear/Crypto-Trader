54cee258950323c88629eb5f4b7742f6faff07a2
theoliverlear <sigwartholiver2@gmail.com>
2024-03-18 19:37:46 -0500

    Add TraderThread.java building in CryptoTrader.java
    Took 3 minutes

--- src/main/java/org/theoliverlear/CryptoTrader.java
+++ src/main/java/org/theoliverlear/CryptoTrader.java
@@ -1,6 +1,11 @@
 package org.theoliverlear;
 //=================================-Imports-==================================
+import org.theoliverlear.entity.Portfolio;
+import org.theoliverlear.entity.PortfolioAsset;
+import org.theoliverlear.model.thread.TraderThread;
+import org.theoliverlear.model.thread.ThreadManager;
 import org.theoliverlear.model.trade.Trader;
+import org.theoliverlear.update.SupportedCurrencies;
 
 import java.util.ArrayList;
 
@@ -10,9 +15,23 @@ import java.util.ArrayList;
 public class CryptoTrader {
     //============================-Variables-=================================
     ArrayList<Trader> traders;
+    ArrayList<Thread> traderThreads;
+    ThreadManager traderThreadManager;
     //===========================-Constructors-===============================
     public CryptoTrader() {
         this.traders = new ArrayList<>();
+        this.traderThreads = new ArrayList<>();
+    }
+    public CryptoTrader(ArrayList<Trader> traders) {
+        this.traders = traders;
+        this.traderThreads = new ArrayList<>();
+        this.buildTraderThreads();
+    }
+    public void buildTraderThreads() {
+        for (Trader trader : this.traders) {
+            this.traderThreads.add(new TraderThread(trader));
+        }
+        this.traderThreadManager = new ThreadManager(this.traderThreads);
     }
     public void addTrader(Trader trader) {
         this.traders.add(trader);
@@ -21,12 +40,19 @@ public class CryptoTrader {
         this.traders.addAll(traders);
     }
     public void startTraders() {
-        for (Trader trader : this.traders) {
-            trader.tradeAllAssets();
-        }
+        this.traderThreadManager.startThreads();
+    }
+    public ArrayList<Trader> getTraders() {
+        return this.traders;
     }
     //===============================-Main-===================================
     public static void main(String[] args) {
-        CryptoTrader cryptoTrader = new CryptoTrader();
+        Portfolio portfolio = new Portfolio();
+        portfolio.getAssets().add(new PortfolioAsset(SupportedCurrencies.BITCOIN, 1.0, 0));
+        Trader trader = new Trader(portfolio);
+        ArrayList<Trader> traders = new ArrayList<>();
+        traders.add(trader);
+        CryptoTrader cryptoTrader = new CryptoTrader(traders);
+        cryptoTrader.startTraders();
     }
 }
