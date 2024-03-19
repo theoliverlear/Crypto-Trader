3bcc5a6cc56a308319b34700c30c44043db37450
theoliverlear <sigwartholiver2@gmail.com>
2024-03-18 19:44:58 -0500

    Remove JPA repository bean and comment out code not yet implemented
    Took 2 minutes

--- src/main/java/org/theoliverlear/repository/CurrencyHistoryRepository.java
+++ src/main/java/org/theoliverlear/repository/CurrencyHistoryRepository.java
@@ -6,10 +6,9 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.theoliverlear.entity.Currency;
 
-@Repository
 public interface CurrencyHistoryRepository extends JpaRepository<Currency, Long> {
-    @Query("SELECT currency FROM currencies currency WHERE currency.currencyCode = ?1")
-    Currency getCurrencyByCurrencyCode(@Param("currency_code") String currencyCode);
+//    @Query("SELECT currency FROM currencies WHERE currency.currencyCode = ?1")
+//    Currency getCurrencyByCurrencyCode(@Param("currency_code") String currencyCode);
 
 
 }
