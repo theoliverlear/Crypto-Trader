aba61d06c1a6acfe82d031f64955ebaf9031a985
theoliverlear <sigwartholiver2@gmail.com>
2024-03-18 19:55:15 -0500

    Add getters, setters, and initialization methods to Trader.java
    Took 1 minute

--- src/main/java/org/theoliverlear/model/trade/Trader.java
+++ src/main/java/org/theoliverlear/model/trade/Trader.java
@@ -3,17 +3,31 @@ package org.theoliverlear.model.trade;
 import org.theoliverlear.entity.Portfolio;
 import org.theoliverlear.entity.PortfolioAsset;
 
+import java.util.ArrayList;
 public class Trader {
     // TODO: A trader may take a full portfolio or a single asset. It will
     //       trade based on target prices and a strategy.
     Portfolio portfolio;
+    ArrayList<AssetTrader> assetTraders;
     public Trader(Portfolio portfolio) {
         this.portfolio = portfolio;
+        this.assetTraders = new ArrayList<>();
+        this.initializeAssetTraders();
+    }
+    public void initializeAssetTraders() {
+        for (PortfolioAsset asset : this.portfolio.getAssets()) {
+            this.assetTraders.add(new AssetTrader(asset));
+        }
     }
     public void tradeAllAssets() {
-        for (PortfolioAsset asset : this.portfolio.getAssets()) {
-            AssetTrader assetTrader = new AssetTrader(asset);
+        for (AssetTrader assetTrader : this.assetTraders) {
             assetTrader.trade();
         }
+    }
+    public Portfolio getPortfolio() {
+        return this.portfolio;
+    }
+    public void setPortfolio(Portfolio portfolio) {
+        this.portfolio = portfolio;
     }
 }
