57fd15f834a61d69b6c434fd7f6cd2cb8334b216
theoliverlear <sigwartholiver2@gmail.com>
2024-03-21 15:40:58 -0500

    Add function which changes the shadow's perspective based on the position of the scolling in home-script.js
    Took 56 seconds

--- src/main/resources/static/script/home-script.js
+++ src/main/resources/static/script/home-script.js
@@ -6,6 +6,7 @@ new Chart(document.getElementById("chart-div"), {
       label: 'Year to Date Percentage Increase',
       data: [10, 15, 16, 22, 25, 40],
       backgroundColor: ['hsl(210, 45%, 80%)', 'hsl(210, 45%, 75%)', 'hsl(210, 45%, 70%)', 'hsl(210, 45%, 65%)', 'hsl(210, 45%, 60%)', 'hsl(210, 45%, 55%)'],
+      // backgroundColor: ['hsl(46,40%,74%)', 'hsl(46,50%,74%)', 'hsl(46,60%,74%)', 'hsl(46,70%,74%)', 'hsl(46,80%,74%)', 'hsl(46,90%,74%)'],
       borderWidth: 1
     }]
   },
@@ -19,3 +20,10 @@ new Chart(document.getElementById("chart-div"), {
     }
   }
 });
+let featureTable = document.getElementById('feature-table');
+
+function shadowPerspective() {
+  let scrollPosition = ((window.scrollY / (document.body.scrollHeight - window.innerHeight)) * -1) + 0.8;
+  featureTable.style.boxShadow = `#2c4557 calc(((0.5vh + 0.5vw) / 2) + 0.5em) calc((((1vh + 1vw) / 2) + 1em) * ${scrollPosition}) 0 0`;
+}
+window.addEventListener('scroll', shadowPerspective);
