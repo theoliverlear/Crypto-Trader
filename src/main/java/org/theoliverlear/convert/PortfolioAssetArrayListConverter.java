20caf6daa56088457ca7f157bf59cd5dded24b30
theoliverlear <sigwartholiver2@gmail.com>
2024-04-11 19:18:53 -0500

    Add converter for PortfolioAsset.java files in an ArrayList in PortfolioAssetArrayListConverter.java
    Took 2 minutes

---
+++ src/main/java/org/theoliverlear/convert/PortfolioAssetArrayListConverter.java
@@ -0,0 +1,45 @@
+package org.theoliverlear.convert;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+import org.theoliverlear.entity.PortfolioAsset;
+
+import java.util.ArrayList;
+
+@Converter(autoApply = true)
+public class PortfolioAssetArrayListConverter implements AttributeConverter<ArrayList<PortfolioAsset>, String> {
+    //============================-Variables-=================================
+    ObjectMapper objectMapper;
+    //===========================-Constructors-===============================
+    public PortfolioAssetArrayListConverter() {
+        this.objectMapper = new ObjectMapper();
+    }
+    //============================-Overrides-=================================
+
+    //---------------------Convert-To-Database-Column-------------------------
+    @Override
+    public String convertToDatabaseColumn(ArrayList<PortfolioAsset> portfolioAssets) {
+        try {
+            return this.objectMapper.writeValueAsString(portfolioAssets);
+        } catch (JsonProcessingException ex) {
+            final String EXCEPTION_MESSAGE = "Error converting portfolio assets to JSON.";
+            throw new RuntimeException(EXCEPTION_MESSAGE, ex);
+        }
+    }
+    //--------------------Convert-From-Database-Column------------------------
+    @Override
+    public ArrayList<PortfolioAsset> convertToEntityAttribute(String portfolioAssetsJson) {
+        try {
+            if (portfolioAssetsJson == null) {
+                return new ArrayList<>();
+            }
+            return this.objectMapper.readValue(portfolioAssetsJson, new TypeReference<>() {});
+        } catch (JsonProcessingException ex) {
+            final String EXCEPTION_MESSAGE = "Error converting JSON to portfolio assets.";
+            throw new RuntimeException(EXCEPTION_MESSAGE, ex);
+        }
+    }
+}
