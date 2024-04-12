661cc59bc6f0e4022323b66ae16be6c209727163
theoliverlear <sigwartholiver2@gmail.com>
2024-04-11 19:19:29 -0500

    Add converter for PortfolioAsset.java files in PortfolioAssetConverter.java
    Took 36 seconds

---
+++ src/main/java/org/theoliverlear/convert/PortfolioAssetConverter.java
@@ -0,0 +1,39 @@
+package org.theoliverlear.convert;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+import org.theoliverlear.entity.PortfolioAsset;
+
+@Converter(autoApply = true)
+public class PortfolioAssetConverter implements AttributeConverter<PortfolioAsset, String> {
+    //============================-Variables-=================================
+    ObjectMapper objectMapper;
+    //===========================-Constructors-===============================
+    public PortfolioAssetConverter() {
+        this.objectMapper = new ObjectMapper();
+    }
+    //============================-Overrides-=================================
+
+    //---------------------Convert-To-Database-Column-------------------------
+    @Override
+    public String convertToDatabaseColumn(PortfolioAsset portfolioAsset) {
+        try {
+            return this.objectMapper.writeValueAsString(portfolioAsset);
+        } catch (JsonProcessingException ex) {
+            final String EXCEPTION_MESSAGE = "Error converting portfolio asset to JSON.";
+            throw new RuntimeException(EXCEPTION_MESSAGE, ex);
+        }
+    }
+    //--------------------Convert-From-Database-Column------------------------
+    @Override
+    public PortfolioAsset convertToEntityAttribute(String portfolioAssetJson) {
+        try {
+            return this.objectMapper.readValue(portfolioAssetJson, PortfolioAsset.class);
+        } catch (JsonProcessingException ex) {
+            final String EXCEPTION_MESSAGE = "Error converting JSON to portfolio asset.";
+            throw new RuntimeException(EXCEPTION_MESSAGE, ex);
+        }
+    }
+}
