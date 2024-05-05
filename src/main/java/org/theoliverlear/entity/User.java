398ae9aeeb3707fa8af5bbe69a99a8852cff1ac5
theoliverlear <sigwartholiver2@gmail.com>
2024-05-05 14:34:08 -0500

    Refactor User.java to use SafePassword for encryption and implement Lombok for Getters/Setters
    Took 20 seconds

--- src/main/java/org/theoliverlear/entity/User.java
+++ src/main/java/org/theoliverlear/entity/User.java
@@ -1,12 +1,16 @@
 package org.theoliverlear.entity;
 //=================================-Imports-==================================
 import jakarta.persistence.*;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import lombok.Getter;
+import lombok.Setter;
+import org.theoliverlear.convert.entity.SafePasswordConverter;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "users")
+@Getter
+@Setter
 public class User {
     //============================-Variables-=================================
     @Id
@@ -16,7 +20,8 @@ public class User {
     @Column(name = "username")
     private String username;
     @Column(name = "password_hash")
-    private String encodedPassword;
+    @Convert(converter = SafePasswordConverter.class)
+    private SafePassword safePassword;
     @Column(name = "portfolio_id")
     private Long portfolioId;
     @Column(name = "last_login")
@@ -24,28 +29,25 @@ public class User {
     //===========================-Constructors-===============================
     public User() {
         this.username = null;
-        this.encodedPassword = null;
+        this.safePassword = null;
         this.portfolioId = null;
         this.lastLogin = null;
     }
     public User(String username, String rawPassword, Long portfolioId) {
         this.username = username;
-        this.encodedPassword = this.encodePassword(rawPassword);
+        this.safePassword = new SafePassword(rawPassword);
         this.portfolioId = portfolioId;
         this.lastLogin = LocalDateTime.now();
     }
     public User(String username, String rawPassword, Long portfolioId, LocalDateTime lastLogin) {
         this.username = username;
-        this.encodedPassword = rawPassword;
+        this.safePassword = new SafePassword(rawPassword);
         this.portfolioId = portfolioId;
         this.lastLogin = lastLogin;
     }
     //=============================-Methods-==================================
 
-    //--------------------------Encode-Password-------------------------------
-    public String encodePassword(String rawPassword) {
-        return new BCryptPasswordEncoder().encode(rawPassword);
-    }
+
     public void updateLoginTime() {
         this.lastLogin = LocalDateTime.now();
     }
@@ -57,36 +59,5 @@ public class User {
 
     //------------------------------To-String---------------------------------
 
-    //=============================-Getters-==================================
-    public Long getId() {
-        return this.id;
-    }
-    public String getUsername() {
-        return this.username;
-    }
-    public String getEncodedPassword() {
-        return this.encodedPassword;
-    }
-    public Long getPortfolioId() {
-        return this.portfolioId;
-    }
-    public LocalDateTime getLastLogin() {
-        return this.lastLogin;
-    }
-    //=============================-Setters-==================================
-    public void setId(Long id) {
-        this.id = id;
-    }
-    public void setUsername(String username) {
-        this.username = username;
-    }
-    public void setEncodedPassword(String encodedPassword) {
-        this.encodedPassword = encodedPassword;
-    }
-    public void setPortfolioId(Long portfolioId) {
-        this.portfolioId = portfolioId;
-    }
-    public void setLastLogin(LocalDateTime lastLogin) {
-        this.lastLogin = lastLogin;
-    }
+
 }
