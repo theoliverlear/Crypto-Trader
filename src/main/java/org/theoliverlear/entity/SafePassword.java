866d79dcd24f726149ccc186311458a2c48b0d1c
theoliverlear <sigwartholiver2@gmail.com>
2024-05-05 14:33:29 -0500

    Add SafePassword.java for encoding and comparing passwords securely
    Took 15 seconds

---
+++ src/main/java/org/theoliverlear/entity/SafePassword.java
@@ -0,0 +1,40 @@
+package org.theoliverlear.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Transient;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Entity
+@Getter
+@Setter
+public class SafePassword {
+    @Id
+    String encodedPassword;
+    @Transient
+    BCryptPasswordEncoder encoder;
+    public SafePassword() {
+        this.encoder = new BCryptPasswordEncoder();
+        this.encodedPassword = null;
+    }
+    public SafePassword(String unencodedPassword) {
+        this.encoder = new BCryptPasswordEncoder();
+        this.encodedPassword = this.encodePassword(unencodedPassword);
+    }
+    public String encodePassword(String unencodedPassword) {
+        return this.encoder.encode(unencodedPassword);
+    }
+    public boolean compareUnencodedPassword(String unencodedPassword) {
+        return this.encoder.matches(unencodedPassword, this.encodedPassword);
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj instanceof SafePassword comparedSafePassword) {
+            return this.encodedPassword.equals(comparedSafePassword.encodedPassword);
+        }
+        return false;
+    }
+}
