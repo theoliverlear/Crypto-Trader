285e007ac1ce87e3c7e9322da1d2ec09aa185424
theoliverlear <sigwartholiver2@gmail.com>
2024-05-04 21:10:05 -0500

    Add TraderController.java to manage trader routing and session handling
    Took 36 seconds

---
+++ src/main/java/org/theoliverlear/controller/TraderController.java
@@ -0,0 +1,27 @@
+package org.theoliverlear.controller;
+
+import jakarta.servlet.http.HttpSession;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.theoliverlear.entity.User;
+
+@Controller
+@RequestMapping("/trader")
+public class TraderController {
+    private User currentUser;
+    @RequestMapping("/")
+    public String trader(HttpSession session) {
+        User user = (User) session.getAttribute("user");
+        if (user == null) {
+            return "redirect:/user/";
+        } else {
+            this.currentUser = user;
+            return "trader";
+        }
+    }
+    @RequestMapping("/history")
+    public ResponseEntity</* make response class */> history() {
+
+    }
+}
