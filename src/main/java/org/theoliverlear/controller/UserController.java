8ab692a5af5d89e01aaa7d94b683ec6221a90a7b
theoliverlear <sigwartholiver2@gmail.com>
2024-05-05 14:35:05 -0500

    Refactor UserController.java to check if user is stored in session and redirect to account page if present
    Took 56 seconds

--- src/main/java/org/theoliverlear/controller/UserController.java
+++ src/main/java/org/theoliverlear/controller/UserController.java
@@ -1,13 +1,27 @@
 package org.theoliverlear.controller;
 
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.theoliverlear.entity.User;
+import org.theoliverlear.service.UserService;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
+    UserService userService;
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
     @RequestMapping("/")
-    public String user() {
-        return "user";
+    public String user(HttpSession session) {
+        User user = (User) session.getAttribute("user");
+        if (user == null) {
+            return "user";
+        } else {
+            return "redirect:/account/";
+        }
     }
 }
