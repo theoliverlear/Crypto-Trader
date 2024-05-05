7fe7888ebf526fe285ed81b4cc1c4837ca037cc5
theoliverlear <sigwartholiver2@gmail.com>
2024-05-04 21:07:18 -0500

    Add UserController.java to handle user routing requests
    Took 27 seconds

---
+++ src/main/java/org/theoliverlear/controller/UserController.java
@@ -0,0 +1,13 @@
+package org.theoliverlear.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+    @RequestMapping("/")
+    public String user() {
+        return "user";
+    }
+}
