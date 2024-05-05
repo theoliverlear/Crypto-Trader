0337eb60db1f4bb4728d328160f72e3e966b63af
theoliverlear <sigwartholiver2@gmail.com>
2024-05-04 21:05:04 -0500

    Refactor PortfolioController.java to redirect unauthenticated users to the user page
    Took 20 seconds

--- src/main/java/org/theoliverlear/controller/PortfolioController.java
+++ src/main/java/org/theoliverlear/controller/PortfolioController.java
@@ -1,8 +1,10 @@
 package org.theoliverlear.controller;
 
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.theoliverlear.entity.User;
 import org.theoliverlear.service.CryptoTraderService;
 import org.theoliverlear.service.CurrencyService;
 import org.theoliverlear.service.PortfolioService;
@@ -16,8 +18,12 @@ public class PortfolioController extends CryptoTraderController {
                                PortfolioService portfolioService) {
         super(cryptoTraderService, currencyService, portfolioService);
     }
-    @RequestMapping("/builder")
-    public String builder() {
-        return "portfolio-builder";
+    @RequestMapping("/")
+    public String builder(HttpSession session) {
+        User user = (User) session.getAttribute("user");
+        if (user == null) {
+            return "redirect:/user/";
+        }
+        return "portfolio";
     }
 }
