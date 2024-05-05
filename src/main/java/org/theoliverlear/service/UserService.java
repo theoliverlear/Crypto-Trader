cc225e7da1679b68be467bf6704b78d0bd7a1f09
theoliverlear <sigwartholiver2@gmail.com>
2024-05-05 14:37:00 -0500

    Add UserService.java to handle user-related operations
    Took 34 seconds

---
+++ src/main/java/org/theoliverlear/service/UserService.java
@@ -0,0 +1,24 @@
+package org.theoliverlear.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.theoliverlear.entity.User;
+import org.theoliverlear.repository.UserRepository;
+
+@Service
+public class UserService {
+    UserRepository userRepository;
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+    public boolean userExists(String username) {
+        return this.userRepository.existsByUsername(username);
+    }
+    public User getUserByUsername(String username) {
+        return this.userRepository.getUserByUsername(username);
+    }
+    public boolean comparePassword(User user, String password) {
+        return user.getSafePassword().compareUnencodedPassword(password);
+    }
+}
