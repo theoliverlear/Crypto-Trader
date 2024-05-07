6a0978711ba4df5f8b69829f1a3aeab54c4f7664
theoliverlear <sigwartholiver2@gmail.com>
2024-05-07 18:49:05 -0500

    Remove unused imports in CryptoTraderController.java
    Took 1 hour 13 minutes

--- src/main/java/org/theoliverlear/controller/CryptoTraderController.java
+++ src/main/java/org/theoliverlear/controller/CryptoTraderController.java
@@ -2,16 +2,9 @@ package org.theoliverlear.controller;
 
 //=================================-Imports-==================================
 import jakarta.servlet.http.HttpSession;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.theoliverlear.comm.UserRequest;
 import org.theoliverlear.entity.User;
-import org.theoliverlear.service.CryptoTraderService;
-import org.theoliverlear.service.CurrencyService;
-import org.theoliverlear.service.PortfolioService;
 
 @Controller
 public class CryptoTraderController {
