fe4c151806f7eed6d6232a246fdd0a4d0aecaae6
theoliverlear <sigwartholiver2@gmail.com>
2024-05-07 09:35:49 -0500

    Refactor CurrencyUpdaterThread.java, included CurrencyService and adjusted updateValue method and temporarily comment out code

--- src/main/java/org/theoliverlear/model/thread/CurrencyUpdaterThread.java
+++ src/main/java/org/theoliverlear/model/thread/CurrencyUpdaterThread.java
@@ -1,22 +1,25 @@
-package org.theoliverlear.model.thread;
-
-import org.theoliverlear.entity.Currency;
-
-public class CurrencyUpdaterThread extends Thread {
-    Currency currency;
-    public CurrencyUpdaterThread(Currency currency) {
-        this.currency = currency;
-    }
-    @Override
-    public void run() {
-        while (true) {
-            try {
-                this.currency.updateValue();
-                System.out.println(this.currency);
-                Thread.sleep(5000);
-            } catch (InterruptedException ex) {
-                throw new RuntimeException(ex);
-            }
-        }
-    }
-}
+//package org.theoliverlear.model.thread;
+//
+//import org.theoliverlear.entity.Currency;
+//import org.theoliverlear.service.CurrencyService;
+//
+//public class CurrencyUpdaterThread extends Thread {
+//    Currency currency;
+//    CurrencyService currencyService;
+//    public CurrencyUpdaterThread(Currency currency, CurrencyService currencyService) {
+//        this.currency = currency;
+//        this.currencyService = currencyService;
+//    }
+//    @Override
+//    public void run() {
+//        while (true) {
+//            try {
+//                this.currency.updateValue();
+////                this.currencyService.saveCurrencies(this.currency);
+//                Thread.sleep(5000);
+//            } catch (InterruptedException ex) {
+//                throw new RuntimeException(ex);
+//            }
+//        }
+//    }
+//}
