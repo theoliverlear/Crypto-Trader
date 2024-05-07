459d4c823d62b49821fe9c92286a8f474dad6270
theoliverlear <sigwartholiver2@gmail.com>
2024-05-07 18:54:01 -0500

    Remove saveCurrencyByCurrencyCode method from CurrencyRepository.java due to redundancy
    Took 26 seconds

--- src/main/java/org/theoliverlear/repository/CurrencyRepository.java
+++ src/main/java/org/theoliverlear/repository/CurrencyRepository.java
@@ -1,12 +1,9 @@
 package org.theoliverlear.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.theoliverlear.entity.Currency;
 
 public interface CurrencyRepository extends JpaRepository<Currency, Long> {
     Currency getCurrencyByCurrencyCode(String currencyCode);
-    @Query("SELECT c FROM Currency c WHERE c.currencyCode = :currencyCode")
-    void saveCurrencyByCurrencyCode(@Param("currencyCode") Currency currency);
+
 }
