7140c8112928c064febfad1831e2deda1caf8bae
theoliverlear <sigwartholiver2@gmail.com>
2024-05-07 18:56:09 -0500

    Refactor saveCurrencies method in CurrencyService.java to use built in JPA methods
    Took 2 minutes

--- src/main/java/org/theoliverlear/service/CurrencyService.java
+++ src/main/java/org/theoliverlear/service/CurrencyService.java
@@ -1,14 +1,12 @@
 package org.theoliverlear.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.theoliverlear.entity.Currency;
 import org.theoliverlear.entity.CurrencyHistory;
 import org.theoliverlear.repository.CurrencyHistoryRepository;
 import org.theoliverlear.repository.CurrencyRepository;
-import org.theoliverlear.update.CurrencyUpdater;
 import org.theoliverlear.update.SupportedCurrencies;
 
 @Service
@@ -23,14 +21,12 @@ public class CurrencyService {
     }
     @Scheduled(fixedRate = 5000)
     public void saveCurrencies() {
-        for (final Currency currency : SupportedCurrencies.SUPPORTED_CURRENCIES) {
+        for (Currency currency : SupportedCurrencies.SUPPORTED_CURRENCIES) {
             Currency previousCurrency = Currency.from(currency);
             currency.updateValue();
-            System.out.println(currency);
             if (!previousCurrency.equals(currency)) {
-                this.currencyRepository.saveCurrencyByCurrencyCode(currency);
-                CurrencyHistory currencyHistory = new CurrencyHistory(currency, currency.getValue());
-                this.currencyHistoryRepository.save(currencyHistory);
+                this.currencyRepository.save(currency);
+                this.currencyHistoryRepository.save(new CurrencyHistory(currency, currency.getValue()));
             }
         }
     }
