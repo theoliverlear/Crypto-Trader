230fe44635e9a34122d5cc48dfb4c249dbfdecd5
theoliverlear <sigwartholiver2@gmail.com>
2024-05-08 17:32:38 -0500

    Add new methods for retrieving and updating portfolio in PortfolioController.java
    Took 22 seconds

--- src/main/java/org/theoliverlear/controller/PortfolioController.java
+++ src/main/java/org/theoliverlear/controller/PortfolioController.java
@@ -2,17 +2,20 @@ package org.theoliverlear.controller;
 
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.theoliverlear.comm.request.PortfolioAssetRequest;
+import org.theoliverlear.entity.Portfolio;
 import org.theoliverlear.entity.User;
-import org.theoliverlear.service.CryptoTraderService;
-import org.theoliverlear.service.CurrencyService;
 import org.theoliverlear.service.PortfolioService;
 
 @Controller
 @RequestMapping("/portfolio")
 public class PortfolioController {
     User currentUser;
+    Portfolio portfolio = new Portfolio();
     PortfolioService portfolioService;
     @Autowired
     public PortfolioController(PortfolioService portfolioService) {
@@ -25,7 +28,16 @@ public class PortfolioController {
             return "redirect:/user/";
         }
         this.currentUser = user;
+        this.portfolio = this.portfolioService.getPortfolio(user.getId());
         return "portfolio";
     }
-    
+    @RequestMapping("/get")
+    public ResponseEntity<Portfolio> getPortfolio() {
+        return ResponseEntity.ok(this.portfolio);
+    }
+    @RequestMapping("/add")
+    public ResponseEntity<String> addPortfolioAsset(@RequestBody PortfolioAssetRequest portfolioAssetRequest) {
+        this.portfolioService.addAssetToPortfolio(this.portfolio, portfolioAssetRequest);
+        return ResponseEntity.ok("Asset added to portfolio");
+    }
 }
