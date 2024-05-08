f004e193a368167ba15236b64e802ee4952d0194
theoliverlear <sigwartholiver2@gmail.com>
2024-05-08 17:26:52 -0500

    Refactor and enhance globalScript.js by replacing let with const, adding getCurrencyLogoFromName function, and modifying exports
    Took 1 minute

--- src/main/resources/static/script/globalScript.js
+++ src/main/resources/static/script/globalScript.js
@@ -1,9 +1,29 @@
-let CryptoJS = require("crypto-js");
+const CryptoJS = require('crypto-js');
 function passwordContainsArtifacts(password) {
     // 0 long or has spaces
+    if (password.length === 0 || password.includes(' ')) {
+        return true;
+    }
+    return false;
 }
 function hashPassword(password) {
     let hashedPassword = CryptoJS.SHA256(password);
     return hashedPassword.toString();
 }
-module.exports = {hashPassword};
+function getCurrencyLogoFromName(currencyName) {
+    let currencyLogoSrc = '';
+    switch (currencyName) {
+        case 'Bitcoin':
+            currencyLogoSrc = '../static/images/logo/currency/bitcoin_logo.png';
+            break;
+        case 'Ethereum':
+            currencyLogoSrc = '../static/images/logo/currency/ethereum_logo.png';
+            break;
+        case 'Litecoin':
+            currencyLogoSrc = '../static/images/logo/currency/litecoin_logo.png';
+            break;    
+    }
+    return currencyLogoSrc;
+}
+export {hashPassword, getCurrencyLogoFromName, passwordContainsArtifacts};
+// module.exports = {hashPassword, getCurrencyLogoFromName, passwordContainsArtifacts};
