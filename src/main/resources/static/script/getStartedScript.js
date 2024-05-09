7eed1f7b266bfdaa89c580b7b1cc256f89bcd062
theoliverlear <sigwartholiver2@gmail.com>
2024-05-09 13:11:25 -0500

    Refactor getStartedScript.js for better structure and readability with comment headers
    Took 51 seconds

--- src/main/resources/static/script/getStartedScript.js
+++ src/main/resources/static/script/getStartedScript.js
@@ -1,66 +1,27 @@
+//=================================-Imports-==================================
+import {hashPassword} from "./globalScript.js";
+//================================-Variables-=================================
+
+//-----------------------------------Inputs-----------------------------------
 let userUsernameInput = document.getElementById('signup-username-input');
-let userUsernameInputValue = userUsernameInput.value;
 let userPasswordInput = document.getElementById('signup-password-input');
-let userPasswordInputValue = userPasswordInput.value;
 let userPasswordConfirmInput = document.getElementById('signup-confirm-password-input');
+let signupTermsCheckbox = document.getElementById('signup-terms-agree-checkbox');
+//--------------------------------Input-Values--------------------------------
+let userUsernameInputValue = userUsernameInput.value;
+let userPasswordInputValue = userPasswordInput.value;
 let userPasswordConfirmInputValue = userPasswordConfirmInput.value;
+let signupTermsCheckboxValue = signupTermsCheckbox.checked;
+//-----------------------------------Popup------------------------------------
 let popupDiv = document.getElementById('signup-prompt-popup-div');
 let popupText = document.getElementById('signup-prompt-popup-text');
-function passwordMatchPopup() {
-    if (!passwordsMatch()) {
-        popupDiv.style.display = 'block';
-        popupText.textContent = 'Passwords do not match.';
-    } else {
-        popupText.textContent = '';
-        popupDiv.style.display = 'none';
-    }
-}
-userPasswordConfirmInput.addEventListener('input', passwordMatchPopup);
-
 let signupButton = document.getElementById('signup-button-container');
-let signupTermsCheckbox = document.getElementById('signup-terms-agree-checkbox');
-let signupTermsCheckboxValue = signupTermsCheckbox.checked;
-function signupTermsPopup() {
-    console.log(signupTermsCheckboxValue);
-    if (!termsAgreed()) {
-        popupDiv.style.display = 'block';
-        popupText.textContent = 'Please agree to the terms and conditions.';
-    } else {
-        popupText.textContent = '';
-        popupDiv.style.display = 'none';
-    }
-}
-signupButton.addEventListener('click', signupTermsPopup);
-function passwordsMatch() {
-    userPasswordInputValue = userPasswordInput.value;
-    userPasswordConfirmInputValue = userPasswordConfirmInput.value;
-    return userPasswordInputValue === userPasswordConfirmInputValue;
-}
-function termsAgreed() {
-    signupTermsCheckboxValue = signupTermsCheckbox.checked;
-    return signupTermsCheckboxValue;
-}
-function updateAllFields() {
-    userUsernameInputValue = userUsernameInput.value;
-    userPasswordInputValue = userPasswordInput.value;
-    userPasswordConfirmInputValue = userPasswordConfirmInput.value;
-    signupTermsCheckboxValue = signupTermsCheckbox.checked;
-}
-function emptyInfo() {
-    updateAllFields();
-    return userUsernameInputValue === '' || userPasswordInputValue === '' || userPasswordConfirmInputValue === '';
-}
-function emptyInfoPopup() {
-    if (emptyInfo()) {
-        popupDiv.style.display = 'block';
-        popupText.textContent = 'Please fill out all fields.';
-    } else {
-        popupText.textContent = '';
-        popupDiv.style.display = 'none';
-    }
-}
-signupButton.addEventListener('click', emptyInfoPopup);
-function sendSignupData() {
+//=============================-Server-Functions-=============================
+
+//-------------------------Send-Signup-Data-To-Server-------------------------
+function sendSignupDataToServer() {
+    let hashedPassword = hashPassword(userPasswordInputValue);
+    console.log(hashedPassword);
     fetch('/signup', {
         method: 'POST',
         headers: {
@@ -79,9 +40,69 @@ function sendSignupData() {
         }
     });
 }
-function signupChain() {
-    if (!emptyInfo() && passwordsMatch() && termsAgreed()) {
-        sendSignupData();
+//=============================-Client-Functions-=============================
+
+//----------------------------Password-Match-Popup----------------------------
+function passwordMatchPopup() {
+    if (!passwordsMatch()) {
+        popupDiv.style.display = 'block';
+        popupText.textContent = 'Passwords do not match.';
+    } else {
+        popupText.textContent = '';
+        popupDiv.style.display = 'none';
     }
 }
-signupButton.addEventListener('click', signupChain);
+//-----------------------------Signup-Terms-Popup-----------------------------
+function signupTermsPopup() {
+    if (!termsAgreed()) {
+        popupDiv.style.display = 'block';
+        popupText.textContent = 'Please agree to the terms and conditions.';
+    } else {
+        popupText.textContent = '';
+        popupDiv.style.display = 'none';
+    }
+}
+//------------------------------Passwords-Match-------------------------------
+function passwordsMatch() {
+    userPasswordInputValue = userPasswordInput.value;
+    userPasswordConfirmInputValue = userPasswordConfirmInput.value;
+    return userPasswordInputValue === userPasswordConfirmInputValue;
+}
+//--------------------------------Terms-Agreed--------------------------------
+function termsAgreed() {
+    signupTermsCheckboxValue = signupTermsCheckbox.checked;
+    return signupTermsCheckboxValue;
+}
+//-----------------------------Update-All-Fields------------------------------
+function updateAllFields() {
+    userUsernameInputValue = userUsernameInput.value;
+    userPasswordInputValue = userPasswordInput.value;
+    userPasswordConfirmInputValue = userPasswordConfirmInput.value;
+    signupTermsCheckboxValue = signupTermsCheckbox.checked;
+}
+//------------------------------Has-Empty-Fields------------------------------
+function hasEmptyFields() {
+    updateAllFields();
+    return userUsernameInputValue === '' || userPasswordInputValue === '' || userPasswordConfirmInputValue === '';
+}
+//-----------------------------Empty-Field-Popup------------------------------
+function emptyFieldPopup() {
+    if (hasEmptyFields()) {
+        popupDiv.style.display = 'block';
+        popupText.textContent = 'Please fill out all fields.';
+    } else {
+        popupText.textContent = '';
+        popupDiv.style.display = 'none';
+    }
+}
+//------------------------------Signup-Sequence-------------------------------
+function signupSequence() {
+    if (!hasEmptyFields() && passwordsMatch() && termsAgreed()) {
+        sendSignupDataToServer();
+    }
+}
+//=============================-Event-Listeners-==============================
+signupButton.addEventListener('click', signupSequence);
+signupButton.addEventListener('click', emptyFieldPopup);
+userPasswordConfirmInput.addEventListener('input', passwordMatchPopup);
+signupButton.addEventListener('click', signupTermsPopup);
