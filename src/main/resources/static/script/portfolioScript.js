ce2bdc023ad7f7e558f010cb9b9854a2baaa8eb8
theoliverlear <sigwartholiver2@gmail.com>
2024-05-09 13:12:36 -0500

    Refactor portfolioScript.js for improved readability and organization by categorizing code blocks and rearranging functions
    Took 19 seconds

--- src/main/resources/static/script/portfolioScript.js
+++ src/main/resources/static/script/portfolioScript.js
@@ -1,74 +1,57 @@
+//=================================-Imports-==================================
+import {
+    defaultCurrencyImage,
+    getCurrencyLogoFromName,
+    sanitizeString
+} from "./globalScript";
+//================================-Variables-=================================
 
-console.log('portfolioScript.js loaded');
-const currencyDropdownCaret = document.getElementById('currency-dropdown-caret');
-const currencyDropdownCaretImage = document.getElementById('currency-dropdown-caret-image');
-
-function changeCaretToHighlight() {
-    currencyDropdownCaretImage.src = '../static/images/down_caret_highlight.svg';
-}
-function changeCaretToBlack() {
-    currencyDropdownCaretImage.src = '../static/images/down_caret.svg';
-}
-currencyDropdownCaret.addEventListener('mouseover', changeCaretToHighlight);
-currencyDropdownCaret.addEventListener('mouseout', changeCaretToBlack);
-const currencyDropdownButton = document.getElementById('currency-dropdown');
-const currencyDropdownSelection = document.getElementById('currency-dropdown-selection');
-function toggleCurrencyDropdown() {
-    currencyDropdownSelection.classList.toggle('hide');
-    currencyDropdownButton.classList.toggle('square-bottom');
-}
-function hideCurrencyDropdown() {
-    currencyDropdownSelection.classList.add('hide');
-    currencyDropdownButton.classList.remove('square-bottom');
-}
-let selectedCurrencyImage = document.getElementById('your-currency-image');
-const defaultCurrencyImage = '../static/images/default_currency_icon.png';
-function setSelectionToNone() {
-    currencySelectionText.textContent = 'Select Currency';
-    selectedCurrencyImage.src = defaultCurrencyImage;
-}
-let addButton = document.getElementById('add-button');
-function addItemSequence() {
-    if (emptyInfo()) {
-        showPopup();
-        return;
-    }
-    sendPortfolioAssetToServer();
-    clearInputs();
-    hideCurrencyDropdown();
-    hideAddCurrencyDiv();
-    updatePage();
-}
-addButton.addEventListener('click', addItemSequence);
-currencyDropdownButton.addEventListener('click', toggleCurrencyDropdown);
+//-----------------------------------Inputs-----------------------------------
+let sharesInput = document.getElementById('shares-input');
+let walletInput = document.getElementById('wallet-input');
+//---------------------------------Containers---------------------------------
+const addCurrencyDiv = document.getElementById('add-currency-div');
+//-----------------------------Currency-Selection-----------------------------
 const currencySelectionText = document.getElementById('currency-selection-text');
 const currencySelectionOptions = document.getElementsByClassName('currency-dropdown-option');
 const currencySelectionOptionsArray = Array.from(currencySelectionOptions);
-
-const addCurrencyButton = document.getElementById('add-currency-button');
-const addCurrencyDiv = document.getElementById('add-currency-div');
+let selectedCurrencyText = document.getElementById('selected-currency-text');
+let selectedCurrencyImage = document.getElementById('your-currency-image');
+//------------------------------Currency-Options------------------------------
+let currencyOptionsDropdown = document.getElementsByClassName('currency-dropdown-option');
+let currencyOptionsArray = Array.from(currencyOptionsDropdown);
+const currencyDropdownSelection = document.getElementById('currency-dropdown-selection');
+//-------------------------------Dropdown-Caret-------------------------------
+const currencyDropdownCaret = document.getElementById('currency-dropdown-caret');
+const currencyDropdownCaretImage = document.getElementById('currency-dropdown-caret-image');
+//-----------------------------------Popup------------------------------------
+let popupDiv = document.getElementById('popup-div');
+//----------------------------------Buttons-----------------------------------
+let addButton = document.getElementById('add-button');
 const cancelButton = document.getElementById('cancel-button');
-
-function showAddCurrencyDiv() {
-    addCurrencyDiv.style.display = 'block';
+const addCurrencyButton = document.getElementById('add-currency-button');
+const currencyDropdownButton = document.getElementById('currency-dropdown');
+//=============================-Server-Functions-=============================
+//-------------------------Get-Portfolio-From-Server--------------------------
+// TODO: Either have a WebSocket updating their portfolio in real-time or have
+//       a refresh button that updates the page.
+async function getPortfolioFromServer() {
+    let response = await fetch('/portfolio/get', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+    let portfolio = await response.json();
+    return portfolio;
 }
-function hideAddCurrencyDiv() {
-    addCurrencyDiv.style.display = 'none';
-}
-
-addCurrencyButton.addEventListener('click', showAddCurrencyDiv);
-cancelButton.addEventListener('click', hideAddCurrencyDiv);
-cancelButton.addEventListener('click', hideCurrencyDropdown);
-
-let sharesInput = document.getElementById('shares-input');
-let walletInput = document.getElementById('wallet-input');
-
+//-----------------------Send-Portfolio-Asset-To-Server-----------------------
 function sendPortfolioAssetToServer() {
-    if (hasSelectedCurrency() && sharesOrWalletHasInput()) {
+    if (hasSelectedCurrency() && sharesOrWalletHaveInput()) {
         let currencyName = sanitizeString(selectedCurrencyText.textContent);
         let shares = initializeEmptyWalletShares(sharesInput.value);
         let wallet = initializeEmptyWalletShares(walletInput.value);
-        
+
         fetch('/portfolio/add', {
             method: 'POST',
             headers: {
@@ -82,44 +65,83 @@ function sendPortfolioAssetToServer() {
         })
     }
 }
+//=============================-Client-Functions-=============================
+
+//-----------------------------Add-Item-Sequence------------------------------
+function addItemSequence() {
+    if (sharesAndWalletAreEmpty()) {
+        showPopup();
+        return;
+    }
+    sendPortfolioAssetToServer();
+    clearInputs();
+    hideCurrencyDropdown();
+    hideAddCurrencyDiv();
+    updatePage();
+}
+//-------------------------Change-Caret-To-Highlight--------------------------
+function changeCaretToHighlight() {
+    currencyDropdownCaretImage.src = '../static/images/down_caret_highlight.svg';
+}
+//---------------------------Change-Caret-To-Black----------------------------
+function changeCaretToBlack() {
+    currencyDropdownCaretImage.src = '../static/images/down_caret.svg';
+}
+//---------------------------Set-Selection-To-None----------------------------
+function setSelectionToNone() {
+    currencySelectionText.textContent = 'Select Currency';
+    selectedCurrencyImage.src = defaultCurrencyImage;
+}
+//---------------------------Show-Add-Currency-Div----------------------------
+function showAddCurrencyDiv() {
+    addCurrencyDiv.style.display = 'block';
+}
+//---------------------------Hide-Add-Currency-Div----------------------------
+function hideAddCurrencyDiv() {
+    addCurrencyDiv.style.display = 'none';
+}
+//--------------------------Toggle-Currency-Dropdown--------------------------
+function toggleCurrencyDropdown() {
+    currencyDropdownSelection.classList.toggle('hide');
+    currencyDropdownButton.classList.toggle('square-bottom');
+}
+//---------------------------Hide-Currency-Dropdown---------------------------
+function hideCurrencyDropdown() {
+    currencyDropdownSelection.classList.add('hide');
+    currencyDropdownButton.classList.remove('square-bottom');
+}
+//--------------------------------Clear-Inputs--------------------------------
 function clearInputs() {
     sharesInput.value = '';
     walletInput.value = '';
     setSelectionToNone();
 }
+//-----------------------Initialize-Empty-Wallet-Shares-----------------------
 function initializeEmptyWalletShares(valueInput) {
     if (valueInput === '') {
         valueInput = 0;
     }
     return valueInput;
 }
-// TODO: Either have a WebSocket updating their portfolio in real-time or have
-//       a refresh button that updates the page.
-async function getPortfolioFromServer() {
-    let response = await fetch('/portfolio/get', {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    });
-    let portfolio = await response.json();
-    return portfolio;
-}
+//--------------------------------Update-Page---------------------------------
 function updatePage() {
     
 }
+//------------------------Shares-And-Wallet-Are-Empty-------------------------
 function sharesAndWalletAreEmpty() {
     return sharesInput.value === '' && walletInput.value === '';
 }
-let popupDiv = document.getElementById('popup-div');
+//---------------------------------Show-Popup---------------------------------
 function showPopup() {
     popupDiv.style.display = 'flex';
 }
+//---------------------------------Hide-Popup---------------------------------
 function hidePopup() {
     popupDiv.style.display = 'none';
 }
+//------------------------Shares-Or-Wallet-Have-Input-------------------------
 // XOR check
-function sharesOrWalletHasInput() {
+function sharesOrWalletHaveInput() {
     if (hasSharesInput() && hasWalletInput()) {
         return false;
     } else if (!hasSharesInput() && !hasWalletInput()) {
@@ -128,47 +150,35 @@ function sharesOrWalletHasInput() {
         return true;
     }
 }
+//---------------------------Has-Selected-Currency----------------------------
 function hasSelectedCurrency() {
     return currencySelectionText.textContent !== 'Select Currency';
 }
+//------------------------------Has-Shares-Input------------------------------
 function hasSharesInput() {
     return sharesInput.value !== '';
 }
+//------------------------------Has-Wallet-Input------------------------------
 function hasWalletInput() {
     return walletInput.value !== '';
 }
-let currencyOptionsDropdown = document.getElementsByClassName('currency-dropdown-option');
-let currencyOptionsArray = Array.from(currencyOptionsDropdown);
-
-currencyOptionsArray.forEach(option => {
-    option.addEventListener('click', toggleCurrencyDropdown);
-    option.addEventListener('click', setSelectedCurrency);
-});
-let selectedCurrencyText = document.getElementById('selected-currency-text');
+//---------------------------Get-Selected-Currency----------------------------
 function setSelectedCurrency() {
     let currencyChoiceText = this.getElementsByClassName('currency-option-text')[0].innerText;
     currencyChoiceText = sanitizeString(currencyChoiceText);
     selectedCurrencyText.textContent = currencyChoiceText;
     selectedCurrencyImage.src = getCurrencyLogoFromName(currencyChoiceText);
 }
-function sanitizeString(input) {
-    return input.trim().replace("\n", "").replace("\r", "");
-}
-function getCurrencyLogoFromName(currencyName) {
-    let currencyLogoSrc = '';
-    switch (currencyName) {
-        case 'Bitcoin':
-            currencyLogoSrc = '../static/images/logo/currency/bitcoin_logo.png';
-            break;
-        case 'Ethereum':
-            currencyLogoSrc = '../static/images/logo/currency/ethereum_logo.png';
-            break;
-        case 'Litecoin':
-            currencyLogoSrc = '../static/images/logo/currency/litecoin_logo.png';
-            break;
-        default:
-            currencyLogoSrc = defaultCurrencyImage;
-            break;
-    }
-    return currencyLogoSrc;
-}
+//=============================-Event-Listeners-==============================
+currencyOptionsArray.forEach(option => {
+    option.addEventListener('click', toggleCurrencyDropdown);
+    option.addEventListener('click', setSelectedCurrency);
+});
+addCurrencyButton.addEventListener('click', showAddCurrencyDiv);
+cancelButton.addEventListener('click', hideAddCurrencyDiv);
+cancelButton.addEventListener('click', hideCurrencyDropdown);
+cancelButton.addEventListener('click', clearInputs);
+addButton.addEventListener('click', addItemSequence);
+currencyDropdownButton.addEventListener('click', toggleCurrencyDropdown);
+currencyDropdownCaret.addEventListener('mouseover', changeCaretToHighlight);
+currencyDropdownCaret.addEventListener('mouseout', changeCaretToBlack);
