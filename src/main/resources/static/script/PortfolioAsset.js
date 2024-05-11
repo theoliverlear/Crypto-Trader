953e98ba0d380d87f30972ba38993a01561c1a98
theoliverlear <sigwartholiver2@gmail.com>
2024-05-11 17:45:23 -0500

    Add PortfolioAsset class constructor and buildHtml method in PortfolioAsset.js
    Took 28 seconds

--- src/main/resources/static/script/PortfolioAsset.js
+++ src/main/resources/static/script/PortfolioAsset.js
@@ -1,3 +1,66 @@
-class PortfolioAsset {
+export class PortfolioAsset {
+    //===========================-Constructors-===============================
+    constructor(currencyName, currencyCode, shares, walletDollars, totalAssetValue, currencyLogoSrc) {
+        this.currencyName = currencyName;
+        this.currencyCode = currencyCode;
+        this.shares = shares;
+        this.walletDollars = walletDollars;
+        this.totalValue = totalAssetValue;
+        this.currencyLogoSrc = currencyLogoSrc;
+    }
+    //=============================-Methods-==================================
 
+    //-----------------------------Build-Html---------------------------------
+    buildHtml() {
+        return `
+        <div class="simple-space-inline-div">
+            <div class="simple-space-inline-div">
+                <div class="currency-image-name-code-div simple-space-inline-div your-currency-info-div">
+                    <div class="your-currency-image-div">
+                        <img class="your-currency-image"
+                            src="${this.currencyLogoSrc}"
+                            th:src="@{${this.currencyLogoSrc.replace('../static', '')}}"
+                            alt="${this.currencyName} logo">
+                    </div>
+                    <div>
+                        <h5 class="your-currency-info-text">
+                            <span class="your-currency-info-name-text">${this.currencyName}</span> -
+                            <span class="your-currency-info-code-text">${this.currencyCode}</span>
+                        </h5>
+                    </div>
+                </div>
+                <div class="simple-space-inline-div your-currency-info-div">
+                    <div>
+                        <h6 class="your-currency-info-text">
+                            Shares: <span class="your-currency-info-shares-text">${this.shares}</span>
+                        </h6>
+                    </div>
+                    <div>
+                        <h6 class="your-currency-info-text">
+                            Wallet: $<span class="your-currency-info-wallet-text${this.walletDollars}</span>
+                        </h6>
+                    </div>
+                </div>
+                <div class="simple-space-inline-div your-currency-info-div">
+                    <div>
+                        <h6 class="your-currency-info-text">
+                            Total Value: $<span class="your-currency-info-total-text">${this.totalValue}</span>
+                        </h6>
+                    </div>
+                </div>
+                <div class="simple-space-inline-div your-currency-info-div">
+                    <div>
+                        <h6 class="your-currency-info-text">
+                            All Time Profit: $<span class="your-currency-info-profit-text">0.00</span>
+                        </h6>
+                    </div>
+                    <div class="small-button">
+                        <h6 class="small-button-text">As Percent</h6>
+                    </div>
+                </div>
+            </div>
+            <hr />
+        </div>
+        `;
+    }
 }
