2d95ffc217ef6355fbbd439ee5634ad12ee510f5
theoliverlear <sigwartholiver2@gmail.com>
2024-05-11 17:48:28 -0500

    Add TaskExecutorConfig.java to configure ThreadPoolTaskExecutor for async tasks execution
    Took 27 seconds

---
+++ src/main/java/org/theoliverlear/config/TaskExecutorConfig.java
@@ -0,0 +1,21 @@
+package org.theoliverlear.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@Configuration
+@EnableAsync
+public class TaskExecutorConfig {
+    @Bean(name = "taskExecutor")
+    public TaskExecutor threadPoolTaskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5);
+        executor.setMaxPoolSize(10);
+        executor.setThreadNamePrefix("default_task_executor_thread");
+        executor.initialize();
+        return executor;
+    }
+}
