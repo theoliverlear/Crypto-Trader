70c699ed4a1c4fe0422e94e1244eb50343097e4e
theoliverlear <sigwartholiver2@gmail.com>
2024-05-11 17:48:01 -0500

    Comment out PortfolioUpdater.java code for temporary deactivation
    Took 25 seconds

--- src/main/java/org/theoliverlear/update/PortfolioUpdater.java
+++ src/main/java/org/theoliverlear/update/PortfolioUpdater.java
@@ -1,32 +1,32 @@
-package org.theoliverlear.update;
-
-import org.theoliverlear.entity.Portfolio;
-import org.theoliverlear.model.thread.PortfolioUpdaterThread;
-import org.theoliverlear.model.thread.ThreadManager;
-
-import java.util.ArrayList;
-
-public class PortfolioUpdater {
-    ArrayList<Portfolio> portfolios;
-    ArrayList<Thread> portfolioUpdaters;
-    ThreadManager portfolioUpdaterThreadManager;
-    public PortfolioUpdater(ArrayList<Portfolio> portfolios) {
-        this.portfolios = portfolios;
-        this.portfolioUpdaters = new ArrayList<>();
-        this.startPortfolioUpdaters();
-    }
-    public void startPortfolioUpdaters() {
-        for (final Portfolio portfolio : this.portfolios) {
-            this.portfolioUpdaters.add(new PortfolioUpdaterThread(portfolio));
-        }
-        this.portfolioUpdaterThreadManager = new ThreadManager(this.portfolioUpdaters);
-        this.portfolioUpdaterThreadManager.startThreads();
-    }
-
-    public ArrayList<Portfolio> getPortfolios() {
-        return this.portfolios;
-    }
-    public ArrayList<Thread> getPortfolioUpdaters() {
-        return this.portfolioUpdaters;
-    }
-}
+//package org.theoliverlear.update;
+//
+//import org.theoliverlear.entity.Portfolio;
+//import org.theoliverlear.model.thread.PortfolioUpdaterThread;
+//import org.theoliverlear.model.thread.ThreadManager;
+//
+//import java.util.ArrayList;
+//
+//public class PortfolioUpdater {
+//    ArrayList<Portfolio> portfolios;
+//    ArrayList<Thread> portfolioUpdaters;
+//    ThreadManager portfolioUpdaterThreadManager;
+//    public PortfolioUpdater(ArrayList<Portfolio> portfolios) {
+//        this.portfolios = portfolios;
+//        this.portfolioUpdaters = new ArrayList<>();
+//        this.startPortfolioUpdaters();
+//    }
+//    public void startPortfolioUpdaters() {
+//        for (final Portfolio portfolio : this.portfolios) {
+//            this.portfolioUpdaters.add(new PortfolioUpdaterThread(portfolio));
+//        }
+//        this.portfolioUpdaterThreadManager = new ThreadManager(this.portfolioUpdaters);
+//        this.portfolioUpdaterThreadManager.startThreads();
+//    }
+//
+//    public ArrayList<Portfolio> getPortfolios() {
+//        return this.portfolios;
+//    }
+//    public ArrayList<Thread> getPortfolioUpdaters() {
+//        return this.portfolioUpdaters;
+//    }
+//}
