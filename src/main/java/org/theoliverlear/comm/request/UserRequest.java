7aa97e10bfe0401415a9aa9b9fcd4a69ed1342cf
theoliverlear <sigwartholiver2@gmail.com>
2024-05-12 10:22:51 -0500

    Refactor UserRequest.java to use Lombok's @Data annotation for getter, setter and constructor generation

--- src/main/java/org/theoliverlear/comm/request/UserRequest.java
+++ src/main/java/org/theoliverlear/comm/request/UserRequest.java
@@ -1,29 +1,14 @@
 package org.theoliverlear.comm.request;
 
+import lombok.Data;
+
+@Data
 public class UserRequest {
     private String username;
     private String password;
     //============================-Constructors-==============================
-    public UserRequest() {
-        this.username = "";
-        this.password = "";
-    }
     public UserRequest(String username, String password) {
         this.username = username;
-        this.password = password;
-    }
-    //=============================-Getters-==================================
-    public String getUsername() {
-        return this.username;
-    }
-    public String getPassword() {
-        return this.password;
-    }
-    //=============================-Setters-==================================
-    public void setUsername(String username) {
-        this.username = username;
-    }
-    public void setPassword(String password) {
         this.password = password;
     }
 }
