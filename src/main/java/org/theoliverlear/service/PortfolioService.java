2b9cbd1826d17bd3a76b30b39ce8bb97eed83024
theoliverlear <sigwartholiver2@gmail.com>
2024-05-12 10:21:06 -0500

    Refactor PortfolioService in PortfolioService.java to include Transactional and UserService, and update methods to use List instead of ArrayList

--- src/main/java/org/theoliverlear/service/PortfolioService.java
+++ src/main/java/org/theoliverlear/service/PortfolioService.java
@@ -1,5 +1,6 @@
 package org.theoliverlear.service;
 
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -9,20 +10,25 @@ import org.theoliverlear.comm.request.PortfolioAssetRequest;
 import org.theoliverlear.entity.Currency;
 import org.theoliverlear.entity.Portfolio;
 import org.theoliverlear.entity.PortfolioAsset;
+import org.theoliverlear.entity.User;
 import org.theoliverlear.model.trade.Trader;
 import org.theoliverlear.repository.PortfolioRepository;
 import org.theoliverlear.update.SupportedCurrencies;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class PortfolioService {
-    ArrayList<Portfolio> allUsersPortfolios;
+    List<Portfolio> allUsersPortfolios;
     PortfolioRepository portfolioRepository;
     CryptoTrader cryptoTrader;
+    UserService userService;
     @Autowired
-    public PortfolioService(PortfolioRepository portfolioRepository) {
+    public PortfolioService(PortfolioRepository portfolioRepository,
+                            UserService userService) {
         this.cryptoTrader = new CryptoTrader();
+        this.userService = userService;
         this.portfolioRepository = portfolioRepository;
         this.allUsersPortfolios = new ArrayList<>();
         this.allUsersPortfolios = this.getPortfolios();
@@ -45,7 +51,13 @@ public class PortfolioService {
             }
         }
     }
+    @Transactional
     public void savePortfolio(Portfolio portfolio) {
+        User user = portfolio.getUser();
+        if (user != null && (user.getId() == null || this.userService.getUserById(user.getId()) == null)) {
+            this.userService.saveUser(user);
+            portfolio.setUser(user);
+        }
         this.portfolioRepository.save(portfolio);
     }
     public Currency getCurrencyFromRequest(PortfolioAssetRequest portfolioAssetRequest) {
@@ -61,14 +73,14 @@ public class PortfolioService {
     public Portfolio getPortfolio(Long userId) {
         return this.portfolioRepository.findPortfolioByUserId(userId);
     }
-    public ArrayList<Portfolio> getPortfolios() {
-        return (ArrayList<Portfolio>) this.portfolioRepository.findAll();
+    public List<Portfolio> getPortfolios() {
+        return (List<Portfolio>) this.portfolioRepository.findAll();
     }
 
     public void addAssetToPortfolio(Portfolio portfolio, PortfolioAssetRequest portfolioAssetRequest) {
         Currency requestCurrency = this.getCurrencyFromRequest(portfolioAssetRequest);
         PortfolioAsset portfolioAsset = new PortfolioAsset(portfolio, requestCurrency, portfolioAssetRequest.getShares(), portfolioAssetRequest.getWalletDollars());
-        portfolio.addAsset(portfolioAsset);
+        portfolio.getAssets().add(portfolioAsset);
         this.savePortfolio(portfolio);
     }
 }
