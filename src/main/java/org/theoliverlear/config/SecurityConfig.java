94f4abd8d589737ce28ceb76e62fcb13c89b7780
theoliverlear <sigwartholiver2@gmail.com>
2024-05-13 14:33:20 -0500

    Add comment header for better readability in SecurityConfig.java
    Took 31 seconds

--- src/main/java/org/theoliverlear/config/SecurityConfig.java
+++ src/main/java/org/theoliverlear/config/SecurityConfig.java
@@ -1,5 +1,5 @@
 package org.theoliverlear.config;
-
+//=================================-Imports-==================================
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
