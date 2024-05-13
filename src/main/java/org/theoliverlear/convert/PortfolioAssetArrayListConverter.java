743bc06f1fd44b16b77563fa54b7901c8f1d220e
theoliverlear <sigwartholiver2@gmail.com>
2024-05-13 14:27:13 -0500

    Add comment header for better readability in PortfolioAssetArrayListConverter.java
    Took 35 seconds

--- src/main/java/org/theoliverlear/convert/PortfolioAssetArrayListConverter.java
+++ src/main/java/org/theoliverlear/convert/PortfolioAssetArrayListConverter.java
@@ -1,11 +1,11 @@
 package org.theoliverlear.convert;
-
+//=================================-Imports-==================================
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.persistence.AttributeConverter;
 import jakarta.persistence.Converter;
-import org.theoliverlear.entity.PortfolioAsset;
+import org.theoliverlear.entity.portfolio.PortfolioAsset;
 
 import java.util.ArrayList;
 
