c5e6c3938e66037f9671b0c23f9b2c402e13443b
theoliverlear <sigwartholiver2@gmail.com>
2024-05-13 14:20:45 -0500

    Refactor import statement and add commment header in CurrencyHistoryRepository.java
    Took 51 seconds

--- src/main/java/org/theoliverlear/repository/CurrencyHistoryRepository.java
+++ src/main/java/org/theoliverlear/repository/CurrencyHistoryRepository.java
@@ -1,11 +1,7 @@
 package org.theoliverlear.repository;
-
+//=================================-Imports-==================================
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
-import org.theoliverlear.entity.Currency;
-import org.theoliverlear.entity.CurrencyHistory;
+import org.theoliverlear.entity.currency.CurrencyHistory;
 
 public interface CurrencyHistoryRepository extends JpaRepository<CurrencyHistory, Long> {
 
