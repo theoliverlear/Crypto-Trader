fa6b493a46f8da09c2f4a609101e188bcb7faab5
theoliverlear <sigwartholiver2@gmail.com>
2024-05-13 14:35:24 -0500

    Refactor imports, add Lombok getters and setters, and add comment headers for better readability in Trader.java
    Took 50 seconds

--- src/main/java/org/theoliverlear/model/trade/Trader.java
+++ src/main/java/org/theoliverlear/model/trade/Trader.java
@@ -1,33 +1,37 @@
 package org.theoliverlear.model.trade;
-
-import org.theoliverlear.entity.Portfolio;
-import org.theoliverlear.entity.PortfolioAsset;
+//=================================-Imports-==================================
+import lombok.Getter;
+import lombok.Setter;
+import org.theoliverlear.entity.portfolio.Portfolio;
+import org.theoliverlear.entity.portfolio.PortfolioAsset;
 
 import java.util.ArrayList;
+@Getter
+@Setter
 public class Trader {
+    //============================-Variables-=================================
     // TODO: A trader may take a full portfolio or a single asset. It will
     //       trade based on target prices and a strategy.
     Portfolio portfolio;
     ArrayList<AssetTrader> assetTraders;
+    //===========================-Constructors-===============================
     public Trader(Portfolio portfolio) {
         this.portfolio = portfolio;
         this.assetTraders = new ArrayList<>();
         this.initializeAssetTraders();
     }
+    //============================-Methods-===================================
+
+    //----------------------Initialize-Asset-Traders--------------------------
     public void initializeAssetTraders() {
         for (PortfolioAsset asset : this.portfolio.getAssets()) {
             this.assetTraders.add(new AssetTrader(asset));
         }
     }
+    //--------------------------Trade-All-Assets------------------------------
     public void tradeAllAssets() {
         for (AssetTrader assetTrader : this.assetTraders) {
             assetTrader.trade();
         }
-    }
-    public Portfolio getPortfolio() {
-        return this.portfolio;
-    }
-    public void setPortfolio(Portfolio portfolio) {
-        this.portfolio = portfolio;
     }
 }
