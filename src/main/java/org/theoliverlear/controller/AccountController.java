448f36b87afe52dc9fc5508045cf49a8cc57b990
theoliverlear <sigwartholiver2@gmail.com>
2024-05-13 14:03:11 -0500

    Add comment headers for better readability in AccountController.java
    Took 53 minutes

--- src/main/java/org/theoliverlear/controller/AccountController.java
+++ src/main/java/org/theoliverlear/controller/AccountController.java
@@ -1,18 +1,22 @@
 package org.theoliverlear.controller;
-
+//=================================-Imports-==================================
 import jakarta.servlet.http.HttpSession;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
-import org.theoliverlear.entity.User;
+import org.theoliverlear.entity.user.User;
 import org.springframework.util.StringUtils;
 
 @Controller
 @RequestMapping("/account")
 public class AccountController {
+    //============================-Variables-=================================
     User currentUser;
+    //=============================-Methods-==================================
+
+    //------------------------------Account-----------------------------------
     @RequestMapping("/")
     public String account(HttpSession session) {
         User user = (User) session.getAttribute("user");
@@ -23,8 +27,9 @@ public class AccountController {
             return "redirect:/user/";
         }
     }
+    //----------------------------Upload-Image--------------------------------
     @RequestMapping("/image/upload")
-    public ResponseEntity<String> uploadImage(@RequestParam("image")MultipartFile imageFile) {
+    public ResponseEntity<String> uploadImage(@RequestParam("image") MultipartFile imageFile) {
         if (this.currentUser != null) {
             String imageFileName = StringUtils.cleanPath(imageFile.getOriginalFilename());
             return null;
