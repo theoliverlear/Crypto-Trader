a4024a6b7c99ed312643c6c3b19f499a10c676de
theoliverlear <sigwartholiver2@gmail.com>
2024-05-13 14:41:15 -0500

    Add comment headers for better readability in UserRepository.java
    Took 55 seconds

--- src/main/java/org/theoliverlear/repository/UserRepository.java
+++ src/main/java/org/theoliverlear/repository/UserRepository.java
@@ -1,10 +1,15 @@
 package org.theoliverlear.repository;
-
+//=================================-Imports-==================================
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.theoliverlear.entity.User;
+import org.theoliverlear.entity.user.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    //============================-Methods-===================================
+
+    //------------------------Get-User-By-Username----------------------------
     User getUserByUsername(String username);
+    //---------------------------Get-User-By-Id-------------------------------
     User getUserById(Long id);
+    //-------------------------Exists-By-Username-----------------------------
     boolean existsByUsername(String username);
 }
