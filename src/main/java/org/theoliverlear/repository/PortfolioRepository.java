d067f675f9c2ed3b9e71d596bdea369cfae0937f
theoliverlear <sigwartholiver2@gmail.com>
2024-05-13 14:30:07 -0500

    Add comment header for better readability in PortfolioRepository.java
    Took 27 seconds

--- src/main/java/org/theoliverlear/repository/PortfolioRepository.java
+++ src/main/java/org/theoliverlear/repository/PortfolioRepository.java
@@ -1,9 +1,11 @@
 package org.theoliverlear.repository;
 //=================================-Imports-==================================
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.theoliverlear.entity.Portfolio;
+import org.theoliverlear.entity.portfolio.Portfolio;
 
 public interface PortfolioRepository extends JpaRepository<Portfolio, Long> {
     //=============================-Methods-==================================
+
+    //---------------------Find-Portfolio-By-User-Id--------------------------
     Portfolio findPortfolioByUserId(Long userId);
 }
