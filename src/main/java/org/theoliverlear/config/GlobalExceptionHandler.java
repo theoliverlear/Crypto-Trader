96f71135aafa715e957a5df2e1ab47463c2a3c9a
theoliverlear <sigwartholiver2@gmail.com>
2024-05-13 14:23:54 -0500

    Add comment headers for better readability in GlobalExceptionHandler.java
    Took 34 seconds

--- src/main/java/org/theoliverlear/config/GlobalExceptionHandler.java
+++ src/main/java/org/theoliverlear/config/GlobalExceptionHandler.java
@@ -1,5 +1,5 @@
 package org.theoliverlear.config;
-
+//=================================-Imports-==================================
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandler {
+    //=============================-Methods-==================================
+
+    //------------------------Handle-Any-Exception----------------------------
     @ExceptionHandler(value = {Exception.class})
     public void handleAnyException(Exception ex) {
         log.error("An exception occurred: ", ex);
