ef01340f2d422e1bdc8edf0f81a370e6e37090d0
theoliverlear <sigwartholiver2@gmail.com>
2024-05-13 14:23:20 -0500

    Add comment headers for better readability in CurrencyTest.java
    Took 27 seconds

--- src/test/java/junit/CurrencyTest.java
+++ src/test/java/junit/CurrencyTest.java
@@ -1,13 +1,17 @@
 package junit;
-
+//=================================-Imports-==================================
 import org.junit.jupiter.api.Test;
-import org.theoliverlear.entity.Currency;
+import org.theoliverlear.entity.currency.Currency;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CurrencyTest {
+    //============================-Variables-=================================
     Currency testCurrency = new Currency("Bitcoin", "BTC", "https://api.coinbase.com/v2/prices/BTC-USD/spot");
+    //==============================-Tests-===================================
+
+    //------------------------Test-Null-Instances-----------------------------
     @Test
     public void testNullInstances() {
         boolean nameIsNull = this.testCurrency.getName() == null;
@@ -19,12 +23,14 @@ public class CurrencyTest {
         assertFalse(urlPathIsNull);
         assertFalse(formattedValueIsNull);
     }
+    //----------------------Test-Get-Value-From-Json--------------------------
     @Test
     public void testGetValueFromJson() {
         double value = this.testCurrency.getValue();
         System.out.println(value);
         assertTrue(value > 0);
     }
+    //---------------------Test-Get-Currency-Api-Json-------------------------
     @Test
     public void testGetCurrencyApiJson() {
         String json = this.testCurrency.getCurrencyApiJson();
