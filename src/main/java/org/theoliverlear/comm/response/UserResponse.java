c8aa574da4201b247c9572c552a8c295091862f2
theoliverlear <sigwartholiver2@gmail.com>
2024-05-13 14:42:04 -0500

    Add comment headers for better readability in UserResponse.java
    Took 17 seconds

--- src/main/java/org/theoliverlear/comm/response/UserResponse.java
+++ src/main/java/org/theoliverlear/comm/response/UserResponse.java
@@ -1,10 +1,12 @@
 package org.theoliverlear.comm.response;
-
+//=================================-Imports-==================================
 import lombok.Data;
 
 @Data
 public class UserResponse {
+    //============================-Variables-=================================
     String message;
+    //===========================-Constructors-===============================
     public UserResponse(String message) {
         this.message = message;
     }
