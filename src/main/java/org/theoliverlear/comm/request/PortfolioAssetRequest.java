e1b04e0dac15678a7dd6ef5ac13c3b99105b5124
theoliverlear <sigwartholiver2@gmail.com>
2024-05-13 14:28:53 -0500

    Add comment headers for better readability in PortfolioAssetRequest.java
    Took 40 seconds

--- src/main/java/org/theoliverlear/comm/request/PortfolioAssetRequest.java
+++ src/main/java/org/theoliverlear/comm/request/PortfolioAssetRequest.java
@@ -1,13 +1,14 @@
 package org.theoliverlear.comm.request;
-
+//=================================-Imports-==================================
 import lombok.Data;
 
 @Data
 public class PortfolioAssetRequest {
+    //============================-Variables-=================================
     String currencyName;
     double shares;
     double walletDollars;
-
+    //===========================-Constructors-===============================
     public PortfolioAssetRequest(String currencyName, double shares, double walletDollars) {
         this.currencyName = currencyName;
         this.shares = shares;
