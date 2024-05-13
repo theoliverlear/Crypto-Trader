364aef86d6e7ab2b5945be8e2ecc75a00a6820d1
theoliverlear <sigwartholiver2@gmail.com>
2024-05-13 14:34:35 -0500

    Add comment headers for better readability in TaskExecutorConfig.java
    Took 30 seconds

--- src/main/java/org/theoliverlear/config/TaskExecutorConfig.java
+++ src/main/java/org/theoliverlear/config/TaskExecutorConfig.java
@@ -1,5 +1,5 @@
 package org.theoliverlear.config;
-
+//=================================-Imports-==================================
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.task.TaskExecutor;
@@ -9,6 +9,9 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 @Configuration
 @EnableAsync
 public class TaskExecutorConfig {
+    //==============================-Beans-===================================
+
+    //---------------------Thread-Pool-Task-Executor--------------------------
     @Bean(name = "taskExecutor")
     public TaskExecutor threadPoolTaskExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
