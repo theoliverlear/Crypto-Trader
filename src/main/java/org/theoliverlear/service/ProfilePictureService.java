d96e897e528b55367a5b84960352fc0f861c13ad
theoliverlear <sigwartholiver2@gmail.com>
2024-05-13 14:31:12 -0500

    Add comment headers for better readability in ProfilePictureService.java
    Took 23 seconds

--- src/main/java/org/theoliverlear/service/ProfilePictureService.java
+++ src/main/java/org/theoliverlear/service/ProfilePictureService.java
@@ -1,7 +1,12 @@
 package org.theoliverlear.service;
-
+//=================================-Imports-==================================
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProfilePictureService {
+    //============================-Variables-=================================
+
+    //===========================-Constructors-===============================
+
+    //============================-Methods-===================================
 }
