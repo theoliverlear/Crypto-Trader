ecadb926229337600fcf95380eba8a776227ce49
theoliverlear <sigwartholiver2@gmail.com>
2024-05-13 14:16:52 -0500

    Refactor CryptoTrader.java to move it to model/trade package, add Lombok getter and setter annotations, and add comment headers
    Took 4 minutes

--- src/main/java/org/theoliverlear/model/trade/CryptoTrader.java
+++ src/main/java/org/theoliverlear/model/trade/CryptoTrader.java
@@ -1,15 +1,15 @@
-package org.theoliverlear;
+package org.theoliverlear.model.trade;
 //=================================-Imports-==================================
-import org.theoliverlear.entity.Portfolio;
-import org.theoliverlear.entity.PortfolioAsset;
-import org.theoliverlear.model.trade.Trader;
-import org.theoliverlear.update.SupportedCurrencies;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.ArrayList;
 
 // TODO: Make this a component or service within the Spring framework. This
 //       will allow for the JPA repository to be autowired and used to
 //       persist the currency data.
+@Getter
+@Setter
 public class CryptoTrader {
     //============================-Variables-=================================
     ArrayList<Trader> traders;
@@ -20,34 +20,28 @@ public class CryptoTrader {
     public CryptoTrader(ArrayList<Trader> traders) {
         this.traders = traders;
     }
+    //=============================-Methods-==================================
+
+    //------------------------Trade-All-Portfolios----------------------------
     public void tradeAllPortfolios() {
         for (Trader trader : this.traders) {
             trader.tradeAllAssets();
         }
     }
+    //---------------------------Add-Portfolio--------------------------------
     public void addTrader(Trader trader) {
         this.traders.add(trader);
     }
+    //----------------------------Add-Traders---------------------------------
     public void addTraders(ArrayList<Trader> traders) {
         this.traders.addAll(traders);
     }
+    //--------------------------------Size------------------------------------
     public int size() {
         return this.traders.size();
     }
+    //------------------------------Is-Empty----------------------------------
     public boolean isEmpty() {
         return this.traders.isEmpty();
-    }
-    public ArrayList<Trader> getTraders() {
-        return this.traders;
-    }
-    //===============================-Main-===================================
-    public static void main(String[] args) {
-        Portfolio portfolio = new Portfolio();
-        portfolio.getAssets().add(new PortfolioAsset(SupportedCurrencies.BITCOIN, 1.0, 0));
-        Trader trader = new Trader(portfolio);
-        ArrayList<Trader> traders = new ArrayList<>();
-        traders.add(trader);
-        CryptoTrader cryptoTrader = new CryptoTrader(traders);
-
     }
 }
