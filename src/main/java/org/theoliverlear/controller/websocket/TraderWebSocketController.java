4bdcb09b2d47ce4a86f7d674eccd0a939dd77923
theoliverlear <sigwartholiver2@gmail.com>
2024-05-13 14:37:10 -0500

    Add comment headers for better readability in TraderWebSocketController.java
    Took 23 seconds

--- src/main/java/org/theoliverlear/controller/websocket/TraderWebSocketController.java
+++ src/main/java/org/theoliverlear/controller/websocket/TraderWebSocketController.java
@@ -1,10 +1,13 @@
 package org.theoliverlear.controller.websocket;
-
+//=================================-Imports-==================================
 import jakarta.websocket.Session;
 import jakarta.websocket.server.ServerEndpoint;
 
 @ServerEndpoint(value = "/websocket/trader")
 public class TraderWebSocketController {
+    //=============================-Methods-==================================
+
+    //------------------------------On-Open-----------------------------------
     public void onOpen(Session session) {
 
     }
