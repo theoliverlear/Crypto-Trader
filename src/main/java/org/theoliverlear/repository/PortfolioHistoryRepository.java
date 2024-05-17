65499fa7fb6c33e6d82f5d69dec0e9a7c552dfb0
theoliverlear <sigwartholiver2@gmail.com>
2024-05-17 17:18:58 -0500

    Refactor Portfolio to PortfolioHistory in JpaRepository interface in PortfolioHistoryRepository.java
    Took 41 seconds

--- src/main/java/org/theoliverlear/repository/PortfolioHistoryRepository.java
+++ src/main/java/org/theoliverlear/repository/PortfolioHistoryRepository.java
@@ -1,7 +1,7 @@
 package org.theoliverlear.repository;
 //=================================-Imports-==================================
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.theoliverlear.entity.portfolio.Portfolio;
+import org.theoliverlear.entity.portfolio.PortfolioHistory;
 
-public interface PortfolioHistoryRepository extends JpaRepository<Portfolio, Long> {
+public interface PortfolioHistoryRepository extends JpaRepository<PortfolioHistory, Long> {
 }
