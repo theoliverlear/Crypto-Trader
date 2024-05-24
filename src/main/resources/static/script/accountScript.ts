7840b203f7666f6d8f2bc54ba449e88c7582ce6a
theoliverlear <sigwartholiver2@gmail.com>
2024-05-24 13:33:03 -0500

    Refactor accountScript.js to accountScript.ts

---
+++ src/main/resources/static/script/accountScript.ts
@@ -0,0 +1,3 @@
+import {logoutSequence} from "./globalScript";
+let accountLogoutButton = document.getElementById('logout-button-div');
+accountLogoutButton.addEventListener('click', logoutSequence);
