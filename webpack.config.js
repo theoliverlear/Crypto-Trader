347c29106435212eefda63d45303fb0fe2fa56a2
theoliverlear <sigwartholiver2@gmail.com>
2024-05-24 15:30:20 -0500

    Add webpack.config.js to compile and bundle JavaScript and TypeScript scripts to a bundle
    Took 53 seconds

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: [
+        './src/main/resources/static/script/accountScript.ts',
+        './src/main/resources/static/script/getStartedScript.ts',
+        './src/main/resources/static/script/globalScript.ts',
+        './src/main/resources/static/script/homeScript.ts',
+        './src/main/resources/static/script/PortfolioAsset.ts',
+        './src/main/resources/static/script/portfolioScript.ts',
+        './src/main/resources/static/script/traderScript.ts',
+        './src/main/resources/static/script/userScript.ts'
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'babel-loader', // Add Babel Loader
+                        options: {
+                            presets: [
+                                '@babel/preset-env' // Use preset-env for all necessary transformations
+                            ]
+                        }
+                    },
+                    'ts-loader' // Keep using ts-loader for TypeScript transpilation
+                ]
+            },
+        ],
+    },
+    resolve: {
+        extensions: [
+            '.ts',
+            '.js'
+        ],
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, './src/main/resources/static/script'),
+    },
+    mode: 'development',
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery'
+        })
+    ]
+};
