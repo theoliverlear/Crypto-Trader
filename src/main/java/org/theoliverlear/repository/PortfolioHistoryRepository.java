7ff741a428ee206ff6664ee10da01fd0835d8e59
theoliverlear <sigwartholiver2@gmail.com>
2024-05-25 20:32:40 -0500

    Add methods to find all entries and get first entry by portfolio ID in PortfolioHistoryRepository.java
    Took 2 minutes

--- src/main/java/org/theoliverlear/repository/PortfolioHistoryRepository.java
+++ src/main/java/org/theoliverlear/repository/PortfolioHistoryRepository.java
@@ -3,5 +3,13 @@ package org.theoliverlear.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.theoliverlear.entity.portfolio.PortfolioHistory;
 
+import java.util.List;
+
 public interface PortfolioHistoryRepository extends JpaRepository<PortfolioHistory, Long> {
+    //=============================-Methods-==================================
+
+    //----------------------Find-All-By-Portfolio-Id--------------------------
+    List<PortfolioHistory> findAllByPortfolioId(Long portfolioId);
+    //---------------------Get-First-By-Portfolio-Id--------------------------
+    PortfolioHistory getFirstByPortfolioId(Long portfolioId);
 }
