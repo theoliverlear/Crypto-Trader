89544230f6dd202717fbe89d9431eab7a7ce7f47
theoliverlear <sigwartholiver2@gmail.com>
2024-05-25 19:14:51 -0500

    Add comments and structure to accountScript.ts for better readability
    Took 4 hours 4 minutes

--- src/main/resources/static/script/accountScript.ts
+++ src/main/resources/static/script/accountScript.ts
@@ -1,5 +1,11 @@
+//=================================-Imports-==================================
 import {loadPage, logoutSequence} from "./globalScript";
+
+//================================-Variables-=================================
+
+//----------------------------------Buttons-----------------------------------
 let accountLogoutButton = document.getElementById('logout-button-div');
+//=============================-Event-Listeners-==============================
 if (loadPage(document.body, 'account')) {
     accountLogoutButton.addEventListener('click', logoutSequence);
 }
