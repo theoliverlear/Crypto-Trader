23a91539113a074f29721b7ba25a996a3e5b97ab
theoliverlear <sigwartholiver2@gmail.com>
2024-05-25 20:25:02 -0500

    Add getFirstByPortfolioAssetId method in PortfolioAssetHistoryRepository.java
    Took 30 seconds

--- src/main/java/org/theoliverlear/repository/PortfolioAssetHistoryRepository.java
+++ src/main/java/org/theoliverlear/repository/PortfolioAssetHistoryRepository.java
@@ -1,8 +1,11 @@
 package org.theoliverlear.repository;
-
+//=================================-Imports-==================================
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.theoliverlear.entity.portfolio.PortfolioAssetHistory;
 
 public interface PortfolioAssetHistoryRepository extends JpaRepository<PortfolioAssetHistory, Long> {
+    //=============================-Methods-==================================
 
+    //------------------Get-First-By-Portfolio-Asset-Id-----------------------
+    PortfolioAssetHistory getFirstByPortfolioAssetId(Long portfolioAssetId);
 }
