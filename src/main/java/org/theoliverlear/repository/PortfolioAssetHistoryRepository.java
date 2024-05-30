6eabe1e0ba8965e01b3886cc3fcc645ab3a3b882
theoliverlear <sigwartholiver2@gmail.com>
2024-05-29 21:18:49 -0500

    Add two new methods in PortfolioAssetHistoryRepository.java for fetching the latest PortfolioAssetHistory and retrieving all histories by Portfolio Id
    Took 20 seconds

--- src/main/java/org/theoliverlear/repository/PortfolioAssetHistoryRepository.java
+++ src/main/java/org/theoliverlear/repository/PortfolioAssetHistoryRepository.java
@@ -3,9 +3,13 @@ package org.theoliverlear.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.theoliverlear.entity.portfolio.PortfolioAssetHistory;
 
+import java.util.List;
+
 public interface PortfolioAssetHistoryRepository extends JpaRepository<PortfolioAssetHistory, Long> {
     //=============================-Methods-==================================
 
     //------------------Get-First-By-Portfolio-Asset-Id-----------------------
     PortfolioAssetHistory getFirstByPortfolioAssetId(Long portfolioAssetId);
+    PortfolioAssetHistory findFirstByPortfolioAssetIdOrderByLastUpdatedDesc(Long portfolioAssetId);
+    List<PortfolioAssetHistory> findAllByPortfolioId(Long portfolioId);
 }
