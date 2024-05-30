b1f8bda4fcf9ba5004f6905d9eac770f09df8df3
theoliverlear <sigwartholiver2@gmail.com>
2024-05-30 13:34:23 -0500

    Refactor BuyTrade.ts to change type of valueIncrease to string and add shareOrWalletChange property
    Took 19 seconds

--- src/main/resources/static/script/BuyTrade.ts
+++ src/main/resources/static/script/BuyTrade.ts
@@ -1,22 +1,22 @@
 import {Trade} from "./Trade";
 
 export class BuyTrade extends Trade {
-    valueIncrease: number;
+    valueIncrease: string;
+    shareOrWalletChange: string;
     currencyName: string;
     timeOccurred: string;
     buyValue: string;
     shareBuy: number;
-    constructor(valueIncrease: number, currencyName: string, timeOccurred: string,  buyValue: string, shareBuy: number) {
-        super(valueIncrease, currencyName, timeOccurred);
+    constructor(valueIncrease: string, shareOrWalletChange: string, currencyName: string, timeOccurred: string, buyValue: string, shareBuy: number) {
+        super(valueIncrease, shareOrWalletChange, currencyName, timeOccurred);
         this.buyValue = buyValue;
         this.shareBuy = shareBuy;
     }
     getTradeHtml(): string {
         return `
-            // TODO: Change phrasing or edit functionality so initial value text is not 0.
             <h4>Value increased <span class="value-increase-text">${this.valueIncrease}</span></h4>
-            <h6>Buy <span class="buy-value-text">${this.buyValue}</span> of <span class="trader-item-currency">${this.currencyName}</span> for <span class="share-buy-text">${this.shareBuy}</span> shares.</h6>
-            <h6>at <span class="trade-time-text">${this.timeOccurred}</span></h6>
+            <h6>Buy <span class="buy-value-text">${this.shareOrWalletChange}</span> of <span class="trader-item-currency">${this.currencyName}</span> for <span class="share-buy-text">${this.shareBuy}</span> shares.</h6>
+            <h6><span class="trade-time-text">${this.timeOccurred}</span></h6>
             `;
     }
 }
