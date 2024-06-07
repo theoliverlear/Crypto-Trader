127e7aa52909f4d12de3b056b8e02413776e661d
theoliverlear <sigwartholiver2@gmail.com>
2024-06-06 21:19:38 -0500

    Add ProfilePictureService functionality in ProfilePictureService.java including saving, finding, and checking the existence of ProfilePicture by User ID
    Took 27 seconds

--- src/main/java/org/theoliverlear/service/ProfilePictureService.java
+++ src/main/java/org/theoliverlear/service/ProfilePictureService.java
@@ -1,6 +1,10 @@
 package org.theoliverlear.service;
 //=================================-Imports-==================================
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.theoliverlear.entity.user.ProfilePicture;
+import org.theoliverlear.repository.ProfilePictureRepository;
 
 @Service
 public class ProfilePictureService {
@@ -9,4 +13,19 @@ public class ProfilePictureService {
     //===========================-Constructors-===============================
 
     //============================-Methods-===================================
+    ProfilePictureRepository profilePictureRepository;
+    @Autowired
+    public ProfilePictureService(ProfilePictureRepository profilePictureRepository) {
+        this.profilePictureRepository = profilePictureRepository;
+    }
+    public void saveProfilePicture(ProfilePicture profilePicture) {
+        this.profilePictureRepository.save(profilePicture);
+    }
+    @Transactional
+    public ProfilePicture findByUserId(Long userId) {
+        return this.profilePictureRepository.findByUserId(userId);
+    }
+    public boolean existsByUserId(Long userId) {
+        return this.profilePictureRepository.existsByUserId(userId);
+    }
 }
