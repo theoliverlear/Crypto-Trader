e2c331955a5f5d09abf146f3ff2e9f96a09cb9af
theoliverlear <sigwartholiver2@gmail.com>
2024-11-28 09:21:48 -0600

    Add elements.ts to centralize and export elements in the Angular application

---
+++ angular/components/elements/elements.ts
@@ -0,0 +1,15 @@
+import {NavBarComponent} from "./nav-bar/nav-bar.component";
+import {SsAnchorComponent} from "./ss-anchor/ss-anchor.component";
+import {SsButtonComponent} from "./ss-button/ss-button.component";
+import {SsFooterComponent} from "./ss-footer/ss-footer.component";
+import {SsHeadComponent} from "./ss-head/ss-head.component";
+import {SsImgComponent} from "./ss-img/ss-img.component";
+
+export const elements = [
+    NavBarComponent,
+    SsAnchorComponent,
+    SsButtonComponent,
+    SsFooterComponent,
+    SsHeadComponent,
+    SsImgComponent
+]
