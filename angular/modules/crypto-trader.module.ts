b110cfd01622c8b1b4288be0a7a1ed977fd2aca7
theoliverlear <sigwartholiver2@gmail.com>
2024-11-28 08:43:41 -0600

    Add crypto-trader.module.ts to bootstrap Angular components and services

---
+++ angular/modules/crypto-trader.module.ts
@@ -0,0 +1,42 @@
+import {NgModule} from "@angular/core";
+import {AppComponent} from "../components/app/app.component";
+import {elements} from "../components/elements/elements";
+import {directives} from "../directives/directives";
+import {pages} from "../components/pages/pages";
+import {BrowserModule} from "@angular/platform-browser";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {FormsModule} from "@angular/forms";
+import {AppRouting} from "./routing/app-routing.module";
+import {RouterOutlet} from "@angular/router";
+import {NgOptimizedImage} from "@angular/common";
+import {HttpClientModule, provideHttpClient, withFetch} from "@angular/common/http";
+import {services} from "../services/services";
+
+@NgModule({
+    declarations: [
+        AppComponent,
+        ...elements,
+        ...directives,
+        ...pages,
+    ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        FormsModule,
+        AppRouting,
+        RouterOutlet,
+        NgOptimizedImage,
+        HttpClientModule],
+    providers: [
+        ...services,
+        provideHttpClient(withFetch()),
+    ],
+    bootstrap: [AppComponent],
+    exports: [],
+    schemas: []
+})
+export class CryptoTraderModule {
+    constructor() {
+        console.log('CryptoTraderModule loaded');
+    }
+}
