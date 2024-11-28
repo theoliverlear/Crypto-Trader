4cc834e35ad59bc8a7320591f3e9a3108fa3b153
theoliverlear <sigwartholiver2@gmail.com>
2024-11-28 09:33:49 -0600

    Add home.component.ts for defining the homepage component in the Angular application

---
+++ angular/components/pages/home/home.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'home',
+  templateUrl: './home.component.html',
+  styleUrl: './home-style.component.css'
+})
+export class HomeComponent {
+
+}
