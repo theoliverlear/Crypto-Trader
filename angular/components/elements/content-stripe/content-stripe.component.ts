fd74c4e29904cdfb665285695c1648cdb7ea27ad
theoliverlear <sigwartholiver2@gmail.com>
2024-11-29 13:57:32 -0600

    Add content-stripe.component.ts for creating a reusable content stripe element in the Angular application

---
+++ angular/components/elements/content-stripe/content-stripe.component.ts
@@ -0,0 +1,12 @@
+import {Component} from "@angular/core";
+
+@Component({
+    selector: 'content-stripe',
+    templateUrl: './content-stripe.component.html',
+    styleUrls: ['./content-stripe-style.component.css']
+})
+export class ContentStripeComponent {
+    constructor() {
+
+    }
+}
