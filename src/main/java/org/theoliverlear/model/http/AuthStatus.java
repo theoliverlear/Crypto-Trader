487d8cb9aab852f31b12285c1a784a433537f39b
theoliverlear <sigwartholiver2@gmail.com>
2024-11-28 19:19:04 -0600

    Add AuthStatus.java to represent user authentication status constants

---
+++ src/main/java/org/theoliverlear/model/http/AuthStatus.java
@@ -0,0 +1,13 @@
+package org.theoliverlear.model.http;
+
+public enum AuthStatus {
+    AUTHORIZED(true),
+    UNAUTHORIZED(false);
+    public final boolean isAuthorized;
+    AuthStatus(boolean authorized) {
+        this.isAuthorized = authorized;
+    }
+    public static AuthStatus from(boolean authorized) {
+        return authorized ? AUTHORIZED : UNAUTHORIZED;
+    }
+}
