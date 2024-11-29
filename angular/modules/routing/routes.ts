b288547f6f7e275e21676aed6c8f5fe8896b4465
theoliverlear <sigwartholiver2@gmail.com>
2024-11-29 11:32:54 -0600

    Add portfolio route with AuthGuard in routes.ts

--- angular/modules/routing/routes.ts
+++ angular/modules/routing/routes.ts
@@ -1,5 +1,9 @@
 import {Route, Routes} from "@angular/router";
 import {HomeComponent} from "../../components/pages/home/home.component";
+import {
+    PortfolioComponent
+} from "../../components/pages/portfolio/portfolio.component";
+import {AuthGuard} from "../../services/guard/auth.guard";
 
 
 const isDevelopment = true;
@@ -13,7 +17,17 @@ export const homeRoute: Route = {
         },
     }
 }
-
+export const portfolioRoute: Route = {
+    path: 'portfolio',
+    component: PortfolioComponent,
+    canActivate: isDevelopment ? [] : [AuthGuard],
+    data: {
+        meta: {
+            title: 'Portfolio | Crypto Trader'
+        },
+    }
+}
 export const routes: Routes = [
-    homeRoute
+    homeRoute,
+    portfolioRoute
 ];
