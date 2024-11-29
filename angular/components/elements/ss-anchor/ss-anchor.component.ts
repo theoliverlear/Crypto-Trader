83a380db10b31caa5fea3e56932382a52ddf14bf
theoliverlear <sigwartholiver2@gmail.com>
2024-11-28 18:53:32 -0600

    Allow ss-anchor.component.ts to accept ElementLink input by updating elementLink type and initialization logic

--- angular/components/elements/ss-anchor/ss-anchor.component.ts
+++ angular/components/elements/ss-anchor/ss-anchor.component.ts
@@ -4,6 +4,7 @@ import {
 } from "@angular/core";
 import {TagType} from "../../../models/html/TagType";
 import {TextElementLink} from "../../../models/link/TextElementLink";
+import {ElementLink} from "../../../models/link/ElementLink";
 
 @Component({
     selector: 'ss-anchor',
@@ -11,16 +12,15 @@ import {TextElementLink} from "../../../models/link/TextElementLink";
     styleUrls: ['./ss-anchor-style.component.css']
 })
 export class SsAnchorComponent implements OnInit {
-    @Input() elementLink: TextElementLink;
+    @Input() elementLink: TextElementLink | ElementLink;
     hasContent: boolean = false;
     constructor() {
         console.log('SsAnchorComponent loaded');
     }
     ngOnInit() {
-        if (this.elementLink) {
+        if (this.elementLink && this.elementLink instanceof TextElementLink) {
             this.hasContent = !this.elementLink.hasText();
         }
     }
-
     protected readonly TagType = TagType;
 }
