b9d0419bb89661ddb316c5a6100c0bd618c36acd
theoliverlear <sigwartholiver2@gmail.com>
2024-11-28 19:14:19 -0600

    Refactor TraderController.java to be a RestController API and no longer serve HTML

--- src/main/java/org/theoliverlear/controller/TraderController.java
+++ src/main/java/org/theoliverlear/controller/TraderController.java
@@ -1,26 +1,14 @@
 package org.theoliverlear.controller;
 //=================================-Imports-==================================
-import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.theoliverlear.entity.user.User;
 
 @Controller
-@RequestMapping("/trader")
+@RequestMapping("/api/trader")
 public class TraderController {
     //============================-Variables-=================================
     private User currentUser;
     //=============================-Methods-==================================
 
-    //-------------------------------Trader-----------------------------------
-    @RequestMapping("/")
-    public String trader(HttpSession session) {
-        User user = (User) session.getAttribute("user");
-        if (user == null) {
-            return "redirect:/user/";
-        } else {
-            this.currentUser = user;
-            return "trader";
-        }
-    }
 }
