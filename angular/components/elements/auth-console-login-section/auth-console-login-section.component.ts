7cdd0f6123ad1c6bdf8b7ca98e8ae42d5a9b744d
theoliverlear <sigwartholiver2@gmail.com>
2024-12-01 08:48:27 -0600

    Add auth-console-login-section.component.ts to be an Angular component for the auth-console-login-section element in the Angular application

---
+++ angular/components/elements/auth-console-login-section/auth-console-login-section.component.ts
@@ -0,0 +1,13 @@
+// auth-console-login-section.component.ts 
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'auth-console-login-section',
+    templateUrl: './auth-console-login-section.component.html',
+    styleUrls: ['./auth-console-login-section-style.component.css']
+})
+export class AuthConsoleLoginSectionComponent {
+    constructor() {
+        
+    }
+}
