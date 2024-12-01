1af6cccd45ee1c0dc57d084e5922f1d01e57a26e
theoliverlear <sigwartholiver2@gmail.com>
2024-12-01 09:13:04 -0600

    Add terms-of-service.component.ts to be an Angular component for the terms-of-service element in the Angular application

---
+++ angular/components/pages/terms-of-service/terms-of-service.component.ts
@@ -0,0 +1,13 @@
+// terms-of-service.component.ts 
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'terms-of-service',
+    templateUrl: './terms-of-service.component.html',
+    styleUrls: ['./terms-of-service-style.component.css']
+})
+export class TermsOfServiceComponent {
+    constructor() {
+        
+    }
+}
