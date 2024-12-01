5f385861fb003d6d3b1284dd87e1ba755e5ec0d8
theoliverlear <sigwartholiver2@gmail.com>
2024-12-01 08:56:35 -0600

    Add performance-chart.component.ts to be an Angular component for the performance-chart element in the Angular application

---
+++ angular/components/elements/performance-chart/performance-chart.component.ts
@@ -0,0 +1,13 @@
+// performance-chart.component.ts 
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'performance-chart',
+    templateUrl: './performance-chart.component.html',
+    styleUrls: ['./performance-chart-style.component.css']
+})
+export class PerformanceChartComponent {
+    constructor() {
+        
+    }
+}
