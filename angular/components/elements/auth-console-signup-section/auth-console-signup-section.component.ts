663b296015b29dd809358c810e1b6db8db371465
theoliverlear <sigwartholiver2@gmail.com>
2024-12-01 08:53:14 -0600

    Add auth-console-signup-section.component.ts to be an Angular component for the auth-console-signup-section element in the Angular application

---
+++ angular/components/elements/auth-console-signup-section/auth-console-signup-section.component.ts
@@ -0,0 +1,13 @@
+// auth-console-signup-section.component.ts 
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'auth-console-signup-section',
+    templateUrl: './auth-console-signup-section.component.html',
+    styleUrls: ['./auth-console-signup-section-style.component.css']
+})
+export class AuthConsoleSignupSectionComponent {
+    constructor() {
+        
+    }
+}
