ed741ca8f9d73023e95406f58f1940d61518fed1
theoliverlear <sigwartholiver2@gmail.com>
2024-12-01 09:16:40 -0600

    Add components to pages.ts for new site navigation pages including Authorize, Trader, Currencies, NotFound, and TermsOfService

--- angular/components/pages/pages.ts
+++ angular/components/pages/pages.ts
@@ -1,7 +1,17 @@
 import {HomeComponent} from "./home/home.component";
 import {PortfolioComponent} from "./portfolio/portfolio.component";
+import {AuthorizeComponent} from "./authorize/authorize.component";
+import {TraderComponent} from "./trader/trader.component";
+import {CurrenciesComponent} from "./currencies/currencies.component";
+import {NotFoundComponent} from "./not-found/not-found.component";
+import {TermsOfServiceComponent} from "./terms-of-service/terms-of-service.component";
 
 export const pages = [
+    AuthorizeComponent,
+    CurrenciesComponent,
     HomeComponent,
-    PortfolioComponent
+    NotFoundComponent,
+    PortfolioComponent,
+    TermsOfServiceComponent,
+    TraderComponent,
 ];
