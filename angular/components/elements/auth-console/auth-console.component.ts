8c3cceb92d974734cc2cae94ed081acd02bada8d
theoliverlear <sigwartholiver2@gmail.com>
2024-12-01 08:47:29 -0600

    Add auth-console.component.ts to be an Angular component for the auth-console element in the Angular application

---
+++ angular/components/elements/auth-console/auth-console.component.ts
@@ -0,0 +1,12 @@
+import {Component} from "@angular/core";
+
+@Component({
+    selector: 'auth-console',
+    templateUrl: './auth-console.component.html',
+    styleUrls: ['./auth-console-style.component.css']
+})
+export class AuthConsoleComponent {
+    constructor() {
+
+    }
+}
