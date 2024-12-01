06b85c321db3427f312adce65cf647890888cc8c
theoliverlear <sigwartholiver2@gmail.com>
2024-12-01 09:01:53 -0600

    Add portfolio-section.component.ts to be an Angular component for the portfolio-section element in the Angular application

---
+++ angular/components/elements/portfolio-section/portfolio-section.component.ts
@@ -0,0 +1,13 @@
+// portfolio-section.component.ts 
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'portfolio-section',
+    templateUrl: './portfolio-section.component.html',
+    styleUrls: ['./portfolio-section-style.component.css']
+})
+export class PortfolioSectionComponent {
+    constructor() {
+        
+    }
+}
