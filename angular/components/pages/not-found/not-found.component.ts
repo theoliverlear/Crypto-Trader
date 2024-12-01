33b22a2ac3026bf222e0fea3db1df5d2d6cda87f
theoliverlear <sigwartholiver2@gmail.com>
2024-12-01 09:12:14 -0600

    Add not-found.component.ts to be an Angular component for the not-found element in the Angular application

---
+++ angular/components/pages/not-found/not-found.component.ts
@@ -0,0 +1,13 @@
+// not-found.component.ts 
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'not-found',
+    templateUrl: './not-found.component.html',
+    styleUrls: ['./not-found-style.component.css']
+})
+export class NotFoundComponent {
+    constructor() {
+        
+    }
+}
