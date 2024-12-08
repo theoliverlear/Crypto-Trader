259862cca6c613100b410bb2522952610d75cc21
theoliverlear <sigwartholiver2@gmail.com>
2024-12-08 12:23:06 -0600

    Add email field to User.java for storing user email addresses

--- src/main/java/org/theoliverlear/entity/user/User.java
+++ src/main/java/org/theoliverlear/entity/user/User.java
@@ -23,6 +23,8 @@ public class User {
     private Long id;
     @Column(name = "username")
     private String username;
+    @Column(name = "email")
+    private String email;
     @Column(name = "password_hash")
     @Convert(converter = SafePasswordConverter.class)
     private SafePassword safePassword;
@@ -38,29 +40,47 @@ public class User {
     //===========================-Constructors-===============================
     public User() {
         this.username = null;
+        this.email = null;
         this.safePassword = null;
         this.portfolio = null;
         this.lastLogin = null;
     }
     public User(String username, String rawPassword) {
         this.username = username;
+        this.email = null;
+        this.safePassword = new SafePassword(rawPassword);
+        this.lastLogin = LocalDateTime.now();
+    }
+    public User(String username, String rawPassword, String email) {
+        this.username = username;
+        this.email = email;
         this.safePassword = new SafePassword(rawPassword);
         this.lastLogin = LocalDateTime.now();
     }
     public User(String username, SafePassword encodedPassword) {
         this.username = username;
+        this.email = null;
+        this.safePassword = encodedPassword;
+        this.portfolio = new Portfolio(this);
+        this.lastLogin = LocalDateTime.now();
+    }
+    public User(String username, String email, SafePassword encodedPassword) {
+        this.username = username;
+        this.email = email;
         this.safePassword = encodedPassword;
         this.portfolio = new Portfolio(this);
         this.lastLogin = LocalDateTime.now();
     }
     public User(String username, String rawPassword, Portfolio portfolio) {
         this.username = username;
+        this.email = null;
         this.safePassword = new SafePassword(rawPassword);
         this.portfolio = portfolio;
         this.lastLogin = LocalDateTime.now();
     }
     public User(String username, String rawPassword, Portfolio portfolio, LocalDateTime lastLogin) {
         this.username = username;
+        this.email = null;
         this.safePassword = new SafePassword(rawPassword);
         this.portfolio = portfolio;
         this.lastLogin = lastLogin;
