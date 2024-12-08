27b1cf702dfc9fea68ce8c7f337e85d4824b1047
theoliverlear <sigwartholiver2@gmail.com>
2024-12-08 12:49:11 -0600

    Add OnInit implementation and updateInputText method in auth-input.component.ts to handle input text changes

--- angular/components/elements/auth-input/auth-input.component.ts
+++ angular/components/elements/auth-input/auth-input.component.ts
@@ -1,5 +1,5 @@
 // auth-input.component.ts 
-import {Component, Input} from "@angular/core";
+import {Component, Input, OnInit} from "@angular/core";
 import {AuthInputType} from "./models/AuthInputType";
 import {InputType} from "../ss-input/models/InputType";
 
@@ -8,18 +8,36 @@ import {InputType} from "../ss-input/models/InputType";
     templateUrl: './auth-input.component.html',
     styleUrls: ['./auth-input-style.component.css']
 })
-export class AuthInputComponent {
+export class AuthInputComponent implements OnInit {
     @Input() authInputType: AuthInputType;
+    @Input() authTypeString: string = "";
+    inputText: string = "";
     constructor() {
         
+    }
+    updateInputText(text: string): void {
+        this.inputText = text;
+    }
+    ngOnInit() {
+        if (this.authInputType !== AuthInputType.AGREED_TERMS) {
+            this.authTypeString = this.authInputType + ":";
+        } else {
+            this.authTypeString = this.authInputType;
+        }
     }
     getInputType() {
         switch (this.authInputType) {
             case AuthInputType.USERNAME:
                 return InputType.TEXT;
+            case AuthInputType.EMAIL:
+                return InputType.EMAIL;
             case AuthInputType.PASSWORD:
             case AuthInputType.CONFIRM_PASSWORD:
                 return InputType.PASSWORD;
+            case AuthInputType.AGREED_TERMS:
+                return InputType.CHECKBOX;
+            default:
+                return InputType.TEXT;
         }
     }
 }
