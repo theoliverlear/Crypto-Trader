e377c8bf95c40b1136f433b40b4003e6d6bcbca1
theoliverlear <sigwartholiver2@gmail.com>
2024-12-08 12:23:47 -0600

    Add email field and update constructor in UserRequest.java for handling user email data

--- src/main/java/org/theoliverlear/comm/request/UserRequest.java
+++ src/main/java/org/theoliverlear/comm/request/UserRequest.java
@@ -6,10 +6,14 @@ import lombok.Data;
 public class UserRequest {
     //============================-Variables-=================================
     private String username;
+    private String email;
     private String password;
     //============================-Constructors-==============================
-    public UserRequest(String username, String password) {
+    public UserRequest(String username,
+                       String email,
+                       String password) {
         this.username = username;
+        this.email = email;
         this.password = password;
     }
 }
