cb380c6e91ccb6e61317a666344019ee099ff458
theoliverlear <sigwartholiver2@gmail.com>
2024-12-08 12:34:56 -0600

    Add signup fields functionality and event emission in auth-console-signup-section.component.ts

--- angular/components/elements/auth-console-signup-section/auth-console-signup-section.component.ts
+++ angular/components/elements/auth-console-signup-section/auth-console-signup-section.component.ts
@@ -1,5 +1,9 @@
 // auth-console-signup-section.component.ts 
-import { Component } from "@angular/core";
+import {Component, EventEmitter, Output} from "@angular/core";
+import {AuthInputType} from "../auth-input/models/AuthInputType";
+import {ElementSize} from "../../../models/ElementSize";
+import {ButtonText} from "../ss-button/models/ButtonText";
+import {SignupCredentials} from "../../../models/auth/SignupCredentials";
 
 @Component({
     selector: 'auth-console-signup-section',
@@ -7,7 +11,29 @@ import { Component } from "@angular/core";
     styleUrls: ['./auth-console-signup-section-style.component.css']
 })
 export class AuthConsoleSignupSectionComponent {
+    signupCredentials: SignupCredentials = new SignupCredentials();
+    @Output() signupButtonClicked: EventEmitter<SignupCredentials> = new EventEmitter<SignupCredentials>();
     constructor() {
         
     }
+    updateUsername(username: string): void {
+        this.signupCredentials.username = username;
+    }
+    updateEmail(email: string): void {
+        this.signupCredentials.email = email;
+    }
+    updatePassword(password: string): void {
+        this.signupCredentials.password = password;
+    }
+    updateConfirmPassword(confirmPassword: string): void {
+        this.signupCredentials.confirmPassword = confirmPassword;
+    }
+    emitFields(): void {
+        this.signupButtonClicked.emit(this.signupCredentials);
+    }
+
+
+    protected readonly AuthInputType = AuthInputType;
+    protected readonly ElementSize = ElementSize;
+    protected readonly ButtonText = ButtonText;
 }
