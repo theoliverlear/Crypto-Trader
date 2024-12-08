886aae6f559e88fc99fa7f637e6e961dc563a61e
theoliverlear <sigwartholiver2@gmail.com>
2024-12-07 19:23:13 -0600

    Add AccountComponent import and registration in pages.ts

--- angular/components/pages/pages.ts
+++ angular/components/pages/pages.ts
@@ -5,8 +5,10 @@ import {TraderComponent} from "./trader/trader.component";
 import {CurrenciesComponent} from "./currencies/currencies.component";
 import {NotFoundComponent} from "./not-found/not-found.component";
 import {TermsOfServiceComponent} from "./terms-of-service/terms-of-service.component";
+import {AccountComponent} from "./account/account.component";
 
 export const pages = [
+    AccountComponent,
     AuthorizeComponent,
     CurrenciesComponent,
     HomeComponent,
