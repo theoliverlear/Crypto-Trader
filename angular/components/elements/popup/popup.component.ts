044c6be6ed8cad9cbca511a5323df3d006eb5dde
theoliverlear <sigwartholiver2@gmail.com>
2024-12-08 12:51:18 -0600

    Add popup.component.ts to create a popup element with message input functionality in the Angular application

---
+++ angular/components/elements/popup/popup.component.ts
@@ -0,0 +1,15 @@
+// popup.component.ts 
+import {Component, Input} from "@angular/core";
+import {AuthPopup} from "../../../models/auth/AuthPopup";
+
+@Component({
+    selector: 'popup',
+    templateUrl: './popup.component.html',
+    styleUrls: ['./popup-style.component.css']
+})
+export class PopupComponent {
+    @Input() popupMessage: AuthPopup | null = null;
+    constructor() {
+        
+    }
+}
