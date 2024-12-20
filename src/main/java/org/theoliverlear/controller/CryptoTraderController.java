bd91da8ee41c06b87fa04833350e23035eb263c6
theoliverlear <sigwartholiver2@gmail.com>
2024-12-19 19:15:05 -0600

    Remove unused currentUser variable in CryptoTraderController.java

--- src/main/java/org/theoliverlear/controller/CryptoTraderController.java
+++ src/main/java/org/theoliverlear/controller/CryptoTraderController.java
@@ -3,12 +3,9 @@ package org.theoliverlear.controller;
 //=================================-Imports-==================================
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.theoliverlear.entity.user.User;
 
 @RequestMapping("/api")
 @RestController
 public class CryptoTraderController {
     //============================-Variables-=================================
-    private User currentUser;
-
 }
