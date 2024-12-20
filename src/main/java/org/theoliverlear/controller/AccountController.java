ecc8013fe6359105e657effba000a39b333dd78a
theoliverlear <sigwartholiver2@gmail.com>
2024-12-19 19:14:12 -0600

    Integrate CryptoTraderService in AccountController.java for user session handling and refactor methods to use Optionals

--- src/main/java/org/theoliverlear/controller/AccountController.java
+++ src/main/java/org/theoliverlear/controller/AccountController.java
@@ -15,21 +15,26 @@ import org.theoliverlear.comm.response.HasProfilePictureResponse;
 import org.theoliverlear.comm.response.OperationSuccessfulResponse;
 import org.theoliverlear.entity.user.ProfilePicture;
 import org.theoliverlear.entity.user.User;
+import org.theoliverlear.service.CryptoTraderService;
 import org.theoliverlear.service.ProfilePictureService;
 import org.theoliverlear.service.UserService;
 
 import java.io.IOException;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/account")
 public class AccountController {
     //============================-Variables-=================================
-    private User currentUser;
+    private CryptoTraderService cryptoTraderService;
     private ProfilePictureService profilePictureService;
     private UserService userService;
     //===========================-Constructors-===============================
     @Autowired
-    public AccountController(ProfilePictureService profilePictureService, UserService userService) {
+    public AccountController(CryptoTraderService cryptoTraderService,
+                             ProfilePictureService profilePictureService,
+                             UserService userService) {
+        this.cryptoTraderService = cryptoTraderService;
         this.profilePictureService = profilePictureService;
         this.userService = userService;
     }
@@ -37,15 +42,25 @@ public class AccountController {
 
     //----------------------------Upload-Image--------------------------------
     @RequestMapping("/image/upload")
-    public ResponseEntity<OperationSuccessfulResponse> uploadProfilePicture(@RequestParam("file") MultipartFile file, HttpSession session) {
-        User sessionUser = (User) session.getAttribute("user");
-        if (sessionUser == null) {
+    public ResponseEntity<OperationSuccessfulResponse> uploadProfilePicture(@RequestParam("file") MultipartFile file,
+                                                                            HttpSession session) {
+        boolean userInSession = this.cryptoTraderService.userInSession(session);
+        if (!userInSession) {
             return new ResponseEntity<>(new OperationSuccessfulResponse(false), HttpStatus.UNAUTHORIZED);
         }
+        Optional<User> possibleSessionUser = this.cryptoTraderService.getUserFromSession(session);
+        if (possibleSessionUser.isEmpty()) {
+            return new ResponseEntity<>(new OperationSuccessfulResponse(false), HttpStatus.UNAUTHORIZED);
+        }
+        User sessionUser = possibleSessionUser.get();
         String fileName = file.getOriginalFilename();
         try {
             byte[] fileData = file.getBytes();
-            ProfilePicture profilePicture = new ProfilePicture(fileName, fileData, sessionUser);
+            ProfilePicture profilePicture = ProfilePicture.builder()
+                                                          .fileName(fileName)
+                                                          .fileData(fileData)
+                                                          .user(sessionUser)
+                                                          .build();
             sessionUser.setProfilePicture(profilePicture);
             profilePicture.setUser(sessionUser);
             this.profilePictureService.saveProfilePicture(profilePicture);
@@ -63,10 +78,12 @@ public class AccountController {
     @Transactional
     @RequestMapping("/get/{id}/profile-picture")
     public ResponseEntity<byte[]> getProfilePicture(@PathVariable String id) {
-        ProfilePicture profilePicture = this.profilePictureService.findByUserId(Long.parseLong(id));
-        if (profilePicture == null) {
+        long userId = Long.parseLong(id);
+        Optional<ProfilePicture> possibleProfilePicture = this.profilePictureService.findByUserId(userId);
+        if (possibleProfilePicture.isEmpty()) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         } else {
+            ProfilePicture profilePicture = possibleProfilePicture.get();
             byte[] imageData = profilePicture.getFileData();
             String fileType = profilePicture.getFileType();
             return ResponseEntity.ok().contentType(MediaType.parseMediaType(fileType)).body(imageData);
