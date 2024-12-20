995e91b689e5d6184663c417d316b0ad0c2901c7
theoliverlear <sigwartholiver2@gmail.com>
2024-12-19 18:13:07 -0600

    Add HasPortfolioResponse.java to represent an HTTP response indicating portfolio existence

---
+++ src/main/java/org/theoliverlear/comm/response/HasPortfolioResponse.java
@@ -0,0 +1,12 @@
+package org.theoliverlear.comm.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class HasPortfolioResponse {
+    private boolean hasPortfolio;
+}
