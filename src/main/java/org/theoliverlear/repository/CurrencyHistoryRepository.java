599b3e36adc56a71542fc08dc08e2bc10b5103b6
theoliverlear <sigwartholiver2@gmail.com>
2025-03-16 13:56:48 -0500

    Add method in CurrencyHistoryRepository.java to check currency existence by code

--- src/main/java/org/theoliverlear/repository/CurrencyHistoryRepository.java
+++ src/main/java/org/theoliverlear/repository/CurrencyHistoryRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.theoliverlear.entity.currency.CurrencyHistory;
 
 public interface CurrencyHistoryRepository extends JpaRepository<CurrencyHistory, Long> {
-
+    boolean existsByCurrencyCurrencyCode(String currencyCode);
 }
