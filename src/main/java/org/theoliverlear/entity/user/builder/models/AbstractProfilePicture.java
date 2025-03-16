db936f07a695ade36578fd62f026bc1e51663824
theoliverlear <sigwartholiver2@gmail.com>
2025-03-16 14:40:40 -0500

    Add AbstractProfilePicture.java as an abstract builder for constructing ProfilePicture objects

---
+++ src/main/java/org/theoliverlear/entity/user/builder/models/AbstractProfilePicture.java
@@ -0,0 +1,12 @@
+package org.theoliverlear.entity.user.builder.models;
+
+import org.theoliverlear.entity.user.ProfilePicture;
+import org.theoliverlear.entity.user.User;
+import org.theoliverlear.model.BuilderFactory;
+
+public abstract class AbstractProfilePicture implements BuilderFactory<ProfilePicture> {
+    public abstract AbstractProfilePicture fileName(String fileName);
+    public abstract AbstractProfilePicture fileType(String fileType);
+    public abstract AbstractProfilePicture fileData(byte[] fileData);
+    public abstract AbstractProfilePicture user(User user);
+}
