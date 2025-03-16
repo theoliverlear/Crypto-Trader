2923f748b9b6b8bde4833c7c970ea17548cd125c
theoliverlear <sigwartholiver2@gmail.com>
2025-03-16 13:20:40 -0500

    Refactor database.py to be able to fetch both historical and current prices

--- Crypto-Trader-Analysis/apps/models/database/database.py
+++ Crypto-Trader-Analysis/apps/models/database/database.py
@@ -7,6 +7,9 @@ from attr import attr
 from attrs import define
 from sqlalchemy import create_engine, QueuePool
 
+from apps.models.database.query_type import QueryType
+from currency_json_generator import get_all_currency_codes
+
 
 @define
 class Database:
@@ -27,18 +30,19 @@ class Database:
             echo=False
         )
 
-    def _get_default_query(self) -> str:
+    def _get_default_history_query(self) -> str:
         return """WITH btc_data AS (
     SELECT last_updated, currency_value AS target_price
     FROM currency_history
     WHERE currency_code = %(target_currency)s
     ORDER BY last_updated DESC LIMIT %(limit)s
 )"""
-    def _get_default_select_query(self):
+    def _get_default_select_history_query(self):
         return """ SELECT 
     btc.last_updated,
     btc.target_price,\n"""
-    def _get_selection_query(self, currency_code: str):
+
+    def _get_selection_history_query(self, currency_code: str):
         currency_code: str = currency_code.lower()
         currency_price: str = currency_code.lower() + '_price'
         return f"COALESCE({currency_code}.currency_value, NULL) AS {currency_price},\n"
@@ -48,20 +52,20 @@ class Database:
         lower_currency_code: str = currency_code.lower()
         return f"""LEFT JOIN LATERAL (
     SELECT currency_value FROM currency_history 
-    WHERE currency_code = '{upper_currency_code}' AND last_updated BETWEEN btc.last_updated - INTERVAL '2 seconds' AND btc.last_updated + INTERVAL '2 seconds'
-    ORDER BY last_updated DESC LIMIT %(limit)s
+    WHERE currency_code = '{upper_currency_code}' AND last_updated BETWEEN btc.last_updated - INTERVAL '2.5 seconds' AND btc.last_updated + INTERVAL '2.5 seconds'
+    ORDER BY last_updated DESC LIMIT 1
 ) {lower_currency_code} ON true\n"""
 
-    def get_full_query(self) -> str:
-        currencies: list[str] = ['ETH', 'LTC', 'DOGE', 'ADA', 'XRP', 'DOT',
-                                 'BCH', 'LINK', 'UNI', 'BNB', 'USDT', 'EOS',
-                                 'TRX', 'XTZ', 'XLM', 'SHIB']
-        query: str = self._get_default_query() + self._get_default_select_query()
+    def get_full_history_query(self) -> str:
+        currencies: list[str] = list(filter(lambda currency_code: currency_code != "BTC", get_all_currency_codes(True)))
+    #     currencies: list[str] = get_all_currency_codes()
+        print(currencies)
+        query: str = self._get_default_history_query() + self._get_default_select_history_query()
         for index, currency in enumerate(currencies):
             if index < len(currencies) - 1:
-                query += self._get_selection_query(currency)
+                query += self._get_selection_history_query(currency)
             else:
-                partial_query = self._get_selection_query(currency)
+                partial_query = self._get_selection_history_query(currency)
                 partial_query = partial_query.replace('price,\n', 'price\n')
                 query += partial_query
         query += "FROM btc_data btc\n"
@@ -73,12 +77,26 @@ class Database:
         query += "ORDER BY btc.last_updated DESC LIMIT %(limit)s;"
         return query
 
-    def fetch_data(self, target_currency: str = 'BTC', limit=20000):
-        query = self.get_full_query()
-        params = {"target_currency": target_currency, "limit": limit}
+    def fetch_data(self,
+                   target_currency: str = 'BTC',
+                   limit=20000,
+                   query_type: QueryType = QueryType.HISTORICAL_PRICE) -> pd.DataFrame:
+        logging.info("Fetching data from database...")
+        query: str = ""
+        params: dict = {"target_currency": target_currency}
+        if query_type == QueryType.CURRENT_PRICE:
+            logging.info("Fetching current price...")
+            query = f"SELECT * FROM {query_type.value} WHERE currency_code = %(target_currency)s;"
+        elif query_type == QueryType.HISTORICAL_PRICE:
+            logging.info("Fetching historical price...")
+            query = self.get_full_history_query()
+            logging.info("Fetched history query.")
+            params = {"target_currency": target_currency, "limit": limit}
         try:
+            logging.debug(f"Executing query...")
             df = pd.read_sql_query(query, self.engine, params=params)
-            logging.debug(f"✅ Successfully retrieved {len(df)} rows for {target_currency}")
+            logging.debug(f"Query executed successfully.")
+            logging.debug(f"Successfully retrieved {len(df)} rows for {target_currency}")
             return df
         except Exception as exception:
             return pd.DataFrame()
