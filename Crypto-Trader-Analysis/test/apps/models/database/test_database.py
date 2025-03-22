40e4e074ae4c293339026b0a160a53a82140743d
theoliverlear <sigwartholiver2@gmail.com>
2025-03-22 18:41:12 -0500

    Add test_database.py to test database functionalities in Crypto-Trader-Analysis

---
+++ Crypto-Trader-Analysis/test/apps/models/database/test_database.py
@@ -0,0 +1,33 @@
+import unittest
+
+from pandas import DataFrame
+
+from apps.models.database.database import Database
+from apps.models.database.query_type import QueryType
+
+
+def setup_database():
+    return Database()
+
+class TestDatabase(unittest.TestCase):
+    # def test_get_full_history_query(self):
+    #     db = setup_database()
+    #     query = db.get_full_history_query()
+    #     print(query)
+    #     self.assertTrue(True)
+    #
+    # def test_get_spaced_history_query(self):
+    #     db = setup_database()
+    #     query = db.get_spaced_history_query()
+    #     print(query)
+    #     self.assertEqual(True, True)
+
+
+    def test_fetch_data(self):
+        db = setup_database()
+        df= db.fetch_data("BTC",10000, QueryType.HISTORICAL_PRICE_SPACED)
+        print(df)
+        self.assertFalse(df.empty)
+
+if __name__ == '__main__':
+    unittest.main()
