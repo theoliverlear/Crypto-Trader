1fb7f176c7bf6b40e3939dc8c124df84f78be97c
theoliverlear <sigwartholiver2@gmail.com>
2025-03-22 10:39:01 -0500

    Add run_gpu_zero.py to train models on GPU 0 with specific currencies and configurations

---
+++ Crypto-Trader-Analysis/apps/models/run_gpu_zero.py
@@ -0,0 +1,21 @@
+# run_gpu_zero.py
+from apps.models.ai.model_type import ModelType
+from apps.models.train_model import get_split_currency_list, \
+    full_train_all_models, setup_tensorflow_env, setup_logging, \
+    train_all_models
+from apps.models.training.training_type import TrainingType
+from currency_json_generator import get_all_currency_codes
+
+
+def main():
+    setup_logging()
+    setup_tensorflow_env()
+    gpu_zero_currencies, gpu_one_currencies = get_split_currency_list()
+    while True:
+        train_all_models(training_type=TrainingType.LARGEST_DATA_DETAILED_TRAINING,
+                     currency_codes=gpu_zero_currencies,
+                     gpu_id=0,
+                     model_type=ModelType.SIMPLE)
+
+if __name__ == "__main__":
+    main()
