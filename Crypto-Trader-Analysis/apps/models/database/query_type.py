c0b769e03ec4791fbad3a29ed87e2f714114d722
theoliverlear <sigwartholiver2@gmail.com>
2025-03-22 18:57:08 -0500

    Add spaced historical query type in query_type.py

--- Crypto-Trader-Analysis/apps/models/database/query_type.py
+++ Crypto-Trader-Analysis/apps/models/database/query_type.py
@@ -4,3 +4,4 @@ from enum import Enum
 class QueryType(Enum):
     CURRENT_PRICE: str = 'currencies'
     HISTORICAL_PRICE: str = 'currency_history'
+    HISTORICAL_PRICE_SPACED: str = 'currency_history_spaced'
