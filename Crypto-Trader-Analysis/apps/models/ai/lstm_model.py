c6d0ab3fbe0c098951af44a3a2316e3f456eaffc
theoliverlear <sigwartholiver2@gmail.com>
2025-03-22 10:33:57 -0500

    Refactor lstm_model.py to inherit attributes from base class and modify LSTM instantiation to be GPU-compatible

--- Crypto-Trader-Analysis/apps/models/ai/lstm_model.py
+++ Crypto-Trader-Analysis/apps/models/ai/lstm_model.py
@@ -1,32 +1,25 @@
 # lstm_model.py
 from abc import ABC
-from typing import override
-from attr import attr
+from typing_extensions import override
 from attrs import define
 from keras import Sequential, Input
-from keras.src.layers import LSTM, Dropout, Dense
-from keras.src.saving import load_model
-from sklearn.preprocessing import MinMaxScaler
+from keras.layers import LSTM, Dropout, Dense
+from keras.saving.save import load_model
 from apps.models.ai.base_model import BaseModel
 
 @define
 class LstmModel(BaseModel, ABC):
-    dimension = attr(default=None)
-    model: Sequential = attr(factory=Sequential)
-    scaler: MinMaxScaler = attr(factory=MinMaxScaler)  
-    sequence_length: int = attr(default=10)
-    target_currency: str = attr(default='BTC')
-
     def __attrs_post_init__(self):
         self.model = Sequential([
             Input(shape=(self.sequence_length, self.dimension)),
-            LSTM(150, return_sequences=True),
+            LSTM(150, return_sequences=True, recurrent_activation="sigmoid", use_bias=True, unroll=True),
             Dropout(0.2),
-            LSTM(100),
+            LSTM(100, recurrent_activation="sigmoid", use_bias=True, unroll=True),
             Dense(50, activation="relu"),
             Dense(1)  
         ])
         self.model.compile(optimizer="adam", loss="mean_squared_error")
+        self.log_model_summary()
 
     @override
     def train(self,
@@ -55,3 +48,9 @@ class LstmModel(BaseModel, ABC):
     def load_model(self,
                    path: str):
         self.model = load_model(path)
+
+    @staticmethod
+    @override
+    def get_model_path(target_currency: str) -> str:
+        from apps.models.model_retriever import LSTM_MODEL_DIRECTORY
+        return LSTM_MODEL_DIRECTORY + target_currency + '_model.keras'
