18a9df1ead5828516d960666260a0bfe5d1f986d
theoliverlear <sigwartholiver2@gmail.com>
2025-03-23 23:18:49 -0500

    Add HttpConfig.java to configure the HTTP-related settings for the back-end

---
+++ src/main/java/org/theoliverlear/config/HttpConfig.java
@@ -0,0 +1,22 @@
+package org.theoliverlear.config;
+
+import org.apache.catalina.connector.Connector;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HttpConfig {
+
+    @Bean
+    public WebServerFactoryCustomizer<TomcatServletWebServerFactory> servletContainer() {
+        return factory -> {
+            Connector httpConnector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+            httpConnector.setScheme("http");
+            httpConnector.setPort(8080);
+            httpConnector.setSecure(false);
+            factory.addAdditionalTomcatConnectors(httpConnector);
+        };
+    }
+}
