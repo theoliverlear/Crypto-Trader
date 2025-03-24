4d10097a17704c70fe5b3e26a516e3b0a0d4d1dc
theoliverlear <sigwartholiver2@gmail.com>
2025-03-23 22:54:02 -0500

    Add prediction.py to represent cryptocurrency price predictions that are sent to the server

---
+++ Crypto-Trader-Analysis/apps/models/prediction/prediction.py
@@ -0,0 +1,30 @@
+# prediction.py
+from datetime import datetime
+
+from attr import attr
+from attrs import define
+
+
+@define
+class Prediction:
+    currency_code: str = attr(default='BTC')
+    predicted_price: float = attr(default=0.0)
+    actual_price: float = attr(default=0.0)
+    price_difference: float = attr(default=0.0)
+    percent_difference: float = attr(default=0.0)
+    model_type: str = attr(default="lstm")
+    num_rows: int = attr(default=10)
+    last_updated: datetime = attr(default=datetime.now())
+
+    def to_json(self) -> dict:
+        return {
+            "currencyCode": self.currency_code,
+            "predictedPrice": float(self.predicted_price),
+            "actualPrice": float(self.actual_price),
+            "priceDifference": float(self.price_difference),
+            "percentDifference": float(self.percent_difference),
+            "lastUpdated": self.last_updated.utcnow().strftime("%Y-%m-%dT%H:%M:%S"),
+            "numRows": int(self.num_rows),
+            "modelType": self.model_type
+        }
+
