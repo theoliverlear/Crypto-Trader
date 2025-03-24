9aa627da67f9e34677056f62b0e82083aca10b0c
theoliverlear <sigwartholiver2@gmail.com>
2025-03-23 23:24:54 -0500

    Refactor PricePrediction.java to extend Identifiable and include additional fields and builder method

--- src/main/java/org/theoliverlear/entity/prediction/PricePrediction.java
+++ src/main/java/org/theoliverlear/entity/prediction/PricePrediction.java
@@ -1,15 +1,14 @@
 package org.theoliverlear.entity.prediction;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import org.theoliverlear.entity.Identifiable;
+import org.theoliverlear.entity.prediction.builder.PricePredictionBuilder;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "predictions")
-public class PricePrediction {
-    @Id
-    private Long id;
+public class PricePrediction extends Identifiable {
     @Column(name = "currency_code")
     private String currencyCode;
     @Column(name = "currency_name")
@@ -22,26 +21,44 @@ public class PricePrediction {
     private double priceDifference;
     @Column(name = "percent_difference")
     private double percentDifference;
+    @Column(name = "model_type")
+    @Enumerated(EnumType.STRING)
+    private ModelType modelType;
+    @Column(name = "num_rows")
+    private int numRows;
     @Column(name = "last_updated")
-    private String lastUpdated;
-    @Column(name = "step_size")
-    private int stepSize;
+    private LocalDateTime lastUpdated;
+    public PricePrediction() {
+        this.currencyCode = "";
+        this.currencyName = "";
+        this.predictedPrice = 0.0;
+        this.actualPrice = 0.0;
+        this.priceDifference = 0.0;
+        this.percentDifference = 0.0;
+        this.modelType = ModelType.LSTM;
+        this.numRows = 0;
+        this.lastUpdated = LocalDateTime.now();
+    }
     public PricePrediction(String currencyCode,
                            String currencyName,
                            double predictedPrice,
                            double actualPrice,
                            double priceDifference,
                            double percentDifference,
-                           String lastUpdated,
-                           int stepSize) {
+                           ModelType modelType,
+                           int numRows,
+                           LocalDateTime lastUpdated) {
         this.currencyCode = currencyCode;
         this.currencyName = currencyName;
         this.predictedPrice = predictedPrice;
         this.actualPrice = actualPrice;
         this.priceDifference = priceDifference;
         this.percentDifference = percentDifference;
+        this.modelType = modelType;
+        this.numRows = numRows;
         this.lastUpdated = lastUpdated;
-        this.stepSize = stepSize;
-
+    }
+    public static PricePredictionBuilder builder() {
+        return new PricePredictionBuilder();
     }
 }
