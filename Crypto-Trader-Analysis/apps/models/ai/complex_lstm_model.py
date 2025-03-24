65afb301bb72069a07673a8e90722986fcb6d04c
theoliverlear <sigwartholiver2@gmail.com>
2025-03-23 22:34:11 -0500

    Add callbacks for training optimization and tensorboard logging in complex_lstm_model.py

--- Crypto-Trader-Analysis/apps/models/ai/complex_lstm_model.py
+++ Crypto-Trader-Analysis/apps/models/ai/complex_lstm_model.py
@@ -1,15 +1,18 @@
-# lstm_model.py
+# complex_lstm_model.py
 import logging
 from abc import ABC
+from datetime import datetime
+
 from typing_extensions import override
-from attr import attr
 from attrs import define
 from keras import Sequential, Input
 from keras.layers import Bidirectional, LSTM, Dropout, BatchNormalization, \
     Dense
 from keras.saving.save import load_model
-from sklearn.preprocessing import MinMaxScaler
 from apps.models.ai.base_model import BaseModel
+import os
+from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
+
 
 
 
@@ -53,16 +56,43 @@ class ComplexLstmModel(BaseModel, ABC):
         self.log_model_summary()
 
     @override
+    @staticmethod
+    def get_tensorboard_callback(target_currency: str):
+        import tensorflow as tf
+        timestamp = datetime.now().strftime("%Y%m%d-%H%M%S")
+        log_dir = os.path.join("logs", "COMPLEX_LSTM_MODEL", target_currency, timestamp)
+
+        return tf.keras.callbacks.TensorBoard(
+            log_dir=log_dir,
+            profile_batch=10,
+            histogram_freq=1
+        )
+
+    @override
     def train(self,
-              training_data,
-              target_values,
+              dataset,
               epochs: int = 20,
               batch_size: int = 32):
-        self.model.fit(training_data,
-                       target_values,
+        from apps.models.model_retriever import COMPLEX_MODEL_DIRECTORY
+        checkpoint_dir = os.path.join(COMPLEX_MODEL_DIRECTORY, "checkpoints")
+        os.makedirs(checkpoint_dir, exist_ok=True)
+        checkpoint_path = os.path.join(checkpoint_dir,
+                                       f"{self.target_currency}_checkpoint.keras")
+
+        callbacks = [
+            EarlyStopping(monitor="loss", patience=5,
+                          restore_best_weights=True),
+            ModelCheckpoint(filepath=checkpoint_path, monitor="loss",
+                            save_best_only=True),
+            ReduceLROnPlateau(monitor='loss', factor=0.5, patience=3,
+                              min_lr=1e-6),
+            ComplexLstmModel.get_tensorboard_callback(self.target_currency)
+        ]
+        self.model.fit(dataset,
                        epochs=epochs,
                        batch_size=batch_size,
-                       verbose=1)
+                       verbose=1,
+                       callbacks=callbacks)
 
     @override
     def predict(self, training_data, target_scaler):
