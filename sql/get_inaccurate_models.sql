01808c55d61d196b9104a8427c710e528ef3dec5
theoliverlear <sigwartholiver2@gmail.com>
2025-03-25 09:41:56 -0500

    Update get_inaccurate_models.sql to include average percent difference and order results by highest difference

--- sql/get_inaccurate_models.sql
+++ sql/get_inaccurate_models.sql
@@ -1,1 +1,11 @@
-SELECT DISTINCT currency_code FROM predictions WHERE percent_difference > 5;
+SELECT
+    currency_code,
+    AVG(percent_difference) AS avg_difference
+FROM
+    predictions
+WHERE
+    percent_difference > 5
+GROUP BY
+    currency_code
+ORDER BY
+    avg_difference DESC;
