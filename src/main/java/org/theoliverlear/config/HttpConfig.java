0409fcf65b751a8d1a2d299ea599bd27dc3830b0
theoliverlear <sigwartholiver2@gmail.com>
2025-05-16 11:29:19 -0500

    Comment out servlet container bean to disable HTTP connector configuration in HttpConfig.java

--- src/main/java/org/theoliverlear/config/HttpConfig.java
+++ src/main/java/org/theoliverlear/config/HttpConfig.java
@@ -9,14 +9,14 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class HttpConfig {
 
-    @Bean
-    public WebServerFactoryCustomizer<TomcatServletWebServerFactory> servletContainer() {
-        return factory -> {
-            Connector httpConnector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
-            httpConnector.setScheme("http");
-            httpConnector.setPort(8080);
-            httpConnector.setSecure(false);
-            factory.addAdditionalTomcatConnectors(httpConnector);
-        };
-    }
+//    @Bean
+//    public WebServerFactoryCustomizer<TomcatServletWebServerFactory> servletContainer() {
+//        return factory -> {
+//            Connector httpConnector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+//            httpConnector.setScheme("http");
+//            httpConnector.setPort(8080);
+//            httpConnector.setSecure(false);
+//            factory.addAdditionalTomcatConnectors(httpConnector);
+//        };
+//    }
 }
