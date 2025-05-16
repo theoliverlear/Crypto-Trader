2cc8b0d826da3475462dab225994509c0efcc1d7
theoliverlear <sigwartholiver2@gmail.com>
2025-05-16 11:28:29 -0500

    Add multiple indexes to optimize queries in create_currency_history_indexes.sql

--- sql/create_currency_history_indexes.sql
+++ sql/create_currency_history_indexes.sql
@@ -1,2 +1,21 @@
 CREATE INDEX idx_currency_history_code_date_value
     ON currency_history (currency_code, last_updated, currency_value);
+
+CREATE INDEX idx_currency_history_code_updated
+    ON currency_history (currency_code, last_updated DESC);
+
+CREATE INDEX idx_currency_history_updated
+    ON currency_history(last_updated DESC);
+
+CREATE INDEX ON currency_history (currency_code, last_updated DESC);
+
+CREATE INDEX idx_code_updated_value ON currency_history (currency_code, last_updated DESC, currency_value);
+
+CREATE INDEX idx_currency_history_code_updated_value
+    ON currency_history (currency_code, last_updated DESC, currency_value);
+
+CREATE INDEX idx_currency_history_code_updated
+    ON currency_history (currency_code, last_updated DESC);
+
+CREATE INDEX idx_currency_history_updated
+    ON currency_history (last_updated DESC);
