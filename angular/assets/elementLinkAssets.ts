6420903ec93631578259ed6d0545e1f39d661da2
theoliverlear <sigwartholiver2@gmail.com>
2025-05-16 11:13:54 -0500

    Change TagType from H5 to H4 for navigation bar links in elementLinkAssets.ts

--- angular/assets/elementLinkAssets.ts
+++ angular/assets/elementLinkAssets.ts
@@ -7,19 +7,19 @@ export const navBarHomeLink = new TextElementLink('',
     TargetType.SELF,
     false,
     'Home',
-    TagType.H5
+    TagType.H4
 );
 export const navBarPortfolioLink = new TextElementLink('/portfolio',
     TargetType.SELF,
     false,
     'Portfolio',
-    TagType.H5
+    TagType.H4
 );
 export const navBarTraderLink = new TextElementLink('/trader',
     TargetType.SELF,
     false,
     'Trader',
-    TagType.H5
+    TagType.H4
 );
 export const navBarAccountLink = new ElementLink('/account',
     TargetType.SELF,
