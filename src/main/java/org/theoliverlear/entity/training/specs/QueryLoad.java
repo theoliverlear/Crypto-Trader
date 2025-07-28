0e60f50bc673fb7c28d06724834bf7f52d94d3c8
theoliverlear <sigwartholiver2@gmail.com>
2025-07-27 20:03:07 -0500

    Add QueryLoad.java to define an enum for query load types

---
+++ src/main/java/org/theoliverlear/entity/training/specs/QueryLoad.java
@@ -0,0 +1,19 @@
+package org.theoliverlear.entity.training.specs;
+
+public enum QueryLoad {
+    BATCHES("batches"),
+    BULK("bulk");
+    public final String load;
+
+    QueryLoad(String load) {
+        this.load = load;
+    }
+
+    public static QueryLoad from(String load) {
+        return switch (load) {
+            case "batches" -> QueryLoad.BATCHES;
+            case "bulk" -> QueryLoad.BULK;
+            default -> throw new IllegalArgumentException("Unknown query load: " + load);
+        };
+    }
+}
