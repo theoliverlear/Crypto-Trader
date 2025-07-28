45464b6342e24273a460e51aa248e825cde81072
theoliverlear <sigwartholiver2@gmail.com>
2025-07-28 14:00:21 -0500

    Refactor multi_layer_lstm_model.py to use localized imports to prevent partially initialized errors

--- Crypto-Trader-Analysis/apps/models/ai/lstm/layered/multi_layer_lstm_model.py
+++ Crypto-Trader-Analysis/apps/models/ai/lstm/layered/multi_layer_lstm_model.py
@@ -15,7 +15,6 @@ from apps.models.ai.lstm.layered.multi_layer_base_model import MultiLayerBaseMod
 import os
 from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
 
-from apps.models.ai.model_retriever import model_exists, delete_model
 
 
 @define
@@ -102,6 +101,9 @@ class MultiLayerLstmModel(MultiLayerBaseModel):
                                callbacks=callbacks)
             except Exception as exception:
                 from apps.models.ai.model_type import ModelType
+                from apps.models.ai.model_retriever import model_exists, \
+                    delete_model
+
                 if "Input 0 of layer" in str(exception):
                     logging.info("Model dimension mismatch. Re-training model.")
                     multi_model_exists: bool = model_exists(self.target_currency,
