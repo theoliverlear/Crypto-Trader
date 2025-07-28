3d9748dd1e55e2606597ca14fd200361549f7da1
theoliverlear <sigwartholiver2@gmail.com>
2025-07-27 19:56:07 -0500

    Refactor query_type.py to update enum values with appropriate name mapping

--- Crypto-Trader-Analysis/apps/models/database/query_type.py
+++ Crypto-Trader-Analysis/apps/models/database/query_type.py
@@ -2,6 +2,6 @@ from enum import Enum
 
 
 class QueryType(Enum):
-    CURRENT_PRICE: str = 'currencies'
-    HISTORICAL_PRICE: str = 'currency_history'
-    HISTORICAL_PRICE_SPACED: str = 'currency_history_spaced'
+    CURRENT_PRICE = 'current_price'
+    HISTORICAL_PRICE = 'historical_price'
+    HISTORICAL_PRICE_SPACED = 'historical_price_spaced'
