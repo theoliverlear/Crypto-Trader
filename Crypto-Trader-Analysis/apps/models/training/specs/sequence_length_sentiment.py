412e2f3b966ef47b35d1fc8f0f7a13739dc094d9
theoliverlear <sigwartholiver2@gmail.com>
2025-07-28 14:04:53 -0500

    Refactor sequence_length_sentiment.py to add dynamic sequence calculation method and remove type annotations in enum constants

--- Crypto-Trader-Analysis/apps/models/training/specs/sequence_length_sentiment.py
+++ Crypto-Trader-Analysis/apps/models/training/specs/sequence_length_sentiment.py
@@ -2,12 +2,25 @@ from enum import Enum
 
 
 class SequenceLengthSentiment(Enum):
-    MICRO_TRENDS: int = 10
-    MINI_TRENDS: int = 20
-    SIGNAL_TRENDS: int = 30
-    CYCLIC_EVENTS: int = 50
-    LONG_TRENDS: int = 75
-    SMALL_SENTIMENT: int = 125
-    STANDARD_SENTIMENT: int = 150
-    LARGE_SENTIMENT: int = 200
-    SEASONALITY: int = 300
+    MICRO_TRENDS = 10
+    MINI_TRENDS = 20
+    SIGNAL_TRENDS = 30
+    CYCLIC_EVENTS = 50
+    LONG_TRENDS = 75
+    SMALL_SENTIMENT = 125
+    STANDARD_SENTIMENT = 150
+    LARGE_SENTIMENT = 200
+    SEASONALITY = 300
+
+    @staticmethod
+    def get_by_time(days: int = 0,
+                    hours: int = 0,
+                    minutes: int = 0,
+                    seconds: int = 0):
+        sequences_in_days: int = days * 17280
+        sequences_in_hours: int = hours * 720
+        sequences_in_minutes: int = minutes * 12
+        sequences_in_seconds: int = seconds // 5
+        total_sequences: int = (sequences_in_days + sequences_in_hours +
+                                sequences_in_minutes + sequences_in_seconds)
+        return total_sequences
