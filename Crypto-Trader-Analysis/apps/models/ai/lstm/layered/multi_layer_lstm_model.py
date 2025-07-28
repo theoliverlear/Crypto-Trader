887ba1a84941e45dda093a33114b95841f2c7e9d
theoliverlear <sigwartholiver2@gmail.com>
2025-07-27 19:45:53 -0500

    Refactor multi_layer_lstm_model.py to relocate into apps.models.ai.lstm.layered package and modularize repeated LSTM behavior with `combine_model()` method

--- Crypto-Trader-Analysis/apps/models/ai/lstm/layered/multi_layer_lstm_model.py
+++ Crypto-Trader-Analysis/apps/models/ai/lstm/layered/multi_layer_lstm_model.py
@@ -1,6 +1,5 @@
 # multi_layer_lstm_model.py
 import logging
-from abc import ABC
 from datetime import datetime
 
 from typing_extensions import override
@@ -12,9 +11,11 @@ from keras.layers import LSTM, Dropout, Dense, Concatenate
 from keras.saving.save import load_model
 from sklearn.preprocessing import MinMaxScaler
 
-from apps.models.ai.multi_layer_base_model import MultiLayerBaseModel
+from apps.models.ai.lstm.layered.multi_layer_base_model import MultiLayerBaseModel
 import os
 from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
+
+from apps.models.ai.model_retriever import model_exists, delete_model
 
 
 @define
@@ -26,34 +27,16 @@ class MultiLayerLstmModel(MultiLayerBaseModel):
         if self.dimension is None:
             raise ValueError(
                 "Please specify 'dimension' for MultiLayerLstmModel.")
-        # TODO: Turn repeated behavior into a function.
         short_input = Input(shape=(self.short_seq_len, self.dimension),
                             name="short_input")
-        short_lstm = LSTM(150, return_sequences=True,
-                          recurrent_activation="sigmoid", use_bias=True,
-                          unroll=True)(short_input)
-        short_lstm = Dropout(0.2)(short_lstm)
-        short_lstm = LSTM(100, recurrent_activation="sigmoid", use_bias=True,
-                          unroll=True)(short_lstm)
-        short_lstm = Dense(50, activation="relu")(short_lstm)
         medium_input = Input(shape=(self.medium_seq_len, self.dimension),
                              name="medium_input")
-        medium_lstm = LSTM(150, return_sequences=True,
-                           recurrent_activation="sigmoid", use_bias=True,
-                           unroll=True)(medium_input)
-        medium_lstm = Dropout(0.2)(medium_lstm)
-        medium_lstm = LSTM(100, recurrent_activation="sigmoid", use_bias=True,
-                           unroll=True)(medium_lstm)
-        medium_lstm = Dense(50, activation="relu")(medium_lstm)
         long_input = Input(shape=(self.long_seq_len, self.dimension),
                            name="long_input")
-        long_lstm = LSTM(150, return_sequences=True,
-                         recurrent_activation="sigmoid", use_bias=True,
-                         unroll=True)(long_input)
-        long_lstm = Dropout(0.2)(long_lstm)
-        long_lstm = LSTM(100, recurrent_activation="sigmoid", use_bias=True,
-                         unroll=True)(long_lstm)
-        long_lstm = Dense(50, activation="relu")(long_lstm)
+        short_lstm = self.combine_model(short_input)
+        medium_lstm = self.combine_model(medium_input)
+        long_lstm = self.combine_model(long_input)
+
         merged = Concatenate()([short_lstm, medium_lstm, long_lstm])
         merged = Dense(50, activation="relu")(merged)
         final_model = Dense(1)(merged)
@@ -64,6 +47,20 @@ class MultiLayerLstmModel(MultiLayerBaseModel):
         )
         self.model.compile(optimizer="adam", loss="mean_squared_error")
         self.log_model_summary()
+
+
+    @staticmethod
+    @override
+    def combine_model(model_input):
+        lstm = LSTM(150, return_sequences=True,
+                    recurrent_activation="sigmoid", use_bias=True,
+                    unroll=True)(model_input)
+        lstm = Dropout(0.2)(lstm)
+        lstm = LSTM(100, recurrent_activation="sigmoid", use_bias=True,
+                    unroll=True)(lstm)
+        lstm = Dense(50, activation="relu")(lstm)
+        return lstm
+
 
     @override
     @staticmethod
@@ -96,34 +93,23 @@ class MultiLayerLstmModel(MultiLayerBaseModel):
                               min_lr=1e-6),
             MultiLayerLstmModel.get_tensorboard_callback(self.target_currency)
         ]
-        # self.model.fit(
-        #     dataset,
-        #     epochs=epochs,
-        #     batch_size=batch_size,
-        #     verbose=1,
-        #     callbacks=callbacks
-        # )
         while True:
             try:
-                self.model.fit(dataset,
+                return self.model.fit(dataset,
                                epochs=epochs,
                                batch_size=batch_size,
                                verbose=1,
                                callbacks=callbacks)
-                break
             except Exception as exception:
                 from apps.models.ai.model_type import ModelType
-                from apps.models.prediction.predictions import model_exists, \
-                    delete_model
                 if "Input 0 of layer" in str(exception):
                     logging.info("Model dimension mismatch. Re-training model.")
-                    model_exists: bool = model_exists(self.target_currency,
+                    multi_model_exists: bool = model_exists(self.target_currency,
                                                       ModelType.LSTM)
-                    if model_exists:
+                    if multi_model_exists:
                         delete_model(self.target_currency,
                                      ModelType.LSTM)
                     self.initialize_model()
-
                 else:
                     raise
 
