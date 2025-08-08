22a963c82bb6909cea3353fae90efbeea741d2d3
theoliverlear <sigwartholiver2@gmail.com>
2025-08-08 18:22:54 -0500

    Add SpringContext.java to manage application context and provide static bean retrieval

---
+++ src/main/java/org/theoliverlear/component/SpringContext.java
@@ -0,0 +1,18 @@
+package org.theoliverlear.component;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SpringContext implements ApplicationContextAware {
+    private static ApplicationContext context;
+    
+    @Override public void setApplicationContext(ApplicationContext context) {
+        SpringContext.context = context;
+    }
+    
+    public static <T> T getBean(Class<T> type) {
+        return context.getBean(type);
+    }
+}
