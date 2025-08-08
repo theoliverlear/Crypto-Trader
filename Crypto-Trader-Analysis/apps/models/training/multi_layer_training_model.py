dacccbcbccf0d2f131b5cb4999450c2d20b77091
theoliverlear <sigwartholiver2@gmail.com>
2025-08-08 16:23:53 -0500

    Add multi_layer_training_model.py to define `MultiLayerTrainingModel` with various sequence length attributes

---
+++ Crypto-Trader-Analysis/apps/models/training/multi_layer_training_model.py
@@ -0,0 +1,23 @@
+from typing import TYPE_CHECKING
+
+from attr import attr
+from attrs import define
+
+if TYPE_CHECKING:
+    from apps.models.training.multi_layer_training_model_builder import \
+        MultiLayerTrainingModelBuilder
+from apps.models.training.training_model import TrainingModel
+
+
+@define
+class MultiLayerTrainingModel(TrainingModel):
+    short_seq_length: int = attr(default=10)
+    medium_seq_length: int = attr(default=50)
+    long_seq_length: int = attr(default=150)
+
+    
+    @staticmethod
+    def builder() -> 'MultiLayerTrainingModelBuilder':
+        from apps.models.training.multi_layer_training_model_builder import MultiLayerTrainingModelBuilder
+        return MultiLayerTrainingModelBuilder()
+    
