d88498da79540483d234f168258de12f69ef51d4
theoliverlear <sigwartholiver2@gmail.com>
2025-08-08 18:18:21 -0500

    Refactor run_gpu_zero.py and run_gpu_one.py to replace `train_all_models` with the `TrainingSession` class and integrate improved `TrainingModel` builder configuration

--- Crypto-Trader-Analysis/gpu/run_gpu_one.py
+++ Crypto-Trader-Analysis/gpu/run_gpu_one.py
@@ -1,24 +1,44 @@
 # run_gpu_one.py
 # AMD RX 6900 XT
 from apps.models.ai.model_type import ModelType
+from apps.models.database.query_type import QueryType
+from apps.models.training.specs.batch_size_evaluations import \
+    BatchSizeEvaluations
+from apps.models.training.specs.dataset_size import DatasetSize
+from apps.models.training.specs.epoch_focus import EpochFocus
+from apps.models.training.specs.sequence_length_sentiment import \
+    SequenceLengthSentiment
 from apps.models.training.train_model import get_split_currency_list, \
     setup_tensorflow_env, setup_logging, \
-    train_all_models
+    train_all_models, train_new_models
+from apps.models.training.training_model import TrainingModel
+from apps.models.training.training_session import TrainingSession
 from apps.models.training.training_type import TrainingType
+from currency_json_generator import get_all_currency_codes
 
 
 def main():
     setup_logging()
     setup_tensorflow_env()
     gpu_zero_currencies, gpu_one_currencies = get_split_currency_list()
+    currencies: list[str] = get_all_currency_codes(True)
+    important_currencies: list[str] = ["BTC", "ETH", "LTC", "XLM", "DOGE", "SOL", "ADA"]
+    currencies.reverse()
 
-    while True:
-        train_all_models(
-            training_type=TrainingType.MODERATE_DATA_DETAILED_TRAINING,
-            currency_codes=gpu_one_currencies,
-            gpu_id=1,
-            model_type=ModelType.LSTM,
-        )
+    training_model: TrainingModel = TrainingModel().builder() \
+                                                   .max_rows(DatasetSize.MASSIVE) \
+                                                   .epochs(EpochFocus.COMPLETE_ANALYSIS) \
+                                                   .batch_size(BatchSizeEvaluations.GENERALIZED) \
+                                                   .sequence_length(SequenceLengthSentiment.get_by_time(days=1)) \
+                                                   .query_type(QueryType.HISTORICAL_PRICE) \
+                                                   .build()
+    training_session: TrainingSession = TrainingSession(
+        gpu_id=1,
+        training_model=training_model,
+        target_currency='BTC',
+        model_type=ModelType.LSTM
+    )
+    training_session.train()
 
 
 
--- Crypto-Trader-Analysis/gpu/run_gpu_zero.py
+++ Crypto-Trader-Analysis/gpu/run_gpu_zero.py
@@ -1,24 +1,41 @@
 # run_gpu_zero.py
 # AMD RX 6700 XT
 from apps.models.ai.model_type import ModelType
+from apps.models.database.query_type import QueryType
+from apps.models.training.specs.batch_size_evaluations import \
+    BatchSizeEvaluations
+from apps.models.training.specs.dataset_size import DatasetSize
+from apps.models.training.specs.epoch_focus import EpochFocus
 from apps.models.training.train_model import get_split_currency_list, \
     setup_tensorflow_env, setup_logging, \
-    train_all_models
+    train_all_models, train_new_models, train_multi_layer_model, train_model
+from apps.models.training.training_model import TrainingModel
 from apps.models.training.training_type import TrainingType
+from currency_json_generator import get_all_currency_codes
 
 
 def main():
     setup_logging()
     setup_tensorflow_env()
     gpu_zero_currencies, gpu_one_currencies = get_split_currency_list()
-
-    while True:
-        train_all_models(
-            training_type=TrainingType.MODERATE_DATA_DETAILED_TRAINING,
-            currency_codes=gpu_zero_currencies,
-            gpu_id=0,
-            model_type=ModelType.COMPLEX_LSTM,
-        )
+    currencies = get_all_currency_codes(True)
+    from apps.models.training.training_session import TrainingSession
+    from apps.models.training.specs.dataset_size import DatasetSize
+    from apps.models.training.specs.sequence_length_sentiment import \
+        SequenceLengthSentiment
+    training_model: TrainingModel = TrainingModel().builder() \
+                                                   .max_rows(DatasetSize.MICRO) \
+                                                   .epochs(EpochFocus.SMALL_ANALYSIS) \
+                                                   .batch_size(BatchSizeEvaluations.BALANCED) \
+                                                   .sequence_length(SequenceLengthSentiment.SEASONALITY) \
+                                                   .query_type(QueryType.HISTORICAL_PRICE) \
+                                                   .build()
+    training_session: TrainingSession = TrainingSession(gpu_id=0,
+                                                        training_model=training_model,
+                                                        target_currency='BTC',
+                                                        model_type=ModelType.LSTM)
+    training_session.train()
+    
 
 if __name__ == "__main__":
     main()
