a2250765e6144d03144f2542079a4a93003511b4
theoliverlear <sigwartholiver2@gmail.com>
2025-08-09 17:10:32 -0500

    Add sentiment_analyzer.py to provide sentiment analysis of news articles

---
+++ Crypto-Trader-Analysis/apps/news/analysis/sentiment/sentiment_analyzer.py
@@ -0,0 +1,12 @@
+from attr import attr
+from attrs import define
+
+from apps.news.analysis.sentiment.sentiment_model import SentimentModel
+
+
+@define
+class SentimentAnalyzer:
+    model: SentimentModel = attr(default=SentimentModel())
+
+    def get_sentiment(self, text: str):
+        return self.model.score_text(text)
