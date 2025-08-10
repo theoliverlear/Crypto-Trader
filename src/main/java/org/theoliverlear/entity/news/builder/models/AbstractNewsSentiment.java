3cb72817ec635ea2da729ac5e0a695ed141210ec
theoliverlear <sigwartholiver2@gmail.com>
2025-08-10 10:36:58 -0500

    Add AbstractNewsSentiment.java to define a builder factory for constructing `NewsSentiment` entities

---
+++ src/main/java/org/theoliverlear/entity/news/builder/models/AbstractNewsSentiment.java
@@ -0,0 +1,20 @@
+package org.theoliverlear.entity.news.builder.models;
+
+import org.theoliverlear.entity.news.NewsSentiment;
+import org.theoliverlear.model.BuilderFactory;
+
+import java.time.LocalDateTime;
+
+public abstract class AbstractNewsSentiment implements BuilderFactory<NewsSentiment> {
+    public abstract AbstractNewsSentiment articleId(Long articleId);
+    public abstract AbstractNewsSentiment title(String title);
+    public abstract AbstractNewsSentiment publishedDate(String publishedDate);
+    public abstract AbstractNewsSentiment publishedDate(LocalDateTime publishedDate);
+    public abstract AbstractNewsSentiment source(String source);
+    public abstract AbstractNewsSentiment url(String url);
+    public abstract AbstractNewsSentiment positiveScore(double positiveScore);
+    public abstract AbstractNewsSentiment neutralScore(double neutralScore);
+    public abstract AbstractNewsSentiment negativeScore(double negativeScore);
+    public abstract AbstractNewsSentiment compositeScore(double compositeScore);
+    public abstract AbstractNewsSentiment cryptoRelevance(double cryptoRelevance);
+}
