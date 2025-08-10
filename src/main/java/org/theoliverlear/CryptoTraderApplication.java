baad6c43b350483b6634ae7c6156c895a8e48ba7
theoliverlear <sigwartholiver2@gmail.com>
2025-08-10 10:35:58 -0500

    Add currency JSON trigger to CryptoTraderApplication.java to initialize currency data before use

--- src/main/java/org/theoliverlear/CryptoTraderApplication.java
+++ src/main/java/org/theoliverlear/CryptoTraderApplication.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.theoliverlear.component.CurrencyJsonGenerator;
 
 @SpringBootApplication
 @EnableAsync
 @EnableScheduling
 public class CryptoTraderApplication {
     public static void main(String[] args) {
+        CurrencyJsonGenerator.standalone().generateAndSave();
         SpringApplication.run(CryptoTraderApplication.class, args);
     }
 }
