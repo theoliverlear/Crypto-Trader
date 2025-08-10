0cf06f077b3301c99d1fa8bf1c1fb08997474e8f
theoliverlear <sigwartholiver2@gmail.com>
2025-08-10 10:37:53 -0500

    Add columns, constructors, and builder method to NewsSentiment.java

--- src/main/java/org/theoliverlear/entity/news/NewsSentiment.java
+++ src/main/java/org/theoliverlear/entity/news/NewsSentiment.java
@@ -1,26 +1,85 @@
 package org.theoliverlear.entity.news;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 import org.theoliverlear.entity.Identifiable;
+import org.theoliverlear.entity.news.builder.NewsSentimentBuilder;
 
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "news_sentiments")
 @Getter
 @Setter
+@Table(name = "news_sentiments")
 public class NewsSentiment extends Identifiable {
-    Long articleId;
-    String title;
-    LocalDateTime publishedDate;
-    String source;
-    String keywords;
-    String url;
-    double positiveScore;
-    double neutralScore;
-    double negativeScore;
-    double compositeScore;
+    @Column(name = "article_id", unique = true)
+    private Long articleId;
+    @Column(name = "article_title", unique = true)
+    private String title;
+    @Column(name = "published_date")
+    private LocalDateTime publishedDate;
+    @Column(name = "source")
+    private String source;
+    @Column(name = "url")
+    private String url;
+    @Column(name = "positive_score")
+    private double positiveScore;
+    @Column(name = "neutral_score")
+    private double neutralScore;
+    @Column(name = "negative_score")
+    private double negativeScore;
+    @Column(name = "composite_score")
+    private double compositeScore;
+    @Column(name = "crypto_relevance")
+    private double cryptoRelevance;
+    @Column(name = "weighted_score")
+    private double weightedScore;
+    
+    public NewsSentiment() {
+        super();
+        this.positiveScore = 0;
+        this.neutralScore = 0;
+        this.negativeScore = 0;
+        this.compositeScore = 0;
+        this.cryptoRelevance = 0;
+        this.weightedScore = 0;
+    }
+    
+    public NewsSentiment(Long articleId,
+                         String title,
+                         LocalDateTime publishedDate,
+                         String source,
+                         String url,
+                         double positiveScore,
+                         double neutralScore,
+                         double negativeScore,
+                         double compositeScore,
+                         double cryptoRelevance) {
+        super();
+        this.articleId = articleId;
+        this.title = title;
+        this.publishedDate = publishedDate;
+        this.source = source;
+        this.url = url;
+        this.positiveScore = positiveScore;
+        this.neutralScore = neutralScore;
+        this.negativeScore = negativeScore;
+        this.compositeScore = compositeScore;
+        this.cryptoRelevance = cryptoRelevance;
+        this.weightedScore = calculateWeightedScore(compositeScore, cryptoRelevance);
+
+    }
+    
+    public static NewsSentimentBuilder builder() {
+        return new NewsSentimentBuilder();
+    }
+    
+    public static double calculateWeightedScore(double compositeScore, double cryptoRelevance) {
+        return compositeScore * cryptoRelevance;
+    }
 }
+
+
