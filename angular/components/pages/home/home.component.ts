c11b31c22a5c00fbf765c447b2564874e9af21f8
theoliverlear <sigwartholiver2@gmail.com>
2025-08-09 21:03:17 -0500

    Add `TagType` import and bind it in home.component.ts for HTML tag categorization

--- angular/components/pages/home/home.component.ts
+++ angular/components/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {TagType} from "../../../models/html/TagType";
 
 @Component({
   selector: 'home',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+    protected readonly TagType = TagType;
 }
