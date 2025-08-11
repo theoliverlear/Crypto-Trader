2beb998fc3160a3b1a5b54cd6ef4ed1522c80129
theoliverlear <sigwartholiver2@gmail.com>
2025-08-11 13:46:20 -0500

    Expand NewsSentimentHarvesterClient.java to support targeted harvest requests with date-range targeting

--- src/main/java/org/theoliverlear/component/NewsSentimentHarvesterClient.java
+++ src/main/java/org/theoliverlear/component/NewsSentimentHarvesterClient.java
@@ -9,16 +9,18 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.theoliverlear.comm.request.NewsSentimentHarvestRequest;
+import org.theoliverlear.comm.request.NewsSentimentTargetedHarvestRequest;
 
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 import java.net.URI;
+import java.time.LocalDate;
 
 @Component
 @Slf4j
 public class NewsSentimentHarvesterClient {
-    private static final String API_URL = "http://localhost:8000/api/news-sentiment/harvest";
-    private static final NewsSentimentHarvestRequest DEFAULT_REQUEST = new NewsSentimentHarvestRequest(50, 0, 1);
+    private static final String DAILY_API_URL = "http://localhost:8000/api/news/sentiment/harvest";
+    private static final String TARGETED_API_URL = "http://localhost:8000/api/news/sentiment/harvest/targeted/by-date";
+    private static final NewsSentimentHarvestRequest DEFAULT_REQUEST = new NewsSentimentHarvestRequest(100, 0, 1);
     private final HttpPost httpPost;
     private final ObjectMapper objectMapper;
     private final CloseableHttpClient httpClient;
@@ -28,16 +30,23 @@ public class NewsSentimentHarvesterClient {
                                         ObjectMapper objectMapper,
                                         CloseableHttpClient httpClient) {
         this.httpPost = httpPost;
-        this.httpPost.setURI(URI.create(API_URL));
+        this.initHeaders();
+        this.httpPost.setURI(URI.create(DAILY_API_URL));
         this.objectMapper = objectMapper;
         this.httpClient = httpClient;
     }
-    
+
+    private void initHeaders() {
+        this.httpPost.setHeader("Content-Type", "application/json");
+        this.httpPost.setHeader("Accept", "application/json");
+        this.httpPost.setHeader("Accept-Charset", "UTF-8");
+    }
+
     public void triggerHarvest() {
         this.triggerHarvest(DEFAULT_REQUEST);
     }
     
-    public String requestToJson(NewsSentimentHarvestRequest request) {
+    public <T> String requestToJson(T request) {
         try {
             return this.objectMapper.writeValueAsString(request);
         } catch (Exception ex) {
@@ -47,17 +56,40 @@ public class NewsSentimentHarvesterClient {
     }
     
     public void triggerHarvest(NewsSentimentHarvestRequest request) {
+        this.httpPost.setURI(URI.create(DAILY_API_URL));
         log.info("Sending harvest request...");
         String json = this.requestToJson(request);
         try {
             this.httpPost.setEntity(new StringEntity(json));
-            this.httpPost.setHeader("Content-Type", "application/json");
-            this.httpPost.setHeader("Accept", "application/json");
-            this.httpPost.setHeader("Accept-Charset", "UTF-8");
             CloseableHttpResponse response = this.httpClient.execute(this.httpPost);
             log.info("Harvest request sent. Response status: {}", response.getStatusLine().getStatusCode());
         } catch (IOException exception) {
             log.error("Failed to set request entity.", exception);
+        }
+    }
+    
+    public void triggerHarvest(NewsSentimentTargetedHarvestRequest request) {
+        this.httpPost.setURI(URI.create(TARGETED_API_URL));
+        log.info("Sending targeted harvest request...");
+        String json = this.requestToJson(request);
+        try {
+            this.httpPost.setEntity(new StringEntity(json));
+            CloseableHttpResponse response = this.httpClient.execute(this.httpPost);
+            log.info("Targeted harvest request sent. Response status: {}", response.getStatusLine().getStatusCode());
+        } catch (IOException exception) {
+            log.error("Failed to set request entity.", exception);
+        }
+    }
+    
+    public void targetedHarvestMonthly() {
+        LocalDate today = LocalDate.now();
+        for (int i = 1; i <= 12; i++) {
+            if (i != 1) {
+                today = today.minusMonths(1);
+            }
+            LocalDate lastMonth = today.minusMonths(1);
+            NewsSentimentTargetedHarvestRequest request = new NewsSentimentTargetedHarvestRequest(100, lastMonth, today);
+            this.triggerHarvest(request);
         }
     }
 }
