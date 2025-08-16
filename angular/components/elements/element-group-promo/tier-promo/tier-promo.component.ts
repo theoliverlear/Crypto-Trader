80a529bb5212b6d53e27b03ca9a7704e65ed89bb
theoliverlear <sigwartholiver2@gmail.com>
2025-08-16 11:09:05 -0500

    Add tier-promo.component.ts to be an Angular component for the tier-promo element in the Angular application

---
+++ angular/components/elements/element-group-promo/tier-promo/tier-promo.component.ts
@@ -0,0 +1,13 @@
+// tier-promo.component.ts
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'tier-promo',
+    templateUrl: './tier-promo.component.html',
+    styleUrls: ['./tier-promo.component.scss']
+})
+export class TierPromoComponent {
+    constructor() {
+        
+    }
+}
