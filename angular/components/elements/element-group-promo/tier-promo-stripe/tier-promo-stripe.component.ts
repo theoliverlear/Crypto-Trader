8114583fa2f4b785e858ad3342cbb6d7bbb01a5b
theoliverlear <sigwartholiver2@gmail.com>
2025-08-16 11:09:56 -0500

    Add tier-promo-stripe.component.ts to be an Angular component for the tier-promo-stripe element in the Angular application

---
+++ angular/components/elements/element-group-promo/tier-promo-stripe/tier-promo-stripe.component.ts
@@ -0,0 +1,13 @@
+// tier-promo-stripe.component.ts
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'tier-promo-stripe',
+    templateUrl: './tier-promo-stripe.component.html',
+    styleUrls: ['./tier-promo-stripe.component.scss']
+})
+export class TierPromoStripeComponent {
+    constructor() {
+        
+    }
+}
