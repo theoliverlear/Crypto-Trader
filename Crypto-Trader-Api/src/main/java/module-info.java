44d55f2d7cb0daa368fafcc3784d1f1e39f4036a
theoliverlear <sigwartholiver2@gmail.com>
2025-08-18 18:37:35 -0500

    Add module-info.java to define dependencies for Crypto-Trader-Api module

---
+++ Crypto-Trader-Api/src/main/java/module-info.java
@@ -0,0 +1,39 @@
+open module org.theoliverlear {
+    requires spring.boot;
+    requires spring.boot.autoconfigure;
+
+    requires spring.context;
+    requires spring.core;
+    requires spring.beans;
+    requires spring.aop;
+    requires spring.web;
+    requires spring.tx;
+    requires spring.data.jpa;
+
+    requires jakarta.persistence;
+
+
+    requires spring.security.config;
+    requires spring.security.web;
+    requires spring.security.crypto;
+
+
+    requires com.fasterxml.jackson.annotation;
+    requires com.fasterxml.jackson.core;
+    requires com.fasterxml.jackson.databind;
+
+    requires org.apache.httpcomponents.httpclient;
+
+    requires spring.jdbc;
+
+    requires org.slf4j;
+
+    requires org.apache.httpcomponents.httpcore;
+
+    requires org.hibernate.orm.core;
+
+
+    requires static lombok;
+    requires tomcat.embed.core;
+    requires tomcat.embed.websocket;
+}
