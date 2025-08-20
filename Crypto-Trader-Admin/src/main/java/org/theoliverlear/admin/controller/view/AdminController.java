8d2b2fadedfaf4ee7618dd104d3c147eedda4e1e
theoliverlear <sigwartholiver2@gmail.com>
2025-08-20 11:12:09 -0500

    Refactor AdminController.java to extend `BaseViewController`, relocate to `controller.view`, and integrate Spring components

---
+++ Crypto-Trader-Admin/src/main/java/org/theoliverlear/admin/controller/view/AdminController.java
@@ -0,0 +1,10 @@
+package org.theoliverlear.admin.controller.view;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class AdminController extends BaseViewController {
+    public AdminController() {
+        
+    }
+}
