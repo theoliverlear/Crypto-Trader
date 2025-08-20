97763fd845fcb0dfe0aea06dab48aaf63e0d60d4
theoliverlear <sigwartholiver2@gmail.com>
2025-08-20 14:49:30 -0500

    Refactor UserController.java to replace `UserService` with `ProductUserService` and update constructor and imports

--- Crypto-Trader-Api/src/main/java/org/theoliverlear/controller/UserController.java
+++ Crypto-Trader-Api/src/main/java/org/theoliverlear/controller/UserController.java
@@ -1,34 +1,25 @@
 package org.theoliverlear.controller;
 //=================================-Imports-==================================
-import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.theoliverlear.comm.request.UserRequest;
-import org.theoliverlear.comm.response.UserResponse;
-import org.theoliverlear.entity.portfolio.Portfolio;
-import org.theoliverlear.entity.user.SafePassword;
 import org.theoliverlear.entity.user.User;
 import org.theoliverlear.service.PortfolioService;
-import org.theoliverlear.service.UserService;
+import org.theoliverlear.service.ProductUserService;
 
 @RestController
 @RequestMapping("/api/user")
 @Slf4j
 public class UserController {
     //============================-Variables-=================================
-    private UserService userService;
+    private ProductUserService productUserService;
     private PortfolioService portfolioService;
     private User currentUser;
     //===========================-Constructors-===============================
     @Autowired
-    public UserController(UserService userService, PortfolioService portfolioService) {
-        this.userService = userService;
+    public UserController(ProductUserService productUserService, PortfolioService portfolioService) {
+        this.productUserService = productUserService;
         this.portfolioService = portfolioService;
     }
     //=============================-Methods-==================================
