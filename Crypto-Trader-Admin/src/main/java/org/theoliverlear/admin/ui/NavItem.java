bbfe11ef342445800551a538a695c3c1764c797e
theoliverlear <sigwartholiver2@gmail.com>
2025-08-20 14:36:07 -0500

    Add NavItem.java to define navigation items with binding for application pages

---
+++ Crypto-Trader-Admin/src/main/java/org/theoliverlear/admin/ui/NavItem.java
@@ -0,0 +1,59 @@
+package org.theoliverlear.admin.ui;
+
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.theoliverlear.admin.route.AppPage;
+
+@Slf4j
+@Component
+public class NavItem extends BaseComponent {
+    @FXML
+    private Button actionButton;
+    
+    private final ObjectProperty<AppPage> page = new SimpleObjectProperty<>(this, "page", null);
+    private final StringProperty navTitle = new SimpleStringProperty(this, "navTitle", "");
+    public NavItem() {
+        this.load();
+    }
+    
+    
+    @FXML
+    public void initialize() {
+//        this.actionButton.textProperty().bind(this.navTitleProperty());
+        this.navTitle.bind(page.asString());
+        this.actionButton.textProperty().bind(this.navTitle);
+    }
+    
+    @FXML
+    public void go() {
+        log.info("Navigating to {}", this.navTitle.get());
+    }
+    
+    public StringProperty navTitleProperty() {
+        return this.navTitle;
+    }
+    public String getNavTitle() {
+        return this.navTitleProperty().get();
+    }
+    public void setNavTitle(String value) {
+        this.navTitleProperty().set(value);
+    }
+
+    public AppPage getPage() {
+        return page.get();
+    }
+
+    public ObjectProperty<AppPage> pageProperty() {
+        return page;
+    }
+    
+    public void setPage(AppPage page) {
+        this.page.set(page);
+    }
+}
