3e1f5189d036a9c119b6067e903d0a97437a44ad
theoliverlear <sigwartholiver2@gmail.com>
2025-08-20 10:57:27 -0500

    Expand AdminApplication.java to integrate `CSSFX` for live CSS reloading and update UI loader to use AppView.fxml

--- Crypto-Trader-Admin/src/main/java/org/theoliverlear/admin/AdminApplication.java
+++ Crypto-Trader-Admin/src/main/java/org/theoliverlear/admin/AdminApplication.java
@@ -7,6 +7,8 @@ import javafx.stage.Stage;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.theoliverlear.admin.config.SpringBootConfig;
+import fr.brouillard.oss.cssfx.CSSFX;
+
 
 import java.io.IOException;
 
@@ -22,10 +24,14 @@ public class AdminApplication extends Application {
     
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(AdminApplication.class.getResource("ui/admin-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(AdminApplication.class.getResource("ui/view/app/AppView.fxml"));
+        fxmlLoader.setControllerFactory(this.applicationContext::getBean);
+        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+        System.out.println("[Loaded Controller] " + fxmlLoader.getController()
+                + " @" + System.identityHashCode(fxmlLoader.getController()));
+        stage.setTitle("Crypto Trader Admin Panel");
         stage.setScene(scene);
+        CSSFX.start();
         stage.show();
     }
     
