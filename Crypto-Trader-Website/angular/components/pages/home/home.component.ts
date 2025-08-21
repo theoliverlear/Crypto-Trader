ab72ba9543f4ef2c148f3376cd4e3848c099087c
theoliverlear <sigwartholiver2@gmail.com>
2025-08-21 14:52:11 -0500

    Expand home.component.ts to include descriptive text, button text, element size, and element link namespace constants

--- Crypto-Trader-Website/angular/components/pages/home/home.component.ts
+++ Crypto-Trader-Website/angular/components/pages/home/home.component.ts
@@ -1,6 +1,12 @@
 import { Component } from '@angular/core';
 import {TagType} from "../../../models/html/TagType";
 import {transparentLogo} from "../../../assets/imageAssets";
+import {cryptoTraderDescription} from "../../../assets/textAssets";
+import {ElementSize} from "../../../models/ElementSize";
+import {
+    ButtonText
+} from "../../elements/element-group-native/ss-button/models/ButtonText";
+import {getStartedElementLink} from "../../../assets/elementLinkAssets";
 
 @Component({
   selector: 'home',
@@ -11,4 +17,9 @@ export class HomeComponent {
 
     protected readonly TagType = TagType;
     protected readonly transparentLogo = transparentLogo;
+    protected readonly cryptoTraderDescription = cryptoTraderDescription;
+    protected readonly crypto = crypto;
+    protected readonly ElementSize = ElementSize;
+    protected readonly ButtonText = ButtonText;
+    protected readonly getStartedElementLink = getStartedElementLink;
 }
