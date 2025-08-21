77520ec2aba7bd58e3e71dec772c1795c897b8ad
theoliverlear <sigwartholiver2@gmail.com>
2025-08-21 14:45:01 -0500

    Expand tier-promo.component.ts to include subscription tier input and dynamic image and text asset retrieval

--- Crypto-Trader-Website/angular/components/elements/element-group-promo/tier-promo/tier-promo.component.ts
+++ Crypto-Trader-Website/angular/components/elements/element-group-promo/tier-promo/tier-promo.component.ts
@@ -1,5 +1,13 @@
 // tier-promo.component.ts
-import { Component } from "@angular/core";
+import {Component, Input} from "@angular/core";
+import {SubscriptionTier} from "./models/SubscriptionTier";
+import {
+    aiStarIcon,
+    brainIcon,
+    ImageAsset,
+    stockIcon
+} from "../../../../assets/imageAssets";
+import {TagType} from "../../../../models/html/TagType";
 
 @Component({
     selector: 'tier-promo',
@@ -7,7 +15,37 @@ import { Component } from "@angular/core";
     styleUrls: ['./tier-promo.component.scss']
 })
 export class TierPromoComponent {
+    @Input() tier: SubscriptionTier = SubscriptionTier.FREE;
     constructor() {
         
     }
+    
+    protected getImageAsset(): ImageAsset {
+        switch (this.tier) {
+            case SubscriptionTier.FREE:
+                return stockIcon;
+            case SubscriptionTier.PRO:
+                return brainIcon;
+            case SubscriptionTier.ELITE:
+                return aiStarIcon;
+            default:
+                throw new Error("Invalid tier: " + this.tier);
+        }
+    }
+
+    protected getDescription(): string {
+        switch (this.tier) {
+            case SubscriptionTier.FREE:
+                return "A disciplined “buy low, sell high” algorithm with " +
+                    "safeguards to avoid chasing volatility.";
+            case SubscriptionTier.PRO:
+                return "Models analyze large, diverse datasets to estimate " +
+                    "multi‑term price direction and confidence.";
+            case SubscriptionTier.ELITE:
+                return "Combines price action with additional context " +
+                    "(e.g., news sentiment) to inform entries and exits.";
+        }
+    }
+    
+    protected readonly TagType = TagType;
 }
