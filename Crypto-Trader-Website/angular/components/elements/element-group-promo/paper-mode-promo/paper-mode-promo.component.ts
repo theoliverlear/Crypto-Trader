0cfde467d92c99746fe12c196d058dccb8d49b70
theoliverlear <sigwartholiver2@gmail.com>
2025-08-21 14:26:14 -0500

    Add paper-mode-promo.component.ts to be an Angular component for the paper-mode-promo element in the Angular application

---
+++ Crypto-Trader-Website/angular/components/elements/element-group-promo/paper-mode-promo/paper-mode-promo.component.ts
@@ -0,0 +1,13 @@
+// paper-mode-promo.component.ts
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'paper-mode-promo',
+    templateUrl: './paper-mode-promo.component.html',
+    styleUrls: ['./paper-mode-promo.component.scss']
+})
+export class PaperModePromoComponent {
+    constructor() {
+        
+    }
+}
