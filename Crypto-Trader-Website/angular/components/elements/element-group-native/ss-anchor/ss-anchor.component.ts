9fd54465054f04257db320cf70d4668665c23887
theoliverlear <sigwartholiver2@gmail.com>
2025-08-21 14:34:32 -0500

    Expand ss-anchor.component.ts to add button rendering and handling for external links

--- Crypto-Trader-Website/angular/components/elements/element-group-native/ss-anchor/ss-anchor.component.ts
+++ Crypto-Trader-Website/angular/components/elements/element-group-native/ss-anchor/ss-anchor.component.ts
@@ -1,5 +1,5 @@
 import {
-    Component, HostListener,
+    Component, HostBinding, HostListener,
     Input, OnInit
 } from "@angular/core";
 import {TagType} from "../../../../models/html/TagType";
@@ -15,16 +15,30 @@ import {Router} from "@angular/router";
 export class SsAnchorComponent implements OnInit {
     @Input() elementLink: TextElementLink | ElementLink;
     hasText: boolean = false;
+    @Input() isButton: boolean = false;
+    @HostBinding('class.link-button') get isButtonClass(): boolean {
+        return this.isButton;
+    }
+
     constructor(private router: Router) {
-        console.log('SsAnchorComponent loaded');
+        
     }
     ngOnInit() {
         this.hasText = this.elementLink instanceof TextElementLink;
     }
-    @HostListener('click')
+    @HostListener('click', ['$event'])
     onClick() {
-        this.router.navigate([this.elementLink.hrefLink]);
+        if (this.isExternalLink(this.elementLink.hrefLink)) {
+            event.preventDefault();
+            window.open(this.elementLink.hrefLink, this.elementLink.targetType);
+        } else {
+            this.router.navigate([this.elementLink.hrefLink]);
+        }
     }
-
+    isExternalLink(href: string): boolean {
+        return href.startsWith('http://') ||
+            href.startsWith('https://') ||
+            href.startsWith('//');
+    }
     protected readonly TagType = TagType;
 }
