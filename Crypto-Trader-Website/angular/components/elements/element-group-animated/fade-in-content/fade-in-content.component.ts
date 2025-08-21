4e25eefa77871323e2217814e59833a232e72151
theoliverlear <sigwartholiver2@gmail.com>
2025-08-21 14:31:52 -0500

    Add fade-in-content.component.ts to be an Angular component for the fade-in-content element in the Angular application

---
+++ Crypto-Trader-Website/angular/components/elements/element-group-animated/fade-in-content/fade-in-content.component.ts
@@ -0,0 +1,13 @@
+// fade-in-content.component.ts
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'fade-in-content',
+    templateUrl: './fade-in-content.component.html',
+    styleUrls: ['./fade-in-content.component.scss']
+})
+export class FadeInContentComponent {
+    constructor() {
+        
+    }
+}
