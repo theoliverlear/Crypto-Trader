993423515f935848bbf87e54ec90bc2975881606
theoliverlear <sigwartholiver2@gmail.com>
2025-08-23 15:59:40 -0500

    Refactor AdminController.java to AdminUsersController.java for improved naming clarity and specificity

--- Crypto-Trader-Admin/src/main/java/org/cryptotrader/admin/controller/AdminUsersController.java
+++ Crypto-Trader-Admin/src/main/java/org/cryptotrader/admin/controller/AdminUsersController.java
@@ -3,8 +3,8 @@ package org.cryptotrader.admin.controller;
 import org.springframework.stereotype.Component;
 
 @Component
-public class AdminController extends BaseViewController {
-    public AdminController() {
+public class AdminUsersController extends BaseViewController {
+    public AdminUsersController() {
         
     }
 }
