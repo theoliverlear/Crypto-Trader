9e448054364b3d910b535dfe59466f40830082af
theoliverlear <sigwartholiver2@gmail.com>
2025-08-23 16:06:42 -0500

    Add DashboardController.java to be the controller of the `DashboardView.fxml` JavaFX component

---
+++ Crypto-Trader-Admin/src/main/java/org/cryptotrader/admin/controller/DashboardController.java
@@ -0,0 +1,11 @@
+package org.cryptotrader.admin.controller;
+
+import javafx.scene.layout.VBox;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class DashboardController extends VBox {
+    
+}
