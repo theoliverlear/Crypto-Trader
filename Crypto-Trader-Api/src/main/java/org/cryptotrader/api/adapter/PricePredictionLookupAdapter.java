21ea28407c2016ffa8a065cf055c35d1e777c491
theoliverlear <sigwartholiver2@gmail.com>
2025-08-23 16:51:01 -0500

    Add `Autowired` annotation for dependency injection in PricePredictionLookupAdapter.java

--- Crypto-Trader-Api/src/main/java/org/cryptotrader/api/adapter/PricePredictionLookupAdapter.java
+++ Crypto-Trader-Api/src/main/java/org/cryptotrader/api/adapter/PricePredictionLookupAdapter.java
@@ -1,6 +1,7 @@
 package org.cryptotrader.api.adapter;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.cryptotrader.api.service.PricePredictionService;
 import org.cryptotrader.entity.prediction.PricePrediction;
@@ -9,6 +10,7 @@ import org.cryptotrader.entity.prediction.PricePredictionLookup;
 @Component
 @RequiredArgsConstructor
 public class PricePredictionLookupAdapter implements PricePredictionLookup {
+    @Autowired
     private final PricePredictionService pricePredictionService;
 
     @Override
