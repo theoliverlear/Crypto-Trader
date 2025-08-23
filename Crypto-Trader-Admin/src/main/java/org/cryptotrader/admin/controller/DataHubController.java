e55c610ec22df35285b0da2188879c680205a06e
theoliverlear <sigwartholiver2@gmail.com>
2025-08-23 16:07:20 -0500

    Add DataHubController.java to be the controller of the `DataHubView.fxml` JavaFX component

---
+++ Crypto-Trader-Admin/src/main/java/org/cryptotrader/admin/controller/DataHubController.java
@@ -0,0 +1,10 @@
+package org.cryptotrader.admin.controller;
+
+import javafx.scene.layout.VBox;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class DataHubController extends VBox {
+}
