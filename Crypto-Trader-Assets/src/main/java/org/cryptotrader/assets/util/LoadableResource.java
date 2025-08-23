479812a4b967c5a48352a399a58c83fa0502094d
theoliverlear <sigwartholiver2@gmail.com>
2025-08-23 16:56:30 -0500

    Add LoadableResource.java for managing resource loading with `ResourceLoader`

---
+++ Crypto-Trader-Assets/src/main/java/org/cryptotrader/assets/util/LoadableResource.java
@@ -0,0 +1,9 @@
+package org.cryptotrader.assets.util;
+
+public class LoadableResource {
+    protected final ResourceLoader resourceLoader;
+    
+    public LoadableResource() {
+        this.resourceLoader = new ResourceLoader();
+    }
+}
