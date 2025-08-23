2b8d0f36dff9148ff77a9355a7d57ce30274a9f7
theoliverlear <sigwartholiver2@gmail.com>
2025-08-23 16:17:04 -0500

    Add SVG image library initialization in AdminLauncher.java for SVG support in JavaFX

--- Crypto-Trader-Admin/src/main/java/org/cryptotrader/admin/AdminLauncher.java
+++ Crypto-Trader-Admin/src/main/java/org/cryptotrader/admin/AdminLauncher.java
@@ -1,9 +1,11 @@
 package org.cryptotrader.admin;
 
+import de.codecentric.centerdevice.javafxsvg.SvgImageLoaderFactory;
 import javafx.application.Application;
 
 public class AdminLauncher {
     public static void main(String[] args) {
+        SvgImageLoaderFactory.install();
         Application.launch(AdminApplication.class, args);
     }
 }
