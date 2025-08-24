ac4ee19729a230c61d78f8d44652537ae99e9286
theoliverlear <sigwartholiver2@gmail.com>
2025-08-23 20:45:39 -0500

    Add `org.cryptotrader.api.repositories` as a transitive module dependency in module-info.java

--- Crypto-Trader-Library/Api-Library/src/main/java/module-info.java
+++ Crypto-Trader-Library/Api-Library/src/main/java/module-info.java
@@ -2,4 +2,5 @@ module org.cryptotrader.api.library {
     requires transitive org.cryptotrader.api.communication;
     requires transitive org.cryptotrader.api.components;
     requires transitive org.cryptotrader.api.models;
+    requires transitive org.cryptotrader.api.repositories;
 }
