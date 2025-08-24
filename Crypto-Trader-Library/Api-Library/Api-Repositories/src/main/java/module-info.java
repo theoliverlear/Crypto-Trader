0b666682180fbddb14a02b21ed7d61ab153f6039
theoliverlear <sigwartholiver2@gmail.com>
2025-08-23 20:45:08 -0500

    Add module-info.java to define module system for Api-Repositories in Crypto-Trader-Library

---
+++ Crypto-Trader-Library/Api-Library/Api-Repositories/src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module org.cryptotrader.api.repositories {
+    exports org.cryptotrader.repository;
+
+    requires org.cryptotrader.api.models;
+    requires spring.data.jpa;
+
+    opens org.cryptotrader.repository to spring.core, spring.beans, spring.context, spring.data.commons, spring.data.jpa;
+    requires jakarta.persistence;
+
+}
