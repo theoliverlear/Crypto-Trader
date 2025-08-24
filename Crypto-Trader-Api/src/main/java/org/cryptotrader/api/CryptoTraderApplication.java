dba6c20bb8cddf3f2cb1261f3f18cd6902e696e3
theoliverlear <sigwartholiver2@gmail.com>
2025-08-23 20:43:41 -0500

    Add `EnableJpaRepositories` annotation in CryptoTraderApplication.java to enable JPA repository scanning for `org.cryptotrader.repository` packages

--- Crypto-Trader-Api/src/main/java/org/cryptotrader/api/CryptoTraderApplication.java
+++ Crypto-Trader-Api/src/main/java/org/cryptotrader/api/CryptoTraderApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.cryptotrader.component.CurrencyJsonGenerator;
@@ -15,6 +16,9 @@ import org.cryptotrader.component.CurrencyJsonGenerator;
 @ComponentScan(basePackages = {
         "org.cryptotrader.api",
         "org.cryptotrader.component"
+})
+@EnableJpaRepositories(basePackages = {
+        "org.cryptotrader.repository"
 })
 public class CryptoTraderApplication {
     public static void main(String[] args) {
