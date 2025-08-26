4c3ba1b6f69cef3890c018ca9ff7c6a6ad8abdfd
theoliverlear <sigwartholiver2@gmail.com>
2025-08-25 19:20:38 -0500

    Add AssetTraderTest.java to test the trading functionality of the `AssetTrader` model

---
+++ Crypto-Trader-Library/Api-Library/Api-Models/src/test/java/org/cryptotrader/model/trade/AssetTraderTest.java
@@ -0,0 +1,53 @@
+package org.cryptotrader.model.trade;
+
+
+import org.cryptotrader.entity.currency.Currency;
+import org.cryptotrader.entity.portfolio.PortfolioAsset;
+import org.cryptotrader.test.CryptoTraderTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AssetTraderTest extends CryptoTraderTest {
+    private Currency testCurrency;
+    private PortfolioAsset testAsset;
+    private AssetTrader testTrader;
+    @BeforeEach
+    void setUp() {
+        this.testCurrency = Currency.builder()
+                                    .name("Test Coin")
+                                    .currencyCode("TEST")
+                                    .urlPath(Currency.TESTING_URL)
+                                    .value(100)
+                                    .build();
+        this.testAsset = new PortfolioAsset(this.testCurrency,0, 0);
+        this.testTrader = new AssetTrader(this.testAsset);
+    }
+    
+    @Test
+    @DisplayName("Should sell at an increased price")
+    public void testShouldSell() {
+        this.testAsset.setShares(1);
+        this.testAsset.setTargetPrice(100);
+        boolean expected = false;
+        assertEquals(expected, this.testTrader.trade());
+        this.testCurrency.setValue(101);
+        expected = true;
+        assertEquals(expected, this.testTrader.trade());
+    }
+    
+    @Test
+    @DisplayName("Should buy at a decreased price")
+    public void testShouldBuy() {
+        this.testAsset.setShares(0);
+        this.testAsset.setAssetWalletDollars(100);
+        this.testAsset.setTargetPrice(100);
+        boolean expected = false;
+        assertEquals(expected, this.testTrader.trade());
+        this.testCurrency.setValue(99);
+        expected = true;
+        assertEquals(expected, this.testTrader.trade());
+    }
+}
