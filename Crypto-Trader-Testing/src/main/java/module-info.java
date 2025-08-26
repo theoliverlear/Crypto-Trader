bb88f2bef41d47858e0464fdc5f5010c8881e916
theoliverlear <sigwartholiver2@gmail.com>
2025-08-26 07:51:05 -0500

    Add JetBrains annotations to module-info.java for enhanced code validation

--- Crypto-Trader-Testing/src/main/java/module-info.java
+++ Crypto-Trader-Testing/src/main/java/module-info.java
@@ -1,21 +1,18 @@
 module org.cryptotrader.test {
-    // Compile-time only for Lombok annotations
     requires static lombok;
 
-    // Logging API used by testing utilities
     requires org.slf4j;
 
     requires org.junit.jupiter.api;
     
-    // Spring bits available to testing helpers (present in POM)
     requires spring.beans;
     requires spring.context;
     requires spring.web;
 
-    // Optional compile-time presence for JPA/Hibernate types referenced by helpers
     requires static jakarta.persistence;
     requires static org.hibernate.orm.core;
-    
+    requires org.jetbrains.annotations;
+
     exports org.cryptotrader.test;
     exports org.cryptotrader.test.logging;
 }
