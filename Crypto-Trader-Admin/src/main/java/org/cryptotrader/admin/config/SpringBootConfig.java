3254677717dee34d22717625136c5eca4791e801
theoliverlear <sigwartholiver2@gmail.com>
2025-08-27 08:23:36 -0500

    Expand SpringBootConfig.java to import `SpringContext` and include Desktop-Components in component scan

--- Crypto-Trader-Admin/src/main/java/org/cryptotrader/admin/config/SpringBootConfig.java
+++ Crypto-Trader-Admin/src/main/java/org/cryptotrader/admin/config/SpringBootConfig.java
@@ -1,10 +1,14 @@
 package org.cryptotrader.admin.config;
 
+import org.cryptotrader.desktop.component.config.SpringContext;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "org.cryptotrader.admin")
+@ComponentScan(basePackages = {"org.cryptotrader.admin", 
+                               "org.cryptotrader.desktop.component"})
+@Import(SpringContext.class)
 public class SpringBootConfig {
     
 }
