b0364bd341a67dbc9413c1265ed2f3748376602a
theoliverlear <sigwartholiver2@gmail.com>
2025-08-27 08:20:11 -0500

    Add module-info.java to define module dependencies and exports for Crypto-Trader-Promo

---
+++ Crypto-Trader-Promo/src/main/java/module-info.java
@@ -0,0 +1,16 @@
+open module org.cryptotrader.promo {
+    requires annotations;
+    requires com.fasterxml.jackson.databind;
+    requires static lombok;
+    requires openai.java.client.okhttp;
+    requires openai.java.core;
+    requires org.apache.httpcomponents.httpclient;
+    requires org.apache.httpcomponents.httpcore;
+    requires scribejava.apis;
+    requires scribejava.core;
+    requires org.slf4j;
+    
+    exports org.cryptotrader.promo;
+    exports org.cryptotrader.promo.models.openai;
+    exports org.cryptotrader.promo.models.twitter;
+}
