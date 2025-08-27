498647a3ec1b01bff8df9b54229906043b956c1f
theoliverlear <sigwartholiver2@gmail.com>
2025-08-26 21:00:22 -0500

    Add CommitPromo.java to orchestrate commit summarization and optional posting to X in Crypto-Trader-Promo

---
+++ Crypto-Trader-Promo/src/main/java/org/cryptotrader/promo/CommitPromo.java
@@ -0,0 +1,25 @@
+package org.cryptotrader.promo;
+
+
+import org.cryptotrader.promo.models.openai.*;
+import org.cryptotrader.promo.models.twitter.Tweeter;
+
+public class CommitPromo {
+
+    public static void main(String[] args) {
+        CommitCapture capture = new CommitCapture();
+        Summarizer summarizer = new Summarizer();
+        String summary = summarizer.summarize(capture, PushPrompts.SYSTEM.getText(), PushPrompts.USER.getText());
+        System.out.println(summary);
+
+        String mode = System.getenv("RELEASEBOT_MODE");
+        boolean shouldPost = mode != null && (mode.equalsIgnoreCase("prod") || mode.equalsIgnoreCase("post"));
+
+        if (shouldPost) {
+            Tweeter tweeter = new Tweeter();
+            tweeter.tweet(summary);
+        } else {
+            System.out.println("[INFO] Dry run mode (RELEASEBOT_MODE=" + mode + "): not posting to X.");
+        }
+    }
+}
