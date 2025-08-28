77bc6681f4ed2248c3eab8af6e9cd28c7435695a
theoliverlear <sigwartholiver2@gmail.com>
2025-08-28 14:11:15 -0500

    Add module-info.java to define modular dependencies and exports in Version-Models library

---
+++ Crypto-Trader-Library/Version-Library/Version-Models/src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module org.cryptotrader.version.model {
+    requires static lombok;
+    requires org.jdom2;
+    requires org.slf4j;
+    requires java.xml;
+
+    exports org.cryptotrader.version.model.config;
+    exports org.cryptotrader.version.model.dependency;
+    exports org.cryptotrader.version.model.dependency.type;
+    exports org.cryptotrader.version.model.module;
+    exports org.cryptotrader.version.model.module.type;
+}
