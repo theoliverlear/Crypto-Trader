3a4764d3229e4a24629ce211983f3eb05a8f13aa
theoliverlear <sigwartholiver2@gmail.com>
2025-08-28 14:09:59 -0500

    Add Pom.java to represent Maven POM modules in the Version-Models library

---
+++ Crypto-Trader-Library/Version-Library/Version-Models/src/main/java/org/cryptotrader/version/model/module/type/Pom.java
@@ -0,0 +1,35 @@
+package org.cryptotrader.version.model.module.type;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+import org.cryptotrader.version.model.config.ConfigFileType;
+import org.cryptotrader.version.model.dependency.type.PomDependency;
+import org.cryptotrader.version.model.module.CryptoTraderModules;
+import org.cryptotrader.version.model.module.Module;
+import org.cryptotrader.version.model.module.ModuleDescendent;
+
+import java.nio.file.Path;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
+public class Pom extends ModuleDescendent {
+    private PomDependency moduleDependency;
+    public Pom(CryptoTraderModules moduleType,
+               Path modulePath,
+               ConfigFileType configFileType,
+               PomDependency moduleDependency) {
+        super(moduleType, modulePath, configFileType);
+        this.moduleDependency = moduleDependency;
+    }
+    public Pom(CryptoTraderModules moduleType,
+               Path modulePath,
+               ConfigFileType configFileType,
+               PomDependency moduleDependency,
+               Module parent,
+               Module child) {
+        super(moduleType, modulePath, configFileType, parent, child);
+        this.moduleDependency = moduleDependency;
+    }
+}
