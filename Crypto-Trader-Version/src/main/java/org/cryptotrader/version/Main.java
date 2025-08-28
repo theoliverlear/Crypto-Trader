5bf67cab3a5c64c00052659f9b937c7a514b2f4a
theoliverlear <sigwartholiver2@gmail.com>
2025-08-28 14:15:25 -0500

    Refactor Main.java to use `Pom` model instead of `Module` to align with updated PomParser.java functionality

--- Crypto-Trader-Version/src/main/java/org/cryptotrader/version/Main.java
+++ Crypto-Trader-Version/src/main/java/org/cryptotrader/version/Main.java
@@ -1,6 +1,6 @@
 package org.cryptotrader.version;
 
-import org.cryptotrader.version.model.Module;
+import org.cryptotrader.version.model.module.type.Pom;
 import org.cryptotrader.version.script.PomParser;
 
 import java.util.List;
@@ -8,7 +8,7 @@ import java.util.List;
 @Deprecated(forRemoval = true)
 public class Main {
     public static void main(String[] args) {
-        List<Module> allPoms = PomParser.getAllPoms();
+        List<Pom> allPoms = PomParser.getAllPoms();
         allPoms.forEach(System.out::println);
     }
 }
