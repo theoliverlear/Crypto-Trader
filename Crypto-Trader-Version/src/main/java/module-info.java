c11825e1e024495389f026b4af69153a85f9708c
theoliverlear <sigwartholiver2@gmail.com>
2025-08-28 14:16:33 -0500

    Update module-info.java to use the library of Version-Models module

--- Crypto-Trader-Version/src/main/java/module-info.java
+++ Crypto-Trader-Version/src/main/java/module-info.java
@@ -3,10 +3,8 @@ module org.cryptotrader.version {
     requires org.jdom2;
     requires org.slf4j;
     requires java.xml;
+    requires org.cryptotrader.version.model;
 
     exports org.cryptotrader.version;
-    exports org.cryptotrader.version.model;
-    exports org.cryptotrader.version.model.config;
-    exports org.cryptotrader.version.model.dependency;
     exports org.cryptotrader.version.script;
 }
