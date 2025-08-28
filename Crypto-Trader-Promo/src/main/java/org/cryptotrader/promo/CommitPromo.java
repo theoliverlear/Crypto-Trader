5a670a323636d29daec51cd7b6fdaf6b20af4f99
theoliverlear <sigwartholiver2@gmail.com>
2025-08-28 15:06:19 -0500

    Refactor CommitPromo.java to replace `Tweeter` boilerplate with `Publisher`

--- Crypto-Trader-Promo/src/main/java/org/cryptotrader/promo/CommitPromo.java
+++ Crypto-Trader-Promo/src/main/java/org/cryptotrader/promo/CommitPromo.java
@@ -3,7 +3,8 @@ package org.cryptotrader.promo;
 
 import org.cryptotrader.promo.models.openai.*;
 import org.cryptotrader.promo.models.openai.commit.CommitCapture;
-import org.cryptotrader.promo.models.twitter.Tweeter;
+import org.cryptotrader.promo.models.openai.prompt.PushPrompts;
+import org.cryptotrader.promo.models.publish.Publisher;
 
 public class CommitPromo {
 
@@ -11,16 +12,6 @@ public class CommitPromo {
         CommitCapture capture = new CommitCapture();
         Summarizer summarizer = new Summarizer();
         String summary = summarizer.summarize(capture, PushPrompts.SYSTEM.getText(), PushPrompts.USER.getText());
-        System.out.println(summary);
-
-        String mode = System.getenv("RELEASEBOT_MODE");
-        boolean shouldPost = mode != null && (mode.equalsIgnoreCase("prod") || mode.equalsIgnoreCase("post"));
-
-        if (shouldPost) {
-            Tweeter tweeter = new Tweeter();
-            tweeter.tweet(summary);
-        } else {
-            System.out.println("[INFO] Dry run mode (RELEASEBOT_MODE=" + mode + "): not posting to X.");
-        }
+        Publisher.publish(summary);
     }
 }
