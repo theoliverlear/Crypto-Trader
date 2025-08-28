6d7e2d397dfcf0cec017d245310cd7684328e364
theoliverlear <sigwartholiver2@gmail.com>
2025-08-28 14:08:26 -0500

    Add PomDependency.java to represent Maven POM dependencies in Version-Models library

---
+++ Crypto-Trader-Library/Version-Library/Version-Models/src/main/java/org/cryptotrader/version/model/dependency/type/PomDependency.java
@@ -0,0 +1,22 @@
+package org.cryptotrader.version.model.dependency.type;
+
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+import org.cryptotrader.version.model.dependency.Dependency;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
+public class PomDependency extends Dependency {
+    private String groupId;
+    private String artifactId;
+    
+    public PomDependency(String name, String version,
+                         String groupId, String artifactId) {
+        super(name, version);
+        this.groupId = groupId;
+        this.artifactId = artifactId;
+    }
+}
