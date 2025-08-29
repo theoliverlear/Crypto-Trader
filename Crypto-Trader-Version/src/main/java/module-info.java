cdbc00cf0ba68d49fe92e328612f8fc0ddd24481
theoliverlear <sigwartholiver2@gmail.com>
2025-08-28 22:25:28 -0500

    Edit module-info.java to add dependencies for OpenAI, Apache Commons IO, and Promo modules in Crypto-Trader-Version module

--- Crypto-Trader-Version/src/main/java/module-info.java
+++ Crypto-Trader-Version/src/main/java/module-info.java
@@ -4,6 +4,9 @@ module org.cryptotrader.version {
     requires org.slf4j;
     requires java.xml;
     requires org.cryptotrader.version.model;
+    requires org.cryptotrader.externals.openai;
+    requires org.apache.commons.io;
+    requires org.cryptotrader.promo;
 
     exports org.cryptotrader.version;
     exports org.cryptotrader.version.script;
