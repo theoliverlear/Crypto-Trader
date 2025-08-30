3392b279f7b5f3bc7e13103635f444d5434e53fc
theoliverlear <sigwartholiver2@gmail.com>
2025-08-29 21:15:18 -0500

    Migrate generalized functionality of changelog generation and PromoBot to Sigwarth-Software-Suite

--- Crypto-Trader-Version/src/main/java/org/cryptotrader/version/Main.java
+++ Crypto-Trader-Version/src/main/java/org/cryptotrader/version/Main.java
@@ -1,5 +1,8 @@
 package org.cryptotrader.version;
 
+import com.sigwarthsoftware.changelog.ReleaseChangelogGenerator;
+import com.sigwarthsoftware.changelog.version.models.module.ModuleLibrary;
+import com.sigwarthsoftware.promo.github.commit.CommitRange;
 import org.cryptotrader.version.model.element.PomElement;
 import org.cryptotrader.version.model.module.type.Pom;
 import org.cryptotrader.version.script.PomParser;
@@ -15,5 +18,33 @@ public class Main {
             System.out.println(pom + "\n");
         });
         System.out.println(PomValidator.cryptoTraderParentMatches());
+    }
+    
+    private static void generateChangelogs(CommitRange commitRange, String version) {
+        ModuleLibrary.initializeModules(
+                List.of(
+                        new ModuleLibrary("Crypto-Trader-Admin"),
+                        new ModuleLibrary("Crypto-Trader-Analysis"),
+                        new ModuleLibrary("Crypto-Trader-Api"),
+                        new ModuleLibrary("Crypto-Trader-Assets"),
+                        new ModuleLibrary("Crypto-Trader-Library"),
+                        new ModuleLibrary("Api-Library"),
+                        new ModuleLibrary("Api-Communication"),
+                        new ModuleLibrary("Api-Components"),
+                        new ModuleLibrary("Api-Models"),
+                        new ModuleLibrary("Api-Repositories"),
+                        new ModuleLibrary("Desktop-Library"),
+                        new ModuleLibrary("Desktop-Components"),
+                        new ModuleLibrary("Version-Library"),
+                        new ModuleLibrary("Version-Models"),
+                        new ModuleLibrary("Externals-Library"),
+                        new ModuleLibrary("Crypto-Trader-Testing"),
+                        new ModuleLibrary("Crypto-Trader-Version"),
+                        new ModuleLibrary("Crypto-Trader-Website"),
+                        new ModuleLibrary("Crypto-Trader")
+                )
+        );
+        String changelog = ReleaseChangelogGenerator.getChangelog(commitRange, version);
+        System.out.println(changelog);
     }
 }
