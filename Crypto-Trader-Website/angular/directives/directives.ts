db0223a195b6668c93ea4a178da0354886175a8d
theoliverlear <sigwartholiver2@gmail.com>
2025-08-30 17:30:04 -0500

    Remove directives from export array that have been migrated to Sigwarth-Software-Suite

--- Crypto-Trader-Website/angular/directives/directives.ts
+++ Crypto-Trader-Website/angular/directives/directives.ts
@@ -1,7 +1,3 @@
-import {UnoptimizedImageDirective} from "./unoptimized-image.directive";
-import {UndraggableDirective} from "./undraggable.directive";
-
-export const directives = [
-    UndraggableDirective,
-    UnoptimizedImageDirective
+export const directives: any[] = [
+    
 ]
