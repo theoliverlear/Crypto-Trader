67e44bebb92b47d4b094fd6959e1615cff52395c
theoliverlear <sigwartholiver2@gmail.com>
2025-08-31 17:20:52 -0500

    Expand currencies.component.ts to fetch currencies from Crypto-Trader-Api for future display

--- Crypto-Trader-Website/angular/components/pages/currencies/currencies.component.ts
+++ Crypto-Trader-Website/angular/components/pages/currencies/currencies.component.ts
@@ -1,13 +1,27 @@
 // currencies.component.ts 
-import { Component } from "@angular/core";
+import {Component, OnInit} from "@angular/core";
+import {
+    GetAllCurrenciesService
+} from "../../../services/net/http/get-all-currencies.service";
+import {CurrencyList} from "../../../models/currency/types";
 
 @Component({
     selector: 'currencies',
     templateUrl: './currencies.component.html',
     styleUrls: ['./currencies.component.scss']
 })
-export class CurrenciesComponent {
-    constructor() {
+export class CurrenciesComponent implements OnInit {
+    constructor(private getAllCurrenciesService: GetAllCurrenciesService) {
         
+    }
+    
+    ngOnInit(): void {
+        this.listenForCurrencies();
+    }
+
+    listenForCurrencies() {
+        this.getAllCurrenciesService.getAllCurrencies().subscribe((data: CurrencyList) => {
+            console.log(data);
+        });
     }
 }
