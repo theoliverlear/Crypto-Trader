643077b002bd6b45b581cef7037b3a2bb636886c
theoliverlear <sigwartholiver2@gmail.com>
2025-08-31 11:01:05 -0500

    Add CryptoTraderApiTestSuite.java to group and run API tests in Crypto-Trader-Api

---
+++ Crypto-Trader-Api/src/test/java/org/cryptotrader/api/CryptoTraderApiTestSuite.java
@@ -0,0 +1,8 @@
+package org.cryptotrader.api;
+
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@SelectPackages({"org.cryptotrader.api"})
+public class CryptoTraderApiTestSuite { }
