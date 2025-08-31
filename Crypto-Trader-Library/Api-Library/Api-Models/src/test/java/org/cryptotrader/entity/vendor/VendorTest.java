cf6018a5911ae4f39ac0aff556d172167dbd4981
theoliverlear <sigwartholiver2@gmail.com>
2025-08-31 10:57:51 -0500

    Add VendorTest.java to test the functionality of `Vendor` entities in Api-Models

---
+++ Crypto-Trader-Library/Api-Library/Api-Models/src/test/java/org/cryptotrader/entity/vendor/VendorTest.java
@@ -0,0 +1,26 @@
+package org.cryptotrader.entity.vendor;
+
+import org.cryptotrader.test.CryptoTraderTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@DisplayName("Vendor")
+public class VendorTest extends CryptoTraderTest {
+    private Vendor testVendor;
+    @BeforeEach
+    void setUp() {
+        this.testVendor = new Vendor("Test Vendor", 0.1);
+    }
+    
+    @Test
+    @DisplayName("Should adjust prices")
+    public void testShouldAdjustPrices() {
+        double price = 100;
+        double expectedPrice = 110;
+        double actualPrice = this.testVendor.getAdjustedPrice(price);
+        assertEquals(expectedPrice, actualPrice);
+    }
+}
