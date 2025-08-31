27d0d2673da4be8779650075fb3d24316148a22b
theoliverlear <sigwartholiver2@gmail.com>
2025-08-31 10:59:09 -0500

    Add `MockitoExtension` to CryptoTraderTest.java for Mockito integration

--- Crypto-Trader-Testing/src/main/java/org/cryptotrader/test/CryptoTraderTest.java
+++ Crypto-Trader-Testing/src/main/java/org/cryptotrader/test/CryptoTraderTest.java
@@ -2,6 +2,8 @@ package org.cryptotrader.test;
 
 import org.cryptotrader.test.logging.LoggingTestWatcher;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(LoggingTestWatcher.class)
+@ExtendWith(MockitoExtension.class)
 public class CryptoTraderTest { }
