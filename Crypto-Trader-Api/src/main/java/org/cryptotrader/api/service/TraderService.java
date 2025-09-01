3489e96c0e9a22eaa719680f3bc8b31ed655ff93
theoliverlear <sigwartholiver2@gmail.com>
2025-08-31 20:10:35 -0500

    Inject `PortfolioService` via constructor in TraderService.java to begin tracking user trades in `TradeEvent` entities

--- Crypto-Trader-Api/src/main/java/org/cryptotrader/api/service/TraderService.java
+++ Crypto-Trader-Api/src/main/java/org/cryptotrader/api/service/TraderService.java
@@ -1,7 +1,14 @@
 package org.cryptotrader.api.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TraderService {
+    private final PortfolioService portfolioService;
+    
+    @Autowired
+    public TraderService(PortfolioService portfolioService) {
+        this.portfolioService = portfolioService;
+    }
 }
