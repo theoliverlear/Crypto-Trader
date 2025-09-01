ac568a33422d6b61fe4d244a2f715dcf2d8c0bd2
theoliverlear <sigwartholiver2@gmail.com>
2025-08-31 20:07:58 -0500

    Remove session management methods from CryptoTraderService.java as they are replaced by `SessionService`

--- Crypto-Trader-Api/src/main/java/org/cryptotrader/api/service/CryptoTraderService.java
+++ Crypto-Trader-Api/src/main/java/org/cryptotrader/api/service/CryptoTraderService.java
@@ -13,34 +13,5 @@ public class CryptoTraderService {
     //============================-Variables-=================================
 
     //===========================-Constructors-===============================
-
-    //=============================-Methods-==================================
-
-    //--------------------------User-In-Session-------------------------------
-    public boolean userInSession(HttpSession session) {
-        return session.getAttribute("user") != null;
-    }
-    //-----------------------Get-User-From-Session----------------------------
-    public Optional<ProductUser> getUserFromSession(HttpSession session) {
-        ProductUser user = (ProductUser) session.getAttribute("product-user");
-        if (user == null) {
-            return Optional.empty();
-        } else {
-            return Optional.of(user);
-        }
-    }
-    public void setSessionUser(HttpSession session, User user) {
-        if (user instanceof ProductUser) {
-            session.setAttribute("product-user", user);
-        } else if (user instanceof AdminUser) {
-            session.setAttribute("admin-user", user);
-        } else {
-            throw new IllegalArgumentException("User is not a ProductUser or AdminUser");
-        }
-    }
-    public void removeSessionUser(HttpSession session) {
-        session.removeAttribute("user");
-        session.removeAttribute("product-user");
-        session.removeAttribute("admin-user");
-    }
+    
 }
