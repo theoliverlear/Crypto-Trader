44de48dba8e2c70fea0c64239884abbca3019482
theoliverlear <sigwartholiver2@gmail.com>
2025-09-01 20:31:57 -0500

    Add TimeValueResponse.java to represent a value of an asset at a point in time in API responses

---
+++ Crypto-Trader-Library/Api-Library/Api-Communication/src/main/java/org/cryptotrader/comm/response/TimeValueResponse.java
@@ -0,0 +1,19 @@
+package org.cryptotrader.comm.response;
+
+import lombok.Data;
+
+@Data
+public class TimeValueResponse {
+    private String timestamp;
+    private double value;
+
+    public TimeValueResponse() {
+        this.timestamp = "";
+        this.value = 0;
+    }
+
+    public TimeValueResponse(String timestamp, double value) {
+        this.timestamp = timestamp;
+        this.value = value;
+    }
+}
