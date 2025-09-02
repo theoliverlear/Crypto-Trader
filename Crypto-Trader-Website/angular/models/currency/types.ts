f5504f4b386aeb34d2666d2c544e4927d310584f
theoliverlear <sigwartholiver2@gmail.com>
2025-09-01 20:14:58 -0500

    Expand types.ts to include observable types, historical data types, and extended currency model definitions

--- Crypto-Trader-Website/angular/models/currency/types.ts
+++ Crypto-Trader-Website/angular/models/currency/types.ts
@@ -1,9 +1,29 @@
+import {Injectable} from "@angular/core";
+import {HttpClientService} from "@theoliverlear/angular-suite";
+import {Observable} from "rxjs";
+import {HttpClient} from "@angular/common/http";
+
 export type Currency = {
     currencyName: string;
     currencyCode: string;
     value: number;
-}
+};
 
 export type CurrencyList = {
     currencies: Currency[];
-}
+};
+
+export type DisplayCurrency = Currency & {logoUrl: string};
+
+export type DisplayCurrencyList = {
+    currencies: DisplayCurrency[];
+};
+
+export type HistoryPoint = {
+    date: Date; value: number
+};
+export type TimeValueResponse = {
+    timestamp: string; value: number
+};
+export type ObservableHistoryPoint = Observable<HistoryPoint[]>;
+export type ObservableHistoryMap = Map<string, ObservableHistoryPoint>;
