2b1f1d6eee86db05bdea586bde05a82a698b82ce
theoliverlear <sigwartholiver2@gmail.com>
2025-09-02 12:56:58 -0500

    Add CryptoTraderDataApplication.java to initialize the Crypto-Trader-Data Spring Boot application with scheduling, async support, and JPA configuration

---
+++ Crypto-Trader-Data/src/main/java/org/cryptotrader/data/CryptoTraderDataApplication.java
@@ -0,0 +1,28 @@
+package org.cryptotrader.data;
+
+import org.cryptotrader.component.CurrencyJsonGenerator;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableAsync
+@EnableScheduling
+@EntityScan(basePackages = "org.cryptotrader.entity")
+@ComponentScan(basePackages = {
+        "org.cryptotrader.api",
+        "org.cryptotrader.component"
+})
+@EnableJpaRepositories(basePackages = {
+        "org.cryptotrader.repository"
+})
+public class CryptoTraderDataApplication {
+    public static void main(String[] args) {
+        CurrencyJsonGenerator.standalone().generateAndSave();
+        SpringApplication.run(CryptoTraderDataApplication.class, args);
+    }
+}
