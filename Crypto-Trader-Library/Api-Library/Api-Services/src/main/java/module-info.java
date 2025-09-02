81fd675c42502b9bf10d1db353d6295154af2250
theoliverlear <sigwartholiver2@gmail.com>
2025-09-02 12:51:17 -0500

    Add module-info.java to define module dependencies and exports for Api-Services in Crypto-Trader-Library module

---
+++ Crypto-Trader-Library/Api-Library/Api-Services/src/main/java/module-info.java
@@ -0,0 +1,17 @@
+open module org.cryptotrader.api.services {
+    requires java.sql;
+    requires static lombok;
+    requires org.cryptotrader.api.communication;
+    requires org.cryptotrader.api.components;
+    requires org.cryptotrader.api.models;
+    requires org.cryptotrader.api.repositories;
+    requires spring.beans;
+    requires spring.context;
+    requires spring.jdbc;
+    requires spring.tx;
+    requires spring.web;
+    requires tomcat.embed.core;
+    requires org.slf4j;
+    
+    exports org.cryptotrader.api.services;
+}
