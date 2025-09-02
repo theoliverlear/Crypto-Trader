6fe25c95d5dc6a4c663f7b69b19f1f7fcd347db2
theoliverlear <sigwartholiver2@gmail.com>
2025-09-01 20:34:07 -0500

    Add currency history endpoint in CurrencyController.java for fetching historical prices

--- Crypto-Trader-Api/src/main/java/org/cryptotrader/api/controller/CurrencyController.java
+++ Crypto-Trader-Api/src/main/java/org/cryptotrader/api/controller/CurrencyController.java
@@ -1,17 +1,22 @@
 package org.cryptotrader.api.controller;
 
-import org.cryptotrader.comm.response.CurrencyValuesListResponse;
+import org.cryptotrader.comm.response.DisplayCurrencyListResponse;
+import org.cryptotrader.comm.response.TimeValueResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.cryptotrader.comm.request.AssetValueRequest;
 import org.cryptotrader.comm.response.AssetValueResponse;
 import org.cryptotrader.entity.currency.Currency;
 import org.cryptotrader.api.service.CurrencyService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/currency")
@@ -31,9 +36,20 @@ public class CurrencyController {
             return new ResponseEntity<>(new AssetValueResponse(assetValue), HttpStatus.OK);
         }
     }
-    
+
     @GetMapping("/all")
-    public ResponseEntity<CurrencyValuesListResponse> getAll() {
+    public ResponseEntity<DisplayCurrencyListResponse> getAll() {
         return new ResponseEntity<>(this.currencyService.getCurrencyValuesResponse(), HttpStatus.OK);
+    }
+
+    @GetMapping("/history/{code}")
+    public ResponseEntity<List<TimeValueResponse>> getHistory(@PathVariable("code") String code,
+                                                              @RequestParam(value = "hours", defaultValue = "24") int hours,
+                                                              @RequestParam(value = "intervalSeconds", defaultValue = "60") int intervalSeconds) {
+        List<TimeValueResponse> history = this.currencyService.getCurrencyHistory(code, hours, intervalSeconds);
+        if (history == null || history.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(history, HttpStatus.OK);
     }
 }
