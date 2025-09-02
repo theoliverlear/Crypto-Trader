99ae27be60a24e4e44de6dcdd9445c044374380a
theoliverlear <sigwartholiver2@gmail.com>
2025-09-01 20:16:44 -0500

    Update currencies.component.ts to use `DisplayCurrencyList`, limit displayed currencies, and add error handling

--- Crypto-Trader-Website/angular/components/pages/currencies/currencies.component.ts
+++ Crypto-Trader-Website/angular/components/pages/currencies/currencies.component.ts
@@ -3,7 +3,10 @@ import {Component, OnInit} from "@angular/core";
 import {
     GetAllCurrenciesService
 } from "../../../services/net/http/get-all-currencies.service";
-import {CurrencyList} from "../../../models/currency/types";
+import {
+    CurrencyList,
+    DisplayCurrencyList
+} from "../../../models/currency/types";
 
 @Component({
     selector: 'currencies',
@@ -11,17 +14,26 @@ import {CurrencyList} from "../../../models/currency/types";
     styleUrls: ['./currencies.component.scss']
 })
 export class CurrenciesComponent implements OnInit {
+    currencies: DisplayCurrencyList;
     constructor(private getAllCurrenciesService: GetAllCurrenciesService) {
         
+    }
+    
+    onError(event: Event) {
+        if (event instanceof ErrorEvent) {
+            console.log(event.message);
+        }
     }
     
     ngOnInit(): void {
         this.listenForCurrencies();
     }
 
-    listenForCurrencies() {
-        this.getAllCurrenciesService.getAllCurrencies().subscribe((data: CurrencyList) => {
+    listenForCurrencies(): void {
+        this.getAllCurrenciesService.getAllCurrencies().subscribe((data: DisplayCurrencyList) => {
             console.log(data);
+            this.currencies = data;
+            this.currencies.currencies = this.currencies.currencies.slice(0, 10);
         });
     }
 }
