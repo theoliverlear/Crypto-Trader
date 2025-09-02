91ab95171df83c932ba2ee977a4fd031b6411cfd
theoliverlear <sigwartholiver2@gmail.com>
2025-09-02 17:34:05 -0500

    Refactor BuildersConfig.java to move to Api-Services module where the config is needed

--- Crypto-Trader-Library/Api-Library/Api-Services/src/main/java/org/cryptotrader/api/config/BuildersConfig.java
+++ Crypto-Trader-Library/Api-Library/Api-Services/src/main/java/org/cryptotrader/api/config/BuildersConfig.java
@@ -1,13 +1,22 @@
-package org.cryptotrader.data.config;
+package org.cryptotrader.api.config;
 
+import org.cryptotrader.api.services.PricePredictionService;
 import org.cryptotrader.entity.prediction.PricePredictionLookup;
 import org.cryptotrader.entity.training.builder.TrainingSessionBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class BuildersConfig {
+
+    @Bean
+    @Primary
+    public PricePredictionLookup pricePredictionLookup(PricePredictionService pricePredictionService) {
+        return pricePredictionService::getById;
+    }
+
     @Bean
     @Scope("prototype")
     public TrainingSessionBuilder trainingSessionBuilder(PricePredictionLookup predictionLookup) {
