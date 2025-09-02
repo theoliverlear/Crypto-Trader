3ff2915b9133cd983c1c5a42699018358e98e4ef
theoliverlear <sigwartholiver2@gmail.com>
2025-09-01 20:25:17 -0500

    Add `spring.data.commons` module requirement in module-info.java to support Spring Data functionality

--- Crypto-Trader-Library/Api-Library/Api-Repositories/src/main/java/module-info.java
+++ Crypto-Trader-Library/Api-Library/Api-Repositories/src/main/java/module-info.java
@@ -3,6 +3,7 @@ module org.cryptotrader.api.repositories {
 
     requires org.cryptotrader.api.models;
     requires spring.data.jpa;
+    requires spring.data.commons;
 
     opens org.cryptotrader.repository to spring.core, spring.beans, spring.context, spring.data.commons, spring.data.jpa;
     requires jakarta.persistence;
