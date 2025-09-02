2fa54c97e134ea1fd9b97a05e2773075623b8e8d
theoliverlear <sigwartholiver2@gmail.com>
2025-09-01 20:33:18 -0500

    Add DisplayCurrencyListResponse.java to represent a time series of an asset value

---
+++ Crypto-Trader-Library/Api-Library/Api-Communication/src/main/java/org/cryptotrader/comm/response/DisplayCurrencyListResponse.java
@@ -0,0 +1,13 @@
+package org.cryptotrader.comm.response;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class DisplayCurrencyListResponse {
+    private List<DisplayCurrencyResponse> currencies;
+    public DisplayCurrencyListResponse(List<DisplayCurrencyResponse> currencies) {
+        this.currencies = currencies;
+    }
+}
