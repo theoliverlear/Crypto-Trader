67b85f6967cdc8e1241fe783ccedf5d8b7289bc5
theoliverlear <sigwartholiver2@gmail.com>
2025-09-02 22:54:35 -0500

    Exclude buggy `HttpClientConfig` class from component scanning in CryptoTraderApiApplication.java

--- Crypto-Trader-Api/src/main/java/org/cryptotrader/api/CryptoTraderApiApplication.java
+++ Crypto-Trader-Api/src/main/java/org/cryptotrader/api/CryptoTraderApiApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -16,7 +17,7 @@ import org.cryptotrader.component.CurrencyJsonGenerator;
 @ComponentScan(basePackages = {
         "org.cryptotrader.api",
         "org.cryptotrader.component"
-})
+}, excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = org.cryptotrader.component.config.HttpClientConfig.class))
 @EnableJpaRepositories(basePackages = {
         "org.cryptotrader.repository"
 })
