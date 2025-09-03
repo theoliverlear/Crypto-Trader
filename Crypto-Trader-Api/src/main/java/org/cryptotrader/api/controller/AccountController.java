c36faaa0a02064ad550e9eb56ff5f304d393fb60
theoliverlear <sigwartholiver2@gmail.com>
2025-09-03 11:06:28 -0500

    Edit AccountController.java to replace `RequestMapping` with specific HTTP method mappings and add JavaDoc comments for clarity

--- Crypto-Trader-Api/src/main/java/org/cryptotrader/api/controller/AccountController.java
+++ Crypto-Trader-Api/src/main/java/org/cryptotrader/api/controller/AccountController.java
@@ -9,10 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.cryptotrader.comm.response.HasProfilePictureResponse;
 import org.cryptotrader.comm.response.OperationSuccessfulResponse;
@@ -22,6 +19,18 @@ import org.cryptotrader.entity.user.ProfilePicture;
 import java.io.IOException;
 import java.util.Optional;
 
+/**
+ * The AccountController class provides RESTful endpoints for managing user
+ * accounts, including functionality for uploading and retrieving profile
+ * pictures, and checking if a user has a profile picture.
+ *
+ * @see org.cryptotrader.api.services.ProfilePictureService
+ * @see org.cryptotrader.api.services.ProductUserService
+ * @see org.cryptotrader.api.services.SessionService
+ * @see org.cryptotrader.entity.user.ProfilePicture
+ * @see org.cryptotrader.entity.user.ProductUser
+ * @author theoliverlear - Oliver Lear Sigwarth
+ */
 @RestController
 @RequestMapping("/api/account")
 public class AccountController {
@@ -41,7 +50,7 @@ public class AccountController {
     //=============================-Methods-==================================
 
     //----------------------------Upload-Image--------------------------------
-    @RequestMapping("/image/upload")
+    @PostMapping("/image/upload")
     public ResponseEntity<OperationSuccessfulResponse> uploadProfilePicture(@RequestParam("file") MultipartFile file,
                                                                             HttpSession session) {
         boolean userInSession = this.sessionService.userInSession(session);
@@ -70,13 +79,32 @@ public class AccountController {
             return new ResponseEntity<>(new OperationSuccessfulResponse(false), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
-    @RequestMapping("/get/{id}/has-profile-picture")
+    @GetMapping("/get/{id}/has-profile-picture")
     public ResponseEntity<HasProfilePictureResponse> hasProfilePicture(@PathVariable String id) {
         boolean hasProfilePicture = this.profilePictureService.existsByUserId(Long.parseLong(id));
         return ResponseEntity.ok(new HasProfilePictureResponse(hasProfilePicture));
     }
+    
+    
+    /**
+     * Retrieves the profile picture for a user given their ID.
+     * If no profile picture is found for the user, a 404 (NOT_FOUND) response
+     * is returned.
+     * If a profile picture exists, the image data is returned with the
+     * appropriate content type.
+     *
+     * @param id the ID of the user whose profile picture is to be retrieved
+     * @return a ResponseEntity containing the profile picture data as a byte
+     *         array with the appropriate HTTP content type if available, or a
+     *         404 NOT_FOUND response if no profile picture is found
+     *
+     * @see org.cryptotrader.entity.user.ProfilePicture
+     * @see org.springframework.http.ResponseEntity
+     *
+     * @author theoliverlear - Oliver Lear Sigwarth
+     */
     @Transactional
-    @RequestMapping("/get/{id}/profile-picture")
+    @GetMapping("/get/{id}/profile-picture")
     public ResponseEntity<byte[]> getProfilePicture(@PathVariable String id) {
         long userId = Long.parseLong(id);
         Optional<ProfilePicture> possibleProfilePicture = this.profilePictureService.findByUserId(userId);
