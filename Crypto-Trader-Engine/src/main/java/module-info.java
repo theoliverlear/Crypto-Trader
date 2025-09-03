8a7cff25928d8ef9d956bd6da54e2e596ea264ca
theoliverlear <sigwartholiver2@gmail.com>
2025-09-02 20:51:40 -0500

    Add module-info.java to define modular structure and dependencies for Crypto-Trader-Engine

---
+++ Crypto-Trader-Engine/src/main/java/module-info.java
@@ -0,0 +1,14 @@
+open module org.cryptotrader.engine {
+    requires spring.boot.autoconfigure;
+    requires spring.context;
+    requires spring.data.jpa;
+    requires spring.boot;
+    requires static lombok;
+    requires org.slf4j;
+    requires org.cryptotrader.api.services;
+    requires spring.beans;
+    requires org.cryptotrader.api.models;
+
+    exports org.cryptotrader.engine;
+    exports org.cryptotrader.engine.services;
+}
