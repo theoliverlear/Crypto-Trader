cbdbf5e41cfe00eeceff1c8be93d86f23d05539c
theoliverlear <sigwartholiver2@gmail.com>
2025-09-02 20:46:41 -0500

    Add CryptoTraderEngineApplication.java to initialize Crypto-Trader-Engine module with Spring Boot configuration

---
+++ Crypto-Trader-Engine/src/main/java/org/cryptotrader/engine/CryptoTraderEngineApplication.java
@@ -0,0 +1,28 @@
+package org.cryptotrader.engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableAsync
+@EnableScheduling
+@EntityScan(basePackages = "org.cryptotrader.entity")
+@ComponentScan(basePackages = {
+        "org.cryptotrader.component",
+        "org.cryptotrader.engine",
+        "org.cryptotrader.api",
+})
+@EnableJpaRepositories(basePackages = {
+        "org.cryptotrader.repository"
+})
+public class CryptoTraderEngineApplication {
+    public static void main(String[] args) {
+        // TODO: Require health check of Crypto-Trader-Data before launch.
+        SpringApplication.run(CryptoTraderEngineApplication.class, args);
+    }
+}
