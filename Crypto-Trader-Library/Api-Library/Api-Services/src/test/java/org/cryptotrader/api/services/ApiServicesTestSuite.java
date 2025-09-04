53d013b92b578bb6d29c2ecbfcc8a43f7e66e8ff
theoliverlear <sigwartholiver2@gmail.com>
2025-09-04 17:17:39 -0500

    Add ApiServicesTestSuite.java to group and run API service tests in Api-Services

---
+++ Crypto-Trader-Library/Api-Library/Api-Services/src/test/java/org/cryptotrader/api/services/ApiServicesTestSuite.java
@@ -0,0 +1,8 @@
+package org.cryptotrader.api.services;
+
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@SelectPackages({"org.cryptotrader.api.services"})
+public class ApiServicesTestSuite { }
