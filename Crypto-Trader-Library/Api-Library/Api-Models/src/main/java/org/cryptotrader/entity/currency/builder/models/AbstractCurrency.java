9942b0cb14896b05b2eac384f8aa0ccfdc2c1cf9
theoliverlear <sigwartholiver2@gmail.com>
2025-09-05 15:13:12 -0500

    Add JavaDoc comments to AbstractCurrency.java to document the class and its builder usage

--- Crypto-Trader-Library/Api-Library/Api-Models/src/main/java/org/cryptotrader/entity/currency/builder/models/AbstractCurrency.java
+++ Crypto-Trader-Library/Api-Library/Api-Models/src/main/java/org/cryptotrader/entity/currency/builder/models/AbstractCurrency.java
@@ -5,6 +5,14 @@ import org.cryptotrader.model.BuilderFactory;
 
 import java.time.LocalDateTime;
 
+/**
+ * Abstract base class for building {@link Currency} instances.
+ *
+ * @param <T> concrete builder type
+ * @see Currency
+ * @see BuilderFactory
+ * @author Oliver Lear Sigwarth (theoliverlear)
+ */
 public abstract class AbstractCurrency implements BuilderFactory<Currency> {
     public abstract AbstractCurrency name(String name);
     public abstract AbstractCurrency currencyCode(String currencyCode);
