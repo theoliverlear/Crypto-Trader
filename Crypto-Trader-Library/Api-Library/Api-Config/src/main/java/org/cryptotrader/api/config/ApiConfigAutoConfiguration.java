ae8a03c86da4fbd889df24bb123ef931bee0043f
theoliverlear <sigwartholiver2@gmail.com>
2025-09-06 20:48:41 -0500

    Add annotation to load default properties in ApiConfigAutoConfiguration.java

--- Crypto-Trader-Library/Api-Library/Api-Config/src/main/java/org/cryptotrader/api/config/ApiConfigAutoConfiguration.java
+++ Crypto-Trader-Library/Api-Library/Api-Config/src/main/java/org/cryptotrader/api/config/ApiConfigAutoConfiguration.java
@@ -2,8 +2,10 @@ package org.cryptotrader.api.config;
 
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
 
 @AutoConfiguration
+@PropertySource(value = "classpath:application-defaults.properties", ignoreResourceNotFound = true)
 @Import({CorsConfig.class, HttpClientConfig.class, SecurityConfig.class})
 public class ApiConfigAutoConfiguration {
 }
