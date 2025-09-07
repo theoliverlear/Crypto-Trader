2c9f4e3952f75acd5f650b49ab2e2a75b97451ba
theoliverlear <sigwartholiver2@gmail.com>
2025-09-07 12:11:02 -0500

    Add constructors to ConfigFile.java for initialization with null values and parameterized creation

--- Crypto-Trader-Library/Version-Library/Version-Models/src/main/java/org/cryptotrader/version/model/config/ConfigFile.java
+++ Crypto-Trader-Library/Version-Library/Version-Models/src/main/java/org/cryptotrader/version/model/config/ConfigFile.java
@@ -12,4 +12,16 @@ public class ConfigFile {
     private ConfigFileType type;
     private Path filePath;
     private Dependency dependency;
+    
+    public ConfigFile() {
+        this.type = null;
+        this.filePath = null;
+        this.dependency = null;
+    }
+    
+    public ConfigFile(ConfigFileType type, Path filePath, Dependency dependency) {
+        this.type = type;
+        this.filePath = filePath;
+        this.dependency = dependency;
+    }
 }
