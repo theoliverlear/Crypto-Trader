e076e621ea556fa9ff41a9dcdd619cca05152d73
theoliverlear <sigwartholiver2@gmail.com>
2025-09-07 17:00:43 -0500

    Add DataPointFetcher.kt to retrieve and map currency history to chart data points in Crypto-Trader-Admin module

---
+++ Crypto-Trader-Admin/src/main/java/org/cryptotrader/admin/component/DataPointFetcher.kt
@@ -0,0 +1,27 @@
+@file:JvmName("DataPointFetcher")
+package org.cryptotrader.admin.component
+
+import org.cryptotrader.admin.models.ChartDataPoint
+import org.cryptotrader.api.services.CurrencyService
+import org.cryptotrader.comm.response.TimeValueResponse
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Component
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.time.Instant
+
+@Component
+class DataPointFetcher(
+    private val currencyService: CurrencyService
+) {
+    fun getLastDayCurrencyHistory(currencyCode: String): List<ChartDataPoint<LocalDateTime, Double>> {
+        val response: List<TimeValueResponse> = this.currencyService.getCurrencyHistory(currencyCode, 24, 300)
+        val asChartDataPoint = response.map {
+            ChartDataPoint(
+                Instant.parse(it.timestamp).atZone(ZoneId.systemDefault()).toLocalDateTime(),
+                it.value
+            )
+        }
+        return asChartDataPoint
+    }
+}
