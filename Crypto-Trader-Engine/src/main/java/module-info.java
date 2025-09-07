86731cb965b873450cc89539064120f4171119ad
theoliverlear <sigwartholiver2@gmail.com>
2025-09-06 21:05:01 -0500

    Add `spring.aop` and `org.cryptotrader.health` dependencies to module-info.java in Crypto-Trader-Engine

--- Crypto-Trader-Engine/src/main/java/module-info.java
+++ Crypto-Trader-Engine/src/main/java/module-info.java
@@ -1,17 +1,21 @@
 open module org.cryptotrader.engine {
-    requires spring.boot.autoconfigure;
+    requires spring.core;
+    requires spring.beans;
     requires spring.context;
+    requires spring.aop;
+
+    requires spring.boot.autoconfigure;
     requires spring.data.jpa;
     requires spring.boot;
     requires static lombok;
     requires org.slf4j;
     requires org.cryptotrader.api.services;
-    requires spring.beans;
     requires org.cryptotrader.api.models;
     requires spring.security.config;
     requires spring.security.crypto;
     requires spring.security.web;
     requires kotlin.stdlib;
+    requires org.cryptotrader.health;
 
     exports org.cryptotrader.engine;
     exports org.cryptotrader.engine.services;
