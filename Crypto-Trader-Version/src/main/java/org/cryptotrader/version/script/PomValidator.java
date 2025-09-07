eab53ca186718daae68714117d93122dfd2d4cc9
theoliverlear <sigwartholiver2@gmail.com>
2025-09-07 12:23:29 -0500

    Refactor PomValidator.java to replace `CryptoTraderModules` with `ModuleLibrary` and add `getAllPomPairs()` method to meet new application parameter requirements

--- Crypto-Trader-Version/src/main/java/org/cryptotrader/version/script/PomValidator.java
+++ Crypto-Trader-Version/src/main/java/org/cryptotrader/version/script/PomValidator.java
@@ -1,23 +1,38 @@
 package org.cryptotrader.version.script;
 
 import org.cryptotrader.version.model.element.PomElement;
-import org.cryptotrader.version.model.module.CryptoTraderModules;
+import org.cryptotrader.version.model.element.PomPair;
+import org.cryptotrader.version.model.module.ModuleLibrary;
 import org.cryptotrader.version.model.module.type.Pom;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class PomValidator {
-
     private static final List<PomElement> allPoms = PomParser.getAllPoms();
-    public static boolean cryptoTraderParentMatches() {
-        List<PomPair> pomPairs = List.of(
-                new PomPair(getByModule(CryptoTraderModules.CRYPTO_TRADER), CryptoTraderModules.CRYPTO_TRADER),
-                new PomPair(getByModule(CryptoTraderModules.LIBRARY), CryptoTraderModules.LIBRARY),
-                new PomPair(getByModule(CryptoTraderModules.API_LIBRARY), CryptoTraderModules.API_LIBRARY),
-                new PomPair(getByModule(CryptoTraderModules.DESKTOP_LIBRARY), CryptoTraderModules.DESKTOP_LIBRARY),
-                new PomPair(getByModule(CryptoTraderModules.EXTERNALS_LIBRARY), CryptoTraderModules.EXTERNALS_LIBRARY),
-                new PomPair(getByModule(CryptoTraderModules.VERSION_LIBRARY), CryptoTraderModules.VERSION_LIBRARY)
-        );
+    private static final List<PomPair> allPomPairs = getAllPomPairs();
+    
+    
+    public static List<PomPair> getAllPomPairs() {
+        
+        List<PomPair> pomPairs = allPoms.stream().map(pomElement -> {
+            String name = pomElement.getPom().getModuleDependency().getName();
+            // These will likely be important later. Deprecated currently.
+            boolean rootChild = Arrays.asList(ModuleLibrary.cryptoTraderParent).contains(name);
+            boolean libraryChild = Arrays.asList(ModuleLibrary.libraryParent).contains(name);
+            ModuleLibrary libraryModule = ModuleLibrary.modules.stream()
+                    .filter(module -> module.getName().equals(name))
+                    .findFirst().orElseThrow(() -> new IllegalStateException("Module not found: " + name));
+            return new PomPair(pomElement, libraryModule);
+        }).toList();
+        return pomPairs;
+    }
+    
+    public static boolean allMatch() {
+        return parentMatches(allPomPairs);
+    }
+    
+    public static boolean parentMatches(List<PomPair> pomPairs) {
         boolean allMatch = pomPairs.stream().allMatch(pomPair -> {
             boolean parentMatches = parentMatches(pomPair.element(), pomPair.module());
             System.out.println(pomPair.module() + " parent matches: " + parentMatches);
@@ -26,19 +41,19 @@ public class PomValidator {
         return allMatch;
     }
 
-    private static PomElement getByModule(CryptoTraderModules module) {
+    private static PomElement getByModule(ModuleLibrary module) {
         PomElement element = allPoms.stream()
-                .filter(pomElement -> pomElement.getPom().getModuleType() == module)
+                .filter(pomElement -> pomElement.getPom().getModuleType().equals(module))
                 .filter(pomElement -> "pom".equalsIgnoreCase(pomElement.getPackaging()))
                 .findFirst()
                 .orElseGet(() -> allPoms.stream()
-                        .filter(pomElement -> pomElement.getPom().getModuleType() == module)
+                        .filter(pomElement -> pomElement.getPom().getModuleType().equals(module))
                         .findFirst()
                         .orElse(null));
         return element;
     }
 
-    public static boolean parentMatches(PomElement topPom, CryptoTraderModules module) {
+    public static boolean parentMatches(PomElement topPom, ModuleLibrary module) {
         if (topPom == null) {
             return false;
         }
@@ -48,7 +63,7 @@ public class PomValidator {
         for (PomElement pomElement : allPoms) {
             Pom parentPom = pomElement.getParentPomModel();
             if (parentPom != null) {
-                if (parentPom.getModuleType() == module) {
+                if (parentPom.getModuleType().equals(module)) {
                     String parentVersion = parentPom.getModuleDependency().getVersion();
                     if (!nullToEmpty(parentVersion).equals(nullToEmpty(topVersion))) {
                         System.out.println("Found version: " + parentVersion);
@@ -59,7 +74,6 @@ public class PomValidator {
         }
         return allMatch;
     }
-    private record PomPair(PomElement element, CryptoTraderModules module) { }
 
     private static String nullToEmpty(String text) {
         return text == null ? "" : text;
