265d575bad47617bad9c4227610ae03bdf8bb2a2
theoliverlear <sigwartholiver2@gmail.com>
2025-09-07 12:18:29 -0500

    Refactor Module.java into ProgramModule.java to adopt updated naming convention and adjust variables to use new naming convention

--- Crypto-Trader-Library/Version-Library/Version-Models/src/main/java/org/cryptotrader/version/model/module/ProgramModule.java
+++ Crypto-Trader-Library/Version-Library/Version-Models/src/main/java/org/cryptotrader/version/model/module/ProgramModule.java
@@ -1,9 +1,6 @@
 package org.cryptotrader.version.model.module;
 
-import lombok.Data;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.cryptotrader.version.model.config.ConfigFileType;
 
 import java.nio.file.Path;
@@ -12,16 +9,17 @@ import java.nio.file.Path;
 @Setter
 @Data
 @NoArgsConstructor
-public class Module {
-    private CryptoTraderModules moduleType;
+@AllArgsConstructor
+public class ProgramModule {
+    private ModuleLibrary moduleType;
     private Path modulePath;
     private ConfigFileType configFileType;
     private String name;
     private String version;
 
-    public Module(CryptoTraderModules moduleType,
-                  Path modulePath,
-                  ConfigFileType configFileType) {
+    public ProgramModule(ModuleLibrary moduleType,
+                         Path modulePath,
+                         ConfigFileType configFileType) {
         this.moduleType = moduleType;
         this.modulePath = modulePath;
         this.configFileType = configFileType;
