90de0ba28834bf93c4607b0d2df6d597128ec6d1
theoliverlear <sigwartholiver2@gmail.com>
2025-09-06 21:07:37 -0500

    Add CryptoTraderService.kt to define services with ports and standardized naming

---
+++ Crypto-Trader-Health/src/main/kotlin/org/cryptotrader/health/models/CryptoTraderService.kt
@@ -0,0 +1,18 @@
+@file:JvmName("CryptoTraderService")
+package org.cryptotrader.health.models
+
+enum class CryptoTraderService(val port: Int) {
+    ANALYSIS(8000),
+    API(8080),
+    DATA(8085),
+    ENGINE(8086);
+
+    companion object {
+        const val PREFIX = "Crypto Trader"
+    }
+    
+    override fun toString(): String {
+        val normalizedName = this.name.lowercase().replaceFirstChar { it.uppercase() }
+        return "$PREFIX $normalizedName"
+    }
+}
