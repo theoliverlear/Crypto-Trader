357399f1834259d58f955fede79aec31429b8525
theoliverlear <sigwartholiver2@gmail.com>
2025-09-07 12:17:13 -0500

    Refactor ModuleDescendent.java into ProgramModuleDescendent.java to align with updated naming conventions in Version-Models

---
+++ Crypto-Trader-Library/Version-Library/Version-Models/src/main/java/org/cryptotrader/version/model/module/ProgramModuleDescendent.java
@@ -0,0 +1,32 @@
+package org.cryptotrader.version.model.module;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.cryptotrader.version.model.config.ConfigFileType;
+
+import java.nio.file.Path;
+
+@NoArgsConstructor
+@Getter
+public class ProgramModuleDescendent extends ProgramModule {
+    private ProgramModule parent;
+    private ProgramModule child;
+
+    public ProgramModuleDescendent(ModuleLibrary moduleType,
+                                   Path modulePath,
+                                   ConfigFileType configFileType) {
+        super(moduleType, modulePath, configFileType);
+        this.parent = null;
+        this.child = null;
+    }
+    
+    public ProgramModuleDescendent(ModuleLibrary moduleType,
+                                   Path modulePath,
+                                   ConfigFileType configFileType,
+                                   ProgramModule parent,
+                                   ProgramModule child) {
+        super(moduleType, modulePath, configFileType);
+        this.parent = parent;
+        this.child = child;
+    }
+}
