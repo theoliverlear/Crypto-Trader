dc7ed345e1d7aad0add244acf967c3ef83c88d0d
theoliverlear <sigwartholiver2@gmail.com>
2025-09-07 16:43:44 -0500

    Add CryptoTraderContactApplication.kt to bootstrap Crypto-Trader-Contact module with Spring Boot

---
+++ Crypto-Trader-Contact/src/main/kotlin/org/cryptotrader/contact/CryptoTraderContactApplication.kt
@@ -0,0 +1,11 @@
+package org.cryptotrader.contact
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+open class CryptoTraderContactApplication
+
+fun main(args: Array<String>) {
+    runApplication<CryptoTraderContactApplication>(*args)
+}
