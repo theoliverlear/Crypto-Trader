8e645dcf671dc6314397eab76cc4b2c23e26d4e2
theoliverlear <sigwartholiver2@gmail.com>
2025-09-07 17:01:16 -0500

    Expand SpringBootConfig.java to enable JPA repositories and entity scanning for seamless database integration within Crypto-Trader-Admin module

--- Crypto-Trader-Admin/src/main/java/org/cryptotrader/admin/config/SpringBootConfig.java
+++ Crypto-Trader-Admin/src/main/java/org/cryptotrader/admin/config/SpringBootConfig.java
@@ -2,13 +2,21 @@ package org.cryptotrader.admin.config;
 
 import org.cryptotrader.desktop.component.config.SpringContext;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"org.cryptotrader.admin", 
-                               "org.cryptotrader.desktop.component"})
-@Import(SpringContext.class)
+@ComponentScan(basePackages = {"org.cryptotrader.admin",
+        "org.cryptotrader.api.services",
+        "org.cryptotrader.component",
+        "org.cryptotrader.desktop.component"})
+@EnableJpaRepositories(basePackages = {
+        "org.cryptotrader.repository"
+})
+@EntityScan(basePackages = "org.cryptotrader.entity")
 public class SpringBootConfig {
-    
+
 }
