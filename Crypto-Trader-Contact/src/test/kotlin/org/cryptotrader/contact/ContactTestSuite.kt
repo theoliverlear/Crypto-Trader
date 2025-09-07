6a625993931b4d99b8281ca8a795cc5f08989bc4
theoliverlear <sigwartholiver2@gmail.com>
2025-09-07 16:39:59 -0500

    Add ContactTestSuite.kt to organize and execute module tests in Crypto-Trader-Contact module

---
+++ Crypto-Trader-Contact/src/test/kotlin/org/cryptotrader/contact/ContactTestSuite.kt
@@ -0,0 +1,8 @@
+package org.cryptotrader.contact
+
+import org.junit.platform.suite.api.SelectPackages
+import org.junit.platform.suite.api.Suite
+
+@Suite
+@SelectPackages("org.cryptotrader.contact")
+class ContactTestSuite { }
