de44374d1e81146b8b2e4c9ab429fdd83fda1fe0
theoliverlear <sigwartholiver2@gmail.com>
2025-09-07 17:15:24 -0500

    Update SpringContext.java to specify itself as a custom component in Desktop-Components

--- Crypto-Trader-Library/Desktop-Library/Desktop-Components/src/main/java/org/cryptotrader/desktop/component/config/SpringContext.java
+++ Crypto-Trader-Library/Desktop-Library/Desktop-Components/src/main/java/org/cryptotrader/desktop/component/config/SpringContext.java
@@ -5,7 +5,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
-@Component
+@Component("desktopSpringContext")
 public class SpringContext implements ApplicationContextAware {
     private static ApplicationContext context;
 
