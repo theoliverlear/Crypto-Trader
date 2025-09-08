512f1508b6916c6c54999876a332f3d3e69b7036
theoliverlear <sigwartholiver2@gmail.com>
2025-09-07 20:46:45 -0500

    Refactor EmailController.kt to use request body for email requests, replace `println` with `KotlinLogging`, and update response model in Crypto-Trader-Contact module

--- Crypto-Trader-Contact/src/main/kotlin/org/cryptotrader/contact/controller/EmailController.kt
+++ Crypto-Trader-Contact/src/main/kotlin/org/cryptotrader/contact/controller/EmailController.kt
@@ -1,30 +1,33 @@
-@file:JvmName("EmailController")
 package org.cryptotrader.contact.controller
 
+import io.github.oshai.kotlinlogging.KotlinLogging
+import org.cryptotrader.contact.comm.email.request.EmailRequest
 import org.cryptotrader.contact.comm.email.response.EmailResponse
 import org.cryptotrader.contact.service.email.EmailService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+
+val log = KotlinLogging.logger {  }
 
 @RestController
 @RequestMapping("/contact/email")
 class EmailController(
     private val emailService: EmailService
 ) {
-    @GetMapping("")
-    fun sendEmail(): ResponseEntity<EmailResponse> {
-        println("Sending email...")
+    @GetMapping("/send")
+    fun sendEmail(@RequestBody emailRequest: EmailRequest): ResponseEntity<EmailResponse> {
+        log.info { "Sending email..." }
         try {
-            val response = EmailResponse("Testing Crypto Trader", "Hello from Spring Boot in Kotlin", "sigwarthsoftware@gmail.com")
-            this.emailService.send(response)
-            println("Email sent.")
+            this.emailService.send(emailRequest)
+            log.info { "Email sent." }
         } catch (e: Exception) {
-            println("Error sending email: ${e.message}")
-            return ResponseEntity<EmailResponse>(HttpStatus.INTERNAL_SERVER_ERROR)
+            log.error { "Error sending email: ${e.message}" }
+            return ResponseEntity<EmailResponse>(EmailResponse(false), HttpStatus.INTERNAL_SERVER_ERROR)
         }
-        return ResponseEntity<EmailResponse>(HttpStatus.OK)
+        return ResponseEntity<EmailResponse>(EmailResponse(true), HttpStatus.OK)
     }
 }
