d2e6a72da784d19e9e29f05d39f07b2b8c3fcb9d
theoliverlear <sigwartholiver2@gmail.com>
2025-09-08 20:40:12 -0500

    Add MainViewModel.kt to handle UI-related data in Crypto-Trader-Mobile with Hilt integration

---
+++ Crypto-Trader-Mobile/src/main/kotlin/org/cryptotrader/mobile/MainViewModel.kt
@@ -0,0 +1,10 @@
+package org.cryptotrader.mobile
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor() : ViewModel() {
+    val greeting: String = "Hello Crypto Trader Mobile"
+}
