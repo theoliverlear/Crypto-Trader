642380ced05d9138e5c7336cdec3229056d76505
theoliverlear <sigwartholiver2@gmail.com>
2025-09-09 10:01:23 -0500

    Add Spring Data JPA and related Spring dependencies in module-info.java for Contact-Models module

--- Crypto-Trader-Library/Contact-Library/Contact-Models/src/main/java/module-info.java
+++ Crypto-Trader-Library/Contact-Library/Contact-Models/src/main/java/module-info.java
@@ -3,6 +3,11 @@ module org.cryptotrader.contact.models {
     requires jakarta.persistence;
     requires org.cryptotrader.api.models;
     requires static lombok;
+    requires spring.data.jpa;
+    requires spring.data.commons;
+    requires spring.core;
+    requires spring.beans;
+    requires spring.context;
 
     exports org.cryptotrader.contact.entity;
 }
