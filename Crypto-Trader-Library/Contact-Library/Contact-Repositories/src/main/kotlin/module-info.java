12a6487059d1512f1cd6ed7ba8940179f1b6a3e1
theoliverlear <sigwartholiver2@gmail.com>
2025-09-09 10:02:02 -0500

    Add Spring dependencies and Jakarta Persistence in module-info.java for Contact-Repositories module

--- Crypto-Trader-Library/Contact-Library/Contact-Repositories/src/main/kotlin/module-info.java
+++ Crypto-Trader-Library/Contact-Library/Contact-Repositories/src/main/kotlin/module-info.java
@@ -1,6 +1,12 @@
 open module org.cryptotrader.repositories {
     requires org.cryptotrader.contact.models;
     requires kotlin.stdlib;
+    requires spring.data.jpa;
+    requires spring.data.commons;
+    requires spring.core;
+    requires spring.beans;
+    requires spring.context;
+    requires jakarta.persistence;
     
     exports org.cryptotrader.repository;
 }
