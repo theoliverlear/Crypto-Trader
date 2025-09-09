4404627a3499a175917498847fd6155db727432c
theoliverlear <sigwartholiver2@gmail.com>
2025-09-08 20:40:43 -0500

    Edit build.gradle.kts to add new plugins, dependencies, and configurations for Hilt, Ktor, Room, and Compose integration

--- Crypto-Trader-Mobile/build.gradle.kts
+++ Crypto-Trader-Mobile/build.gradle.kts
@@ -2,6 +2,9 @@ plugins {
     id("com.android.application") version "8.6.1"
     kotlin("android") version "2.0.20"
     id("org.jetbrains.kotlin.plugin.compose") version "2.0.20"
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.0.20"
+    id("com.google.dagger.hilt.android") version "2.52"
+    id("com.google.devtools.ksp") version "2.0.20-1.0.24"
 }
 
 kotlin { jvmToolchain(21) }
@@ -23,24 +26,84 @@ android {
     buildFeatures { compose = true }
     composeOptions { kotlinCompilerExtensionVersion = "1.5.15" }
 
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_21
         targetCompatibility = JavaVersion.VERSION_21
     }
     kotlinOptions { jvmTarget = "21" }
+    buildToolsVersion = "36.0.0"
 }
+
+val ktorVersion = "2.3.12"
+val roomVersion = "2.6.1"
+val accompanistVersion = "0.36.0"
+val vicoVersion = "2.0.0"
+
 //noinspection GradleDependency
 dependencies {
+    // Compose BOM and core UI
     implementation(platform("androidx.compose:compose-bom:2024.09.01"))
     implementation("androidx.activity:activity-compose:1.9.2")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.navigation:navigation-compose:2.8.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.5")
 
+    // Images, animations, charts
+    implementation("io.coil-kt:coil-compose:2.6.0")
+    implementation("com.airbnb.android:lottie-compose:6.5.2")
+    implementation("com.patrykandpatrick.vico:compose:$vicoVersion")
+
+    // Accompanist
+    implementation("com.google.accompanist:accompanist-systemuicontroller:$accompanistVersion")
+    implementation("com.google.accompanist:accompanist-permissions:$accompanistVersion")
+    implementation("com.google.accompanist:accompanist-placeholder-material:$accompanistVersion")
+
+    // Coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
+
+    // Paging
+    implementation("androidx.paging:paging-runtime-ktx:3.3.2")
+    implementation("androidx.paging:paging-compose:3.3.2")
+
+    // Ktor Client
+    implementation("io.ktor:ktor-client-okhttp:$ktorVersion")
+    implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
+    implementation("io.ktor:ktor-client-logging:$ktorVersion")
+    implementation("io.ktor:ktor-client-websockets:$ktorVersion")
+
+    // Room
+    implementation("androidx.room:room-ktx:$roomVersion")
+    ksp("androidx.room:room-compiler:$roomVersion")
+
+    // DataStore
+    implementation("androidx.datastore:datastore-preferences:1.1.1")
+
+    // Hilt
+    implementation("com.google.dagger:hilt-android:2.52")
+    ksp("com.google.dagger:hilt-compiler:2.52")
+    implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
+
+    // Baseline Profile Installer
+    implementation("androidx.profileinstaller:profileinstaller:1.4.1")
+
+    // Testing
     testImplementation("junit:junit:4.13.2")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.9.0")
+    testImplementation("app.cash.turbine:turbine:1.1.0")
+    testImplementation("io.mockk:mockk:1.13.12")
+
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
     androidTestImplementation(platform("androidx.compose:compose-bom:2024.09.01"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
 }
