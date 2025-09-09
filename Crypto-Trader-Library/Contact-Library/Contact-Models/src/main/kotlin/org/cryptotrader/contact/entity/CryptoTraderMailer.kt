477bb1a9ff215d683e577a12add6a68771966f38
theoliverlear <sigwartholiver2@gmail.com>
2025-09-08 22:30:46 -0500

    Add CryptoTraderMailer.kt to define email addresses used by Crypto Trader

---
+++ Crypto-Trader-Library/Contact-Library/Contact-Models/src/main/kotlin/org/cryptotrader/contact/entity/CryptoTraderMailer.kt
@@ -0,0 +1,7 @@
+package org.cryptotrader.contact.entity
+
+enum class CryptoTraderMailer(address: String) {
+    CONTACT("contact@sscryptotrader.com"),
+    NO_REPLY("no-reply@sscryptotrader.com"),
+    SUPPORT("support@sscryptotrader.com");
+}
