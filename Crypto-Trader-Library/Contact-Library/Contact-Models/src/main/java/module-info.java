8024aaea32a91136db907054e39473abc513b9d7
theoliverlear <sigwartholiver2@gmail.com>
2025-09-08 22:28:56 -0500

    Edit module-info.java to add necessary module dependencies and export `entity` package in Contact-Models module

--- Crypto-Trader-Library/Contact-Library/Contact-Models/src/main/java/module-info.java
+++ Crypto-Trader-Library/Contact-Library/Contact-Models/src/main/java/module-info.java
@@ -1,3 +1,7 @@
 module org.cryptotrader.contact.models {
+    requires jakarta.persistence;
+    requires org.cryptotrader.api.models;
+    requires static lombok;
 
+    exports org.cryptotrader.contact.entity;
 }
