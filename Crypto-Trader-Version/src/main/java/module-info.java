58fc574e55fdc42b0e3a8dad0b3320902d17b5b9
theoliverlear <sigwartholiver2@gmail.com>
2025-09-09 21:46:19 -0500

    Edit Crypto-Trader-Version module to update imports and dependency definitions to align with new namespace definitions

--- Crypto-Trader-Version/src/main/java/module-info.java
+++ Crypto-Trader-Version/src/main/java/module-info.java
@@ -3,7 +3,7 @@ open module org.cryptotrader.version {
     requires org.jdom2;
     requires org.slf4j;
     requires java.xml;
-    requires org.cryptotrader.version.model;
+    requires org.cryptotrader.version.library.model;
     requires org.apache.commons.io;
     requires org.apache.httpcomponents.httpcore;
     requires org.apache.httpcomponents.httpclient;
