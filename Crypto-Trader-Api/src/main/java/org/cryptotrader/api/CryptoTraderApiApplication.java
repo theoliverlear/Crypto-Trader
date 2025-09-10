e3f9666d13bd35d9b23c77e6155a4b404b93e3db
theoliverlear <sigwartholiver2@gmail.com>
2025-09-09 22:31:43 -0500

    Edit CryptoTraderApiApplication.java to update package paths for entities, components, services, and repositories to align with new library namespace definitions

--- Crypto-Trader-Api/src/main/java/org/cryptotrader/api/CryptoTraderApiApplication.java
+++ Crypto-Trader-Api/src/main/java/org/cryptotrader/api/CryptoTraderApiApplication.java
@@ -4,23 +4,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.cryptotrader.component.CurrencyJsonGenerator;
 
 @SpringBootApplication
 @EnableAsync
 @EnableScheduling
-@EntityScan(basePackages = "org.cryptotrader.entity")
+@EntityScan(basePackages = "org.cryptotrader.api.library.entity")
 @ComponentScan(basePackages = {
         "org.cryptotrader.api",
-        "org.cryptotrader.component",
-        "org.cryptotrader.api.services",
+        "org.cryptotrader.api.library.component",
+        "org.cryptotrader.api.library.services",
 })
 @EnableJpaRepositories(basePackages = {
-        "org.cryptotrader.repository"
+        "org.cryptotrader.api.library.repository"
 })
 public class CryptoTraderApiApplication {
     public static void main(String[] args) {
