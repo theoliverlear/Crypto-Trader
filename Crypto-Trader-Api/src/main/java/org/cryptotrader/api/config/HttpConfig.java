24972bf8bbb2c294c957816ee43ecd63eeceb965
theoliverlear <sigwartholiver2@gmail.com>
2025-09-09 22:01:02 -0500

    Remove `RestTemplate` bean definition from HttpConfig.java as it is imported from Api-Config in Crypto-Trader-Api

--- Crypto-Trader-Api/src/main/java/org/cryptotrader/api/config/HttpConfig.java
+++ Crypto-Trader-Api/src/main/java/org/cryptotrader/api/config/HttpConfig.java
@@ -6,9 +6,5 @@ import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class HttpConfig {
-
-    @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
+    
 }
