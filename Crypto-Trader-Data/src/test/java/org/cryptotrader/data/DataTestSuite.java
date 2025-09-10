8428e4c1ce29d88e535fdd29529899eb36be3a85
theoliverlear <sigwartholiver2@gmail.com>
2025-09-10 08:00:46 -0500

    Add DataTestSuite.java to group and run all tests in the Crypto-Trader-Data module

---
+++ Crypto-Trader-Data/src/test/java/org/cryptotrader/data/DataTestSuite.java
@@ -0,0 +1,8 @@
+package org.cryptotrader.data;
+
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@SelectPackages({"org.cryptotrader.data"})
+public class DataTestSuite { }
