c4cb52e7206a4054f9d64a227b8cc30693bac2b2
theoliverlear <sigwartholiver2@gmail.com>
2025-09-11 18:34:36 -0500

    Edit ApiConfigAutoConfiguration.java to replace `PropertySource` with `DefaultPropertiesConfig` import in Api-Config

--- Crypto-Trader-Library/Api-Library/Api-Config/src/main/java/org/cryptotrader/api/library/config/ApiConfigAutoConfiguration.java
+++ Crypto-Trader-Library/Api-Library/Api-Config/src/main/java/org/cryptotrader/api/library/config/ApiConfigAutoConfiguration.java
@@ -2,10 +2,8 @@ package org.cryptotrader.api.library.config;
 
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.PropertySource;
 
 @AutoConfiguration
-@PropertySource(value = "classpath:application-defaults.properties", ignoreResourceNotFound = true)
-@Import({ApplicationYamlConfig.class, CorsConfig.class, HttpClientConfig.class, SecurityConfig.class, EventPublisher.class, YamlPropertySourceFactory.class})
+@Import({DefaultPropertiesConfig.class, CorsConfig.class, HttpClientConfig.class, SecurityConfig.class, EventPublisher.class})
 public class ApiConfigAutoConfiguration {
 }
