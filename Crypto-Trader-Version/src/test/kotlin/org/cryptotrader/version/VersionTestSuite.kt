f8e5b154d67af1cfde7a60356bf5d819e1dd1524
theoliverlear <sigwartholiver2@gmail.com>
2025-09-12 09:38:23 -0500

    Add VersionTestSuite.kt to run all tests in the Crypto-Trader-Version module

---
+++ Crypto-Trader-Version/src/test/kotlin/org/cryptotrader/version/VersionTestSuite.kt
@@ -0,0 +1,8 @@
+package org.cryptotrader.version
+
+import org.junit.platform.suite.api.SelectPackages
+import org.junit.platform.suite.api.Suite
+
+@Suite
+@SelectPackages("org.cryptotrader.version")
+class VersionTestSuite
