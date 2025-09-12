639ce35aceb4de8645ea9e0035d028e472ab210e
theoliverlear <sigwartholiver2@gmail.com>
2025-09-12 15:58:57 -0500

    Add auth-aliases.kt to define type aliases for authentication-related payloads in Api-Communication module

---
+++ Crypto-Trader-Library/Api-Library/Api-Communication/src/main/java/org/cryptotrader/api/library/comm/request/alias/auth-aliases.kt
@@ -0,0 +1,8 @@
+@file:JvmName("AuthAliases")
+package org.cryptotrader.api.library.comm.request.alias
+
+import org.cryptotrader.api.library.comm.response.AuthResponse
+import org.cryptotrader.api.library.model.http.PayloadStatusResponse
+//
+//
+typealias HttpAuthStatus = PayloadStatusResponse<AuthResponse>
