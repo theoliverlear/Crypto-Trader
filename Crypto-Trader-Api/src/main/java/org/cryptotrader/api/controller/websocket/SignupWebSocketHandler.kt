6c427255ae35eb8043d60a78700fcc6d6e83bd94
theoliverlear <sigwartholiver2@gmail.com>
2025-09-12 15:55:22 -0500

    Update SignupWebSocketHandler.kt to replace `UserRequest` with `SignupRequest` and refactor to use alias types

--- Crypto-Trader-Api/src/main/java/org/cryptotrader/api/controller/websocket/SignupWebSocketHandler.kt
+++ Crypto-Trader-Api/src/main/java/org/cryptotrader/api/controller/websocket/SignupWebSocketHandler.kt
@@ -2,22 +2,22 @@ package org.cryptotrader.api.controller.websocket
 
 import com.sigwarthsoftware.springboot.websocket.WebSocketHandler
 import io.github.oshai.kotlinlogging.KotlinLogging
-import org.cryptotrader.api.library.comm.request.UserRequest
+import org.cryptotrader.api.library.comm.request.SignupRequest
+import org.cryptotrader.api.library.comm.request.alias.HttpAuthStatus
 import org.cryptotrader.api.library.comm.response.AuthResponse
-import org.cryptotrader.api.library.model.http.PayloadStatusResponse
 import org.cryptotrader.api.library.services.AuthService
 import org.springframework.stereotype.Component
 
-val log = KotlinLogging.logger {  }
+private val log = KotlinLogging.logger {  }
 
 @Component
 class SignupWebSocketHandler(
     private val authService: AuthService
-) : WebSocketHandler<UserRequest, AuthResponse>() {
+) : WebSocketHandler<SignupRequest, AuthResponse>() {
     
-    override fun makeResponse(userRequest: UserRequest): AuthResponse {
-        log.info { "Signup: ${userRequest.email}" }
-        val response: PayloadStatusResponse<AuthResponse> = authService.signup(userRequest)
+    override fun makeResponse(signupRequest: SignupRequest): AuthResponse {
+        log.info { "Signup: ${signupRequest.email}" }
+        val response: HttpAuthStatus = this.authService.signup(signupRequest)
         return AuthResponse(response.payload.isAuthorized)
     }
 }
