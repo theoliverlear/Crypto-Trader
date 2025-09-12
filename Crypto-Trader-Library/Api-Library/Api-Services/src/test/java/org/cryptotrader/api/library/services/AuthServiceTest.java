7524a43d2b3af441818e087296bcfbb0f3ba9fb3
theoliverlear <sigwartholiver2@gmail.com>
2025-09-12 16:14:27 -0500

    Refactor AuthServiceTest.java to use `LoginRequest` and `SignupRequest` for improved clarity and alignment with `AuthService` changes

--- Crypto-Trader-Library/Api-Library/Api-Services/src/test/java/org/cryptotrader/api/library/services/AuthServiceTest.java
+++ Crypto-Trader-Library/Api-Library/Api-Services/src/test/java/org/cryptotrader/api/library/services/AuthServiceTest.java
@@ -1,5 +1,7 @@
 package org.cryptotrader.api.library.services;
 
+import org.cryptotrader.api.library.comm.request.LoginRequest;
+import org.cryptotrader.api.library.comm.request.SignupRequest;
 import org.cryptotrader.api.library.events.publisher.UserEventsPublisher;
 import org.cryptotrader.api.library.comm.request.UserRequest;
 import org.cryptotrader.api.library.comm.response.AuthResponse;
@@ -33,16 +35,29 @@ public class AuthServiceTest extends CryptoTraderTest {
     @Mock
     private UserEventsPublisher userEventsPublisher;
     
+    private LoginRequest loginRequest;
+    
+    private SignupRequest signupRequest;
+    
     private UserRequest userRequest;
     
     private ProductUser user;
     
+    private String testUsername;
+    private String testEmail;
+    private String testPassword;
+    
     @BeforeEach
     void setup() {
-        SafePassword clientSideHashedPassword = new SafePassword("password");
+        this.testUsername = "Ollie";
+        this.testEmail = "ollie@ollie.com";
+        this.testPassword = "password";
+        SafePassword clientSideHashedPassword = new SafePassword(this.testPassword);
         String encodedPassword = clientSideHashedPassword.getEncodedPassword();
-        this.userRequest = new UserRequest("Ollie", "ollie@ollie.com", encodedPassword);
-        this.user = new ProductUser("Ollie", new SafePassword("password"));
+        this.userRequest = new UserRequest(this.testUsername, this.testEmail, encodedPassword);
+        this.loginRequest = new LoginRequest(this.testEmail, encodedPassword);
+        this.signupRequest = new SignupRequest(this.testEmail, this.testUsername, encodedPassword);
+        this.user = new ProductUser(this.testUsername, new SafePassword(this.testPassword));
     }
     
     @AfterAll
@@ -63,10 +78,10 @@ public class AuthServiceTest extends CryptoTraderTest {
     @Test
     @DisplayName("Should sign up users with valid requests")
     public void signup_SignUpUsers_WithValidRequests() {
-        when(this.productUserService.userExistsByUsername("Ollie")).thenReturn(false);
+        when(this.productUserService.userExistsByUsername(this.testUsername)).thenReturn(false);
         AuthResponse expectedResponse = new AuthResponse(true);
-        PayloadStatusResponse<AuthResponse> actualResponse = this.authService.signup(this.userRequest);
-        verify(this.productUserService).userExistsByUsername("Ollie");
+        PayloadStatusResponse<AuthResponse> actualResponse = this.authService.signup(this.signupRequest);
+        verify(this.productUserService).userExistsByUsername(this.testUsername);
         AuthResponse actualAuthResponse = actualResponse.getPayload();
         assertEquals(expectedResponse, actualAuthResponse);
     }
@@ -74,9 +89,9 @@ public class AuthServiceTest extends CryptoTraderTest {
     @Test
     @DisplayName("Should not sign up existing users")
     public void signup_NotSignUp_WhenUserExists() {
-        when(this.productUserService.userExistsByUsername("Ollie")).thenReturn(true);
+        when(this.productUserService.userExistsByUsername(this.testUsername)).thenReturn(true);
         AuthResponse expectedResponse = new AuthResponse(false);
-        PayloadStatusResponse<AuthResponse> actualResponse = this.authService.signup(this.userRequest);
+        PayloadStatusResponse<AuthResponse> actualResponse = this.authService.signup(this.signupRequest);
         AuthResponse actualAuthResponse = actualResponse.getPayload();
         assertEquals(expectedResponse, actualAuthResponse);
     }
@@ -84,11 +99,11 @@ public class AuthServiceTest extends CryptoTraderTest {
     @Test
     @DisplayName("Should login users with valid requests")
     public void login_LoginUsers_WithValidRequests() {
-        when(this.productUserService.getUserByUsername("Ollie")).thenReturn(this.user);
+        when(this.productUserService.getUserByEmail(this.testEmail)).thenReturn(this.user);
         AuthResponse expectedResponse = new AuthResponse(true);
         when(this.productUserService.comparePassword(this.user, this.userRequest.getPassword())).thenReturn(true);
-        PayloadStatusResponse<AuthResponse> actualResponse = this.authService.login(this.userRequest);
-        verify(this.productUserService).getUserByUsername("Ollie");
+        PayloadStatusResponse<AuthResponse> actualResponse = this.authService.login(this.loginRequest);
+        verify(this.productUserService).getUserByEmail(this.testEmail);
         verify(this.productUserService).comparePassword(this.user, this.userRequest.getPassword());
         assertEquals(expectedResponse, actualResponse.getPayload());
     }
@@ -96,21 +111,21 @@ public class AuthServiceTest extends CryptoTraderTest {
     @Test
     @DisplayName("Should not login users with invalid requests")
     public void login_NotLoginUsers_WithInvalidRequests() {
-        when(this.productUserService.getUserByUsername("Ollie")).thenReturn(null);
+        when(this.productUserService.getUserByEmail(this.testEmail)).thenReturn(null);
         AuthResponse expectedResponse = new AuthResponse(false);
-        PayloadStatusResponse<AuthResponse> actualResponse = this.authService.login(this.userRequest);
-        verify(this.productUserService).getUserByUsername("Ollie");
+        PayloadStatusResponse<AuthResponse> actualResponse = this.authService.login(this.loginRequest);
+        verify(this.productUserService).getUserByEmail(this.testEmail);
         assertEquals(expectedResponse, actualResponse.getPayload());
     }
     
     @Test
     @DisplayName("Should not login users with mismatched passwords")
     public void login_NotLoginUsers_WithMismatchedPasswords() {
-        when(this.productUserService.getUserByUsername("Ollie")).thenReturn(this.user);
+        when(this.productUserService.getUserByEmail(this.testEmail)).thenReturn(this.user);
         AuthResponse expectedResponse = new AuthResponse(false);
         when(this.productUserService.comparePassword(this.user, this.userRequest.getPassword())).thenReturn(false);
-        PayloadStatusResponse<AuthResponse> actualResponse = this.authService.login(this.userRequest);
-        verify(this.productUserService).getUserByUsername("Ollie");
+        PayloadStatusResponse<AuthResponse> actualResponse = this.authService.login(this.loginRequest);
+        verify(this.productUserService).getUserByEmail(this.testEmail);
         verify(this.productUserService).comparePassword(this.user, this.userRequest.getPassword());
         assertEquals(expectedResponse, actualResponse.getPayload());
     }
