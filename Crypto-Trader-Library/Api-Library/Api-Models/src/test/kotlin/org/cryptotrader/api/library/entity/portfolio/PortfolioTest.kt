ec275203fcf336d1f96306bf5ea221371d1c1134
theoliverlear <sigwartholiver2@gmail.com>
2025-09-11 21:57:35 -0500

    Add PortfolioTest.kt to test `Portfolio` entity functionality in Api-Models library

---
+++ Crypto-Trader-Library/Api-Library/Api-Models/src/test/kotlin/org/cryptotrader/api/library/entity/portfolio/PortfolioTest.kt
@@ -0,0 +1,54 @@
+package org.cryptotrader.api.library.entity.portfolio
+
+import org.assertj.core.api.Assertions.assertThat
+import org.cryptotrader.api.library.entity.currency.Currency
+import org.cryptotrader.api.library.entity.user.ProductUser
+import org.cryptotrader.test.CryptoTraderTest
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.mockito.Mockito
+import org.mockito.Mockito.verify
+
+class PortfolioTest : CryptoTraderTest() {
+    private lateinit var bitcoinCurrency: Currency
+    private lateinit var ethereumCurrency: Currency
+    private lateinit var portfolio: Portfolio
+    
+    @BeforeEach
+    fun setUp() {
+        val testUser = ProductUser("cryptotrader", "password")
+        this.bitcoinCurrency = Currency.builder().currencyCode("BTC").value(2.0).build()
+        this.ethereumCurrency = Currency.builder().currencyCode("ETH").value(1.0).build()
+        this.portfolio = Mockito.spy(Portfolio(testUser))
+    }
+
+    @Test
+    @DisplayName("Should accept new assets")
+    fun addAsset_AcceptsAssets() {
+        val bitcoinAsset = PortfolioAsset(this.bitcoinCurrency, 0.0, 0.0)
+        val ethereumAsset = PortfolioAsset(this.ethereumCurrency, 0.0, 0.0)
+        assertThat(this.portfolio.assets).isEmpty()
+        this.portfolio.addAsset(bitcoinAsset)
+        assertEquals(this.portfolio.assets.size, 1)
+        this.portfolio.addAsset(ethereumAsset)
+        assertEquals(this.portfolio.assets.size, 2)
+    }
+
+    @Test
+    @DisplayName("Should update portfolio value correctly")
+    fun updateValues_UpdatesValueCorrectly() {
+        val bitcoinWalletDollars = 100.0
+        val testEthereumShares = 10.0
+        val bitcoinAsset = PortfolioAsset(this.bitcoinCurrency, 0.0, bitcoinWalletDollars)
+        val ethereumAsset = PortfolioAsset(this.ethereumCurrency, testEthereumShares, 0.0)
+        val expectedEthereumValue: Double = testEthereumShares * this.ethereumCurrency.value
+        this.portfolio.addAsset(bitcoinAsset)
+        verify(this.portfolio, Mockito.times(1)).updateValues()
+        this.portfolio.addAsset(ethereumAsset)
+        verify(this.portfolio, Mockito.times(2)).updateValues()
+        val expectedValue = bitcoinWalletDollars + expectedEthereumValue
+        assertEquals(expectedValue, this.portfolio.totalWorth)
+    }
+}
