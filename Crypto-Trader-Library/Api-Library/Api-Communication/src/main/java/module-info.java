6dc6d9e87165687446a08afac176d445758e4839
theoliverlear <sigwartholiver2@gmail.com>
2025-09-12 16:10:24 -0500

    Expand module-info.java to include new alias exports and dependency on `api-models` module in Api-Communication module

--- Crypto-Trader-Library/Api-Library/Api-Communication/src/main/java/module-info.java
+++ Crypto-Trader-Library/Api-Library/Api-Communication/src/main/java/module-info.java
@@ -1,8 +1,10 @@
 open module org.cryptotrader.api.library.communication {
-    exports org.cryptotrader.api.library.comm.request;
-    exports org.cryptotrader.api.library.comm.response;
     requires kotlin.stdlib;
     requires static lombok;
     requires static com.fasterxml.jackson.annotation;
-
+    requires org.cryptotrader.api.library.models;
+    
+    exports org.cryptotrader.api.library.comm.request;
+    exports org.cryptotrader.api.library.comm.request.alias;
+    exports org.cryptotrader.api.library.comm.response;
 }
