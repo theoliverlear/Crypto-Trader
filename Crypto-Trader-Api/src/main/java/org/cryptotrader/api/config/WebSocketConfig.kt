0b3c7ccf863d127a484e2bd69eafcc46ad1ff95f
theoliverlear <sigwartholiver2@gmail.com>
2025-09-13 11:57:33 -0500

    Expand WebSocketConfig.kt to register `LoginWebSocketHandler` for handling login over WebSocket at `/ws/login`

--- Crypto-Trader-Api/src/main/java/org/cryptotrader/api/config/WebSocketConfig.kt
+++ Crypto-Trader-Api/src/main/java/org/cryptotrader/api/config/WebSocketConfig.kt
@@ -1,5 +1,6 @@
 package org.cryptotrader.api.config
 
+import org.cryptotrader.api.controller.websocket.LoginWebSocketHandler
 import org.cryptotrader.api.controller.websocket.SignupWebSocketHandler
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.socket.config.annotation.EnableWebSocket
@@ -10,10 +11,12 @@ import org.springframework.web.socket.server.support.DefaultHandshakeHandler
 @Configuration
 @EnableWebSocket
 open class WebSocketConfig(
-    val signupWebsocket: SignupWebSocketHandler
+    val signupWebsocket: SignupWebSocketHandler,
+    val loginWebSocket: LoginWebSocketHandler
 ) : WebSocketConfigurer {
     override fun registerWebSocketHandlers(registry: WebSocketHandlerRegistry) {
         registry.addHandler(this.signupWebsocket, "/ws/signup")
+                .addHandler(this.loginWebSocket, "/ws/login")
                 .setAllowedOrigins("*")
                 .setHandshakeHandler(DefaultHandshakeHandler())
     }
