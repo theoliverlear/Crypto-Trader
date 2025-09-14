a0024124af5401616ddeae0c6ed94c1e52a30034
theoliverlear <sigwartholiver2@gmail.com>
2025-09-13 21:13:04 -0500

    Add HealthTestSuite.kt to organize and run health-related tests in Crypto-Trader-Health

---
+++ Crypto-Trader-Health/src/test/kotlin/org/cryptotrader/health/HealthTestSuite.kt
@@ -0,0 +1,8 @@
+package org.cryptotrader.health
+
+import org.junit.platform.suite.api.SelectPackages
+import org.junit.platform.suite.api.Suite
+
+@Suite
+@SelectPackages("org.cryptotrader.health")
+class HealthTestSuite
