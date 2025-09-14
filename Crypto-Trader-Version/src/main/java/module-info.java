5a6ebc92737c4db71a9f56d89cc6c0ca4b62535e
theoliverlear <sigwartholiver2@gmail.com>
2025-09-13 19:20:40 -0500

    Expand module-info.java to include Spring and JavaFX in Crypto-Trader-Version

--- Crypto-Trader-Version/src/main/java/module-info.java
+++ Crypto-Trader-Version/src/main/java/module-info.java
@@ -5,12 +5,26 @@ open module org.cryptotrader.version {
     requires java.xml;
     requires org.cryptotrader.version.library.model;
     requires org.apache.commons.io;
-    requires org.apache.httpcomponents.httpcore;
-    requires org.apache.httpcomponents.httpclient;
     requires kotlin.stdlib;
     requires com.sigwarthsoftware.changelog;
     requires org.jetbrains.annotations;
     requires com.sigwarthsoftware.promo;
+    requires javafx.graphics;
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires spring.boot;
+    requires spring.boot.autoconfigure;
+    requires spring.context;
+    requires spring.beans;
+    requires spring.core;
+    requires spring.jcl;
+    requires org.yaml.snakeyaml;
+    requires java.sql;
+    requires javafxsvg;
+    requires net.rgielen.fxweaver.core;
+    requires org.cryptotrader.assets;
+    requires org.cryptotrader.desktop.library.components;
+    requires fr.brouillard.oss.cssfx;
 
     exports org.cryptotrader.version;
     exports org.cryptotrader.version.script;
