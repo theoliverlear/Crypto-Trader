e0b91cba8ee02560cbd6bbb28b19018682684394
theoliverlear <sigwartholiver2@gmail.com>
2025-09-13 19:22:51 -0500

    Add ModuleVersionController.kt to define version control UI component in Crypto-Trader-Version

---
+++ Crypto-Trader-Version/src/main/kotlin/org/cryptotrader/version/controller/ModuleVersionController.kt
@@ -0,0 +1,9 @@
+package org.cryptotrader.version.controller
+
+import javafx.scene.layout.VBox
+import org.springframework.stereotype.Component
+
+@Component
+class ModuleVersionController : VBox() {
+    
+}
