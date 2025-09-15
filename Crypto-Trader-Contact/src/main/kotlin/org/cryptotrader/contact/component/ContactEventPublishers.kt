27edd2fb42ebce264651bbf8dd78f3d23c2e4d18
theoliverlear <sigwartholiver2@gmail.com>
2025-09-15 08:13:37 -0500

    Add `@ConditionalOnBean` annotation in ContactEventPublishers.kt to load configuration only when `EventPublisher` bean is present

--- Crypto-Trader-Contact/src/main/kotlin/org/cryptotrader/contact/component/ContactEventPublishers.kt
+++ Crypto-Trader-Contact/src/main/kotlin/org/cryptotrader/contact/component/ContactEventPublishers.kt
@@ -1,8 +1,10 @@
 package org.cryptotrader.contact.component
 
 import org.cryptotrader.api.library.config.EventPublisher
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean
 import org.springframework.context.annotation.Configuration
 
+@ConditionalOnBean(EventPublisher::class)
 @Configuration
 open class ContactEventPublishers(
     private val eventPublisher: EventPublisher
