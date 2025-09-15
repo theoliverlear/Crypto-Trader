20998f892f53e60c6a59af1be39092e03e580700
theoliverlear <sigwartholiver2@gmail.com>
2025-09-15 11:05:48 -0500

    Expand AdminUsersController.java to include JavaFX integration with FXML annotations and initialize welcome text

--- Crypto-Trader-Admin/src/main/java/org/cryptotrader/admin/controller/AdminUsersController.java
+++ Crypto-Trader-Admin/src/main/java/org/cryptotrader/admin/controller/AdminUsersController.java
@@ -1,10 +1,21 @@
 package org.cryptotrader.admin.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import org.springframework.stereotype.Component;
 
 @Component
-public class AdminUsersController extends BaseViewController {
+public class AdminUsersController extends VBox {
+    @FXML
+    private Label welcomeText;
     public AdminUsersController() {
         
+    }
+    
+    @FXML
+    private void initialize() {
+        this.welcomeText.setText("Welcome to Crypto Trader Admin");
     }
 }
