71bfb8e1fb7511428f1daceb0c0ff1318ff72475
theoliverlear <sigwartholiver2@gmail.com>
2025-09-15 11:05:12 -0500

    Exclude unused autoconfiguration classes and add library configuration and events package in SpringBootConfig.java for Crypto-Trader-Admin

--- Crypto-Trader-Admin/src/main/java/org/cryptotrader/admin/config/SpringBootConfig.java
+++ Crypto-Trader-Admin/src/main/java/org/cryptotrader/admin/config/SpringBootConfig.java
@@ -2,13 +2,18 @@ package org.cryptotrader.admin.config;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.http.client.HttpClientAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.client.RestClientAutoConfiguration;
+import org.springframework.boot.autoconfigure.webservices.client.WebServiceTemplateAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {HttpClientAutoConfiguration.class, RestClientAutoConfiguration.class, WebServiceTemplateAutoConfiguration.class})
 @ComponentScan(basePackages = {"org.cryptotrader.admin",
         "org.cryptotrader.api.library.services",
         "org.cryptotrader.api.library.component",
+        "org.cryptotrader.api.library.events",
+        "org.cryptotrader.api.library.config",
         "org.cryptotrader.desktop.library.component"})
 @EnableJpaRepositories(basePackages = {
         "org.cryptotrader.api.library.repository"
