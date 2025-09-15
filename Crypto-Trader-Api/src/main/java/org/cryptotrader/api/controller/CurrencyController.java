04db7a2003834cf341a80748055b77ccec36ba0c
theoliverlear <sigwartholiver2@gmail.com>
2025-09-15 16:40:03 -0500

    Add method to CurrencyController.java to retrieve daily performance ratings by currency code in Crypto-Trader-Api

--- Crypto-Trader-Api/src/main/java/org/cryptotrader/api/controller/CurrencyController.java
+++ Crypto-Trader-Api/src/main/java/org/cryptotrader/api/controller/CurrencyController.java
@@ -1,14 +1,13 @@
 package org.cryptotrader.api.controller;
 
+import org.cryptotrader.api.library.communication.response.PerformanceRatingResponse;
+import org.cryptotrader.api.library.model.currency.PerformanceRating;
 import org.cryptotrader.api.library.services.CurrencyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.cryptotrader.api.library.communication.request.AssetValueRequest;
 import org.cryptotrader.api.library.communication.response.AssetValueResponse;
 import org.cryptotrader.api.library.entity.currency.Currency;
@@ -30,6 +29,16 @@ public class CurrencyController {
         } else {
             double assetValue = currency.getValue() * assetValueRequest.getShares();
             return new ResponseEntity<>(new AssetValueResponse(assetValue), HttpStatus.OK);
+        }
+    }
+    @GetMapping("/performance/{currencyCode}")
+    public ResponseEntity<PerformanceRatingResponse> getCurrencyPerformance(@PathVariable String currencyCode) {
+        if (currencyCode == null) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        } else {
+            PerformanceRating performanceRatingResponse = this.currencyService.getDayPerformance(currencyCode);
+            return new ResponseEntity<>(new PerformanceRatingResponse(performanceRatingResponse),
+                                        HttpStatus.OK);
         }
     }
 }
