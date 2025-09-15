670a971ffb3aeed08f6f830c3527f0f994fb11bb
theoliverlear <sigwartholiver2@gmail.com>
2025-09-15 08:24:59 -0500

    Add `@ConditionalOnBean` annotation in EventPublisher.java to load configuration only when `BindingService` bean is present

--- Crypto-Trader-Library/Api-Library/Api-Config/src/main/java/org/cryptotrader/api/library/config/EventPublisher.java
+++ Crypto-Trader-Library/Api-Library/Api-Config/src/main/java/org/cryptotrader/api/library/config/EventPublisher.java
@@ -2,10 +2,12 @@ package org.cryptotrader.api.library.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.cloud.stream.binding.BindingService;
 import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.stereotype.Component;
 
-
+@ConditionalOnBean(BindingService.class)
 @ConditionalOnProperty(prefix = "spring.cloud.stream", name = "enabled", havingValue = "true", matchIfMissing = true)
 @Component
 public class EventPublisher {
