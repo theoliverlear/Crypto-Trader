992c3609213c09f7e4f958b801736fcbe045e562
theoliverlear <sigwartholiver2@gmail.com>
2025-09-15 11:19:46 -0500

    Refactor AdminLauncher.java to dynamically load and install `SvgImageLoaderFactory` with exception handling for better resilience

--- Crypto-Trader-Admin/src/main/java/org/cryptotrader/admin/AdminLauncher.java
+++ Crypto-Trader-Admin/src/main/java/org/cryptotrader/admin/AdminLauncher.java
@@ -1,11 +1,22 @@
 package org.cryptotrader.admin;
 
-import de.codecentric.centerdevice.javafxsvg.SvgImageLoaderFactory;
 import javafx.application.Application;
+
+import java.lang.reflect.Method;
 
 public class AdminLauncher {
     public static void main(String[] args) {
-        SvgImageLoaderFactory.install();
+        attemptInitSvgFactory();
         Application.launch(AdminApplication.class, args);
+    }
+
+    private static void attemptInitSvgFactory() {
+        try {
+            Class<?> svgLoaderClass = Class.forName("de.codecentric.centerdevice.javafxsvg.SvgImageLoaderFactory");
+            Method method = svgLoaderClass.getMethod("install");
+            method.invoke(null);
+        } catch (Throwable throwable) {
+            System.err.println("[WARN] SVG loader not installed: " + throwable.getClass().getName() + ": " + throwable.getMessage());
+        }
     }
 }
