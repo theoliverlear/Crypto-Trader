0749b656268d87a9655d0df41b27cee4cf8ec565
theoliverlear <sigwartholiver2@gmail.com>
2025-09-15 11:18:30 -0500

    Add `DASHBOARD` and `EMAIL` constants in AppPage.java for new admin page links

--- Crypto-Trader-Admin/src/main/java/org/cryptotrader/admin/route/AppPage.java
+++ Crypto-Trader-Admin/src/main/java/org/cryptotrader/admin/route/AppPage.java
@@ -3,6 +3,8 @@ package org.cryptotrader.admin.route;
 public enum AppPage {
     ADMIN_USERS("Admin Users"),
     AUTH("Authorize"),
+    DASHBOARD("Dashboard"),
+    EMAIL("Email"),
     TABLES("Tables");
     public final String pageName;
     AppPage(String pageName) {
