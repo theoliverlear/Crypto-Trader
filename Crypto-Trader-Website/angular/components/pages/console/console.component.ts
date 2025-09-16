2e1e24ac19e7d18648f34be3e10adb4e969763da
theoliverlear <sigwartholiver2@gmail.com>
2025-09-15 20:39:21 -0500

    Add `console` component files to create console page in Crypto-Trader-Website

---
+++ Crypto-Trader-Website/angular/components/pages/console/console.component.ts
@@ -0,0 +1,14 @@
+// console.component.ts
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'console',
+    templateUrl: './console.component.html',
+    styleUrls: ['./console.component.scss'],
+    standalone: false,
+})
+export class ConsoleComponent {
+    constructor() {
+        
+    }
+}
