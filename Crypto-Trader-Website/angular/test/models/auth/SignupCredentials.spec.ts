3a10e77d776b52e726665bd475c891463a18ff89
theoliverlear <sigwartholiver2@gmail.com>
2025-09-16 15:47:09 -0500

    Edit SignupCredentials.spec.ts to add tests for password matching, issue validation, and update email-based flow

--- Crypto-Trader-Website/angular/test/models/auth/SignupCredentials.spec.ts
+++ Crypto-Trader-Website/angular/test/models/auth/SignupCredentials.spec.ts
@@ -1,4 +1,5 @@
 import {SignupCredentials} from "../../../models/auth/SignupCredentials";
+import {AuthPopup} from "@theoliverlear/angular-suite";
 
 describe('SignupCredentials', () => {
     let signupCredentials: SignupCredentials;
@@ -18,7 +19,8 @@ describe('SignupCredentials', () => {
             }
             const validEmails: string[] = [
                 "ollie@gmail.com",
-                "hello@sscryptotrader.com"
+                "hello@sscryptotrader.com",
+                "dotted.email@emailed.dotted.com"
             ];
             for (let validEmail of validEmails) {
                 signupCredentials.email = validEmail;
@@ -30,12 +32,51 @@ describe('SignupCredentials', () => {
         const password: string = "password";
         const confirmPassword: string = "password";
         const email: string = "invalid-email";
-        const username: string = "ollie";
         const agreedTerms: boolean = true;
-        signupCredentials = new SignupCredentials(username, email, password, confirmPassword, agreedTerms);
+        signupCredentials = new SignupCredentials(email, password, confirmPassword, agreedTerms);
         it('should not allow invalid request generation', () => {
+            console.log('SignupCredentials popup: ', signupCredentials.getAnyIssue());
             expect(() => signupCredentials.getSignupRequest()).toThrowError(
                 "Cannot get signup request due to input issues.")
         });
     });
+    describe('getAnyIssue', () => {
+        it('should return an issue if present', () => {
+            const password: string = "password";
+            const confirmPassword: string = "password";
+            const email: string = "invalid-email";
+            const agreedTerms: boolean = true;
+            signupCredentials = new SignupCredentials(email, password, confirmPassword, agreedTerms);
+            expect(signupCredentials.getAnyIssue()).toBe(AuthPopup.INVALID_EMAIL);
+            const validEmail: string = "valid@email.com";
+            signupCredentials.email = validEmail;
+            expect(signupCredentials.getAnyIssue()).toBe(AuthPopup.NONE);
+        });
+    });
+    describe('isPasswordMatch', () => {
+        it('should return false if passwords do not match', () => {
+            const password: string = "password";
+            const confirmPassword: string = "different-password";
+            const email: string = "invalid-email";
+            const agreedTerms: boolean = true;
+            signupCredentials = new SignupCredentials(email, password, confirmPassword, agreedTerms);
+            expect(signupCredentials.isPasswordMatch()).toBeFalse();
+        });
+        it('should return true if passwords are empty', () => {
+            const password: string = "";
+            const confirmPassword: string = "";
+            const email: string = "invalid-email";
+            const agreedTerms: boolean = true;
+            signupCredentials = new SignupCredentials(email, password, confirmPassword, agreedTerms);
+            expect(signupCredentials.isPasswordMatch()).toBeTrue();
+        });
+        it('should return true if passwords match', () => {
+            const password: string = "password";
+            const confirmPassword: string = "password";
+            const email: string = "invalid-email";
+            const agreedTerms: boolean = true;
+            signupCredentials = new SignupCredentials(email, password, confirmPassword, agreedTerms);
+            expect(signupCredentials.isPasswordMatch()).toBeTrue();
+        });
+    })
 });
