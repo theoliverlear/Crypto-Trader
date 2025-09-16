7015f84b8362b3acc4e3e07045ada29b8476e407
theoliverlear <sigwartholiver2@gmail.com>
2025-09-16 10:04:13 -0500

    Expand auth-console.component.ts to integrate WebSocket services for signup and login processes in Crypto-Trader-Website

--- Crypto-Trader-Website/angular/components/elements/element-group-auth/auth-console/auth-console.component.ts
+++ Crypto-Trader-Website/angular/components/elements/element-group-auth/auth-console/auth-console.component.ts
@@ -1,5 +1,18 @@
 import {Component, EventEmitter, Input, Output} from "@angular/core";
-import {AuthPopup, AuthType} from "@theoliverlear/angular-suite";
+import {
+    AuthPopup,
+    AuthType,
+    WebSocketCapable
+} from "@theoliverlear/angular-suite";
+import {
+    SignupWebSocketService
+} from "../../../../services/net/websocket/signup-websocket.service";
+import {
+    LoginWebSocketService
+} from "../../../../services/net/websocket/login-websocket.service";
+import {Subscription} from "rxjs";
+import {SignupCredentials} from "../../../../models/auth/SignupCredentials";
+import {SignupRequest} from "../../../../models/auth/types";
 
 @Component({
     selector: 'auth-console',
@@ -7,17 +20,31 @@ import {AuthPopup, AuthType} from "@theoliverlear/angular-suite";
     templateUrl: './auth-console.component.html',
     styleUrls: ['./auth-console.component.scss']
 })
-export class AuthConsoleComponent {
+export class AuthConsoleComponent implements WebSocketCapable {
     @Input() currentAuthType: AuthType = AuthType.SIGN_UP;
     @Output() authPopupEvent: EventEmitter<AuthPopup> = new EventEmitter<AuthPopup>();
-    constructor() {
+    webSocketSubscriptions: Record<string, Subscription> = {};
+    constructor(private signupWebSocket: SignupWebSocketService,
+                private loginWebSocket: LoginWebSocketService) {
 
     }
     
+    attemptSignup(signupCredentials: SignupCredentials) {
+        if (signupCredentials.getSubmitIssue() !== AuthPopup.NONE) {
+            this.signup(signupCredentials.getSignupRequest());
+        } else {
+            this.emitAuthPopup(signupCredentials.getSubmitIssue());
+        }
+    }
+    
+    signup(signupRequest: SignupRequest) {
+        this.signupWebSocket.sendMessage(signupRequest);
+    }
+
     emitAuthPopup(authPopup: AuthPopup): void {
         this.authPopupEvent.emit(authPopup);
     }
-    
+
     setAuthType(authType: AuthType): void {
         this.currentAuthType = authType;
     }
@@ -26,6 +53,44 @@ export class AuthConsoleComponent {
     }
     isLoginSection(): boolean {
         return this.currentAuthType === AuthType.LOGIN;
+    }
+
+    ngOnInit(): void {
+        this.initializeWebSockets();
+    }
+    initializeWebSockets(): void {
+        this.initSignupWebSocket();
+        this.initLoginWebSocket();
+    }
+
+    private initSignupWebSocket() {
+        this.signupWebSocket.connect();
+        this.webSocketSubscriptions['signup'] = this.signupWebSocket.getMessages().subscribe({
+            next: (message) => {
+                console.log(message);
+            },
+            error: (error) => {
+                console.log(error);
+            },
+            complete: () => {
+                console.log('complete');
+            }
+        });
+    }
+
+    private initLoginWebSocket() {
+        this.loginWebSocket.connect();
+        this.webSocketSubscriptions['login'] = this.loginWebSocket.getMessages().subscribe({
+            next: (message) => {
+                console.log(message);
+            },
+            error: (error) => {
+                console.log(error);
+            },
+            complete: () => {
+                console.log('complete');
+            }
+        });
     }
 
     protected readonly AuthPopup = AuthPopup;
