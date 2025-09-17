babe9a056b5417bd3db5c829839aad1f52b5249a
theoliverlear <sigwartholiver2@gmail.com>
2025-09-17 13:47:15 -0500

    Remove `@EnableJpaRepositories` from CryptoTraderApiApplication.java as JPA repository support is now handled by `JpaRepositoriesConfig`

--- Crypto-Trader-Api/src/main/java/org/cryptotrader/api/CryptoTraderApiApplication.java
+++ Crypto-Trader-Api/src/main/java/org/cryptotrader/api/CryptoTraderApiApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -17,9 +16,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
         "org.cryptotrader.api.library.component",
         "org.cryptotrader.api.library.services",
         "org.cryptotrader.security.library.service"
-})
-@EnableJpaRepositories(basePackages = {
-        "org.cryptotrader.api.library.repository"
 })
 public class CryptoTraderApiApplication {
     public static void main(String[] args) {
