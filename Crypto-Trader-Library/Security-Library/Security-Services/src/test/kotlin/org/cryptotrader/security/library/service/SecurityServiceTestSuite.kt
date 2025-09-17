8bacc08ae70a8acb63ca0624bf28bbca44f53b4a
theoliverlear <sigwartholiver2@gmail.com>
2025-09-17 12:41:40 -0500

    Add SecurityServiceTestSuite.kt to group and run security service tests in Security-Services module

---
+++ Crypto-Trader-Library/Security-Library/Security-Services/src/test/kotlin/org/cryptotrader/security/library/service/SecurityServiceTestSuite.kt
@@ -0,0 +1,8 @@
+package org.cryptotrader.security.library.service
+
+import org.junit.platform.suite.api.SelectPackages
+import org.junit.platform.suite.api.Suite
+
+@Suite
+@SelectPackages("org.cryptotrader.security.library.service")
+class SecurityServiceTestSuite
