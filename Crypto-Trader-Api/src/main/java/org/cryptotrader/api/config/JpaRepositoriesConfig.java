4ed6f372b3f178084d8a7fe0308e189b3d7a3d5f
theoliverlear <sigwartholiver2@gmail.com>
2025-09-17 13:45:37 -0500

    Add JpaRepositoriesConfig.java to enable conditional JPA repository support based on properties

---
+++ Crypto-Trader-Api/src/main/java/org/cryptotrader/api/config/JpaRepositoriesConfig.java
@@ -0,0 +1,12 @@
+package org.cryptotrader.api.config;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@Configuration
+@ConditionalOnProperty(name = "cryptotrader.api.jpa.enabled", havingValue = "true", matchIfMissing = true)
+@EnableJpaRepositories(basePackages = {
+        "org.cryptotrader.api.library.repository"
+})
+public class JpaRepositoriesConfig { }
