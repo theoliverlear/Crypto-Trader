eeb27a15a6b4405c5f2d183f2a4a10c37a614118
theoliverlear <sigwartholiver2@gmail.com>
2025-09-16 21:27:18 -0500

    Add SecurityPropertiesConfig.kt to define security-related configurable properties for Security-Config module

---
+++ Crypto-Trader-Library/Security-Library/Security-Config/src/main/kotlin/org/cryptotrader/security/library/config/SecurityPropertiesConfig.kt
@@ -0,0 +1,28 @@
+package org.cryptotrader.security.library.config
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+
+@ConfigurationProperties(prefix = "security")
+data class SecurityPropertiesConfig(
+    val bans: Bans = Bans(),
+    val http: Http = Http(),
+    val crypto: Encryption = Encryption(),
+    val encryption: Encryption? = null,
+) {
+    data class Bans(
+        val enabled: Boolean = true,
+        val denylist: List<String> = emptyList(),
+    )
+
+    data class Http(
+        val blockResponseCode: Int = 403
+    )
+
+    data class Encryption(
+        val tink: Tink = Tink()
+    ) {
+        data class Tink(
+            val keysetPath: String = "file:./tink/aead.json"
+        )
+    }
+}
