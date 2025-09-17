1a6f1c0cf0dd69d10b111cfb911863d7e57fce00
theoliverlear <sigwartholiver2@gmail.com>
2025-09-17 18:41:29 -0500

    Add token field and constructor overload in AuthResponse.java to support token management

--- Crypto-Trader-Library/Api-Library/Api-Communication/src/main/java/org/cryptotrader/api/library/communication/response/AuthResponse.java
+++ Crypto-Trader-Library/Api-Library/Api-Communication/src/main/java/org/cryptotrader/api/library/communication/response/AuthResponse.java
@@ -5,7 +5,12 @@ import lombok.Data;
 @Data
 public class AuthResponse {
     private boolean authorized;
+    private String token;
     public AuthResponse(boolean isAuthorized) {
         this.authorized = isAuthorized;
+    }
+    public AuthResponse(boolean isAuthorized, String token) {
+        this.authorized = isAuthorized;
+        this.token = token;
     }
 }
