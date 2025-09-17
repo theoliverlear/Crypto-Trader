c24fd508d5308c08cd4783c7063dc0932fd6c6de
theoliverlear <sigwartholiver2@gmail.com>
2025-09-16 22:38:37 -0500

    Add UserIpAddressSet.java to represent a user's set of IP addresses in Security-Models module to detect suspicious login attempts

---
+++ Crypto-Trader-Library/Security-Library/Security-Models/src/main/java/org/cryptotrader/security/library/entity/UserIpAddressSet.java
@@ -0,0 +1,26 @@
+package org.cryptotrader.security.library.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.cryptotrader.api.library.entity.Identifiable;
+import org.cryptotrader.api.library.entity.user.ProductUser;
+
+import java.util.Set;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "user_ip_address_sets")
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserIpAddressSet extends Identifiable {
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    private ProductUser user;
+
+    @OneToMany(mappedBy = "userIpAddressSet", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<UserIpAddress> ipAddresses;
+}
