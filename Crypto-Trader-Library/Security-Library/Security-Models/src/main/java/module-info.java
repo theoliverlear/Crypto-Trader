7cc148d357e08bb3f34579c0813867f2c0dc25bc
theoliverlear <sigwartholiver2@gmail.com>
2025-09-17 12:50:17 -0500

    Expand module-info.java in Security-Models to make the module open, add static dependencies, and export the `entity` package

--- Crypto-Trader-Library/Security-Library/Security-Models/src/main/java/module-info.java
+++ Crypto-Trader-Library/Security-Library/Security-Models/src/main/java/module-info.java
@@ -1,5 +1,7 @@
-module org.cryptotrader.security.library.models {
-    requires jakarta.persistence;
+open module org.cryptotrader.security.library.models {
+    requires static jakarta.persistence;
     requires static lombok;
     requires org.cryptotrader.api.library.models;
+
+    exports org.cryptotrader.security.library.entity;
 }
