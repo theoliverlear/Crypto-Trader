744cedf4b978fd665a7d819d03366c6602645016
theoliverlear <sigwartholiver2@gmail.com>
2025-09-17 13:07:28 -0500

    Refactor InMemoryIpBanService.kt to encapsulate `blockedAddresses` behind a getter

--- Crypto-Trader-Library/Security-Library/Security-Services/src/main/kotlin/org/cryptotrader/security/library/service/InMemoryIpBanService.kt
+++ Crypto-Trader-Library/Security-Library/Security-Services/src/main/kotlin/org/cryptotrader/security/library/service/InMemoryIpBanService.kt
@@ -5,15 +5,19 @@ import inet.ipaddr.IPAddressString
 import java.util.concurrent.ConcurrentHashMap
 
 class InMemoryIpBanService : IpBanService {
-    private val blockedAddresses: MutableMap<String, IPAddressString> =
+    private val _blockedAddresses: MutableMap<String, IPAddressString> =
         ConcurrentHashMap()
+    val blockedAddresses: MutableMap<String, IPAddressString>
+        get() = this._blockedAddresses
 
     override fun isBanned(ip: String): Boolean {
         val clientAddress: IPAddress = this.parseAddress(ip) ?: return false
         if (this.blockedAddresses.containsKey(ip)) return true
         for (blocked in this.blockedAddresses.values) {
             val blockedAddress: IPAddress = blocked.address ?: continue
-            if (blockedAddress.contains(clientAddress) || blockedAddress == clientAddress) return true
+            if (blockedAddress.contains(clientAddress) || blockedAddress == clientAddress) {
+                return true
+            }
         }
         return false
     }
