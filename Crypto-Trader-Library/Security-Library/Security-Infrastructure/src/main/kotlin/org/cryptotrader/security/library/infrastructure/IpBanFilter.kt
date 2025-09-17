0e66440d5e88e7262cda9f0c737a66e6b586c3db
theoliverlear <sigwartholiver2@gmail.com>
2025-09-16 21:38:02 -0500

    Add IpBanFilter.kt to enforce IP banning in Security-Infrastructure module

---
+++ Crypto-Trader-Library/Security-Library/Security-Infrastructure/src/main/kotlin/org/cryptotrader/security/library/infrastructure/IpBanFilter.kt
@@ -0,0 +1,38 @@
+package org.cryptotrader.security.library.infrastructure
+
+import jakarta.servlet.FilterChain
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.cryptotrader.security.library.service.IpBanService
+import org.springframework.core.Ordered
+import org.springframework.core.annotation.Order
+import org.springframework.web.filter.OncePerRequestFilter
+
+@Order(Ordered.HIGHEST_PRECEDENCE + 10)
+class IpBanFilter(
+    private val ipBanService: IpBanService,
+    private val blockResponseCode: Int
+) : OncePerRequestFilter() {
+
+    override fun doFilterInternal(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        filterChain: FilterChain
+    ) {
+        val clientIp = request.remoteAddr
+        if (this.isClientBanned(clientIp)) {
+            this.blockRequest(response)
+            return
+        }
+        filterChain.doFilter(request, response)
+    }
+
+    private fun isClientBanned(clientIp: String): Boolean {
+        return this.ipBanService.isBanned(clientIp)
+    }
+
+    private fun blockRequest(response: HttpServletResponse) {
+        response.status = this.blockResponseCode
+        response.flushBuffer()
+    }
+}
