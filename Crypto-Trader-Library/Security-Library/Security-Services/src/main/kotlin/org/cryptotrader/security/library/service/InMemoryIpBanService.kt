aa65816f735d96923d7799d7e7557705c858831a
theoliverlear <sigwartholiver2@gmail.com>
2025-09-16 21:45:09 -0500

    Add InMemoryIpBanService.kt to manage in-memory IP banning for Security-Services module

---
+++ Crypto-Trader-Library/Security-Library/Security-Services/src/main/kotlin/org/cryptotrader/security/library/service/InMemoryIpBanService.kt
@@ -0,0 +1,32 @@
+package org.cryptotrader.security.library.service
+
+import inet.ipaddr.IPAddress
+import inet.ipaddr.IPAddressString
+import java.util.concurrent.ConcurrentHashMap
+
+class InMemoryIpBanService : IpBanService {
+    private val blockedAddresses: MutableMap<String, IPAddressString> =
+        ConcurrentHashMap()
+
+    override fun isBanned(ip: String): Boolean {
+        val clientAddress: IPAddress = this.parseAddress(ip) ?: return false
+        if (this.blockedAddresses.containsKey(ip)) return true
+        for (blocked in this.blockedAddresses.values) {
+            val blockedAddress: IPAddress = blocked.address ?: continue
+            if (blockedAddress.contains(clientAddress) || blockedAddress == clientAddress) return true
+        }
+        return false
+    }
+
+    override fun ban(ipOrCidr: String) {
+        this.blockedAddresses[ipOrCidr] = IPAddressString(ipOrCidr)
+    }
+
+    override fun unban(ipOrCidr: String) {
+        this.blockedAddresses.remove(ipOrCidr)
+    }
+
+    private fun parseAddress(value: String): IPAddress? = IPAddressString(
+        value
+    ).address
+}
