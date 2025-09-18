4648ce1f3ec35b77c2b4d577e966203c385b9136
theoliverlear <sigwartholiver2@gmail.com>
2025-09-18 15:35:39 -0500

    Expand CryptoTraderSecurityApplication.kt to include JPA repository and entity scanning with custom system property initialization

--- Crypto-Trader-Security/src/main/kotlin/org/cryptotrader/security/CryptoTraderSecurityApplication.kt
+++ Crypto-Trader-Security/src/main/kotlin/org/cryptotrader/security/CryptoTraderSecurityApplication.kt
@@ -1,14 +1,19 @@
 package org.cryptotrader.security
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Import
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @SpringBootApplication
 @Import(SecurityAutoConfiguration::class)
+@EnableJpaRepositories(basePackages = ["org.cryptotrader.security.library.repository"])
+@EntityScan(basePackages = ["org.cryptotrader.security.library.entity", "org.cryptotrader.api.library.entity"])
 open class CryptoTraderSecurityApplication
 
 fun main(args: Array<String>) {
+    System.setProperty("cryptotrader.loadCurrencies", "false")
     runApplication<CryptoTraderSecurityApplication>(*args)
 }
