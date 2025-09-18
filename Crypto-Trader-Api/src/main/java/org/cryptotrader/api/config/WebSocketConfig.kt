418b3c54672aad9c57e3e6e6926122045dd96743
theoliverlear <sigwartholiver2@gmail.com>
2025-09-17 22:21:06 -0500

    Expand WebSocketConfig.kt to include `JwtHandshakeInterceptor` for WebSocket handshake validation

--- Crypto-Trader-Api/src/main/java/org/cryptotrader/api/config/WebSocketConfig.kt
+++ Crypto-Trader-Api/src/main/java/org/cryptotrader/api/config/WebSocketConfig.kt
@@ -2,6 +2,7 @@ package org.cryptotrader.api.config
 
 import org.cryptotrader.api.controller.websocket.LoginWebSocketHandler
 import org.cryptotrader.api.controller.websocket.SignupWebSocketHandler
+import org.cryptotrader.api.library.infrastructure.JwtHandshakeInterceptor
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.socket.config.annotation.EnableWebSocket
@@ -14,12 +15,18 @@ import org.springframework.web.socket.server.support.DefaultHandshakeHandler
 @ConditionalOnProperty(name = ["cryptotrader.api.websocket.enabled"], havingValue = "true", matchIfMissing = true)
 open class WebSocketConfig(
     val signupWebsocket: SignupWebSocketHandler,
-    val loginWebSocket: LoginWebSocketHandler
+    val loginWebSocket: LoginWebSocketHandler,
+    val jwtHandshakeInterceptor: JwtHandshakeInterceptor
 ) : WebSocketConfigurer {
     override fun registerWebSocketHandlers(registry: WebSocketHandlerRegistry) {
         registry.addHandler(this.signupWebsocket, "/ws/signup")
-                .addHandler(this.loginWebSocket, "/ws/login")
-                .setAllowedOrigins("*")
-                .setHandshakeHandler(DefaultHandshakeHandler())
+            .addInterceptors(jwtHandshakeInterceptor)
+            .setAllowedOrigins("*")
+            .setHandshakeHandler(DefaultHandshakeHandler())
+
+        registry.addHandler(this.loginWebSocket, "/ws/login")
+            .addInterceptors(jwtHandshakeInterceptor)
+            .setAllowedOrigins("*")
+            .setHandshakeHandler(DefaultHandshakeHandler())
     }
 }
