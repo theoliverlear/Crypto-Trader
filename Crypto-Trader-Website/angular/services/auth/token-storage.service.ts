6ad67b3882b04849084a67643c2fb4e80a4950e4
theoliverlear <sigwartholiver2@gmail.com>
2025-09-17 22:20:13 -0500

    Add token-storage.service.ts to manage token persistence in memory, session, and local storage for authentication

---
+++ Crypto-Trader-Website/angular/services/auth/token-storage.service.ts
@@ -0,0 +1,38 @@
+import {Injectable} from "@angular/core";
+import {PersistMethod, PossibleToken} from "../../models/auth/types";
+
+@Injectable({ providedIn: 'root' })
+export class TokenStorageService {
+    private tokenInMemory: PossibleToken = null;
+    private readonly storageKey = 'ct_jwt';
+
+    getToken(): PossibleToken {
+        if (this.tokenInMemory) return this.tokenInMemory;
+        try {
+            return sessionStorage.getItem(this.storageKey) || localStorage.getItem(this.storageKey);
+        } catch {
+            return null;
+        }
+    }
+
+    setToken(token: PossibleToken, persist: PersistMethod = 'session'): void {
+        this.tokenInMemory = token;
+        try {
+            sessionStorage.removeItem(this.storageKey);
+            localStorage.removeItem(this.storageKey);
+            if (token) {
+                if (persist === 'local') {
+                    localStorage.setItem(this.storageKey, token);
+                } else if (persist === 'session') {
+                    sessionStorage.setItem(this.storageKey, token);
+                }
+            }
+        } catch {
+            console.error("Key storage failure.")
+        }
+    }
+
+    clear(): void {
+        this.setToken(null);
+    }
+}
