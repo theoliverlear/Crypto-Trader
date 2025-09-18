e87e54bd87dbc63d3e32a330ad82e3955abc2d00
theoliverlear <sigwartholiver2@gmail.com>
2025-09-17 22:19:05 -0500

    Expand crypto-trader.module.ts to include `authInterceptor` with `withInterceptors` for HTTP client configuration

--- Crypto-Trader-Website/angular/modules/crypto-trader.module.ts
+++ Crypto-Trader-Website/angular/modules/crypto-trader.module.ts
@@ -5,7 +5,7 @@ import {FormsModule} from "@angular/forms";
 import {AppRouting} from "./routing/app-routing.module";
 import {RouterOutlet} from "@angular/router";
 import {CommonModule, NgOptimizedImage} from "@angular/common";
-import {HttpClientModule, provideHttpClient, withFetch} from "@angular/common/http";
+import {HttpClientModule, provideHttpClient, withFetch, withInterceptors} from "@angular/common/http";
 import {services} from "../services/services";
 import {BaseChartDirective} from "ng2-charts";
 import {
@@ -15,6 +15,7 @@ import {AppComponent} from "../components/app/app.component";
 import {directives} from "../directives/directives";
 import {elements} from "../components/elements/elements";
 import {pages} from "../components/pages/pages";
+import {authInterceptor} from "../services/intercept/auth.interceptor";
 
 @NgModule({
     declarations: [
@@ -35,7 +36,7 @@ import {pages} from "../components/pages/pages";
     ],
     providers: [
         ...services,
-        provideHttpClient(withFetch()),
+        provideHttpClient(withFetch(), withInterceptors([authInterceptor])),
     ],
     bootstrap: [AppComponent],
     exports: [],
