8e018cabec6cc48f1861bc712e515faf923e5895
theoliverlear <sigwartholiver2@gmail.com>
2025-09-17 20:39:57 -0500

    Expand module-info.java to include additional Spring dependencies and configuration exports in Security-Infrastructure module

--- Crypto-Trader-Library/Security-Library/Security-Infrastructure/src/main/java/module-info.java
+++ Crypto-Trader-Library/Security-Library/Security-Infrastructure/src/main/java/module-info.java
@@ -2,8 +2,12 @@ open module org.cryptotrader.security.library.infrastructure {
     requires kotlin.stdlib;
     requires spring.web;
     requires spring.core;
+    requires spring.beans;
+    requires spring.context;
+    requires spring.boot.autoconfigure;
     requires org.apache.tomcat.embed.core;
     requires org.cryptotrader.security.library.services;
 
     exports org.cryptotrader.security.library.infrastructure;
+    exports org.cryptotrader.security.library.infrastructure.config;
 }
