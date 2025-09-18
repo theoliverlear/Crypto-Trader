858c109dafbe89287ee352b92341e0ceb238532e
theoliverlear <sigwartholiver2@gmail.com>
2025-09-18 08:34:39 -0500

    Expand CryptoTraderContactApplication.kt to include component scanning for internal and external API libraries

--- Crypto-Trader-Contact/src/main/kotlin/org/cryptotrader/contact/CryptoTraderContactApplication.kt
+++ Crypto-Trader-Contact/src/main/kotlin/org/cryptotrader/contact/CryptoTraderContactApplication.kt
@@ -2,8 +2,11 @@ package org.cryptotrader.contact
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.ComponentScan
 
 @SpringBootApplication
+@ComponentScan(basePackages = ["org.cryptotrader.contact.component",
+                               "org.cryptotrader.api.library.component"])
 open class CryptoTraderContactApplication
 
 fun main(args: Array<String>) {
