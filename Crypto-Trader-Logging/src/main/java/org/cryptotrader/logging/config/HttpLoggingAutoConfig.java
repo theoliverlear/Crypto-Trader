1461ef51f81f113852267d047334bf07c564b9f8
theoliverlear <sigwartholiver2@gmail.com>
2025-09-17 22:34:03 -0500

    Refactor HttpLoggingAutoConfig.java to replace `CommonsRequestLoggingFilter` with `HttpExchangeLoggingFilter` and update import paths

--- Crypto-Trader-Logging/src/main/java/org/cryptotrader/logging/config/HttpLoggingAutoConfig.java
+++ Crypto-Trader-Logging/src/main/java/org/cryptotrader/logging/config/HttpLoggingAutoConfig.java
@@ -1,27 +1,30 @@
 package org.cryptotrader.logging.config;
 
-import org.cryptotrader.logging.CryptoTraderHttpLoggingProperties;
+import org.cryptotrader.logging.properties.CryptoTraderHttpLoggingProperties;
+import org.cryptotrader.logging.http.HttpExchangeLoggingFilter;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.filter.CommonsRequestLoggingFilter;
+import org.springframework.web.util.ContentCachingRequestWrapper;
 
 @AutoConfiguration
-@ConditionalOnClass(CommonsRequestLoggingFilter.class)
+@ConditionalOnClass(ContentCachingRequestWrapper.class)
 @ConditionalOnProperty(prefix = "cryptotrader.http.logging", name = "enabled", havingValue = "true", matchIfMissing = true)
 public class HttpLoggingAutoConfig {
     @Bean
     @ConditionalOnMissingBean
-    public CommonsRequestLoggingFilter requestLoggingFilter(CryptoTraderHttpLoggingProperties props) {
-        CommonsRequestLoggingFilter filter = new CommonsRequestLoggingFilter();
-        filter.setIncludeQueryString(props.isIncludeQueryString());
-        filter.setIncludePayload(props.isIncludePayload());
-        filter.setMaxPayloadLength(props.getMaxPayloadLength());
-        filter.setIncludeHeaders(props.isIncludeHeaders());
-        filter.setAfterMessagePrefix(props.getAfterMessagePrefix());
-        return filter;
+    public HttpExchangeLoggingFilter httpExchangeLoggingFilter(CryptoTraderHttpLoggingProperties props) {
+        return new HttpExchangeLoggingFilter(
+                props.isIncludeQueryString(),
+                props.isIncludePayload(),
+                props.getMaxPayloadLength(),
+                props.isIncludeHeaders(),
+                props.isIncludeResponsePayload(),
+                props.getMaxResponsePayloadLength(),
+                props.isColorEnabled()
+        );
     }
 
     @Bean
