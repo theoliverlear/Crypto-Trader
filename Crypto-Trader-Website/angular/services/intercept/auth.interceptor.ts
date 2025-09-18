cce382ca896afa18a2df55556733bf83e8b64a61
theoliverlear <sigwartholiver2@gmail.com>
2025-09-17 22:55:04 -0500

    Add auth.interceptor.ts for injecting Bearer token into HTTP headers in Crypto-Trader-Website

---
+++ Crypto-Trader-Website/angular/services/intercept/auth.interceptor.ts
@@ -0,0 +1,17 @@
+import {inject} from '@angular/core';
+import {HttpInterceptorFn} from '@angular/common/http';
+import {TokenStorageService} from '../auth/token-storage.service';
+
+export const authInterceptor: HttpInterceptorFn = (req, next) => {
+  const tokenStorage = inject(TokenStorageService);
+  const token = tokenStorage.getToken();
+  if (token) {
+    const authReq = req.clone({
+      setHeaders: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+    return next(authReq);
+  }
+  return next(req);
+};
