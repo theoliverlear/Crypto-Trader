42c32ab5bd8ffc10a2a3ed7016f51421a21cf19f
theoliverlear <sigwartholiver2@gmail.com>
2025-09-18 15:37:31 -0500

    Edit module-info.java to remove buggy `jakarta.servlet` dependency in Api-Infrastructure module

--- Crypto-Trader-Library/Api-Library/Api-Infrastructure/src/main/java/module-info.java
+++ Crypto-Trader-Library/Api-Library/Api-Infrastructure/src/main/java/module-info.java
@@ -5,8 +5,7 @@ open module org.cryptotrader.api.library.infrastructure {
     requires spring.web;
     requires spring.websocket;
     requires org.slf4j;
-    requires jakarta.servlet;
-
+    
     requires org.cryptotrader.api.library.services;
     requires org.cryptotrader.api.library.models;
 
