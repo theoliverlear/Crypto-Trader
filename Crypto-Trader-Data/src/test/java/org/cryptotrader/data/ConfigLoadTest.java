23a0c52a09482581e7ec0874551ba6341db785ec
theoliverlear <sigwartholiver2@gmail.com>
2025-09-18 08:06:23 -0500

    Expand ConfigLoadTest.java to include mocked `CurrencyDataRetriever` and exclude `CryptoTraderDocsAutoConfiguration`

--- Crypto-Trader-Data/src/test/java/org/cryptotrader/data/ConfigLoadTest.java
+++ Crypto-Trader-Data/src/test/java/org/cryptotrader/data/ConfigLoadTest.java
@@ -5,11 +5,23 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.env.Environment;
+import org.cryptotrader.api.library.component.CurrencyDataRetriever;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest(classes = CryptoTraderDataApplication.class, webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@SpringBootTest(
+        classes = CryptoTraderDataApplication.class,
+        webEnvironment = SpringBootTest.WebEnvironment.NONE,
+        properties = {
+                "spring.autoconfigure.exclude=org.cryptotrader.docs.autoconfigure.CryptoTraderDocsAutoConfiguration",
+                "docs.autoconfigure.enabled=false"
+        }
+)
 class ConfigLoadTest extends CryptoTraderTest {
+
+    @MockitoBean
+    private CurrencyDataRetriever currencyDataRetriever;
 
     @Autowired
     private Environment environment;
