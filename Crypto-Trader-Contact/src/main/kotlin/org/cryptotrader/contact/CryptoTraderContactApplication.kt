5284b98e4b8899dcb0a0c384781aace4ada71259
theoliverlear <sigwartholiver2@gmail.com>
2025-09-18 08:39:03 -0500

    Refactor CryptoTraderContactApplication.kt to remove buggy component scanning annotations

--- Crypto-Trader-Contact/src/main/kotlin/org/cryptotrader/contact/CryptoTraderContactApplication.kt
+++ Crypto-Trader-Contact/src/main/kotlin/org/cryptotrader/contact/CryptoTraderContactApplication.kt
@@ -2,11 +2,8 @@ package org.cryptotrader.contact
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.ComponentScan
 
 @SpringBootApplication
-@ComponentScan(basePackages = ["org.cryptotrader.contact.component",
-                               "org.cryptotrader.api.library.component"])
 open class CryptoTraderContactApplication
 
 fun main(args: Array<String>) {
