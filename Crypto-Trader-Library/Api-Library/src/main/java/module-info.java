ae9b107d9c4262d43fd3c3e158d12fc96ed13a8f
theoliverlear <sigwartholiver2@gmail.com>
2025-09-18 07:11:58 -0500

    Add `org.cryptotrader.api.library.services` as a transitive dependency in module-info.java for Api-Library module

--- Crypto-Trader-Library/Api-Library/src/main/java/module-info.java
+++ Crypto-Trader-Library/Api-Library/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module org.cryptotrader.api.library {
     requires kotlin.stdlib;
     requires transitive org.cryptotrader.api.library.communication;
     requires transitive org.cryptotrader.api.library.components;
+    requires transitive org.cryptotrader.api.library.services;
     requires transitive org.cryptotrader.api.library.models;
     requires transitive org.cryptotrader.api.library.repositories;
     requires transitive org.cryptotrader.api.library.events;
