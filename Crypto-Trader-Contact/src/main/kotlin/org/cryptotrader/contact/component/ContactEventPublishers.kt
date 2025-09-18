a7b6a6e40a512eeaecc65a810f28616e9154a9a1
theoliverlear <sigwartholiver2@gmail.com>
2025-09-18 08:33:44 -0500

    Clear out unimplemented, buggy code in ContactEventPublishers.kt for Crypto-Trader-Contact

--- Crypto-Trader-Contact/src/main/kotlin/org/cryptotrader/contact/component/ContactEventPublishers.kt
+++ Crypto-Trader-Contact/src/main/kotlin/org/cryptotrader/contact/component/ContactEventPublishers.kt
@@ -1,13 +1,6 @@
 package org.cryptotrader.contact.component
 
-import org.cryptotrader.api.library.component.EventPublisher
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean
 import org.springframework.context.annotation.Configuration
 
-@ConditionalOnBean(EventPublisher::class)
 @Configuration
-open class ContactEventPublishers(
-    private val eventPublisher: EventPublisher
-) {
-
-}
+open class ContactEventPublishers
