1a2482fe05c01afe94420ad7a1f4780cc5768d5d
theoliverlear <sigwartholiver2@gmail.com>
2025-09-19 11:01:57 -0500

    Refactor CurrencyController.java files to migrate appropriate endpoints between Crypto-Trader-Api and Crypto-Trader-Data

--- Crypto-Trader-Api/src/main/java/org/cryptotrader/api/controller/CurrencyController.java
+++ Crypto-Trader-Api/src/main/java/org/cryptotrader/api/controller/CurrencyController.java
@@ -1,6 +1,9 @@
 package org.cryptotrader.api.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.cryptotrader.api.library.communication.response.DisplayCurrencyListResponse;
 import org.cryptotrader.api.library.communication.response.PerformanceRatingResponse;
+import org.cryptotrader.api.library.communication.response.TimeValueResponse;
 import org.cryptotrader.api.library.model.currency.PerformanceRating;
 import org.cryptotrader.api.library.services.CurrencyService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +15,9 @@ import org.cryptotrader.api.library.communication.request.AssetValueRequest;
 import org.cryptotrader.api.library.communication.response.AssetValueResponse;
 import org.cryptotrader.api.library.entity.currency.Currency;
 
+import java.util.List;
+
+@Slf4j
 @RestController
 @Profile("!docs")
 @RequestMapping("/api/currency")
@@ -37,8 +43,26 @@ public class CurrencyController {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         } else {
             PerformanceRating performanceRatingResponse = this.currencyService.getDayPerformance(currencyCode);
-            return new ResponseEntity<>(new PerformanceRatingResponse(performanceRatingResponse),
+            String changePercent = this.currencyService.getPercentageDayPerformance(currencyCode);
+            log.info("Currency performance: {} - {}", performanceRatingResponse, changePercent);
+            return new ResponseEntity<>(new PerformanceRatingResponse(performanceRatingResponse, changePercent),
                                         HttpStatus.OK);
         }
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<DisplayCurrencyListResponse> getAll() {
+        return new ResponseEntity<>(this.currencyService.getCurrencyValuesResponse(), HttpStatus.OK);
+    }
+
+    @GetMapping("/history/{code}")
+    public ResponseEntity<List<TimeValueResponse>> getHistory(@PathVariable("code") String code,
+                                                              @RequestParam(value = "hours", defaultValue = "24") int hours,
+                                                              @RequestParam(value = "intervalSeconds", defaultValue = "60") int intervalSeconds) {
+        List<TimeValueResponse> history = this.currencyService.getCurrencyHistory(code, hours, intervalSeconds);
+        if (history == null || history.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(history, HttpStatus.OK);
     }
 }
--- Crypto-Trader-Data/src/main/java/org/cryptotrader/data/controller/CurrencyController.java
+++ Crypto-Trader-Data/src/main/java/org/cryptotrader/data/controller/CurrencyController.java
@@ -1,5 +1,10 @@
 package org.cryptotrader.data.controller;
 
+import org.cryptotrader.api.library.communication.request.AssetValueRequest;
+import org.cryptotrader.api.library.communication.response.AssetValueResponse;
+import org.cryptotrader.api.library.communication.response.PerformanceRatingResponse;
+import org.cryptotrader.api.library.entity.currency.Currency;
+import org.cryptotrader.api.library.model.currency.PerformanceRating;
 import org.cryptotrader.api.library.services.CurrencyService;
 import org.cryptotrader.api.library.communication.response.DisplayCurrencyListResponse;
 import org.cryptotrader.api.library.communication.response.TimeValueResponse;
@@ -19,21 +24,5 @@ public class CurrencyController {
     @Autowired
     public CurrencyController(CurrencyService currencyService) {
         this.currencyService = currencyService;
-    }
-
-    @GetMapping("/all")
-    public ResponseEntity<DisplayCurrencyListResponse> getAll() {
-        return new ResponseEntity<>(this.currencyService.getCurrencyValuesResponse(), HttpStatus.OK);
-    }
-
-    @GetMapping("/history/{code}")
-    public ResponseEntity<List<TimeValueResponse>> getHistory(@PathVariable("code") String code,
-                                                              @RequestParam(value = "hours", defaultValue = "24") int hours,
-                                                              @RequestParam(value = "intervalSeconds", defaultValue = "60") int intervalSeconds) {
-        List<TimeValueResponse> history = this.currencyService.getCurrencyHistory(code, hours, intervalSeconds);
-        if (history == null || history.isEmpty()) {
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        }
-        return new ResponseEntity<>(history, HttpStatus.OK);
     }
 }
