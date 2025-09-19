3e0724019eaf99173eab1894c7029b481ff81160
theoliverlear <sigwartholiver2@gmail.com>
2025-09-19 10:58:23 -0500

    Edit currency-day-performance.service.ts to replace `PerformanceRatingResponse` with `PerformanceRating` for type consistency

--- Crypto-Trader-Website/angular/services/net/http/currency-day-performance.service.ts
+++ Crypto-Trader-Website/angular/services/net/http/currency-day-performance.service.ts
@@ -1,6 +1,6 @@
 import {Injectable} from "@angular/core";
 import {HttpClientService} from "@theoliverlear/angular-suite";
-import {PerformanceRatingResponse} from "../../../models/currency/types";
+import {PerformanceRating} from "../../../models/currency/types";
 import {environment} from "../../../environments/environment";
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
@@ -8,14 +8,14 @@ import {Observable} from "rxjs";
 @Injectable({
     providedIn: 'root'
 })
-export class CurrencyDayPerformanceService extends HttpClientService<any, PerformanceRatingResponse> {
+export class CurrencyDayPerformanceService extends HttpClientService<any, PerformanceRating> {
     private static readonly URL: string = `${environment.apiUrl}/currency/performance/`;
     
     constructor(httpClient: HttpClient) {
         super(CurrencyDayPerformanceService.URL, httpClient);
     }
     
-    public getCurrencyDayPerformance(currencyCode: string): Observable<PerformanceRatingResponse> {
+    public getCurrencyDayPerformance(currencyCode: string): Observable<PerformanceRating> {
         this.url = `${CurrencyDayPerformanceService.URL}${currencyCode}`;
         return this.get();
     }
