e679d3a3b75b5f135ef17097aa735a07e3f67754
theoliverlear <sigwartholiver2@gmail.com>
2025-09-18 19:44:13 -0500

    Add Kotlin as a required dependency in module-info.java for Security-Models

--- Crypto-Trader-Library/Security-Library/Security-Models/src/main/java/module-info.java
+++ Crypto-Trader-Library/Security-Library/Security-Models/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 open module org.cryptotrader.security.library.models {
     requires static jakarta.persistence;
     requires static lombok;
+    requires kotlin.stdlib;
     requires transitive org.cryptotrader.api.library.models;
 
     exports org.cryptotrader.security.library.entity;
