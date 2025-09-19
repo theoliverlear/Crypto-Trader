d9832860d8b9f5b9da680a03df84dd69d6020ce9
theoliverlear <sigwartholiver2@gmail.com>
2025-09-18 21:07:18 -0500

    Edit module-info.java to include `org.slf4j` module for logging functionality in Api-Events module

--- Crypto-Trader-Library/Api-Library/Api-Events/src/main/java/module-info.java
+++ Crypto-Trader-Library/Api-Library/Api-Events/src/main/java/module-info.java
@@ -5,6 +5,7 @@ open module org.cryptotrader.api.library.events {
     requires spring.core;
     requires org.cryptotrader.api.library.models;
     requires org.cryptotrader.api.library.components;
+    requires org.slf4j;
 
     exports org.cryptotrader.api.library.events;
     exports org.cryptotrader.api.library.events.publisher;
