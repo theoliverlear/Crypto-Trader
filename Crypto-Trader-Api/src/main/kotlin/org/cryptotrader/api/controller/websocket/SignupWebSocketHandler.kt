68b68708fcb7cd40ec6c53494a9e98233eed0748
theoliverlear <sigwartholiver2@gmail.com>
2025-09-19 11:25:20 -0500

    Refactor SignupWebSocketHandler.kt to replace KotlinLogging with SLF4J and adjust logging format

--- Crypto-Trader-Api/src/main/kotlin/org/cryptotrader/api/controller/websocket/SignupWebSocketHandler.kt
+++ Crypto-Trader-Api/src/main/kotlin/org/cryptotrader/api/controller/websocket/SignupWebSocketHandler.kt
@@ -1,14 +1,14 @@
 package org.cryptotrader.api.controller.websocket
 
 import com.sigwarthsoftware.springboot.websocket.WebSocketHandler
-import io.github.oshai.kotlinlogging.KotlinLogging
+import org.slf4j.LoggerFactory
 import org.cryptotrader.api.library.communication.request.SignupRequest
 import org.cryptotrader.api.library.communication.request.alias.HttpAuthStatus
 import org.cryptotrader.api.library.communication.response.AuthResponse
 import org.cryptotrader.api.library.services.AuthService
 import org.springframework.stereotype.Component
 
-private val log = KotlinLogging.logger {  }
+private val log = LoggerFactory.getLogger(SignupWebSocketHandler::class.java)
 
 @Component
 class SignupWebSocketHandler(
@@ -16,7 +16,7 @@ class SignupWebSocketHandler(
 ) : WebSocketHandler<SignupRequest, AuthResponse>() {
     
     override fun makeResponse(signupRequest: SignupRequest): AuthResponse {
-        log.info { "Signup: ${signupRequest.email}" }
+        log.info("Signup: {}", signupRequest.email)
         val response: HttpAuthStatus = this.authService.signup(signupRequest)
         return AuthResponse(response.payload.isAuthorized)
     }
