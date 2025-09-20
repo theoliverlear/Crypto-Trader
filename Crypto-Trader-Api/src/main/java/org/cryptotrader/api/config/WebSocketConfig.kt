4a5a0aaaa535cc12450c6e799e92b2fa2d895c36
theoliverlear <sigwartholiver2@gmail.com>
2025-09-19 21:53:32 -0500

    Refactor WebSocketConfig.kt to rename `JwtService` to `JwtTokenService` in Crypto-Trader-Api

--- Crypto-Trader-Api/src/main/java/org/cryptotrader/api/config/WebSocketConfig.kt
+++ Crypto-Trader-Api/src/main/java/org/cryptotrader/api/config/WebSocketConfig.kt
@@ -4,7 +4,7 @@ import org.cryptotrader.api.controller.websocket.CurrencyValueWebSocketHandler
 import org.cryptotrader.api.controller.websocket.LoginWebSocketHandler
 import org.cryptotrader.api.controller.websocket.SignupWebSocketHandler
 import org.cryptotrader.api.library.infrastructure.JwtHandshakeInterceptor
-import org.cryptotrader.api.library.services.JwtService
+import org.cryptotrader.api.library.services.JwtTokenService
 import org.cryptotrader.api.library.services.ProductUserService
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
@@ -29,7 +29,7 @@ open class WebSocketConfig(
 
     @Bean
     @ConditionalOnMissingBean(JwtHandshakeInterceptor::class)
-    open fun jwtHandshakeInterceptor(jwtService: JwtService,
+    open fun jwtHandshakeInterceptor(jwtService: JwtTokenService,
                                      productUserService: ProductUserService
     ): JwtHandshakeInterceptor = JwtHandshakeInterceptor(jwtService, productUserService)
 
