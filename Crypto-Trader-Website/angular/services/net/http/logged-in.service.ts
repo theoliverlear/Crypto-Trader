dc4b6ae95f452a176f04357a56ede5e99d0f1d1b
theoliverlear <sigwartholiver2@gmail.com>
2025-09-20 15:47:33 -0500

    Inject `TokenStorageService` and update response handling in logged-in.service.ts for token management in Crypto-Trader-Website

--- Crypto-Trader-Website/angular/services/net/http/logged-in.service.ts
+++ Crypto-Trader-Website/angular/services/net/http/logged-in.service.ts
@@ -2,19 +2,29 @@ import {Injectable} from "@angular/core";
 import {HttpClientService} from "@theoliverlear/angular-suite";
 import {environment} from "../../../environments/environment";
 import {HttpClient} from "@angular/common/http";
-import {Observable} from "rxjs";
+import {Observable, map} from "rxjs";
+import {AuthResponse} from "../../../models/auth/types";
+import {TokenStorageService} from "../../auth/token-storage.service";
 
 @Injectable({
     providedIn: 'root'
 })
-export class LoggedInService extends HttpClientService<any, boolean> {
+export class LoggedInService extends HttpClientService<any, AuthResponse> {
     private static readonly URL: string = `${environment.apiUrl}/auth/logged-in`;
     
-    constructor(httpClient: HttpClient) {
+    constructor(httpClient: HttpClient, private tokenStorage: TokenStorageService) {
         super(LoggedInService.URL, httpClient);
     }
     
-    public isLoggedIn(): Observable<boolean> {
-        return this.get();
+    public isLoggedIn(): Observable<AuthResponse> {
+        return this.get().pipe(
+            map((response: AuthResponse) => {
+                if (response?.authorized && !response.token) {
+                    const token: string = this.tokenStorage.getToken();
+                    return token ? { ...response, token } : response;
+                }
+                return response;
+            })
+        );
     }
 }
