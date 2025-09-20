483b4fb49db15a8b8d026a209cce1aee1916fe31
theoliverlear <sigwartholiver2@gmail.com>
2025-09-20 15:25:32 -0500

    Add ApiInfrastructureTestSuite.kt to group and execute API infrastructure tests

---
+++ Crypto-Trader-Library/Api-Library/Api-Infrastructure/src/test/kotlin/org/cryptotrader/api/library/infrastructure/ApiInfrastructureTestSuite.kt
@@ -0,0 +1,8 @@
+package org.cryptotrader.api.library.infrastructure
+
+import org.junit.platform.suite.api.SelectPackages
+import org.junit.platform.suite.api.Suite
+
+@Suite
+@SelectPackages("org.cryptotrader.api.library.infrastructure")
+class ApiInfrastructureTestSuite
