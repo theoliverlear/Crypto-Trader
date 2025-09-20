94d1899d230f6b95390dcd7bbea040bba8aefa2b
theoliverlear <sigwartholiver2@gmail.com>
2025-09-19 22:20:57 -0500

    Refactor HoneypotControllerTest.kt to replace deprecated `@SpyBean` with `@MockitoSpyBean`

--- Crypto-Trader-Api/src/test/kotlin/org/cryptotrader/api/controller/HoneypotControllerTest.kt
+++ Crypto-Trader-Api/src/test/kotlin/org/cryptotrader/api/controller/HoneypotControllerTest.kt
@@ -2,20 +2,16 @@ package org.cryptotrader.api.controller
 
 import org.cryptotrader.api.service.HoneypotService
 import org.junit.jupiter.api.Assertions.assertFalse
-import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.mockito.ArgumentMatchers
-import org.mockito.Mockito.atLeastOnce
-import org.mockito.Mockito.clearInvocations
-import org.mockito.Mockito.verify
+import org.mockito.Mockito.*
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.boot.test.mock.mockito.SpyBean
 import org.springframework.core.env.Environment
 import org.springframework.test.context.TestPropertySource
+import org.springframework.test.context.bean.override.mockito.MockitoSpyBean
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
@@ -34,8 +30,8 @@ class HoneypotControllerTest {
 
     @Autowired
     private lateinit var mockMvc: MockMvc
-
-    @SpyBean
+    
+    @MockitoSpyBean
     private lateinit var honeypotService: HoneypotService
 
     @Test
