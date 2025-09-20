392f027cd7e9a51be6e634e6d7654400fcbdb84b
theoliverlear <sigwartholiver2@gmail.com>
2025-09-20 15:44:34 -0500

    Refactor auth.interceptor.ts to use consistent indentation and spacing in authorization header logic

--- Crypto-Trader-Website/angular/services/intercept/auth.interceptor.ts
+++ Crypto-Trader-Website/angular/services/intercept/auth.interceptor.ts
@@ -3,15 +3,15 @@ import {HttpInterceptorFn} from '@angular/common/http';
 import {TokenStorageService} from '../auth/token-storage.service';
 
 export const authInterceptor: HttpInterceptorFn = (request, next) => {
-  const tokenStorage: TokenStorageService = inject(TokenStorageService);
-  const token: string = tokenStorage.getToken();
-  if (token) {
-    const authRequest = request.clone({
-      setHeaders: {
-        Authorization: `Bearer ${token}`
-      }
-    });
-    return next(authRequest);
-  }
-  return next(request);
+    const tokenStorage: TokenStorageService = inject(TokenStorageService);
+    const token: string = tokenStorage.getToken();
+    if (token) {
+        const authRequest = request.clone({
+            setHeaders: {
+                Authorization: `Bearer ${token}`
+            }
+        });
+        return next(authRequest);
+    }
+    return next(request);
 };
