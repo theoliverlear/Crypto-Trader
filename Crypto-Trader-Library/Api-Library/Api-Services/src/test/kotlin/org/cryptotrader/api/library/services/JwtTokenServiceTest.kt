d35b4b8d77d05553a61f9bed6fa89dc7ae265ef8
theoliverlear <sigwartholiver2@gmail.com>
2025-09-19 22:33:25 -0500

    Add JwtTokenServiceTest.kt to test `JwtTokenService` functionality in Api-Services

---
+++ Crypto-Trader-Library/Api-Library/Api-Services/src/test/kotlin/org/cryptotrader/api/library/services/JwtTokenServiceTest.kt
@@ -0,0 +1,9 @@
+package org.cryptotrader.api.library.services
+
+import org.cryptotrader.test.CryptoTraderTest
+import org.junit.jupiter.api.DisplayName
+
+@DisplayName("JWT Token Service")
+class JwtTokenServiceTest : CryptoTraderTest() {
+    
+}
