169b5d0321355d4e8a120ebfc0ade4ea5b940658
theoliverlear <sigwartholiver2@gmail.com>
2025-09-19 21:52:19 -0500

    Add token-storage.service.spec.ts to test `TokenStorageService` methods in Crypto-Trader-Website

---
+++ Crypto-Trader-Website/angular/test/services/auth/token-storage.service.spec.ts
@@ -0,0 +1,27 @@
+import {PossibleToken} from "../../../models/auth/types";
+import {
+    TokenStorageService
+} from "../../../services/auth/token-storage.service";
+
+describe('TokenStorageService', () => {
+    let tokenStorageService: TokenStorageService;
+    beforeEach(() => {
+        sessionStorage.clear();
+        tokenStorageService = new TokenStorageService();
+    });
+    describe('setToken', () => {
+        it('should set a token in session storage', () => {
+            const token: PossibleToken = "mock-token";
+            tokenStorageService.setToken(token);
+            expect(sessionStorage.getItem('ct_jwt')).toBe(token);
+        });
+    });
+    describe('getToken', () => {
+        it('should return a token if present', () => {
+            const token: string = "mock-token";
+            tokenStorageService.setToken(token);
+            const possibleToken: PossibleToken = tokenStorageService.getToken();
+            expect(possibleToken).toBe(token);
+        });
+    });
+});
