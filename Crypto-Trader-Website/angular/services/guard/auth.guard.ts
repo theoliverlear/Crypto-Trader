88ab369fcb5d6419efa610dcd6f302cfc63ff771
theoliverlear <sigwartholiver2@gmail.com>
2025-09-20 15:44:11 -0500

    Inject `TokenStorageService` into auth.guard.ts to manage token-related authorization logic

--- Crypto-Trader-Website/angular/services/guard/auth.guard.ts
+++ Crypto-Trader-Website/angular/services/guard/auth.guard.ts
@@ -7,29 +7,35 @@ import {
 } from "@angular/router";
 import {catchError, map, Observable} from "rxjs";
 import {LoggedInService} from "../net/http/logged-in.service";
+import {TokenStorageService} from "../auth/token-storage.service";
 
 @Injectable({
     providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
     constructor(private router: Router,
-                private loggedInService: LoggedInService) {
+                private loggedInService: LoggedInService,
+                private tokenStorageService: TokenStorageService) {
         
     }
     
     canActivate(route: ActivatedRouteSnapshot,
                 state: RouterStateSnapshot): Observable<boolean> {
         return this.loggedInService.isLoggedIn().pipe(
-            map(isAuthorized => {
-                if (isAuthorized) {
+            map(authResponse => {
+                console.log('AuthGuard: isAuthorized', authResponse);
+                if (authResponse.authorized) {
                     return true;
                 } else {
-                    this.router.navigate(['/authorize']);
-                    return false;
+                    this.router.navigate(['/authorize']).then(navigated => {
+                        return false;
+                    });
                 }
             }),
             catchError(() => {
-                this.router.navigate(['/authorize']);
+                this.router.navigate(['/authorize']).then(navigated => {
+                    
+                });
                 return new Observable<boolean>(observer => {
                     observer.next(false);
                 });
