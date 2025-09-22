6e6b71e92989522412d742efb81a01424b8613be
theoliverlear <sigwartholiver2@gmail.com>
2025-09-22 15:40:16 -0500

    Add `asset-buttons` component to manage asset-related buttons in Crypto-Trader-Website

---
+++ Crypto-Trader-Website/angular/components/elements/element-group-portfolio/asset-buttons/asset-buttons.component.ts
@@ -0,0 +1,14 @@
+// asset-buttons.component.ts
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'asset-buttons',
+    templateUrl: './asset-buttons.component.html',
+    styleUrls: ['./asset-buttons.component.scss'],
+    standalone: false
+})
+export class AssetButtonsComponent {
+    constructor() {
+        
+    }
+}
