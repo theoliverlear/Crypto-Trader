50e551ec674b9cb478e025c9d98452d246ee13fb
theoliverlear <sigwartholiver2@gmail.com>
2025-09-22 16:24:16 -0500

    Update nav-bar.component.ts to integrate login status display and sync navigation links with updated assets in Crypto-Trader-Website

--- Crypto-Trader-Website/angular/components/elements/element-group-nav/nav-bar/nav-bar.component.ts
+++ Crypto-Trader-Website/angular/components/elements/element-group-nav/nav-bar/nav-bar.component.ts
@@ -1,12 +1,16 @@
 import {
-    Component
+    Component, OnInit
 } from '@angular/core';
 import {
-    navBarCurrenciesLink,
+    navBarCurrenciesTextLink,
     navBarHomeLink,
-    navBarPortfolioLink,
-    navBarTraderLink
+    navBarPortfolioTextLink,
+    navBarTraderTextLink
 } from "../../../../assets/elementLinkAssets";
+import {
+    LoggedInService
+} from "../../../../services/net/http/auth/status/logged-in.service";
+import {NavBarItemOption} from "../nav-bar-item/models/NavBarItemOption";
 
 
 @Component({
@@ -15,13 +19,22 @@ import {
     templateUrl: './nav-bar.component.html',
     styleUrls: ['./nav-bar.component.scss']
 })
-export class NavBarComponent {
-    constructor() {
-        console.log('NavBarComponent loaded');
+export class NavBarComponent implements OnInit {
+    isLoggedIn: boolean = false;
+    constructor(private loggedInService: LoggedInService) {
+        
+    }
+
+    ngOnInit() {
+        this.loggedInService.getAuthState().subscribe((authStatus: boolean) => {
+            this.isLoggedIn = authStatus;
+        });
+        this.loggedInService.isLoggedIn().subscribe();
     }
 
     protected readonly navBarHomeLink = navBarHomeLink;
-    protected readonly navBarPortfolioLink = navBarPortfolioLink;
-    protected readonly navBarTraderLink = navBarTraderLink;
-    protected readonly navBarCurrenciesLink = navBarCurrenciesLink;
+    protected readonly navBarPortfolioLink = navBarPortfolioTextLink;
+    protected readonly navBarTraderLink = navBarTraderTextLink;
+    protected readonly navBarCurrenciesLink = navBarCurrenciesTextLink;
+    protected readonly NavBarItemOption = NavBarItemOption;
 }
