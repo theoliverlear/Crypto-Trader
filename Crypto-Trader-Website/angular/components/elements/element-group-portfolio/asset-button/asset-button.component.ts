f93241da23c7865cdd64e32262829c6d18366bbb
theoliverlear <sigwartholiver2@gmail.com>
2025-09-22 15:39:44 -0500

    Add `asset-button` element to be a generalized button for asset functions in Crypto-Trader-Website

---
+++ Crypto-Trader-Website/angular/components/elements/element-group-portfolio/asset-button/asset-button.component.ts
@@ -0,0 +1,14 @@
+// asset-button.component.ts
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'asset-button',
+    templateUrl: './asset-button.component.html',
+    styleUrls: ['./asset-button.component.scss'],
+    standalone: false
+})
+export class AssetButtonComponent {
+    constructor() {
+        
+    }
+}
