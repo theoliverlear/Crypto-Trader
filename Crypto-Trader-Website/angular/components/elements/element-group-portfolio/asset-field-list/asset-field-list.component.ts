397359a549c2170721f75b001ec041924728b544
theoliverlear <sigwartholiver2@gmail.com>
2025-09-22 15:42:10 -0500

    Add `asset-field-list` component to list fields for portfolio assets in Crypto-Trader-Website

---
+++ Crypto-Trader-Website/angular/components/elements/element-group-portfolio/asset-field-list/asset-field-list.component.ts
@@ -0,0 +1,14 @@
+// asset-field-list.component.ts
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'asset-field-list',
+    templateUrl: './asset-field-list.component.html',
+    styleUrls: ['./asset-field-list.component.scss'],
+    standalone: false
+})
+export class AssetFieldListComponent {
+    constructor() {
+        
+    }
+}
