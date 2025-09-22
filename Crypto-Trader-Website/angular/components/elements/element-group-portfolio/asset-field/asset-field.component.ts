cf3a48208e0d5aa862be996d99d7c954307d6f0a
theoliverlear <sigwartholiver2@gmail.com>
2025-09-22 15:41:28 -0500

    Add `asset-field` component to display portfolio asset data in Crypto-Trader-Website

---
+++ Crypto-Trader-Website/angular/components/elements/element-group-portfolio/asset-field/asset-field.component.ts
@@ -0,0 +1,14 @@
+// asset-field.component.ts
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'asset-field',
+    templateUrl: './asset-field.component.html',
+    styleUrls: ['./asset-field.component.scss'],
+    standalone: false
+})
+export class AssetFieldComponent {
+    constructor() {
+        
+    }
+}
