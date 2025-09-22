6da856be6a7ffd1ec1a243df24648646e50e4279
theoliverlear <sigwartholiver2@gmail.com>
2025-09-22 15:42:58 -0500

    Add `portfolio-asset` component to represent individual portfolio assets in Crypto-Trader-Website

---
+++ Crypto-Trader-Website/angular/components/elements/element-group-portfolio/portfolio-asset/portfolio-asset.component.ts
@@ -0,0 +1,14 @@
+// portfolio-asset.component.ts
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'portfolio-asset',
+    templateUrl: './portfolio-asset.component.html',
+    styleUrls: ['./portfolio-asset.component.scss'],
+    standalone: false
+})
+export class PortfolioAssetComponent {
+    constructor() {
+        
+    }
+}
