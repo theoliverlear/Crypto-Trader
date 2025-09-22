9b120f984cf41df2e06fdfb72a91aaaf785905ad
theoliverlear <sigwartholiver2@gmail.com>
2025-09-21 19:41:05 -0500

    Add `dashboard` page to act as the homepage for logged-in users in Crypto-Trader-Website

---
+++ Crypto-Trader-Website/angular/components/pages/dashboard/dashboard.component.ts
@@ -0,0 +1,14 @@
+// dashboard.component.ts
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'dashboard',
+    templateUrl: './dashboard.component.html',
+    styleUrls: ['./dashboard.component.scss'],
+    standalone: false,
+})
+export class DashboardComponent {
+    constructor() {
+        
+    }
+}
