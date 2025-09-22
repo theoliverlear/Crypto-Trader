2d9cc9514c410046f6214b4ac24083d4e28a3482
theoliverlear <sigwartholiver2@gmail.com>
2025-09-22 15:54:18 -0500

    Add `portfolio-statistic` component to display a portfolio statistic in Crypto-Trader-Website

---
+++ Crypto-Trader-Website/angular/components/elements/element-group-portfolio/portfolio-statistic/portfolio-statistic.component.ts
@@ -0,0 +1,14 @@
+// portfolio-statistic.component.ts
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'portfolio-stat',
+    templateUrl: './portfolio-statistic.component.html',
+    styleUrls: ['./portfolio-statistic.component.scss'],
+    standalone: false
+})
+export class PortfolioStatisticComponent {
+    constructor() {
+        
+    }
+}
