89c6d0a7dd08e04a1fb54f178917cad7214806ba
theoliverlear <sigwartholiver2@gmail.com>
2025-09-22 15:54:50 -0500

    Add `portfolio-statistics` component to display multiple portfolio statistics in Crypto-Trader-Website

---
+++ Crypto-Trader-Website/angular/components/elements/element-group-portfolio/portfolio-statistics/portfolio-statistics.component.ts
@@ -0,0 +1,14 @@
+// portfolio-statistics.component.ts
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'portfolio-stats',
+    templateUrl: './portfolio-statistics.component.html',
+    styleUrls: ['./portfolio-statistics.component.scss'],
+    standalone: false
+})
+export class PortfolioStatisticsComponent {
+    constructor() {
+        
+    }
+}
