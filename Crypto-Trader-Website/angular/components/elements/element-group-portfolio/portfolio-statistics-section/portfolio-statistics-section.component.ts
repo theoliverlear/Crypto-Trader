b743e23024f341c4e9bde9f5f44652dbe5afe3ab
theoliverlear <sigwartholiver2@gmail.com>
2025-09-22 15:55:50 -0500

    Add `portfolio-statistics-section` component to represent a section displaying portfolio statistics in Crypto-Trader-Website

---
+++ Crypto-Trader-Website/angular/components/elements/element-group-portfolio/portfolio-statistics-section/portfolio-statistics-section.component.ts
@@ -0,0 +1,14 @@
+// portfolio-statistics-section.component.ts
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'portfolio-stats-section',
+    templateUrl: './portfolio-statistics-section.component.html',
+    styleUrls: ['./portfolio-statistics-section.component.scss'],
+    standalone: false
+})
+export class PortfolioStatisticsSectionComponent {
+    constructor() {
+        
+    }
+}
