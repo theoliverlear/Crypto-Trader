0d74a33c8243be5f6d80693decfb046c8da1e98a
theoliverlear <sigwartholiver2@gmail.com>
2025-09-22 15:44:17 -0500

    Add `portfolio-asset-list` component to display a list of portfolio assets in Crypto-Trader-Website

---
+++ Crypto-Trader-Website/angular/components/elements/element-group-portfolio/portfolio-asset-list/portfolio-asset-list.component.ts
@@ -0,0 +1,14 @@
+// portfolio-asset-list.component.ts
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'portfolio-asset-list',
+    templateUrl: './portfolio-asset-list.component.html',
+    styleUrls: ['./portfolio-asset-list.component.scss'],
+    standalone: false
+})
+export class PortfolioAssetListComponent {
+    constructor() {
+        
+    }
+}
