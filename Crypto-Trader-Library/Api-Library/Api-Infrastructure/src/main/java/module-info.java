586d55a7780af50171f66825745e5d94bc537d63
theoliverlear <sigwartholiver2@gmail.com>
2025-09-21 19:04:03 -0500

    Expand module-info.java to require additional dependencies and export DPoP infrastructure package for Api-Infrastructure

--- Crypto-Trader-Library/Api-Library/Api-Infrastructure/src/main/java/module-info.java
+++ Crypto-Trader-Library/Api-Library/Api-Infrastructure/src/main/java/module-info.java
@@ -4,11 +4,16 @@ open module org.cryptotrader.api.library.infrastructure {
     requires spring.beans;
     requires spring.web;
     requires spring.websocket;
+    requires spring.boot.autoconfigure;
     requires org.slf4j;
     requires org.apache.tomcat.embed.core;
     
     requires org.cryptotrader.api.library.services;
     requires org.cryptotrader.api.library.models;
+    requires org.cryptotrader.api.library.scripts;
+    requires org.cryptotrader.api.library.components;
+    requires com.auth0.jwt;
 
     exports org.cryptotrader.api.library.infrastructure;
+    exports org.cryptotrader.api.library.infrastructure.dpop;
 }
