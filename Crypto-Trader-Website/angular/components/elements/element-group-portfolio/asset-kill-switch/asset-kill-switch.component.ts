c69071b821bcb25e0abde9df8202cc884070168a
theoliverlear <sigwartholiver2@gmail.com>
2025-09-22 15:42:42 -0500

    Add `asset-kill-switch` component to enable kill switch functionality for portfolio assets in Crypto-Trader-Website

---
+++ Crypto-Trader-Website/angular/components/elements/element-group-portfolio/asset-kill-switch/asset-kill-switch.component.ts
@@ -0,0 +1,14 @@
+// asset-kill-switch.component.ts
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'asset-kill-switch',
+    templateUrl: './asset-kill-switch.component.html',
+    styleUrls: ['./asset-kill-switch.component.scss'],
+    standalone: false
+})
+export class AssetKillSwitchComponent {
+    constructor() {
+        
+    }
+}
