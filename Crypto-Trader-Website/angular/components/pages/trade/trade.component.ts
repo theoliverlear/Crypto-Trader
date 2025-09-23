fa4e5245dd822a538fa8203150a9c5c97669560a
theoliverlear <sigwartholiver2@gmail.com>
2025-09-23 14:22:15 -0500

    Add `trade` page for buying and selling currencies through vendors in Crypto-Trader-Website

---
+++ Crypto-Trader-Website/angular/components/pages/trade/trade.component.ts
@@ -0,0 +1,14 @@
+// trade.component.ts
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'trade',
+    templateUrl: './trade.component.html',
+    styleUrls: ['./trade.component.scss'],
+    standalone: false,
+})
+export class TradeComponent {
+    constructor() {
+        
+    }
+}
