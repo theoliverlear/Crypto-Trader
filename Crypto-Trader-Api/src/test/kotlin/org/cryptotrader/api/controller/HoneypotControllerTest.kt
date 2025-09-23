58e3c8474cda54f220d1d96ad61e03eaac0b51fc
theoliverlear <sigwartholiver2@gmail.com>
2025-09-22 19:30:30 -0500

    Add profile annotation to HoneypotControllerTest.kt for test environment configuration

--- Crypto-Trader-Api/src/test/kotlin/org/cryptotrader/api/controller/HoneypotControllerTest.kt
+++ Crypto-Trader-Api/src/test/kotlin/org/cryptotrader/api/controller/HoneypotControllerTest.kt
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.core.env.Environment
+import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.TestPropertySource
 import org.springframework.test.context.bean.override.mockito.MockitoSpyBean
 import org.springframework.test.web.servlet.MockMvc
@@ -18,6 +19,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 @DisplayName("Honeypot Controller")
 @TestPropertySource(properties = [
     "spring.web.resources.add-mappings=false",
