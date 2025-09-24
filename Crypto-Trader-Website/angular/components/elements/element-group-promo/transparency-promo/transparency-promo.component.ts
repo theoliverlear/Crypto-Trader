f04c8da6a1116e833b1af8ca27344df6c54efd5e
theoliverlear <sigwartholiver2@gmail.com>
2025-09-24 11:33:50 -0500

    Refactor transparency-promo.component.ts to implement `IntersectionObserver` for animations and add docs link integration

--- Crypto-Trader-Website/angular/components/elements/element-group-promo/transparency-promo/transparency-promo.component.ts
+++ Crypto-Trader-Website/angular/components/elements/element-group-promo/transparency-promo/transparency-promo.component.ts
@@ -1,7 +1,10 @@
 // transparency-promo.component.ts
-import { Component } from "@angular/core";
+import { AfterViewInit, Component, ElementRef, HostBinding, OnDestroy } from "@angular/core";
 import {transparencyParagraph} from "../../../../assets/textAssets";
-import {repositoryElementLink} from "../../../../assets/elementLinkAssets";
+import {
+    docsElementLink,
+    repositoryElementLink
+} from "../../../../assets/elementLinkAssets";
 import {TagType} from "@theoliverlear/angular-suite";
 
 @Component({
@@ -10,12 +13,39 @@ import {TagType} from "@theoliverlear/angular-suite";
     templateUrl: './transparency-promo.component.html',
     styleUrls: ['./transparency-promo.component.scss']
 })
-export class TransparencyPromoComponent {
-    constructor() {
-        
+export class TransparencyPromoComponent implements AfterViewInit, OnDestroy {
+    @HostBinding('class.in-view') inView = false;
+
+    private observer?: IntersectionObserver;
+
+    constructor(private readonly elRef: ElementRef<HTMLElement>) {}
+
+    ngAfterViewInit(): void {
+        if (typeof window !== 'undefined' && 'IntersectionObserver' in window) {
+            this.observer = new IntersectionObserver((entries) => {
+                for (const entry of entries) {
+                    if (entry.isIntersecting) {
+                        this.inView = true;
+                        if (this.observer) {
+                            this.observer.unobserve(entry.target);
+                        }
+                    }
+                }
+            }, { threshold: 0.2 });
+            this.observer.observe(this.elRef.nativeElement);
+        } else {
+            this.inView = true;
+        }
+    }
+
+    ngOnDestroy(): void {
+        if (this.observer) {
+            this.observer.disconnect();
+        }
     }
 
     protected readonly TagType = TagType;
     protected readonly transparencyParagraph = transparencyParagraph;
     protected readonly repositoryElementLink = repositoryElementLink;
+    protected readonly docsElementLink = docsElementLink;
 }
