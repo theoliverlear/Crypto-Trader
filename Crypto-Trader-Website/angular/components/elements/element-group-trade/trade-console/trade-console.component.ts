2b07273e06d60576529085214a27e390dde51b72
theoliverlear <sigwartholiver2@gmail.com>
2025-09-25 18:55:07 -0500

    Expand trade-console.component.ts to fetch names using `CurrencyNamesService` and alphabetically sort currency names

--- Crypto-Trader-Website/angular/components/elements/element-group-trade/trade-console/trade-console.component.ts
+++ Crypto-Trader-Website/angular/components/elements/element-group-trade/trade-console/trade-console.component.ts
@@ -1,10 +1,14 @@
 // trade-console.component.ts
-import { Component } from "@angular/core";
+import {Component, OnInit} from "@angular/core";
 import {PossibleVendor} from "../../../../models/vendor/types";
 import {VendorOption} from "../../../../models/vendor/VendorOption";
 import {TradeConsoleTitle} from "./models/types";
 import {TradeConsoleTitles} from "./models/TradeConsoleTitles";
 import {TagType} from "@theoliverlear/angular-suite";
+import {
+    CurrencyNamesService
+} from "../../../../services/net/http/currency/currency-names.service";
+import {CurrencyNames} from "../../../../models/currency/types";
 
 @Component({
     selector: 'trade-console',
@@ -12,13 +16,38 @@ import {TagType} from "@theoliverlear/angular-suite";
     styleUrls: ['./trade-console.component.scss'],
     standalone: false
 })
-export class TradeConsoleComponent {
+export class TradeConsoleComponent implements OnInit {
     protected currentTitle: TradeConsoleTitle = TradeConsoleTitles.VENDOR;
     protected selectedVendor: PossibleVendor = null;
-    protected isVendorSelected = false;
-    constructor() {
+    protected isVendorSelected: boolean = false;
+    protected currencyNames: string[] = [];
+    constructor(private currencyNamesService: CurrencyNamesService) {
         
     }
+    
+    ngOnInit(): void {
+        this.currencyNamesService.getCurrencyNames(true).subscribe({
+            next: (currencyNames: CurrencyNames) => {
+                console.log(currencyNames);
+                this.currencyNames = currencyNames.currencyNames;
+                this.sortCurrencyNamesAlphabetically();
+            }
+        })
+    }
+    
+    private sortCurrencyNamesAlphabetically(): void {
+        this.currencyNames = [...this.currencyNames].sort((initialString, compareString) =>
+            initialString.localeCompare(compareString, undefined, { 
+                sensitivity: 'base' 
+            })
+        );
+    }
+    
+    shouldShowCurrencySearch(): boolean {
+        const isEmpty: boolean = this.currencyNames.length === 0;
+        return !isEmpty;
+    }
+    
     setVendorOption(vendorOption: VendorOption): void {
         this.selectedVendor = vendorOption;
         this.isVendorSelected = true;
