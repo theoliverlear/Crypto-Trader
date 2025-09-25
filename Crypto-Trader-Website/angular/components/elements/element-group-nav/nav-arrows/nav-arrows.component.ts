8a2a922b01cdb01a7ced9cf3516b29b908080f76
theoliverlear <sigwartholiver2@gmail.com>
2025-09-25 18:42:30 -0500

    Add `nav-arrows` component for navigation via arrow icons functionality

---
+++ Crypto-Trader-Website/angular/components/elements/element-group-nav/nav-arrows/nav-arrows.component.ts
@@ -0,0 +1,15 @@
+// nav-arrows.component.ts
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'nav-arrows',
+    templateUrl: './nav-arrows.component.html',
+    styleUrls: ['./nav-arrows.component.scss'],
+    standalone: false
+})
+export class NavArrowsComponent {
+    // TODO: Add emitters.
+    constructor() {
+        
+    }
+}
