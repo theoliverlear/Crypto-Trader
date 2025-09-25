add0e57220dbf95311b176fa5c598d06afefed1a
theoliverlear <sigwartholiver2@gmail.com>
2025-09-24 21:11:46 -0500

    Refactor PerformanceRatingTest.kt to add nested test class, display names, and tags for better test structure and clarity

--- Crypto-Trader-Library/Api-Library/Api-Models/src/test/kotlin/org/cryptotrader/api/library/model/PerformanceRatingTest.kt
+++ Crypto-Trader-Library/Api-Library/Api-Models/src/test/kotlin/org/cryptotrader/api/library/model/PerformanceRatingTest.kt
@@ -4,20 +4,29 @@ import org.cryptotrader.api.library.model.currency.PerformanceRating
 import org.cryptotrader.test.CryptoTraderTest
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Tag
 import org.junit.jupiter.api.Test
 
+@Tag("PerformanceRating")
+@Tag("model")
 @DisplayName("Performance Rating")
 class PerformanceRatingTest : CryptoTraderTest() {
-    @Test
-    @DisplayName("fromValues should return rating with valid values")
-    fun fromValues_ReturnValidRating_WithValidValues() {
-        var rating = PerformanceRating.fromValues(100.0, 100.0)
-        assertEquals(rating, PerformanceRating.NEUTRAL)
+    
+    @Nested
+    @DisplayName("fromValues")
+    inner class FromValues {
+        @Test
+        @DisplayName("Should return rating with valid values")
+        fun fromValues_ReturnValidRating_WithValidValues() {
+            var rating = PerformanceRating.fromValues(100.0, 100.0)
+            assertEquals(rating, PerformanceRating.NEUTRAL)
 
-        rating = PerformanceRating.fromValues(100.0, 99.0)
-        assertEquals(rating, PerformanceRating.DOWN)
+            rating = PerformanceRating.fromValues(100.0, 99.0)
+            assertEquals(rating, PerformanceRating.DOWN)
 
-        rating = PerformanceRating.fromValues(99.0, 100.0)
-        assertEquals(rating, PerformanceRating.UP)
+            rating = PerformanceRating.fromValues(99.0, 100.0)
+            assertEquals(rating, PerformanceRating.UP)
+        }
     }
 }
