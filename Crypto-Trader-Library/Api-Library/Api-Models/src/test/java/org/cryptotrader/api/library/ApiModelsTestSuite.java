13613bfe9926618fd16bb12ac919a0f378e45bfe
theoliverlear <sigwartholiver2@gmail.com>
2025-09-24 21:10:09 -0500

    Add display name and tag annotations in ApiModelsTestSuite.java for better test clarity and categorization

--- Crypto-Trader-Library/Api-Library/Api-Models/src/test/java/org/cryptotrader/api/library/ApiModelsTestSuite.java
+++ Crypto-Trader-Library/Api-Library/Api-Models/src/test/java/org/cryptotrader/api/library/ApiModelsTestSuite.java
@@ -1,9 +1,13 @@
 package org.cryptotrader.api.library;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
 
 @Suite
 @SelectPackages({"org.cryptotrader.api.library.entity",
                  "org.cryptotrader.api.library.model"})
+@DisplayName("API Models Test Suite")
+@Tag("suite")
 public class ApiModelsTestSuite { }
