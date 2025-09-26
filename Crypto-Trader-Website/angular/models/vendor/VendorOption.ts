c7e0b60f4f17caae7c73ade5a7fd680a19e6e8fb
theoliverlear <sigwartholiver2@gmail.com>
2025-09-26 09:39:30 -0500

    Expand VendorOption.ts to include Coinbase and Binance vendor options with corresponding image assets

--- Crypto-Trader-Website/angular/models/vendor/VendorOption.ts
+++ Crypto-Trader-Website/angular/models/vendor/VendorOption.ts
@@ -1,19 +1,31 @@
-import {ImageAsset, paperIcon} from "../../assets/imageAssets";
+import {
+    binanceBrandLogo,
+    coinbaseBrandLogo,
+    ImageAsset,
+    paperIcon
+} from "../../assets/imageAssets";
 
 export enum VendorOption {
-    PAPER_MODE = "Paper Mode"
-    // TODO: Add Coinbase and Binance.
+    PAPER_MODE = "Paper Mode",
+    COINBASE = "Coinbase",
+    BINANCE = "Binance",
 }
 export namespace VendorOption {
     export function getImageAsset(option: VendorOption): ImageAsset {
         switch (option) {
             case VendorOption.PAPER_MODE:
                 return paperIcon;
+            case VendorOption.COINBASE:
+                return coinbaseBrandLogo;
+            case VendorOption.BINANCE:
+                return binanceBrandLogo;
         }
     }
     export function values(): VendorOption[] {
         return [
             VendorOption.PAPER_MODE,
+            VendorOption.COINBASE,
+            VendorOption.BINANCE
         ];
     }
 }
