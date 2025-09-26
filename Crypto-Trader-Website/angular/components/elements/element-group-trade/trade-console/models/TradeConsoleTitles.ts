90ef338f9c97833121000547b391f9a005ef3f55
theoliverlear <sigwartholiver2@gmail.com>
2025-09-26 09:36:29 -0500

    Expand TradeConsoleTitles.ts to add ordered titles, new constants, and a getter method for title retrieval by page

--- Crypto-Trader-Website/angular/components/elements/element-group-trade/trade-console/models/TradeConsoleTitles.ts
+++ Crypto-Trader-Website/angular/components/elements/element-group-trade/trade-console/models/TradeConsoleTitles.ts
@@ -1,6 +1,22 @@
+import {TradeConsoleTitle} from "./types";
+
 export class TradeConsoleTitles {
-    public static readonly VENDOR = {
+    public static readonly VENDOR: TradeConsoleTitle = {
         title: "Select a vendor",
         subtitle: "Recommended: always start in paper mode."
     };
+    public static readonly CURRENCY: TradeConsoleTitle = {
+        title: "Select a currency",
+        subtitle: "Choose from over 300+ cryptocurrencies."
+    }
+    public static readonly ORDERED_TITLES = [
+        TradeConsoleTitles.VENDOR,
+        TradeConsoleTitles.CURRENCY
+    ]
+    public static getByPage(page: number): TradeConsoleTitle {
+        if (page < 0 || page >= TradeConsoleTitles.ORDERED_TITLES.length) {
+            throw new Error("Invalid page number");
+        }
+        return TradeConsoleTitles.ORDERED_TITLES[page - 1];
+    }
 }
