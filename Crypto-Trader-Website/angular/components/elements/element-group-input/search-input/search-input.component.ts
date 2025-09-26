a1fec43c0e60f45c89f358e3772238ae6c401e8b
theoliverlear <sigwartholiver2@gmail.com>
2025-09-26 09:35:56 -0500

    Expand search-input.component.ts to add output event emitter for selection change

--- Crypto-Trader-Website/angular/components/elements/element-group-input/search-input/search-input.component.ts
+++ Crypto-Trader-Website/angular/components/elements/element-group-input/search-input/search-input.component.ts
@@ -1,5 +1,5 @@
 // search-input.component.ts
-import {Component, Input} from "@angular/core";
+import {Component, EventEmitter, Input, Output} from "@angular/core";
 import {FormControl} from "@angular/forms";
 import {map, Observable, startWith} from "rxjs";
 
@@ -16,16 +16,22 @@ export class SearchInputComponent {
         startWith(''),
         map(value => this._filter(value || ''))
     );
+    
+    @Output() selectionChange: EventEmitter<string> = new EventEmitter();
     constructor() {
         
     }
 
+    emitSelectionChange() {
+        this.selectionChange.emit(this.searchControl.value);
+    }
+    
     clear(): void {
         this.searchControl.setValue('');
     }
 
     private _filter(value: string): string[] {
-        const filterValue = value.toLowerCase();
+        const filterValue: string = value.toLowerCase();
         return this.options.filter(option =>
             option.toLowerCase().includes(filterValue)
         );
