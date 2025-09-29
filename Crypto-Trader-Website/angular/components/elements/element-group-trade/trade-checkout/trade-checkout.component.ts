e623dac65341b83407ea4b38b0173261fbb812ec
theoliverlear <sigwartholiver2@gmail.com>
2025-09-28 21:45:59 -0500

    Add `trade-checkout` component with initial structure in Crypto-Trader-Website

---
+++ Crypto-Trader-Website/angular/components/elements/element-group-trade/trade-checkout/trade-checkout.component.ts
@@ -0,0 +1,14 @@
+// trade-checkout.component.ts
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'trade-checkout',
+    templateUrl: './trade-checkout.component.html',
+    styleUrls: ['./trade-checkout.component.scss'],
+    standalone: false
+})
+export class TradeCheckoutComponent {
+    constructor() {
+        
+    }
+}
