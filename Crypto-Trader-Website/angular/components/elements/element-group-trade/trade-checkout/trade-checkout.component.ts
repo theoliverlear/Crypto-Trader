1c95b1d7a34df3c6a2f0247faacd652a3f7f8620
theoliverlear <sigwartholiver2@gmail.com>
2025-09-30 21:20:22 -0500

    Expand trade-checkout.component.ts to include buy type management and input placeholder text

--- Crypto-Trader-Website/angular/components/elements/element-group-trade/trade-checkout/trade-checkout.component.ts
+++ Crypto-Trader-Website/angular/components/elements/element-group-trade/trade-checkout/trade-checkout.component.ts
@@ -1,6 +1,8 @@
 // trade-checkout.component.ts
 import {Component, Input} from "@angular/core";
 import {DisplayCurrency} from "../../../../models/currency/types";
+import {BuyType} from "../buy-type/models/BuyType";
+import {ElementSize, InputType, TagType} from "@theoliverlear/angular-suite";
 
 @Component({
     selector: 'trade-checkout',
@@ -10,7 +12,34 @@ import {DisplayCurrency} from "../../../../models/currency/types";
 })
 export class TradeCheckoutComponent {
     @Input() displayCurrency: DisplayCurrency;
+    protected buyType: BuyType = BuyType.DOLLARS;
     constructor() {
         
     }
+    
+    setBuyType(buyType: BuyType): void {
+        this.buyType = buyType;
+    }
+    
+    getYouPayText(): string {
+        // TODO: Implement this and price WebSocket.
+        return "$0.00";
+    }
+    
+    getYouGetText(): string {
+        // TODO: Implement this and price WebSocket.
+        return "1.255 BTC";
+    }
+    
+    getPlaceholderText(): string {
+        if (this.buyType === BuyType.DOLLARS) {
+            return "# of Dollars";
+        } else {
+            return "# of Shares";
+        }
+    }
+
+    protected readonly ElementSize = ElementSize;
+    protected readonly InputType = InputType;
+    protected readonly TagType = TagType;
 }
