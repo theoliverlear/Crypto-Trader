38fee3c70e3d789f0789c99c5512a771f1e788a2
theoliverlear <sigwartholiver2@gmail.com>
2025-11-10 15:00:48 -0600

    Edit TradeRequest.kt to include `vendor` field for specifying trade vendor in request objects for Api-Communication

--- Crypto-Trader-Library/Api-Library/Api-Communication/src/main/kotlin/org/cryptotrader/api/library/communication/request/TradeRequest.kt
+++ Crypto-Trader-Library/Api-Library/Api-Communication/src/main/kotlin/org/cryptotrader/api/library/communication/request/TradeRequest.kt
@@ -3,5 +3,6 @@ package org.cryptotrader.api.library.communication.request
 data class TradeRequest(
     val currencyCode: String,
     val numDollars: Double,
-    val numShares: Double
+    val numShares: Double,
+    val vendor: String
 )
