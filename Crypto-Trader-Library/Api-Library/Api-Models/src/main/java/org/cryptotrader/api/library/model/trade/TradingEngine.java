d14df6ab78eeea0f78167ffaa38583df5b0c4ba1
theoliverlear <sigwartholiver2@gmail.com>
2025-11-10 15:03:25 -0600

    Add `getAsset()` method to `TradingEngine` interface in TradingEngine.java for retrieving `PortfolioAsset`

--- Crypto-Trader-Library/Api-Library/Api-Models/src/main/java/org/cryptotrader/api/library/model/trade/TradingEngine.java
+++ Crypto-Trader-Library/Api-Library/Api-Models/src/main/java/org/cryptotrader/api/library/model/trade/TradingEngine.java
@@ -1,8 +1,11 @@
 package org.cryptotrader.api.library.model.trade;
 
+import org.cryptotrader.api.library.entity.portfolio.PortfolioAsset;
+
 public interface TradingEngine {
     boolean trade();
     boolean trade(double currentPrice);
     void sell(double currentPrice);
     void buy(double currentPrice);
+    PortfolioAsset getAsset();
 }
