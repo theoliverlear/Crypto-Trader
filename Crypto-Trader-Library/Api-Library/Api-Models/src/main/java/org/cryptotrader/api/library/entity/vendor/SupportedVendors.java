c7822639a3e2f78bf59c17ad5492f5812e614857
theoliverlear <sigwartholiver2@gmail.com>
2025-11-10 15:02:20 -0600

    Edit SupportedVendors.java to add a vendor list and a factory `from()` method for resolving vendors by name in Api-Models

--- Crypto-Trader-Library/Api-Library/Api-Models/src/main/java/org/cryptotrader/api/library/entity/vendor/SupportedVendors.java
+++ Crypto-Trader-Library/Api-Library/Api-Models/src/main/java/org/cryptotrader/api/library/entity/vendor/SupportedVendors.java
@@ -1,6 +1,14 @@
 package org.cryptotrader.api.library.entity.vendor;
 
+import java.util.List;
+
 public class SupportedVendors {
     public static final Vendor PAPER_MODE = new Vendor("Paper Mode", 0.0);
     public static final Vendor COINBASE = new Vendor("Coinbase", (0.5) / 100);
+    public static final List<Vendor> VENDOR_LIST = List.of(PAPER_MODE, COINBASE);
+    public static Vendor from(String vendorName) {
+        return VENDOR_LIST.stream().filter(vendor -> {
+                    return vendor.getName().equalsIgnoreCase(vendorName);
+                }).findFirst().orElse(PAPER_MODE);
+    }
 }
