809bedcc26351bc497136bf17808f23876cf1c9e
theoliverlear <sigwartholiver2@gmail.com>
2025-11-09 20:36:51 -0600

    Edit TradeController.kt to add user authentication check and return success response for trade checkout

--- Crypto-Trader-Api/src/main/kotlin/org/cryptotrader/api/controller/TradeController.kt
+++ Crypto-Trader-Api/src/main/kotlin/org/cryptotrader/api/controller/TradeController.kt
@@ -1,15 +1,28 @@
 package org.cryptotrader.api.controller
 
 import org.cryptotrader.api.library.communication.request.TradeRequest
+import org.cryptotrader.api.library.communication.response.OperationSuccessfulResponse
+import org.cryptotrader.api.library.services.AuthContextService
+import org.cryptotrader.api.library.services.TradeService
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/trade")
-class TradeController {
+class TradeController(
+    val tradeService: TradeService,
+    val authContextService: AuthContextService
+){
     @PostMapping("/checkout")
-    fun checkout(tradeRequest: TradeRequest) {
-        
+    fun checkout(tradeRequest: TradeRequest): ResponseEntity<OperationSuccessfulResponse> {
+        val isLoggedIn: Boolean = this.authContextService.isUserAuthenticated()
+        if (!isLoggedIn) {
+            return ResponseEntity(OperationSuccessfulResponse(false), HttpStatus.UNAUTHORIZED)
+        }
+        val tradeSuccessful: Boolean = this.tradeService.checkout(tradeRequest)
+        return ResponseEntity.ok(OperationSuccessfulResponse(tradeSuccessful))
     }
 }
