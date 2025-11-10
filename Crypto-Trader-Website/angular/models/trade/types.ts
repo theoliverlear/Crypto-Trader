485dcd2b6cd11bd03e6b581ae75265914fe1b7b6
theoliverlear <sigwartholiver2@gmail.com>
2025-11-10 15:06:41 -0600

    Edit types.ts to include vendor field in `TradeCheckoutRequest` for trade vendor specification in Crypto-Trader-Website

--- Crypto-Trader-Website/angular/models/trade/types.ts
+++ Crypto-Trader-Website/angular/models/trade/types.ts
@@ -1,5 +1,8 @@
+import {VendorOption} from "../vendor/VendorOption";
+
 export type TradeCheckoutRequest = {
     numDollars: number,
     numShares: number,
     currencyCode: string,
+    vendor: VendorOption,
 };
