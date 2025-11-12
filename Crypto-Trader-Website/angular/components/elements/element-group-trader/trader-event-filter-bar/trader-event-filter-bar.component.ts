d48fe0066f6a42eb893bc92fbea0886159d405e4
theoliverlear <sigwartholiver2@gmail.com>
2025-11-11 21:17:53 -0600

    Add `trader-event-filter-bar` component in Crypto-Trader-Website for event filtering functionality

---
+++ Crypto-Trader-Website/angular/components/elements/element-group-trader/trader-event-filter-bar/trader-event-filter-bar.component.ts
@@ -0,0 +1,14 @@
+// trader-event-filter-bar.component.ts
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'trader-event-filter-bar',
+    templateUrl: './trader-event-filter-bar.component.html',
+    styleUrls: ['./trader-event-filter-bar.component.scss'],
+    standalone: false
+})
+export class TraderEventFilterBarComponent {
+    constructor() {
+        
+    }
+}
