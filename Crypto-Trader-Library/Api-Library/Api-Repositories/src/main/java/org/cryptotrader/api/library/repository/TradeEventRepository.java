f96d098befd742f57be355cc198792c6c25abc14
theoliverlear <sigwartholiver2@gmail.com>
2025-11-11 21:21:17 -0600

    Add `findAllByAssetHistoryId()` and `findAllByPortfolioId()` methods to TradeEventRepository.java for retrieving trade events by asset history and portfolio IDs in Api-Repositories

--- Crypto-Trader-Library/Api-Library/Api-Repositories/src/main/java/org/cryptotrader/api/library/repository/TradeEventRepository.java
+++ Crypto-Trader-Library/Api-Library/Api-Repositories/src/main/java/org/cryptotrader/api/library/repository/TradeEventRepository.java
@@ -3,6 +3,9 @@ package org.cryptotrader.api.library.repository;
 import org.cryptotrader.api.library.entity.trade.TradeEvent;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TradeEventRepository extends JpaRepository<TradeEvent, Long> {
-    
+    List<TradeEvent> findAllByAssetHistoryId(Long assetId);
+    List<TradeEvent> findAllByPortfolioId(Long portfolioId);
 }
