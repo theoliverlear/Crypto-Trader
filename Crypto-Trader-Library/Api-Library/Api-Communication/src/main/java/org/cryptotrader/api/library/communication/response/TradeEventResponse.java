7a1ff1a3c84e6ad74d7e8d4381914978fa4c1b0e
theoliverlear <sigwartholiver2@gmail.com>
2025-11-11 21:15:26 -0600

    Remove `atPrice` field from TradeEventResponse.java as it is unused in trade event responses in Api-Communication

--- Crypto-Trader-Library/Api-Library/Api-Communication/src/main/java/org/cryptotrader/api/library/communication/response/TradeEventResponse.java
+++ Crypto-Trader-Library/Api-Library/Api-Communication/src/main/java/org/cryptotrader/api/library/communication/response/TradeEventResponse.java
@@ -7,20 +7,17 @@ import java.time.LocalDateTime;
 @Data
 public class TradeEventResponse {
     private String currency;
-    private double atPrice;
     private double valueChange;
     private double sharesChange;
     private LocalDateTime tradeTime;
     private String tradeType;
     
     public TradeEventResponse(String currency,
-                              double atPrice,
                               double valueChange,
                               double sharesChange,
                               LocalDateTime tradeTime,
                               String tradeType) {
         this.currency = currency;
-        this.atPrice = atPrice;
         this.valueChange = valueChange;
         this.sharesChange = sharesChange;
         this.tradeTime = tradeTime;
