1667652e80ba58115f877897a7b59e29f6383c84
theoliverlear <sigwartholiver2@gmail.com>
2025-11-11 21:14:34 -0600

    Add `/events/all` endpoint with authentication and trade event retrieval logic in TraderController.java for Crypto-Trader-Api

--- Crypto-Trader-Api/src/main/java/org/cryptotrader/api/controller/TraderController.java
+++ Crypto-Trader-Api/src/main/java/org/cryptotrader/api/controller/TraderController.java
@@ -1,10 +1,35 @@
 package org.cryptotrader.api.controller;
 //=================================-Imports-==================================
+import org.cryptotrader.api.library.communication.response.TradeEventListResponse;
+import org.cryptotrader.api.library.entity.user.ProductUser;
+import org.cryptotrader.api.library.services.AuthContextService;
+import org.cryptotrader.api.library.services.TraderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/trader")
 public class TraderController {
+    private final TraderService traderService;
+    private final AuthContextService authContextService;
     
+    @Autowired
+    public TraderController(TraderService traderService,
+                            AuthContextService authContextService) {
+        this.traderService = traderService;
+        this.authContextService = authContextService;
+    }
+    
+    @RequestMapping("/events/all")
+    public ResponseEntity<TradeEventListResponse> getAllEvents() {
+        boolean isAuthorized = this.authContextService.isAuthenticated();
+        if (!isAuthorized) {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+        ProductUser user = this.authContextService.getAuthenticatedProductUser();
+        return new ResponseEntity<>(this.traderService.getTradeEvents(user), HttpStatus.OK);
+    }
 }
