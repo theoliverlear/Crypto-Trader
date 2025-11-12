1d19baa71cbcd9ae14c72fb08f0fb4b660aea623
theoliverlear <sigwartholiver2@gmail.com>
2025-11-11 21:29:41 -0600

    Expand TradeEventService.kt to include methods for retrieving portfolio assets by trade events and all trade events by product user in Api-Services

--- Crypto-Trader-Library/Api-Library/Api-Services/src/main/kotlin/org/cryptotrader/api/library/services/TradeEventService.kt
+++ Crypto-Trader-Library/Api-Library/Api-Services/src/main/kotlin/org/cryptotrader/api/library/services/TradeEventService.kt
@@ -1,14 +1,28 @@
 package org.cryptotrader.api.library.services
 
+import org.cryptotrader.api.library.entity.portfolio.Portfolio
+import org.cryptotrader.api.library.entity.portfolio.PortfolioAsset
+import org.cryptotrader.api.library.entity.portfolio.PortfolioAssetHistory
 import org.cryptotrader.api.library.entity.trade.TradeEvent
+import org.cryptotrader.api.library.entity.user.ProductUser
 import org.cryptotrader.api.library.repository.TradeEventRepository
 import org.springframework.stereotype.Service
 
 @Service
 class TradeEventService(
-    val tradeEventRepository: TradeEventRepository
+    val tradeEventRepository: TradeEventRepository,
+    val portfolioService: PortfolioService
 ) {
     fun saveTradeEvent(tradeEvent: TradeEvent): TradeEvent {
         return this.tradeEventRepository.save(tradeEvent)
+    }
+
+    fun getAssetByTradeEvent(tradeEvent: TradeEvent): PortfolioAsset {
+        val assetHistory: PortfolioAssetHistory = tradeEvent.assetHistory
+        return assetHistory.portfolioAsset
+    }
+
+    fun getAllByProductUser(productUser: ProductUser): List<TradeEvent> {
+        return this.tradeEventRepository.findAllByPortfolioId(productUser.portfolio.id)
     }
 }
