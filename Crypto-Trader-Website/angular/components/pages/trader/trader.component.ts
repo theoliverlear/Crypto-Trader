6dbcad567ef4921d174b94c2d84e15614809bb6f
theoliverlear <sigwartholiver2@gmail.com>
2025-11-24 20:03:44 -0600

    Expand trader.component.ts to include lifecycle hooks, trade event fetching, and dynamic event handling

--- Crypto-Trader-Website/angular/components/pages/trader/trader.component.ts
+++ Crypto-Trader-Website/angular/components/pages/trader/trader.component.ts
@@ -1,5 +1,10 @@
 // trader.component.ts 
-import { Component } from "@angular/core";
+import {Component, OnChanges, OnInit, SimpleChanges} from "@angular/core";
+import {
+    AllTradeEventsService
+} from "../../../services/net/http/trader/all-trade-events.service";
+import {TradeEvent, TradeEventList} from "../../../models/trader/types";
+import {TagType} from "@theoliverlear/angular-suite";
 
 @Component({
     selector: 'trader',
@@ -7,8 +12,31 @@ import { Component } from "@angular/core";
     templateUrl: './trader.component.html',
     styleUrls: ['./trader.component.scss']
 })
-export class TraderComponent {
-    constructor() {
+export class TraderComponent implements OnInit, OnChanges {
+    tradeEvents: TradeEventList = {
+        events: []
+    };
+    constructor(private allTradeEventsService: AllTradeEventsService) {
         
     }
+    ngOnInit(): void {
+        this.allTradeEventsService.getAllTradeEvents().subscribe((data: TradeEventList) => {
+            this.tradeEvents = data || {
+                events: []
+            };
+            this.tradeEvents.events.reverse();
+        });
+    }
+    
+    ngOnChanges(changes: SimpleChanges): void {
+        if (changes['tradeEvents']) {
+            this.tradeEvents = changes['tradeEvents'].currentValue;
+        }
+    }
+    
+    getTradeEvents(): TradeEvent[] {
+        return this.tradeEvents.events;
+    }
+
+    protected readonly TagType = TagType;
 }
