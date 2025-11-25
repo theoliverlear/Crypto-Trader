426cd124ee42c354ad3ac9f32361dd102c6a68f1
theoliverlear <sigwartholiver2@gmail.com>
2025-11-24 19:15:30 -0600

    Expand TradeEventResponse.java to include vendor and id fields, update constructor, and format trade time string in Api-Communication

--- Crypto-Trader-Library/Api-Library/Api-Communication/src/main/java/org/cryptotrader/api/library/communication/response/TradeEventResponse.java
+++ Crypto-Trader-Library/Api-Library/Api-Communication/src/main/java/org/cryptotrader/api/library/communication/response/TradeEventResponse.java
@@ -1,26 +1,33 @@
 package org.cryptotrader.api.library.communication.response;
 
 import lombok.Data;
+import org.cryptotrader.api.library.entity.vendor.Vendor;
 
 import java.time.LocalDateTime;
 
 @Data
 public class TradeEventResponse {
+    private Long id;
     private String currency;
     private double valueChange;
     private double sharesChange;
-    private LocalDateTime tradeTime;
+    private String tradeTime;
     private String tradeType;
-    
-    public TradeEventResponse(String currency,
+    private String vendor;
+
+    public TradeEventResponse(Long id,
+                              String currency,
                               double valueChange,
                               double sharesChange,
                               LocalDateTime tradeTime,
-                              String tradeType) {
+                              String tradeType,
+                              Vendor vendor) {
+        this.id = id;
         this.currency = currency;
         this.valueChange = valueChange;
         this.sharesChange = sharesChange;
-        this.tradeTime = tradeTime;
+        this.tradeTime = tradeTime.toString();
         this.tradeType = tradeType;
+        this.vendor = vendor.getName();
     }
 }
