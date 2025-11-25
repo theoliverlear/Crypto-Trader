fe80be545e46f4a39d07a2a7d92efb707c032df9
theoliverlear <sigwartholiver2@gmail.com>
2025-11-24 19:56:43 -0600

    Add `trader-report-range-selector` component for range selection functionality in trader reports

---
+++ Crypto-Trader-Website/angular/components/elements/element-group-trader/element-group-trader-report/trader-report-range-selector/trader-report-range-selector.component.ts
@@ -0,0 +1,14 @@
+// trader-report-range-selector.component.ts
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'trader-report-range-selector',
+    templateUrl: './trader-report-range-selector.component.html',
+    styleUrls: ['./trader-report-range-selector.component.scss'],
+    standalone: false
+})
+export class TraderReportRangeSelectorComponent {
+    constructor() {
+        
+    }
+}
