3712d5bcbe54eb348e0492514e8124fe8c3f7bea
theoliverlear <sigwartholiver2@gmail.com>
2025-11-24 20:05:12 -0600

    Expand trader-event-bubble.component.ts to include trade event details, input bindings, and utility methods for formatting and asset retrieval

--- Crypto-Trader-Website/angular/components/elements/element-group-trader/trader-event-bubble/trader-event-bubble.component.ts
+++ Crypto-Trader-Website/angular/components/elements/element-group-trader/trader-event-bubble/trader-event-bubble.component.ts
@@ -1,5 +1,20 @@
 // trader-event-bubble.component.ts 
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
+import {TradeEvent} from "../../../../models/trader/types";
+import {defaultTradeEvent} from "../../../../assets/tradeEventAssets";
+import {TagType} from "@theoliverlear/angular-suite";
+import {
+    CurrencyFormatterService
+} from "../../../../services/ui/currency-formatter.service";
+import {
+    TimeFormatterService
+} from "../../../../services/ui/time-formatter.service";
+import {
+    defaultCurrencyIcon,
+    ImageAsset,
+    paperIcon
+} from "../../../../assets/imageAssets";
+import {VendorOption} from "../../../../models/vendor/VendorOption";
 
 @Component({
     selector: 'trader-event-bubble',
@@ -8,7 +23,84 @@ import { Component } from "@angular/core";
     styleUrls: ['./trader-event-bubble.component.scss']
 })
 export class TraderEventBubbleComponent {
-    constructor() {
+    @Input() tradeEvent: TradeEvent = defaultTradeEvent;
+    constructor(private currencyFormatter: CurrencyFormatterService,
+                private timeFormatter: TimeFormatterService) {
         
     }
+
+    shouldLoadImage(): boolean {
+        return this.getCurrencyImageAsset() !== defaultCurrencyIcon;
+    }
+    
+    // TODO: Make into dedicated Angular service.
+    getCurrencyImageAsset(): ImageAsset {
+        try {
+            return {
+                src: `/assets/cryptofont/${this.getCurrencyCode().toLowerCase()}.svg`,
+                alt: this.getCurrencyName() + " icon"
+            }
+        } catch (error) {
+            return defaultCurrencyIcon;
+        }
+    }
+    
+    getVendorImageAsset(): ImageAsset {
+        try {
+            const vendorOption: VendorOption = VendorOption.from(this.tradeEvent.vendor);
+            return VendorOption.getImageAsset(vendorOption);
+        } catch (error) {
+            return paperIcon;
+        }
+    }
+    
+    getValueChange(): string {
+        let valueChange: number = this.tradeEvent.valueChange;
+        if (valueChange < 1) {
+            valueChange = Number(valueChange.toFixed(2));
+        }
+        if (valueChange < 0.01) {
+            return "< +¢1"
+        }
+        const formatedCurrency: string = this.currencyFormatter.formatCurrency(valueChange);
+        return "+" + formatedCurrency;
+    }
+    
+    getTradeType(): string {
+        return this.tradeEvent.tradeType.toUpperCase();
+    }
+    
+    getTradeTime(): string {
+        let formatedTime: string = this.timeFormatter.formatTime(this.tradeEvent.tradeTime, true);
+        formatedTime = formatedTime.split(",")[1];
+        return formatedTime;
+    }
+    
+    getTradeDay(): string {
+        let formatedTime: string = this.timeFormatter.formatTime(this.tradeEvent.tradeTime, true);
+        formatedTime = formatedTime.split(",")[0];
+        return formatedTime;
+    }
+    
+    getCurrencyName(): string {
+        if (this.tradeEvent.currency !== "") {
+            return this.tradeEvent.currency.split(" (")[0];
+        } else {
+            return "";
+        }
+    }
+    
+    getCurrencyCode(): string {
+        if (this.tradeEvent.currency !== "") {
+            return this.tradeEvent.currency.split(" (")[1].split(")")[0];
+        } else {
+            return "";
+        }
+    }
+    
+    getSharesChange(): string {
+        return this.tradeEvent.sharesChange.toString();
+    }
+    
+    protected readonly TagType = TagType;
 }
