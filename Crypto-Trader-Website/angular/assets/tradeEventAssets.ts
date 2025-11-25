56028ece582b575713c2bbc509fb6741e3e0ce68
theoliverlear <sigwartholiver2@gmail.com>
2025-11-24 19:19:16 -0600

    Add tradeEventAssets.ts for initializing default trade event assets in Crypto-Trader-Website

---
+++ Crypto-Trader-Website/angular/assets/tradeEventAssets.ts
@@ -0,0 +1,11 @@
+import {TradeEvent} from "../models/trader/types";
+
+export const defaultTradeEvent: TradeEvent = {
+    id: 0,
+    currency: "",
+    sharesChange: 0,
+    tradeTime: "",
+    tradeType: "",
+    valueChange: 0,
+    vendor: ""
+};
