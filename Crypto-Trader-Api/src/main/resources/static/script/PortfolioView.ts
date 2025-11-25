918e5c46583776076e8d413e4f89deea5af658c8
theoliverlear <sigwartholiver2@gmail.com>
2025-11-24 19:13:26 -0600

    Remove legacy front-end files as they are no longer used in Crypto-Trader-Api

--- Crypto-Trader-Api/src/main/resources/static/script/PortfolioView.ts
+++
@@ -1,4 +0,0 @@
-export enum PortfolioView {
-    PORTFOLIO_VIEW = 'portfolio-option',
-    PERFORMANCE_VIEW = 'performance-option'
-}
--- Crypto-Trader-Api/src/main/resources/static/script/TradeType.ts
+++
@@ -1,4 +0,0 @@
-export enum TradeType {
-    BUY = 'BUY',
-    SELL = 'SELL'
-}
