bfdd0501ba54b9c964823f38d985332e991f24ea
theoliverlear <sigwartholiver2@gmail.com>
2025-11-26 16:31:24 -0600

    Expand TraderControllerTest.kt to include mock `TraderService` and `AuthContextService` initialization, and update controller setup

--- Crypto-Trader-Api/src/test/kotlin/org/cryptotrader/api/controller/TraderControllerTest.kt
+++ Crypto-Trader-Api/src/test/kotlin/org/cryptotrader/api/controller/TraderControllerTest.kt
@@ -1,5 +1,8 @@
 package org.cryptotrader.api.controller
 
+import org.cryptotrader.api.library.services.AuthContextService
+import org.cryptotrader.api.library.services.PortfolioService
+import org.cryptotrader.api.library.services.TraderService
 import org.cryptotrader.test.CryptoTraderTest
 import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.BeforeEach
@@ -7,6 +10,7 @@ import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Tag
 import org.junit.jupiter.api.Test
+import org.mockito.Mockito.mock
 
 @Tag("TraderController")
 @Tag("controller")
@@ -14,10 +18,14 @@ import org.junit.jupiter.api.Test
 class TraderControllerTest : CryptoTraderTest() {
 
     private lateinit var controller: TraderController
-
+    private lateinit var traderService: TraderService
+    private lateinit var authContextService: AuthContextService
+    
     @BeforeEach
     fun setUp() {
-        this.controller = TraderController()
+        this.traderService = mock()
+        this.authContextService = mock()
+        this.controller = TraderController(this.traderService, this.authContextService)
     }
 
     @Nested
