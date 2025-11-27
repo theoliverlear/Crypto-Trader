09cbe9ab26c9412082f26f77343fda732d32e057
theoliverlear <sigwartholiver2@gmail.com>
2025-11-27 13:25:24 -0600

    Refactor import in BannedIpAddressesRepository.kt to align with IP-related package restructuring

--- Crypto-Trader-Library/Security-Library/Security-Repositories/src/main/kotlin/org/cryptotrader/security/library/repository/BannedIpAddressesRepository.kt
+++ Crypto-Trader-Library/Security-Library/Security-Repositories/src/main/kotlin/org/cryptotrader/security/library/repository/BannedIpAddressesRepository.kt
@@ -1,6 +1,6 @@
 package org.cryptotrader.security.library.repository
 
-import org.cryptotrader.security.library.entity.BannedIpAddress
+import org.cryptotrader.security.library.entity.ip.BannedIpAddress
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface BannedIpAddressesRepository : JpaRepository<BannedIpAddress, Long>
