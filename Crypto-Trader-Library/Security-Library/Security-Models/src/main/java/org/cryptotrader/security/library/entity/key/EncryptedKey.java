b2ede2cdb1992de7e83cb7e2183684192c1b30de
theoliverlear <sigwartholiver2@gmail.com>
2025-11-27 13:20:39 -0600

    Add EncryptedKey.java to represent encrypted API keys with encryption and decryption capabilities in Security-Models module

---
+++ Crypto-Trader-Library/Security-Library/Security-Models/src/main/java/org/cryptotrader/security/library/entity/key/EncryptedKey.java
@@ -0,0 +1,62 @@
+package org.cryptotrader.security.library.entity.key;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.cryptotrader.api.library.entity.Identifiable;
+import org.cryptotrader.api.library.entity.user.ProductUser;
+import org.cryptotrader.api.library.entity.user.User;
+
+// Encrypts API keys
+@Entity
+@Table(name = "encrypted_keys")
+@Getter
+@Setter
+@NoArgsConstructor
+public class EncryptedKey extends Identifiable implements KeyEncrypter {
+    
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private ProductUser user;
+    @Column(name = "api_key", nullable = false)
+    private String apiKey;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "key_type", nullable = false)
+    private KeyType keyType;
+
+    public EncryptedKey(ProductUser user, KeyType keyType) {
+        this.user = user;
+        this.apiKey = null;
+        this.keyType = keyType;
+    }
+
+    public EncryptedKey(ProductUser user, String apiKey, KeyType keyType) {
+        this.user = user;
+        this.apiKey = apiKey;
+        this.keyType = keyType;
+    }
+    
+    public static void setEncrypterDelegate(KeyEncrypter encrypterDelegate) {
+        EncryptedKey.encrypterDelegate = encrypterDelegate;
+    }
+    
+    @Transient
+    private static KeyEncrypter encrypterDelegate;
+    
+    @Override
+    public String encrypt(String key) {
+        if (encrypterDelegate == null) {
+            throw new IllegalStateException("Encrypter delegate not set in EncryptedKey class.");
+        }
+        return encrypterDelegate.encrypt(key);
+    }
+
+    @Override
+    public String decrypt(String key) {
+        if (encrypterDelegate == null) {
+            throw new IllegalStateException("Encrypter delegate not set in EncryptedKey class.");
+        }
+        return encrypterDelegate.decrypt(key);
+    }
+}
