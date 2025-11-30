1f6c143a0441e1aa956fb338000348a84c786ab7
theoliverlear <sigwartholiver2@gmail.com>
2025-11-29 20:09:49 -0600

    Edit module-info.java in Api-Repositories to add dependency for `data.library.models`

--- Crypto-Trader-Library/Api-Library/Api-Repositories/src/main/java/module-info.java
+++ Crypto-Trader-Library/Api-Library/Api-Repositories/src/main/java/module-info.java
@@ -7,6 +7,7 @@ module org.cryptotrader.api.library.repositories {
     requires spring.beans;
     requires spring.context;
     requires jakarta.persistence;
+    requires org.cryptotrader.data.library.models;
 
     opens org.cryptotrader.api.library.repository to spring.beans, spring.context, spring.core, spring.data.commons, spring.data.jpa;
     exports org.cryptotrader.api.library.repository;
