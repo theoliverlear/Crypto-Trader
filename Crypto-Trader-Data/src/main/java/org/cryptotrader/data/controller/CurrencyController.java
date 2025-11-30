a3e0b94631eb5878c34cf8c7dcfcd0cf045ab925
theoliverlear <sigwartholiver2@gmail.com>
2025-11-29 19:41:04 -0600

    Refactor imports in CurrencyController.java to use Data-Library services and remove unused dependencies in Crypto-Trader-Data

--- Crypto-Trader-Data/src/main/java/org/cryptotrader/data/controller/CurrencyController.java
+++ Crypto-Trader-Data/src/main/java/org/cryptotrader/data/controller/CurrencyController.java
@@ -1,26 +1,15 @@
 package org.cryptotrader.data.controller;
 
-import org.cryptotrader.api.library.communication.request.AssetValueRequest;
-import org.cryptotrader.api.library.communication.response.AssetValueResponse;
-import org.cryptotrader.api.library.communication.response.PerformanceRatingResponse;
-import org.cryptotrader.api.library.entity.currency.Currency;
-import org.cryptotrader.api.library.model.currency.PerformanceRating;
-import org.cryptotrader.api.library.services.CurrencyService;
-import org.cryptotrader.api.library.communication.response.DisplayCurrencyListResponse;
-import org.cryptotrader.api.library.communication.response.TimeValueResponse;
+import org.cryptotrader.data.library.services.CurrencyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @Profile("!docs")
 @RequestMapping("/data/currency")
 public class CurrencyController {
-    private CurrencyService currencyService;
+    private final CurrencyService currencyService;
     @Autowired
     public CurrencyController(CurrencyService currencyService) {
         this.currencyService = currencyService;
