7bf35489c317d9f0a20ea23a71940d68197cbb28
theoliverlear <sigwartholiver2@gmail.com>
2025-11-29 19:57:32 -0600

    Refactor imports in AuthAliases.kt to reference `PayloadStatusResponse` from `universal-models` package in Api-Communication module

--- Crypto-Trader-Library/Api-Library/Api-Communication/src/main/kotlin/org/cryptotrader/api/library/communication/request/alias/AuthAliases.kt
+++ Crypto-Trader-Library/Api-Library/Api-Communication/src/main/kotlin/org/cryptotrader/api/library/communication/request/alias/AuthAliases.kt
@@ -2,7 +2,6 @@
 package org.cryptotrader.api.library.communication.request.alias
 
 import org.cryptotrader.api.library.communication.response.AuthResponse
-import org.cryptotrader.api.library.model.http.PayloadStatusResponse
-//
-//
+import org.cryptotrader.universal.library.model.http.PayloadStatusResponse
+
 typealias HttpAuthStatus = PayloadStatusResponse<AuthResponse>
