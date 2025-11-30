7331e494aeb79cf76c3f02473f735a2d5bb6312e
theoliverlear <sigwartholiver2@gmail.com>
2025-11-29 20:08:45 -0600

    Refactor module-info.java in Api-Models to trim outdated exports and add dependencies for `data.library.models` and `universal.library.models`

--- Crypto-Trader-Library/Api-Library/Api-Models/src/main/java/module-info.java
+++ Crypto-Trader-Library/Api-Library/Api-Models/src/main/java/module-info.java
@@ -11,35 +11,20 @@ open module org.cryptotrader.api.library.models {
     requires spring.security.crypto;
     requires spring.web;
     requires org.hibernate.orm.core;
+    requires org.cryptotrader.data.library.models;
+    requires org.cryptotrader.universal.library.models;
 
-    exports org.cryptotrader.api.library.entity;
-    exports org.cryptotrader.api.library.entity.currency;
-    exports org.cryptotrader.api.library.entity.currency.builder;
-    exports org.cryptotrader.api.library.entity.currency.builder.models;
-    exports org.cryptotrader.api.library.entity.news;
-    exports org.cryptotrader.api.library.entity.news.builder;
-    exports org.cryptotrader.api.library.entity.news.builder.models;
     exports org.cryptotrader.api.library.entity.portfolio;
     exports org.cryptotrader.api.library.entity.portfolio.builder;
     exports org.cryptotrader.api.library.entity.portfolio.builder.models;
-    exports org.cryptotrader.api.library.entity.prediction;
-    exports org.cryptotrader.api.library.entity.prediction.builder;
-    exports org.cryptotrader.api.library.entity.prediction.builder.models;
     exports org.cryptotrader.api.library.entity.trade;
-    exports org.cryptotrader.api.library.entity.training;
-    exports org.cryptotrader.api.library.entity.training.specs;
-    exports org.cryptotrader.api.library.entity.training.builder;
-    exports org.cryptotrader.api.library.entity.training.builder.models;
     exports org.cryptotrader.api.library.entity.user;
     exports org.cryptotrader.api.library.entity.user.admin;
     exports org.cryptotrader.api.library.entity.user.builder;
     exports org.cryptotrader.api.library.entity.user.builder.models;
     exports org.cryptotrader.api.library.entity.vendor;
 
-    exports org.cryptotrader.api.library.model;
-    exports org.cryptotrader.api.library.model.http;
     exports org.cryptotrader.api.library.model.trade;
-    exports org.cryptotrader.api.library.model.currency;
     exports org.cryptotrader.api.library.model.dpop;
     exports org.cryptotrader.api.library.model.jwt;
 }
