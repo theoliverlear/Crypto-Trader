a0006804d868880fc682be321ef589544509c1aa
theoliverlear <sigwartholiver2@gmail.com>
2025-11-29 19:47:09 -0600

    Expand CryptoTraderApiApplication.java to include additional entity packages and services for data and security modules in Crypto-Trader-Api

--- Crypto-Trader-Api/src/main/java/org/cryptotrader/api/CryptoTraderApiApplication.java
+++ Crypto-Trader-Api/src/main/java/org/cryptotrader/api/CryptoTraderApiApplication.java
@@ -10,21 +10,33 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableAsync
 @EnableScheduling
-@EntityScan(basePackages = "org.cryptotrader.api.library.entity")
+@EntityScan(basePackages = {
+        "org.cryptotrader.api.library.entity",
+        "org.cryptotrader.data.library.entity",
+        "org.cryptotrader.security.library.entity"
+})
 @ComponentScan(basePackages = {
         "org.cryptotrader.api",
         "org.cryptotrader.api.library.component",
         "org.cryptotrader.api.library.services",
         "org.cryptotrader.api.library.infrastructure",
+        "org.cryptotrader.data.library.services",
+        "org.cryptotrader.data.library.services.harvest",
+        "org.cryptotrader.data.library.component",
         "org.cryptotrader.security.library.service"
 })
 public class CryptoTraderApiApplication {
     public static void main(String[] args) {
         blockCurrencyLoading();
+        blockCurrencyHarvesting();
         SpringApplication.run(CryptoTraderApiApplication.class, args);
     }
 
     private static void blockCurrencyLoading() {
         System.setProperty("cryptotrader.loadCurrencies", "false");
+    }
+    
+    private static void blockCurrencyHarvesting() {
+        System.setProperty("cryptotrader.harvest.currency", "false");
     }
 }
