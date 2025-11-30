6d02d312d373a910d30a44dba623738eb02ca840
theoliverlear <sigwartholiver2@gmail.com>
2025-11-29 19:52:02 -0600

    Refactor module-info.java to update dependencies for Data-Library services, models, repositories, and components in Crypto-Trader-Data

--- Crypto-Trader-Data/src/main/java/module-info.java
+++ Crypto-Trader-Data/src/main/java/module-info.java
@@ -5,10 +5,10 @@ open module org.cryptotrader.data {
     requires org.cryptotrader.api.library.components;
     requires spring.boot;
     requires static lombok;
-    requires org.cryptotrader.api.library.repositories;
-    requires org.cryptotrader.api.library.services;
+    requires org.cryptotrader.data.library.services;
     requires spring.beans;
-    requires org.cryptotrader.api.library.models;
+    requires org.cryptotrader.data.library.models;
+    requires org.cryptotrader.data.library.communication;
     requires org.cryptotrader.api.library.communication;
     requires java.sql;
     requires spring.web;
@@ -24,10 +24,10 @@ open module org.cryptotrader.data {
     requires jakarta.xml.bind;
     requires jakarta.activation;
     requires jakarta.persistence;
+    requires org.cryptotrader.data.library.repositories;
+    requires org.cryptotrader.data.library.components;
 
     exports org.cryptotrader.data;
-    exports org.cryptotrader.data.adapter;
     exports org.cryptotrader.data.config;
     exports org.cryptotrader.data.controller;
-    exports org.cryptotrader.data.service;
 }
