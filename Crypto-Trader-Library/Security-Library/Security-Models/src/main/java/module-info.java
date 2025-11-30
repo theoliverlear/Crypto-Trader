fdb0bad2f17ec2e3c1a5f3ceef969ec7dc73c130
theoliverlear <sigwartholiver2@gmail.com>
2025-11-29 20:17:55 -0600

    Edit pom.xml and module-info.java in Security-Models to add dependencies for `universal-models` module

--- Crypto-Trader-Library/Security-Library/Security-Models/src/main/java/module-info.java
+++ Crypto-Trader-Library/Security-Library/Security-Models/src/main/java/module-info.java
@@ -3,6 +3,7 @@ open module org.cryptotrader.security.library.models {
     requires static lombok;
     requires kotlin.stdlib;
     requires transitive org.cryptotrader.api.library.models;
+    requires org.cryptotrader.universal.library.models;
 
     exports org.cryptotrader.security.library.entity.ip;
     exports org.cryptotrader.security.library.entity.key;
