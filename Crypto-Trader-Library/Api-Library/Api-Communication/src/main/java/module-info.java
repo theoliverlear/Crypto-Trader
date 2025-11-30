2e0d027aba7cd57e128c5ebb30fa422426d9a117
theoliverlear <sigwartholiver2@gmail.com>
2025-11-29 19:56:32 -0600

    Refactor module-info.java to include dependencies for `data-models` and `universal-models` modules in Api-Communication

--- Crypto-Trader-Library/Api-Library/Api-Communication/src/main/java/module-info.java
+++ Crypto-Trader-Library/Api-Library/Api-Communication/src/main/java/module-info.java
@@ -3,6 +3,8 @@ open module org.cryptotrader.api.library.communication {
     requires static lombok;
     requires static com.fasterxml.jackson.annotation;
     requires org.cryptotrader.api.library.models;
+    requires org.cryptotrader.data.library.models;
+    requires org.cryptotrader.universal.library.models;
     
     exports org.cryptotrader.api.library.communication.request;
     exports org.cryptotrader.api.library.communication.request.alias;
