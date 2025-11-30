a19a0a259b0e2da0f2137f3088ab62ae3a75e110
theoliverlear <sigwartholiver2@gmail.com>
2025-11-29 20:05:25 -0600

    Refactor UserRegisteredEvent.kt to convert the class from data class to standard class in Api-Events module

--- Crypto-Trader-Library/Api-Library/Api-Events/src/main/kotlin/org/cryptotrader/api/library/events/UserRegisteredEvent.kt
+++ Crypto-Trader-Library/Api-Library/Api-Events/src/main/kotlin/org/cryptotrader/api/library/events/UserRegisteredEvent.kt
@@ -5,6 +5,6 @@ import java.time.LocalDateTime
 
 // TODO: Expand this to determine whether this was OAuth (ex. Sign up via
 //       Google account, Coinbase account, etc..
-data class UserRegisteredEvent(
+class UserRegisteredEvent(
     val user: User, 
     val dateTime: LocalDateTime)
