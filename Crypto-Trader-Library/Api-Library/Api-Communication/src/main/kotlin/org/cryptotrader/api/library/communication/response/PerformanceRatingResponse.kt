90fe7ce688ae1c4972cc5c2c2f6d2a721ddb9531
theoliverlear <sigwartholiver2@gmail.com>
2025-11-29 19:59:00 -0600

    Refactor imports in PerformanceRatingResponse.kt to reference `PerformanceRating` from `data-library` module in Api-Communication

--- Crypto-Trader-Library/Api-Library/Api-Communication/src/main/kotlin/org/cryptotrader/api/library/communication/response/PerformanceRatingResponse.kt
+++ Crypto-Trader-Library/Api-Library/Api-Communication/src/main/kotlin/org/cryptotrader/api/library/communication/response/PerformanceRatingResponse.kt
@@ -1,6 +1,6 @@
 package org.cryptotrader.api.library.communication.response
 
-import org.cryptotrader.api.library.model.currency.PerformanceRating
+import org.cryptotrader.data.library.model.currency.PerformanceRating
 
 data class PerformanceRatingResponse(val rating: String, val changePercent: String) {
     constructor(performanceRating: PerformanceRating, changePercent: String) : this(performanceRating.rating, changePercent)
