53ec187182d0498ce190ab8b13470599d78c5a4a
theoliverlear <sigwartholiver2@gmail.com>
2025-11-29 19:29:17 -0600

    Refactor imports in LoginWebSocketHandler.kt to remove problematic alias and add TODO for classpath error handling in Crypto-Trader-Api

--- Crypto-Trader-Api/src/main/kotlin/org/cryptotrader/api/controller/websocket/LoginWebSocketHandler.kt
+++ Crypto-Trader-Api/src/main/kotlin/org/cryptotrader/api/controller/websocket/LoginWebSocketHandler.kt
@@ -3,7 +3,6 @@ package org.cryptotrader.api.controller.websocket
 import com.sigwarthsoftware.springboot.websocket.WebSocketHandler
 import org.slf4j.LoggerFactory
 import org.cryptotrader.api.library.communication.request.LoginRequest
-import org.cryptotrader.api.library.communication.request.alias.HttpAuthStatus
 import org.cryptotrader.api.library.communication.response.AuthResponse
 import org.cryptotrader.api.library.services.AuthService
 import org.springframework.stereotype.Component
@@ -16,7 +15,8 @@ class LoginWebSocketHandler(
 ) : WebSocketHandler<LoginRequest, AuthResponse>() {
     override fun makeResponse(loginRequest: LoginRequest): AuthResponse {
         log.info("Login: {}", loginRequest.email)
-        val response: HttpAuthStatus = this.authService.login(loginRequest)
+        // TODO: Handle classpath errors with explicit alias typing.
+        val response = this.authService.login(loginRequest)
         return response.payload
     }
 }
