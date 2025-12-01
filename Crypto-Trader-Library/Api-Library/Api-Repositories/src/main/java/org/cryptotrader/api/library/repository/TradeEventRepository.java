5456d04215115cdf1c6b557da9573c97b37de466
theoliverlear <sigwartholiver2@gmail.com>
2025-12-01 11:29:21 -0600

    Expand TradeEventRepository.java to add paginated query methods and existence checks for trade events

--- Crypto-Trader-Library/Api-Library/Api-Repositories/src/main/java/org/cryptotrader/api/library/repository/TradeEventRepository.java
+++ Crypto-Trader-Library/Api-Library/Api-Repositories/src/main/java/org/cryptotrader/api/library/repository/TradeEventRepository.java
@@ -1,6 +1,7 @@
 package org.cryptotrader.api.library.repository;
 
 import org.cryptotrader.api.library.entity.trade.TradeEvent;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -8,4 +9,6 @@ import java.util.List;
 public interface TradeEventRepository extends JpaRepository<TradeEvent, Long> {
     List<TradeEvent> findAllByAssetHistoryId(Long assetId);
     List<TradeEvent> findAllByPortfolioId(Long portfolioId);
+    List<TradeEvent> findAllByPortfolioIdOrderByTradeTimeDesc(Long portfolioId, Pageable pageable);
+    boolean existsTradeEventByPortfolioId(Long portfolioId);
 }
