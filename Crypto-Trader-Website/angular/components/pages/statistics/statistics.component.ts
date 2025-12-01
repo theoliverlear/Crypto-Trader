449663731dd9a2b9e423815c361ccd2ac024562d
theoliverlear <sigwartholiver2@gmail.com>
2025-12-01 11:17:02 -0600

    Add `statistics` component in Crypto-Trader-Website to create a portfolio statistics page

---
+++ Crypto-Trader-Website/angular/components/pages/statistics/statistics.component.ts
@@ -0,0 +1,14 @@
+// statistics.component.ts
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'statistics',
+    templateUrl: './statistics.component.html',
+    styleUrls: ['./statistics.component.scss'],
+    standalone: false
+})
+export class StatisticsComponent {
+    constructor() {
+        
+    }
+}
