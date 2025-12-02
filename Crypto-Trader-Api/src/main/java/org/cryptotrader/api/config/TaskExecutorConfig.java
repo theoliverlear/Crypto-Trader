00a97b55e455e8d075981c96223e0538b26fa48b
theoliverlear <sigwartholiver2@gmail.com>
2025-12-01 20:51:32 -0600

    Add profile annotation to TaskExecutorConfig.java to exclude configuration in Beast Mode profile

--- Crypto-Trader-Api/src/main/java/org/cryptotrader/api/config/TaskExecutorConfig.java
+++ Crypto-Trader-Api/src/main/java/org/cryptotrader/api/config/TaskExecutorConfig.java
@@ -3,11 +3,13 @@ package org.cryptotrader.api.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.task.TaskExecutor;
+import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @Configuration
 @EnableAsync
+@Profile("!beast")
 public class TaskExecutorConfig {
     //==============================-Beans-===================================
 
