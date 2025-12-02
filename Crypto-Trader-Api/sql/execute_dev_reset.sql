906c4b276f60a9474400be29d3fb8822d62771ac
theoliverlear <sigwartholiver2@gmail.com>
2025-12-01 20:49:42 -0600

    Add execute_dev_reset.sql to reset development database tables in Crypto-Trader-Api

---
+++ Crypto-Trader-Api/sql/execute_dev_reset.sql
@@ -0,0 +1,11 @@
+delete from product_users;
+
+delete from trade_events;
+
+delete from portfolio_asset_history;
+
+delete from portfolio_assets;
+
+delete from portfolio_history;
+
+delete from portfolios;
