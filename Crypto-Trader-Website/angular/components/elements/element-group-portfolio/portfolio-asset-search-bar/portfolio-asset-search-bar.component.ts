be29e1c0e5f6ae14d504b42213869d861a3c93e5
theoliverlear <sigwartholiver2@gmail.com>
2025-12-03 15:00:40 -0600

    Add `portfolio-asset-search-bar` component for asset searching functionality in Crypto-Trader-Website

---
+++ Crypto-Trader-Website/angular/components/elements/element-group-portfolio/portfolio-asset-search-bar/portfolio-asset-search-bar.component.ts
@@ -0,0 +1,14 @@
+// portfolio-asset-search-bar.component.ts
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'portfolio-asset-search-bar',
+    templateUrl: './portfolio-asset-search-bar.component.html',
+    styleUrls: ['./portfolio-asset-search-bar.component.scss'],
+    standalone: false
+})
+export class PortfolioAssetSearchBarComponent {
+    constructor() {
+        
+    }
+}
