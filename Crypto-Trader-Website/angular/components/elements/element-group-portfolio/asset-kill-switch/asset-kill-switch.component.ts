6de8ffa32a91da2eb435548c8605f5e15813c905
theoliverlear <sigwartholiver2@gmail.com>
2025-12-03 15:13:32 -0600

    Expand asset-kill-switch.component.ts to include `HostBinding`, `HostListener`, toggle functionality, and electric plug icon rendering in Crypto-Trader-Website

--- Crypto-Trader-Website/angular/components/elements/element-group-portfolio/asset-kill-switch/asset-kill-switch.component.ts
+++ Crypto-Trader-Website/angular/components/elements/element-group-portfolio/asset-kill-switch/asset-kill-switch.component.ts
@@ -1,5 +1,6 @@
 // asset-kill-switch.component.ts
-import { Component } from "@angular/core";
+import {Component, HostBinding, HostListener, Input} from "@angular/core";
+import {electricPlugIcon} from "../../../../assets/imageAssets";
 
 @Component({
     selector: 'asset-kill-switch',
@@ -8,7 +9,22 @@ import { Component } from "@angular/core";
     standalone: false
 })
 export class AssetKillSwitchComponent {
+    @Input() isKilled: boolean = false;
+    @HostBinding('class.is-killed') get isKilledClass(): boolean { 
+        return this.isKilled; 
+    }
     constructor() {
         
     }
+
+    @HostListener('click')
+    onClick(): void {
+        this.toggleKillSwitch();
+    }
+    
+    toggleKillSwitch(): void {
+        this.isKilled = !this.isKilled;
+    }
+    
+    protected readonly electricPlugIcon = electricPlugIcon;
 }
