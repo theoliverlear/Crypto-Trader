b42139dcd2b64cfc79aa7efebd996bd1037357f0
theoliverlear <sigwartholiver2@gmail.com>
2025-12-03 19:01:37 -0600

    Add types.ts to define types for sorting functionality in Crypto-Trader-Website

---
+++ Crypto-Trader-Website/angular/models/sort/types.ts
@@ -0,0 +1,6 @@
+import {
+    PortfolioAssetFieldType
+} from "../../components/elements/element-group-portfolio/asset-field/models/PortfolioAssetFieldType";
+import {SimpleSortState} from "./SimpleSortState";
+
+export type AssetFieldSortType = [PortfolioAssetFieldType, SimpleSortState];
