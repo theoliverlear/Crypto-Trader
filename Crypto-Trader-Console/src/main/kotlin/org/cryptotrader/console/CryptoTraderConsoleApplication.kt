0d905db7df8c336c454e60f5d4b909336024bc49
theoliverlear <sigwartholiver2@gmail.com>
2025-12-05 10:19:05 -0600

    Expand CryptoTraderConsoleApplication.kt to include JPA repositories and entity scanning for data library integration

--- Crypto-Trader-Console/src/main/kotlin/org/cryptotrader/console/CryptoTraderConsoleApplication.kt
+++ Crypto-Trader-Console/src/main/kotlin/org/cryptotrader/console/CryptoTraderConsoleApplication.kt
@@ -1,14 +1,21 @@
 package org.cryptotrader.console
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @SpringBootApplication(scanBasePackages = ["org.cryptotrader.console.library.component",
     "org.cryptotrader.console.library.services",
     "org.cryptotrader.console.library.model",
     "org.cryptotrader.console.library.communication",
     "org.cryptotrader.console.library.events",
+    "org.cryptotrader.data.library.services",
+    "org.cryptotrader.data.library.component",
     "org.cryptotrader.universal.library.component"])
+@EnableJpaRepositories(basePackages = ["org.cryptotrader.data.library.repository"])
+@EntityScan(basePackages = ["org.cryptotrader.data.library.model",
+    "org.cryptotrader.data.library.entity"])
 open class CryptoTraderConsoleApplication
 
 fun main(args: Array<String>) {
