fe0d69f3730880eb03f48072656052823b66fee2
theoliverlear <sigwartholiver2@gmail.com>
2025-12-05 10:24:38 -0600

    Expand ConsoleCommandService.kt to include command parsing and delegation to `CurrencyCommandService` in Console-Services module

--- Crypto-Trader-Library/Console-Library/Console-Services/src/main/kotlin/org/cryptotrader/console/library/services/ConsoleCommandService.kt
+++ Crypto-Trader-Library/Console-Library/Console-Services/src/main/kotlin/org/cryptotrader/console/library/services/ConsoleCommandService.kt
@@ -2,12 +2,20 @@ package org.cryptotrader.console.library.services
 
 import org.cryptotrader.console.library.communication.request.ConsoleCommandRequest
 import org.cryptotrader.console.library.communication.response.ConsoleCommandResponse
+import org.cryptotrader.console.library.model.SupportedConsoleCommand
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
 @Service
-class ConsoleCommandService {
+class ConsoleCommandService @Autowired constructor(
+    private val commandParser: ConsoleCommandParser,
+    private val currencyCommandService: CurrencyCommandService
+) {
     fun executeCommand(command: ConsoleCommandRequest): ConsoleCommandResponse {
-        // TODO: Implement this.
-        return ConsoleCommandResponse("The event chain succeeded.", null)
+        return when (commandParser.parseCommand(command.command)) {
+            SupportedConsoleCommand.HELP -> ConsoleCommandResponse("The event chain succeeded.", null)
+            SupportedConsoleCommand.CURRENCY -> currencyCommandService.executeCommand(command.command)
+            else -> ConsoleCommandResponse("The event chain succeeded.", null)
+        }
     }
 }
