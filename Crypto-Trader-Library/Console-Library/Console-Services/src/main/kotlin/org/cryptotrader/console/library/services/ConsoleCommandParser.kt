b83e71941b700093ff5d1ce95c48140cac163160
theoliverlear <sigwartholiver2@gmail.com>
2025-12-05 10:23:32 -0600

    Expand ConsoleCommandParser.kt to include command parsing, validation, and exception handling methods in Console-Services

--- Crypto-Trader-Library/Console-Library/Console-Services/src/main/kotlin/org/cryptotrader/console/library/services/ConsoleCommandParser.kt
+++ Crypto-Trader-Library/Console-Library/Console-Services/src/main/kotlin/org/cryptotrader/console/library/services/ConsoleCommandParser.kt
@@ -1,6 +1,26 @@
 package org.cryptotrader.console.library.services
 
+import org.cryptotrader.console.library.model.SupportedConsoleCommand
+import org.cryptotrader.console.library.model.exception.CommandNotSupportedException
 import org.springframework.stereotype.Service
 
 @Service
-class ConsoleCommandParser
+class ConsoleCommandParser {
+    
+    fun parseCommand(command: String): SupportedConsoleCommand {
+        if (this.containsSupportedCommand(command)) {
+            return this.getCommandType(command)
+        }
+        throw CommandNotSupportedException(command)
+    }
+    
+    fun containsSupportedCommand(command: String): Boolean {
+        val firstWord: String = command.split(" ")[0]
+        return firstWord in SupportedConsoleCommand.entries.map { it.commandBase }
+    }
+    
+    fun getCommandType(command: String): SupportedConsoleCommand {
+        val firstWord: String = command.split(" ")[0]
+        return SupportedConsoleCommand.entries.first { it.commandBase == firstWord }
+    }
+}
