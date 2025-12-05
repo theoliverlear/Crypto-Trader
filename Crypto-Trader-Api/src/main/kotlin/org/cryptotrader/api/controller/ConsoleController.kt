115261ce4f3d329954b333858b718243ca030f91
theoliverlear <sigwartholiver2@gmail.com>
2025-12-04 18:39:08 -0600

    Add ConsoleController.kt to handle console command routing in Crypto-Trader-Api

---
+++ Crypto-Trader-Api/src/main/kotlin/org/cryptotrader/api/controller/ConsoleController.kt
@@ -0,0 +1,39 @@
+package org.cryptotrader.api.controller
+
+import jakarta.annotation.security.PermitAll
+import org.cryptotrader.console.library.communication.request.ConsoleCommandRequest
+import org.cryptotrader.console.library.communication.response.ConsoleCommandResponse
+import org.cryptotrader.console.library.component.ConsoleRequestGateway
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import java.time.Duration
+
+@RestController
+@RequestMapping("/api/console")
+class ConsoleController @Autowired constructor(
+    private val consoleRequestGateway: ConsoleRequestGateway
+) {
+
+    companion object {
+        private val log: Logger = LoggerFactory.getLogger(ConsoleController::class.java)
+    }
+    
+    @PermitAll
+    @PostMapping("/execute")
+    fun executeCommand(@RequestBody commandRequest: ConsoleCommandRequest): ResponseEntity<ConsoleCommandResponse> {
+        log.info("Received console command: {}", commandRequest.command)
+        try {
+            val result: ConsoleCommandResponse = this.consoleRequestGateway.execute(commandRequest, Duration.ofSeconds(15))
+            return ResponseEntity.ok(result)
+        } catch (exception: Exception) {
+            log.error("Error executing console command: {}", commandRequest.command, exception)
+            return ResponseEntity.badRequest().body(ConsoleCommandResponse("An error occurred: ${exception.message}", null))
+        }
+    }
+}
