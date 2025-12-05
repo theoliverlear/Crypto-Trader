1872474856b85f1b5e067253cd2f24cb39365af6
theoliverlear <sigwartholiver2@gmail.com>
2025-12-05 10:27:10 -0600

    Expand CryptoTraderConsole.ts to include command history navigation, buffer clearing, and local command validation

--- Crypto-Trader-Website/angular/models/console/CryptoTraderConsole.ts
+++ Crypto-Trader-Website/angular/models/console/CryptoTraderConsole.ts
@@ -7,6 +7,7 @@ import {
     ConsoleCommandService
 } from "../../services/net/http/console/console-command.service";
 import {catchError, concatMap, of, Subject, tap} from "rxjs";
+import {LocalConsoleCommand} from "./LocalConsoleCommand";
 
 export class CryptoTraderConsole {
     private _terminal: Terminal;
@@ -17,6 +18,8 @@ export class CryptoTraderConsole {
     private readonly _consoleCommandService: ConsoleCommandService;
     private _command$: Subject<string>;
     private static readonly PROMPT_CHAR: string = '$ ';
+    private _history: string[];
+    private _historyIndex: number;
     
     private static readonly RED = '\x1b[31m';
     private static readonly GREEN = '\x1b[32m';
@@ -33,24 +36,30 @@ export class CryptoTraderConsole {
         this._links = new WebLinksAddon();
         this._consoleCommandService = consoleCommandService;
         this._command$ = new Subject<string>();
+        this._history = [];
+        this._historyIndex = -1;
         this.initTerminal();
         this.initReactivePipeline();
     }
 
-    private initTerminal() {
+    private initTerminal(): void {
         this._terminal.loadAddon(this._fit);
         this._terminal.loadAddon(this._links);
         this._terminal.open(this._elementRef.nativeElement);
         this._fit.fit();
         this._terminal.onData(this.onData.bind(this));
+        this._terminal.onKey(this.onKey.bind(this));
+        this.writeDefaultText();
+        this.showPrompt();
+    }
 
-        let welcomeText = CryptoTraderConsole.getBoldText('Welcome to the Crypto Trader console!');
+    private writeDefaultText(): void {
+        let welcomeText: string = CryptoTraderConsole.getBoldText('Welcome to the Crypto Trader console!');
         welcomeText = CryptoTraderConsole.getColoredText(welcomeText, CryptoTraderConsole.CYAN);
         this._terminal.writeln(welcomeText);
         let helpText = 'Type "help" to see commands.';
         this._terminal.writeln(helpText);
         this._terminal.writeln('');
-        this.showPrompt();
     }
 
     private initReactivePipeline(): void {
@@ -58,10 +67,15 @@ export class CryptoTraderConsole {
             .pipe(
                 concatMap((line) => {
                     if (!line) {
+                        if (this.isLocalCommand(line)) {
+                            this.exec(line);
+                            return of({consoleOutput: ""});
+                        }
+                    }
+                    if (this.isLocalCommand(line)) {
                         this.exec(line);
                         return of({consoleOutput: ""});
                     }
-                    this.exec(line);
                     return this._consoleCommandService.executeCommand({command: line}).pipe(
                         catchError(err => {
                             this._terminal.writeln(
@@ -82,13 +96,20 @@ export class CryptoTraderConsole {
     }
 
     private onData(data: string) {
-        const code = data.charCodeAt(0);
+        const code: number = data.charCodeAt(0);
 
         if (data === '\r') { // Enter
             const line = this._buffer.trim();
             this._terminal.write('\r\n');
-            this._command$.next(line);
 
+            if (line.length > 0) {
+                if (this._history.length === 0 || this._history[this._history.length - 1] !== line) {
+                    this._history.push(line);
+                }
+                this._historyIndex = this._history.length;
+            }
+
+            this._command$.next(line);
             this._buffer = '';
             return;
         }
@@ -105,6 +126,88 @@ export class CryptoTraderConsole {
         }
     }
 
+    private onKey(event: { key: string; domEvent: KeyboardEvent }) {
+        const { key, domEvent } = event;
+
+        if (domEvent.key === 'ArrowUp') {
+            domEvent.preventDefault();
+            this.showPreviousHistory();
+            return;
+        }
+        
+        if (domEvent.key === 'ArrowDown') {
+            domEvent.preventDefault();
+            this.showNextHistory();
+            return;
+        }
+
+        if (domEvent.key === 'Backspace' && domEvent.ctrlKey) {
+            domEvent.preventDefault();
+            this.deletePreviousWord();
+            return;
+        }
+    }
+
+    private deletePreviousWord(): void {
+        if (!this._buffer.length) {
+            return;
+        }
+
+        let i: number = this._buffer.length - 1;
+        while (i >= 0 && this._buffer[i] === ' ') {
+            i--;
+        }
+        
+        while (i >= 0 && this._buffer[i] !== ' ') {
+            i--;
+        }
+
+        this._buffer = this._buffer.slice(0, i + 1);
+        this.clearCurrentLine();
+        this.redrawPromptAndBuffer();
+    }
+
+    private clearCurrentLine(): void {
+        this._terminal.write('\x1b[2K\r');
+    }
+
+    private redrawPromptAndBuffer(): void {
+        this._terminal.write(CryptoTraderConsole.PROMPT_CHAR + this._buffer);
+    }
+
+    private showPreviousHistory(): void {
+        if (this._history.length === 0) {
+            return;
+        }
+        if (this._historyIndex > 0) {
+            this._historyIndex--;
+        } else {
+            this._historyIndex = 0;
+        }
+        this._buffer = this._history[this._historyIndex] ?? '';
+        this.clearCurrentLine();
+        this.redrawPromptAndBuffer();
+    }
+
+    private showNextHistory(): void {
+        if (this._history.length === 0) {
+            return;
+        }
+        if (this._historyIndex < this._history.length - 1) {
+            this._historyIndex++;
+            this._buffer = this._history[this._historyIndex] ?? '';
+        } else {
+            this._historyIndex = this._history.length;
+            this._buffer = '';
+        }
+        this.clearCurrentLine();
+        this.redrawPromptAndBuffer();
+    }
+    
+    removeTrailingSpaces(): void {
+        this._buffer = this._buffer.trimEnd();
+    }
+
     private showPrompt(): void {
         this._terminal.write(CryptoTraderConsole.PROMPT_CHAR);
     }
@@ -119,9 +222,10 @@ export class CryptoTraderConsole {
                 break;
             case 'clear':
                 this._terminal.clear();
+                this.writeDefaultText();
                 break;
             default:
-                // this._terminal.writeln(`${CryptoTraderConsole.RED}Unknown command: ${cmd}${CryptoTraderConsole.RESET}`);
+                this._terminal.writeln(`${CryptoTraderConsole.RED}Unknown command: ${cmd}${CryptoTraderConsole.RESET}`);
         }
     }
 
@@ -129,8 +233,8 @@ export class CryptoTraderConsole {
         this._fit.fit();
     }
     
-    public isLocalCommand() {
-        // TODO: Create enum and namespace for local commands.
+    public isLocalCommand(command: string): boolean {
+        return LocalConsoleCommand.isLocalCommand(command);
     }
     
     public dispose(): void {
