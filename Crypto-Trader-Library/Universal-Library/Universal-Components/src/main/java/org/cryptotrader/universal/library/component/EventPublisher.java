b5f2fb2fca6126406fa7ad741a9124174a732f8b
theoliverlear <sigwartholiver2@gmail.com>
2025-12-04 19:25:38 -0600

    Add method to publish events with headers using `StreamBridge` in EventPublisher.java

--- Crypto-Trader-Library/Universal-Library/Universal-Components/src/main/java/org/cryptotrader/universal/library/component/EventPublisher.java
+++ Crypto-Trader-Library/Universal-Library/Universal-Components/src/main/java/org/cryptotrader/universal/library/component/EventPublisher.java
@@ -3,7 +3,11 @@ package org.cryptotrader.universal.library.component;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cloud.stream.function.StreamBridge;
+import org.springframework.integration.support.MessageBuilder;
+import org.springframework.messaging.Message;
 import org.springframework.stereotype.Component;
+
+import java.util.Map;
 
 @ConditionalOnProperty(prefix = "spring.cloud.stream", name = "enabled", havingValue = "true", matchIfMissing = true)
 @Component
@@ -17,5 +21,13 @@ public class EventPublisher {
 
     public <T> void publish(String bindingName, T payload) {
         this.streamBridge.send(bindingName, payload);
+    }
+    public <T> void publish(String bindingName, T payload, Map<String, Object> headers) {
+        Message<T> message = MessageBuilder
+                .withPayload(payload)
+                .copyHeaders(headers)
+                .build();
+
+        this.streamBridge.send(bindingName, message);
     }
 }
