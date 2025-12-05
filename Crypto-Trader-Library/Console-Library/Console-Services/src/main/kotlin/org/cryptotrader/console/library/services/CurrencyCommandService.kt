5e3f338d94851faa2945c54137576349ca474968
theoliverlear <sigwartholiver2@gmail.com>
2025-12-05 17:56:33 -0600

    Expand CurrencyCommandService.kt to include `currency show` command handling and extract `currency list` logic

--- Crypto-Trader-Library/Console-Library/Console-Services/src/main/kotlin/org/cryptotrader/console/library/services/CurrencyCommandService.kt
+++ Crypto-Trader-Library/Console-Library/Console-Services/src/main/kotlin/org/cryptotrader/console/library/services/CurrencyCommandService.kt
@@ -2,6 +2,7 @@ package org.cryptotrader.console.library.services
 
 import org.cryptotrader.api.library.communication.response.CurrencyNamesResponse
 import org.cryptotrader.console.library.communication.response.ConsoleCommandResponse
+import org.cryptotrader.data.library.entity.currency.Currency
 import org.cryptotrader.data.library.services.CurrencyService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
@@ -14,15 +15,37 @@ class CurrencyCommandService @Autowired constructor(
         return command == "currency list"
     }
 
+    private fun isShowCommand(command: String): Boolean {
+        return command.contains("currency show")
+    }
+    
     fun executeCommand(command: String): ConsoleCommandResponse {
         when {
             isListCommand(command) -> {
-                val currencyNames: List<String> = this.currencyService.getCurrencyNames(false)
-                val namesResponse: CurrencyNamesResponse = CurrencyNamesResponse(currencyNames)
-                val consoleString = currencyNames.joinToString(separator = "\n") { it }
-                return ConsoleCommandResponse(consoleString, namesResponse)
+                return this.executeListCommand()
+            }
+            isShowCommand(command) -> {
+                return this.executeShowCommand(command)
             }
         }
         return ConsoleCommandResponse("The event chain succeeded.", null)
+    }
+
+
+    private fun executeShowCommand(command: String): ConsoleCommandResponse {
+        val commandParts: List<String> = command.split(" ")
+        if (commandParts.size != 3) {
+            return ConsoleCommandResponse("Invalid command format.", null)
+        }
+        val currencyCode: String = commandParts[2].uppercase()
+        val currency: Currency = this.currencyService.getCurrencyByCurrencyCode(currencyCode)
+        return ConsoleCommandResponse(currency.toString(), currency)
+    }
+    
+    private fun executeListCommand(): ConsoleCommandResponse {
+        val currencyNames: List<String> = this.currencyService.getCurrencyNames(true).sorted()
+        val namesResponse = CurrencyNamesResponse(currencyNames)
+        val consoleString = currencyNames.joinToString(separator = "\n") { it }
+        return ConsoleCommandResponse(consoleString, namesResponse)
     }
 }
