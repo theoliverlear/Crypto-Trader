49d56555908be2dfc6879f619da152023452043a
theoliverlear <sigwartholiver2@gmail.com>
2025-12-05 17:58:06 -0600

    Expand terminal.component.ts to integrate authentication services and refactor terminal resizing with callback function

--- Crypto-Trader-Website/angular/components/elements/element-group-console/terminal/terminal.component.ts
+++ Crypto-Trader-Website/angular/components/elements/element-group-console/terminal/terminal.component.ts
@@ -10,6 +10,13 @@ import { CryptoTraderConsole } from "../../../../models/console/CryptoTraderCons
 import {
     ConsoleCommandService
 } from "../../../../services/net/http/console/console-command.service";
+import {AuthService} from "../../../../services/net/http/auth/auth.service";
+import {
+    LoggedInService
+} from "../../../../services/net/http/auth/status/logged-in.service";
+import {
+    TokenStorageService
+} from "../../../../services/auth/token-storage.service";
 
 @Component({
     selector: 'terminal',
@@ -20,19 +27,35 @@ import {
 export class TerminalComponent implements AfterViewInit, OnDestroy {
     @ViewChild('host', { static: true }) host!: ElementRef<HTMLDivElement>;
     private terminal: CryptoTraderConsole;
-    constructor(private consoleCommandService: ConsoleCommandService) {
+    private resizeHandler?: () => void;
+    constructor(private consoleCommandService: ConsoleCommandService,
+                private authService: AuthService,
+                private loggedInService: LoggedInService,
+                private tokenStorageService: TokenStorageService) {
         
     }
     ngAfterViewInit(): void {
-        this.terminal = new CryptoTraderConsole(this.host, this.consoleCommandService);
-        window.addEventListener('resize', this.onResize.bind(this), { passive: true });
+        this.terminal = new CryptoTraderConsole(this.host, 
+                                                this.consoleCommandService,
+                                                this.authService,
+                                                this.loggedInService,
+                                                this.tokenStorageService);
+        this.resizeHandler = this.onResize.bind(this);
+        window.addEventListener('resize', this.resizeHandler, { passive: true });
+        requestAnimationFrame(() => this.terminal?.fit());
+        setTimeout(() => this.terminal?.fit(), 0);
     }
 
     private onResize(): void {
         return this.terminal.fit();
     }
     ngOnDestroy(): void {
-        window.removeEventListener('resize', this.onResize.bind(this));
-        this.terminal.dispose();
+        if (this.resizeHandler) {
+            window.removeEventListener('resize', this.resizeHandler);
+            this.resizeHandler = undefined;
+        }
+        if (this.terminal) {
+            this.terminal.dispose();
+        }
     }
 }
