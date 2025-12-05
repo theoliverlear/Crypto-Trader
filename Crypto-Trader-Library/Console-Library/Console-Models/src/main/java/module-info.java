cf6e4de4f453bcb168c424c12c74ee27822e7375
theoliverlear <sigwartholiver2@gmail.com>
2025-12-05 10:18:03 -0600

    Expand module-info.java to include Kotlin standard library dependency and export model packages in Console-Models module

--- Crypto-Trader-Library/Console-Library/Console-Models/src/main/java/module-info.java
+++ Crypto-Trader-Library/Console-Library/Console-Models/src/main/java/module-info.java
@@ -1,2 +1,6 @@
-module org.cryptotrader.console.library.models {
+open module org.cryptotrader.console.library.models {
+    requires kotlin.stdlib;
+    
+    exports org.cryptotrader.console.library.model;
+    exports org.cryptotrader.console.library.model.exception;
 }
