8c6ec6976ec3b688891a368bd7de76b95edb0bad
theoliverlear <sigwartholiver2@gmail.com>
2025-12-05 17:59:50 -0600

    Expand LocalConsoleCommand.ts to include `auth` command for authentication handling in Crypto-Trader-Website module

--- Crypto-Trader-Website/angular/models/console/LocalConsoleCommand.ts
+++ Crypto-Trader-Website/angular/models/console/LocalConsoleCommand.ts
@@ -1,10 +1,12 @@
 export enum LocalConsoleCommand {
+    AUTH = "auth",
     CLEAR = "clear",
     TIME = "time"
 }
 export namespace LocalConsoleCommand {
     export function values(): LocalConsoleCommand[] {
         return [
+            LocalConsoleCommand.AUTH,
             LocalConsoleCommand.CLEAR,
             LocalConsoleCommand.TIME
         ];
