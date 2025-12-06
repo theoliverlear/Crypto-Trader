604207eb2f91c7b7f6f393315ad6d91d3a195f19
theoliverlear <sigwartholiver2@gmail.com>
2025-12-06 11:04:04 -0600

    Expand module-info.java to include additional dependencies for HTTP, AOP, Jackson, and Crypto-Trader libraries in Console-Services

--- Crypto-Trader-Library/Console-Library/Console-Services/src/main/java/module-info.java
+++ Crypto-Trader-Library/Console-Library/Console-Services/src/main/java/module-info.java
@@ -1,6 +1,8 @@
 open module org.cryptotrader.console.library.services {
     requires java.sql;
+    requires java.net.http;
     requires static lombok;
+    requires spring.aop;
     requires spring.beans;
     requires spring.context;
     requires spring.jdbc;
@@ -12,6 +14,9 @@ open module org.cryptotrader.console.library.services {
     requires kotlin.stdlib;
     requires jjwt.api;
     requires com.auth0.jwt;
+    requires com.fasterxml.jackson.core;
+    requires com.fasterxml.jackson.databind;
+    requires com.fasterxml.jackson.annotation;
     requires spring.data.commons;
     requires org.cryptotrader.console.library.communication;
     requires org.cryptotrader.api.library.communication;
@@ -19,6 +24,10 @@ open module org.cryptotrader.console.library.services {
     requires org.cryptotrader.data.library.services;
     requires org.cryptotrader.data.library.components;
     requires org.cryptotrader.data.library.models;
+    requires org.cryptotrader.api.library.services;
+    requires org.cryptotrader.api.library.models;
+    requires org.cryptotrader.console.library.components;
     
     exports org.cryptotrader.console.library.services;
+    exports org.cryptotrader.console.library.services.models;
 }
