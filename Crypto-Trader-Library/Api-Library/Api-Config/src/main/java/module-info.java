81839bc8751bd0190e75a0d3e13f9c82cfd1db56
theoliverlear <sigwartholiver2@gmail.com>
2025-12-06 10:48:52 -0600

    Expand module-info.java to include additional dependencies for security, data management, and JSON processing in Api-Config module

--- Crypto-Trader-Library/Api-Library/Api-Config/src/main/java/module-info.java
+++ Crypto-Trader-Library/Api-Library/Api-Config/src/main/java/module-info.java
@@ -16,6 +16,36 @@ open module org.cryptotrader.api.library.config {
     requires org.jetbrains.annotations;
     requires org.slf4j;
     requires kotlin.stdlib;
+    requires org.cryptotrader.security.library.services;
+    requires org.cryptotrader.api.library.infrastructure;
+    requires spring.aop;
+    requires spring.tx;
+    requires spring.data.jpa;
+
+    requires jakarta.persistence;
+    requires jakarta.xml.bind;
+    requires jakarta.activation;
+
+
+    requires com.fasterxml.jackson.annotation;
+    requires com.fasterxml.jackson.core;
+    requires com.fasterxml.jackson.databind;
+
+
+    requires spring.jdbc;
+
+
+    requires org.apache.httpcomponents.httpcore;
+
+    requires org.hibernate.orm.core;
+
+    requires spring.security.core;
+    requires com.auth0.jwt;
+    requires spring.data.commons;
+    requires static lombok;
+    requires org.apache.tomcat.embed.core;
+    requires org.apache.tomcat.embed.websocket;
+
 
     exports org.cryptotrader.api.library.config;
 }
