f8e60e3e4cd875a282cce5f6e6d48b9c4e10d3ca
theoliverlear <sigwartholiver2@gmail.com>
2025-12-06 10:53:26 -0600

    Set default value of `payload` to `null` in ConsoleCommandResponse.kt for improving initialization clarity in Console-Communication

--- Crypto-Trader-Library/Console-Library/Console-Communication/src/main/kotlin/org/cryptotrader/console/library/communication/response/ConsoleCommandResponse.kt
+++ Crypto-Trader-Library/Console-Library/Console-Communication/src/main/kotlin/org/cryptotrader/console/library/communication/response/ConsoleCommandResponse.kt
@@ -2,5 +2,5 @@ package org.cryptotrader.console.library.communication.response
 
 data class ConsoleCommandResponse(
     val consoleOutput: String,
-    val payload: Any?
+    val payload: Any? = null
 )
