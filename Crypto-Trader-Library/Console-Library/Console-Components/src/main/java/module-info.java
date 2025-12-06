36534bc2077d5bec6ff7fd595d503fd7a21f83e4
theoliverlear <sigwartholiver2@gmail.com>
2025-12-06 10:53:55 -0600

    Expand module-info.java to include additional dependencies for HTTP, JWT, Spring Data, and Crypto-Trader libraries in Console-Components

--- Crypto-Trader-Library/Console-Library/Console-Components/src/main/java/module-info.java
+++ Crypto-Trader-Library/Console-Library/Console-Components/src/main/java/module-info.java
@@ -1,6 +1,8 @@
 open module org.cryptotrader.console.library.components {
     requires com.fasterxml.jackson.core;
     requires com.fasterxml.jackson.databind;
+    requires com.fasterxml.jackson.annotation;
+    requires java.net.http;
     requires static lombok;
     requires static org.slf4j;
     requires org.apache.httpcomponents.httpclient;
@@ -14,8 +16,22 @@ open module org.cryptotrader.console.library.components {
     requires spring.boot.autoconfigure;
     requires spring.cloud.stream;
     requires org.cryptotrader.universal.library.components;
+    requires org.cryptotrader.universal.library.extensions;
     requires org.cryptotrader.console.library.communication;
-    requires org.cryptotrader.console.library.services;
-    
+    requires org.cryptotrader.api.library.services;
+
+    requires java.sql;
+    requires spring.aop;
+    requires spring.tx;
+    requires org.apache.tomcat.embed.core;
+    requires jjwt.api;
+    requires com.auth0.jwt;
+    requires spring.data.commons;
+    requires org.cryptotrader.api.library.communication;
+    requires org.cryptotrader.data.library.services;
+    requires org.cryptotrader.data.library.components;
+    requires org.cryptotrader.data.library.models;
+    requires org.cryptotrader.api.library.models;
+
     exports org.cryptotrader.console.library.component;
 }
