1f742339c08e8a21a1b752ff98722c08d085c5ad
theoliverlear <sigwartholiver2@gmail.com>
2025-12-06 11:02:19 -0600

    Expand ConsoleEventsListener.kt to include `ConsoleAuthenticationRunner` for handling authorization headers in Api-Events

--- Crypto-Trader-Library/Console-Library/Console-Events/src/main/kotlin/org/cryptotrader/console/library/events/ConsoleEventsListener.kt
+++ Crypto-Trader-Library/Console-Library/Console-Events/src/main/kotlin/org/cryptotrader/console/library/events/ConsoleEventsListener.kt
@@ -2,6 +2,7 @@ package org.cryptotrader.console.library.events
 
 import org.cryptotrader.console.library.communication.request.ConsoleCommandRequest
 import org.cryptotrader.console.library.communication.response.ConsoleCommandResponse
+import org.cryptotrader.console.library.component.ConsoleAuthenticationRunner
 import org.cryptotrader.console.library.services.ConsoleCommandService
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
@@ -16,7 +17,8 @@ import java.util.function.Consumer
 @Configuration
 open class ConsoleEventsListener @Autowired constructor(
     private val streamBridge: StreamBridge,
-    private val consoleCommandService: ConsoleCommandService
+    private val consoleCommandService: ConsoleCommandService,
+    private val consoleAuthenticationRunner: ConsoleAuthenticationRunner
 ) {
     private val log: Logger = LoggerFactory.getLogger(ConsoleEventsListener::class.java)
 
@@ -26,8 +28,24 @@ open class ConsoleEventsListener @Autowired constructor(
             val command: ConsoleCommandRequest = message.payload
             log.info("Received command: {}", command)
             val correlationIdHeader = message.headers["correlationId"]
+            val rawAuth: Any? = message.headers["Authorization"]
+            val authorizationHeader: String? = when (rawAuth) {
+                is String -> rawAuth
+                is ByteArray -> try {
+                    String(rawAuth, Charsets.UTF_8)
+                } catch (e: Exception) { null }
+                else -> null
+            }
+            if (authorizationHeader != null) {
+                val sample = authorizationHeader.take(16) + "..."
+                log.debug("Decoded Authorization header (type={}): {}", rawAuth?.javaClass?.simpleName, sample)
+            } else if (rawAuth != null) {
+                log.debug("Authorization header present but could not decode (type={})", rawAuth.javaClass.simpleName)
+            }
             val result: ConsoleCommandResponse = try {
-                this.consoleCommandService.executeCommand(command) 
+                this.consoleAuthenticationRunner.runWithAuthorizationHeader(authorizationHeader) {
+                    this.consoleCommandService.executeCommand(command)
+                }
             } catch (exception: Exception) {
                 ConsoleCommandResponse(
                     "An error occurred: ${exception.message}",
