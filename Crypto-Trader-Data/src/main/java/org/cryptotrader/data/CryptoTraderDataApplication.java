1449af67b6afd4bbf810593fde53b50ab3b26f4d
theoliverlear <sigwartholiver2@gmail.com>
2025-12-09 16:26:11 -0600

    Expand CryptoTraderDataApplication.java to include `org.cryptotrader.data` package in component scan

--- Crypto-Trader-Data/src/main/java/org/cryptotrader/data/CryptoTraderDataApplication.java
+++ Crypto-Trader-Data/src/main/java/org/cryptotrader/data/CryptoTraderDataApplication.java
@@ -17,6 +17,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
         "org.cryptotrader.data.library.entity"
 })
 @ComponentScan(basePackages = {
+        "org.cryptotrader.data",
         "org.cryptotrader.api.library.component",
         "org.cryptotrader.api.library",
         "org.cryptotrader.data.library",
