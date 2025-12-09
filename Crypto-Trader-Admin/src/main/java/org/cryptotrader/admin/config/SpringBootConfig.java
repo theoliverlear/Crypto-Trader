a6170723d89411a2c00a6f96a992421921ed4662
theoliverlear <sigwartholiver2@gmail.com>
2025-12-09 16:15:30 -0600

    Expand SpringBootConfig.java to include Crypto-Trader data libraries for services, repositories, and entities in Crypto-Trader-Admin

--- Crypto-Trader-Admin/src/main/java/org/cryptotrader/admin/config/SpringBootConfig.java
+++ Crypto-Trader-Admin/src/main/java/org/cryptotrader/admin/config/SpringBootConfig.java
@@ -11,14 +11,19 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication(exclude = {HttpClientAutoConfiguration.class, RestClientAutoConfiguration.class, WebServiceTemplateAutoConfiguration.class})
 @ComponentScan(basePackages = {"org.cryptotrader.admin",
         "org.cryptotrader.api.library.services",
+        "org.cryptotrader.data.library.services",
         "org.cryptotrader.api.library.component",
         "org.cryptotrader.api.library.events",
         "org.cryptotrader.api.library.config",
         "org.cryptotrader.desktop.library.component"})
 @EnableJpaRepositories(basePackages = {
-        "org.cryptotrader.api.library.repository"
+        "org.cryptotrader.api.library.repository",
+        "org.cryptotrader.data.library.repository"
 })
-@EntityScan(basePackages = "org.cryptotrader.api.library.entity")
+@EntityScan(basePackages = {
+        "org.cryptotrader.api.library.entity",
+        "org.cryptotrader.data.library.entity"
+})
 public class SpringBootConfig {
 
 }
