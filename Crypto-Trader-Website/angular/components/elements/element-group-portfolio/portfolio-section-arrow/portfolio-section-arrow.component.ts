c5773add9d5a588953cb0e08ee6184175cc6bc23
theoliverlear <sigwartholiver2@gmail.com>
2025-12-09 16:17:10 -0600

    Add `portfolio-section-arrow` component to represent directional arrows in the portfolio section in Crypto-Trader-Website

---
+++ Crypto-Trader-Website/angular/components/elements/element-group-portfolio/portfolio-section-arrow/portfolio-section-arrow.component.ts
@@ -0,0 +1,14 @@
+// portfolio-page-arrow.component.ts
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'portfolio-page-arrow',
+    templateUrl: './portfolio-section-arrow.component.html',
+    styleUrls: ['./portfolio-section-arrow.component.scss'],
+    standalone: false
+})
+export class PortfolioSectionArrowComponent {
+    constructor() {
+        
+    }
+}
