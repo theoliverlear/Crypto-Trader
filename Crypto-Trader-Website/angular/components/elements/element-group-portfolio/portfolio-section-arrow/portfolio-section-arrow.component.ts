01e6cede6826f36867db9eb3f3e8d61ee406f7fd
theoliverlear <sigwartholiver2@gmail.com>
2025-12-13 20:28:25 -0600

    Add input property and `getArrowDirection()` method to portfolio-section-arrow.component.ts for dynamic arrow behavior

--- Crypto-Trader-Website/angular/components/elements/element-group-portfolio/portfolio-section-arrow/portfolio-section-arrow.component.ts
+++ Crypto-Trader-Website/angular/components/elements/element-group-portfolio/portfolio-section-arrow/portfolio-section-arrow.component.ts
@@ -1,14 +1,28 @@
-// portfolio-page-arrow.component.ts
-import { Component } from "@angular/core";
+// portfolio-section-arrow.component.ts
+import {Component, Input} from "@angular/core";
+import {ArrowDirection} from "../../arrow/models/ArrowDirection";
+import {PortfolioSectionArrowType} from "./models/PortfolioSectionArrowType";
 
 @Component({
-    selector: 'portfolio-page-arrow',
+    selector: 'portfolio-section-arrow',
     templateUrl: './portfolio-section-arrow.component.html',
     styleUrls: ['./portfolio-section-arrow.component.scss'],
     standalone: false
 })
 export class PortfolioSectionArrowComponent {
+    @Input() sectionType: PortfolioSectionArrowType;
     constructor() {
         
+    }
+    
+    getArrowDirection(): ArrowDirection {
+        switch (this.sectionType) {
+            case PortfolioSectionArrowType.REPORT:
+                return ArrowDirection.LEFT;
+            case PortfolioSectionArrowType.MANAGE:
+                return ArrowDirection.RIGHT;
+            default:
+                return ArrowDirection.RIGHT;
+        }
     }
 }
