e0fdf20b66bfc4437da00455884376eea413047c
theoliverlear <sigwartholiver2@gmail.com>
2026-01-18 13:01:32 -0600

    Add prettier.config.js for code formatting configuration in Crypto-Trader-Website

---
+++ Crypto-Trader-Website/prettier.config.js
@@ -0,0 +1,10 @@
+export default {
+  semi: true,
+  singleQuote: false,
+  printWidth: 120,
+  tabWidth: 4,
+  useTabs: false,
+  bracketSpacing: false,
+  arrowParens: "avoid",
+  endOfLine: "auto",
+};
