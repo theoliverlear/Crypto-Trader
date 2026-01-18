94b51b96f5b9106cc2e59871ef5492d2310ce022
theoliverlear <sigwartholiver2@gmail.com>
2026-01-18 17:30:14 -0600

    Refactor animations.ts to format imports, apply trailing commas, and improve code readability

--- Crypto-Trader-Website/angular/components/animations/animations.ts
+++ Crypto-Trader-Website/angular/components/animations/animations.ts
@@ -1,16 +1,24 @@
-import { animate, query, stagger, style, transition, trigger } from '@angular/animations';
+import {
+    animate,
+    query,
+    stagger,
+    style,
+    transition,
+    trigger,
+} from '@angular/animations';
 
 export const listStagger = trigger('listStagger', [
-  transition('* => *', [
-    query(
-      ':enter',
-      [
-        style({ opacity: 0, transform: 'translateY(8px)' }),
-        stagger(800, [
-          animate('600ms ease-out', style({ opacity: 1, transform: 'translateY(0)' })),
-        ]),
-      ],
-      { optional: true }
-    ),
-  ]),
+    transition('* => *', [
+        query(
+            ':enter',
+            [
+                style({ opacity: 0, transform: 'translateY(8px)' }),
+                stagger(800, [
+                    animate('600ms ease-out', style({ opacity: 1, transform: 'translateY(0)' }),
+                    ),
+                ]),
+            ],
+            { optional: true },
+        ),
+    ]),
 ]);
