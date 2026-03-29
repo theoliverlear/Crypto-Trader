f9aa4faf5928893fa5ac3e066ddfa9fab5ec8343
theoliverlear <sigwartholiver2@gmail.com>
2026-03-29 17:47:56 -0500

    Add CryptoTraderChatApplication.kt to bootstrap the Crypto-Trader-Chat module with Spring Boot

---
+++ Crypto-Trader-Chat/src/main/kotlin/org/cryptotrader/chat/CryptoTraderChatApplication.kt
@@ -0,0 +1,11 @@
+package org.cryptotrader.chat
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CryptoTraderChatApplication
+
+fun main(args: Array<String>) {
+    runApplication<CryptoTraderChatApplication>(*args)
+}
