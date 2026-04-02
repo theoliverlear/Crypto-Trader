17f05ccbfd6e1676db2391d0a486f0040f4ebac6
theoliverlear <sigwartholiver2@gmail.com>
2026-04-01 19:10:48 -0500

    Edit CryptoTraderAgentApplication.kt to configure base package scanning for component and configuration classes

--- Crypto-Trader-Agent/src/main/kotlin/org/cryptotrader/agent/CryptoTraderAgentApplication.kt
+++ Crypto-Trader-Agent/src/main/kotlin/org/cryptotrader/agent/CryptoTraderAgentApplication.kt
@@ -3,8 +3,10 @@ package org.cryptotrader.agent
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
-class CryptoTraderAgentApplication
+@SpringBootApplication(
+    scanBasePackages = ["org.cryptotrader.agent.library.component", "org.cryptotrader.agent.library.config"]
+)
+open class CryptoTraderAgentApplication
 
 fun main(args: Array<String>) {
     runApplication<CryptoTraderAgentApplication>(*args)
