97350fafe8c26265823e0d028f44b8b0c48e35d6
theoliverlear <sigwartholiver2@gmail.com>
2026-04-05 18:10:49 -0500

    Add module-info.java to define module dependencies and exports for Agent-Communication module in Crypto-Trader-Library

---
+++ Crypto-Trader-Library/Agent-Library/Agent-Communication/src/main/java/module-info.java
@@ -0,0 +1,5 @@
+module org.cryptotrader.agent.library.communication {
+    requires kotlin.stdlib;
+    requires org.cryptotrader.agent.library.models;
+    exports org.cryptotrader.agent.library.communication.response;
+}
