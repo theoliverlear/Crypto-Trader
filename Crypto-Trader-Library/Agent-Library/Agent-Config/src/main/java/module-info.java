b5fb67e0b7ae224705fabca9b59dc755930890f7
theoliverlear <sigwartholiver2@gmail.com>
2026-04-05 18:30:42 -0500

    Add module dependencies for Universal-Config and Apache HttpClient in module-info.java for Agent-Config

--- Crypto-Trader-Library/Agent-Library/Agent-Config/src/main/java/module-info.java
+++ Crypto-Trader-Library/Agent-Library/Agent-Config/src/main/java/module-info.java
@@ -6,6 +6,8 @@ module org.cryptotrader.agent.library.config {
     requires spring.boot.autoconfigure;
     requires spring.security.config;
     requires spring.security.web;
+    requires org.cryptotrader.universal.library.config;
+    requires org.apache.httpcomponents.httpclient;
 
     exports org.cryptotrader.agent.library.config;
     opens org.cryptotrader.agent.library.config;
