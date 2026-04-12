96eb4e98d4247e4dede7a51c0ddbb5b71f023cfb
theoliverlear <sigwartholiver2@gmail.com>
2026-04-12 09:27:42 -0500

    Add Jakarta XML Bind and Activation dependencies in module-info.java for XML processing in Crypto-Trader-Agent

--- Crypto-Trader-Agent/src/main/java/module-info.java
+++ Crypto-Trader-Agent/src/main/java/module-info.java
@@ -7,6 +7,8 @@ module org.cryptotrader.agent {
     requires org.cryptotrader.agent.library.components;
     requires org.cryptotrader.agent.library.config;
     requires org.cryptotrader.universal.library.config;
+    requires jakarta.xml.bind;
+    requires jakarta.activation;
 
     exports org.cryptotrader.agent;
     opens org.cryptotrader.agent to spring.core, spring.beans, spring.context;
