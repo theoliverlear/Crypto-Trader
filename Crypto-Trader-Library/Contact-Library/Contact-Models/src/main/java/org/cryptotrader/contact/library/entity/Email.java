a02644b02363f1d9d9e970573668cad2216b755f
theoliverlear <sigwartholiver2@gmail.com>
2026-04-12 16:16:03 -0500

    Refactor Email.java to replace `Builder` annotation with a static `EmailBuilder` method in Contact-Models

--- Crypto-Trader-Library/Contact-Library/Contact-Models/src/main/java/org/cryptotrader/contact/library/entity/Email.java
+++ Crypto-Trader-Library/Contact-Library/Contact-Models/src/main/java/org/cryptotrader/contact/library/entity/Email.java
@@ -2,20 +2,18 @@ package org.cryptotrader.contact.library.entity;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+import org.cryptotrader.contact.library.entity.builder.EmailBuilder;
 import org.cryptotrader.universal.library.entity.Identifiable;
 
 @Entity
 @Getter
 @Setter
 @Table(name = "emails")
-// TODO: Build dedicated builder model for robust handling.
-@Builder
 @AllArgsConstructor
 public class Email extends Identifiable {
-    
+
     @Enumerated(EnumType.STRING)
     private CryptoTraderMailer cryptoTraderMailer;
     @Column(name = "to_address", nullable = false)
@@ -24,13 +22,17 @@ public class Email extends Identifiable {
     private String subject;
     @Column(name = "body", nullable = false)
     private String body;
-    
+
     @Enumerated(EnumType.STRING)
     private EmailType type;
-    
+
     public Email() {
         super();
         this.type = EmailType.OTHER;
+    }
+
+    public static EmailBuilder builder() {
+        return new EmailBuilder();
     }
 }
 
