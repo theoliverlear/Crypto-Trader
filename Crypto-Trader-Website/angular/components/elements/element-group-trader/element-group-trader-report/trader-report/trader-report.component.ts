9ab8e662a8982194a4cc2cee6c7ecc8711185082
theoliverlear <sigwartholiver2@gmail.com>
2026-04-12 09:08:41 -0500

    Add `trader-report` component for displaying trader reports in Crypto-Trader-Website

---
+++ Crypto-Trader-Website/angular/components/elements/element-group-trader/element-group-trader-report/trader-report/trader-report.component.ts
@@ -0,0 +1,12 @@
+// trader-report.component.ts
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'trader-report',
+    templateUrl: './trader-report.component.html',
+    styleUrls: ['./trader-report.component.scss'],
+    standalone: false,
+})
+export class TraderReportComponent {
+    constructor() {}
+}
