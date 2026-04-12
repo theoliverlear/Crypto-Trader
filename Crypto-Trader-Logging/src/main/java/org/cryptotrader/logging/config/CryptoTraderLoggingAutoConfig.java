874c9d76c983102e613b6867ec825b800ef34794
theoliverlear <sigwartholiver2@gmail.com>
2026-04-12 13:57:27 -0500

    Add `EnableConfigurationProperties` for `CryptoTraderLoggingProperties` in CryptoTraderLoggingAutoConfig.java

--- Crypto-Trader-Logging/src/main/java/org/cryptotrader/logging/config/CryptoTraderLoggingAutoConfig.java
+++ Crypto-Trader-Logging/src/main/java/org/cryptotrader/logging/config/CryptoTraderLoggingAutoConfig.java
@@ -1,12 +1,15 @@
 package org.cryptotrader.logging.config;
 
+import org.cryptotrader.logging.properties.CryptoTraderLoggingProperties;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
 @AutoConfiguration
 @ConditionalOnClass(name = "org.qos.logback.classic.Logger")
+@EnableConfigurationProperties({CryptoTraderLoggingProperties.class})
 public class CryptoTraderLoggingAutoConfig {
 
     @Bean
