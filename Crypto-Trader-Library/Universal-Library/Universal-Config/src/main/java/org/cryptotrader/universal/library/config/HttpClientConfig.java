b9d696b266890fdc4ef0f268116f61836fd85fed
theoliverlear <sigwartholiver2@gmail.com>
2026-04-12 09:24:41 -0500

    Migrate and extend `HttpClientConfig` to Universal-Config with added Java `HttpClient` bean

--- Crypto-Trader-Library/Universal-Library/Universal-Config/src/main/java/org/cryptotrader/universal/library/config/HttpClientConfig.java
+++ Crypto-Trader-Library/Universal-Library/Universal-Config/src/main/java/org/cryptotrader/universal/library/config/HttpClientConfig.java
@@ -1,4 +1,4 @@
-package org.cryptotrader.api.library.config;
+package org.cryptotrader.universal.library.config;
 
 import org.apache.http.client.methods.*;
 import org.apache.http.impl.client.CloseableHttpClient;
@@ -8,7 +8,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
-@Configuration
+import java.net.http.HttpClient;
+import java.time.Duration;
+
+@Configuration(proxyBeanMethods = false)
 public class HttpClientConfig {
     @Bean
     @ConditionalOnMissingBean(RestTemplate.class)
@@ -21,34 +24,43 @@ public class HttpClientConfig {
     public CloseableHttpClient httpClient() {
         return HttpClients.createDefault();
     }
-    
+
     @Bean
     @ConditionalOnMissingBean(HttpGet.class)
     public HttpGet getHttpClient() {
         return new HttpGet();
     }
-    
+
     @Bean
     @ConditionalOnMissingBean(HttpPost.class)
     public HttpPost postHttpClient() {
         return new HttpPost();
     }
-    
+
     @Bean
     @ConditionalOnMissingBean(HttpPut.class)
     public HttpPut putHttpClient() {
         return new HttpPut();
     }
-    
+
     @Bean
     @ConditionalOnMissingBean(HttpDelete.class)
     public HttpDelete deleteHttpClient() {
         return new HttpDelete();
     }
-    
+
     @Bean
     @ConditionalOnMissingBean(HttpOptions.class)
     public HttpOptions optionsHttpClient() {
         return new HttpOptions();
+    }
+
+    @Bean
+    @ConditionalOnMissingBean(HttpClient.class)
+    public HttpClient javaHttpClient() {
+        return HttpClient.newBuilder()
+                .connectTimeout(Duration.ofSeconds(5))
+                .followRedirects(HttpClient.Redirect.NORMAL)
+                .build();
     }
 }
