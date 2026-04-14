ae42891706051ab4bf7ffa4968314edeb2c1b534
theoliverlear <sigwartholiver2@gmail.com>
2026-04-13 19:35:33 -0500

    Add constructor to initialize `PortfolioHistoryResponse` from `PortfolioHistory` in PortfolioHistoryResponse.kt

--- Crypto-Trader-Library/Api-Library/Api-Communication/src/main/kotlin/org/cryptotrader/api/library/communication/response/PortfolioHistoryResponse.kt
+++ Crypto-Trader-Library/Api-Library/Api-Communication/src/main/kotlin/org/cryptotrader/api/library/communication/response/PortfolioHistoryResponse.kt
@@ -1,12 +1,24 @@
 package org.cryptotrader.api.library.communication.response
 
 import java.time.LocalDateTime
+import org.cryptotrader.api.library.entity.portfolio.PortfolioHistory
 
 data class PortfolioHistoryResponse(
+    val id: Long,
     val dollarBalance: Double,
     val shareBalance: Double,
     val totalWorth: Double,
     val valueChange: Double,
     val tradeOccurred: Boolean,
     val lastUpdated: LocalDateTime
-)
+) {
+    constructor(history: PortfolioHistory) : this(
+        history.id,
+        history.dollarBalance,
+        history.shareBalance,
+        history.totalWorth,
+        history.valueChange,
+        history.isTradeOccurred,
+        history.lastUpdated
+    )
+}
