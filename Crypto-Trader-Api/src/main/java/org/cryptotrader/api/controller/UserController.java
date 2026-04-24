40905d91cccf9bf805bb923f0d81ec898fe163db
theoliverlear <sigwartholiver2@gmail.com>
2026-04-23 19:59:25 -0500

    Add `/me/tier` endpoint to fetch user's subscription tier in UserController.java

--- Crypto-Trader-Api/src/main/java/org/cryptotrader/api/controller/UserController.java
+++ Crypto-Trader-Api/src/main/java/org/cryptotrader/api/controller/UserController.java
@@ -1,9 +1,15 @@
 package org.cryptotrader.api.controller;
 //=================================-Imports-==================================
 import lombok.extern.slf4j.Slf4j;
+import org.cryptotrader.api.library.communication.response.SubscriptionTierResponse;
+import org.cryptotrader.api.library.entity.user.ProductUser;
 import org.cryptotrader.api.library.services.PortfolioService;
 import org.cryptotrader.api.library.services.ProductUserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.cryptotrader.api.library.entity.user.User;
@@ -22,5 +28,11 @@ public class UserController {
         this.portfolioService = portfolioService;
     }
     //=============================-Methods-==================================
+
+    @GetMapping("/me/tier")
+    @PreAuthorize("isAuthenticated()")
+    public ResponseEntity<SubscriptionTierResponse> getMySubscriptionTier(@AuthenticationPrincipal ProductUser user) {
+        return ResponseEntity.ok(new SubscriptionTierResponse(user.getSubscriptionTier()));
+    }
 
 }
