b3c862f679e1d17ab3a7c7b0f44432db4ff3e847
theoliverlear <sigwartholiver2@gmail.com>
2026-04-24 18:57:23 -0500

    Add PageEndpoints.ts to define routes for testing pages in Crypto-Trader-Website

---
+++ Crypto-Trader-Website/angular/test/models/PageEndpoints.ts
@@ -0,0 +1,9 @@
+export enum PageEndpoints {
+    Home = '/',
+    Currencies = '/currencies',
+    Console = '/console',
+    Trade = '/trade',
+    Trader = '/trader',
+    Dashboard = '/dashboard',
+    Statistics = '/statistics',
+}
