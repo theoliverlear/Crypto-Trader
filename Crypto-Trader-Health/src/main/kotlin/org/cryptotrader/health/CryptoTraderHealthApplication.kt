01fe2728f36341b45cae207ee1922443b727cd7a
theoliverlear <sigwartholiver2@gmail.com>
2026-04-25 11:09:08 -0500

    Add CryptoTraderHealthApplication.kt to initialize the Crypto-Trader-Health application with scheduling, JPA repositories, and entity scanning

---
+++ Crypto-Trader-Health/src/main/kotlin/org/cryptotrader/health/CryptoTraderHealthApplication.kt
@@ -0,0 +1,22 @@
+package org.cryptotrader.health
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.domain.EntityScan
+import org.springframework.boot.runApplication
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+import org.springframework.scheduling.annotation.EnableScheduling
+
+@SpringBootApplication
+@EnableScheduling
+@ComponentScan(basePackages = [
+    "org.cryptotrader.health",
+    "org.cryptotrader.health.library"
+])
+@EntityScan(basePackages = ["org.cryptotrader.health.library.entity"])
+@EnableJpaRepositories(basePackages = ["org.cryptotrader.health.library.repository"])
+open class CryptoTraderHealthApplication
+
+fun main(args: Array<String>) {
+    runApplication<CryptoTraderHealthApplication>(*args)
+}
