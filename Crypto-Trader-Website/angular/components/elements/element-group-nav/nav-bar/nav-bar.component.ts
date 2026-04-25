fedad7e2175cbd28876245e95cc54e3975c2303d
theoliverlear <sigwartholiver2@gmail.com>
2026-04-25 13:20:36 -0500

    Add `CryptoTraderLoggerService` to log initialization, auth status changes, and login verification in nav-bar.component.ts for Crypto-Trader-Website

--- Crypto-Trader-Website/angular/components/elements/element-group-nav/nav-bar/nav-bar.component.ts
+++ Crypto-Trader-Website/angular/components/elements/element-group-nav/nav-bar/nav-bar.component.ts
@@ -10,6 +10,9 @@ import {
 import { LoggedInService } from '@http/auth/status/logged-in.service';
 
 import { NavBarItemOption } from '../nav-bar-item/models/NavBarItemOption';
+import {
+    CryptoTraderLoggerService
+} from '@services/logging/crypto-trader-logger.service';
 
 /** A navigation bar that contains links to different pages.
  *
@@ -22,27 +25,35 @@ import { NavBarItemOption } from '../nav-bar-item/models/NavBarItemOption';
 })
 export class NavBarComponent implements OnInit {
     protected isLoggedIn: boolean = false;
-    constructor(private readonly loggedInService: LoggedInService) {}
+    constructor(private readonly loggedInService: LoggedInService, private readonly log: CryptoTraderLoggerService) {}
 
     /** On init, listen for auth status changes and verify login status.
      *
      */
     public ngOnInit(): void {
+        this.log.setContext('NavBar');
+        this.log.info('NavBar component initialized');
         this.listenForAuthStatus();
         this.verifyLoginStatus();
     }
 
     private listenForAuthStatus(): void {
         this.loggedInService.getAuthState().subscribe((authStatus: boolean): void => {
+            this.log.debug(`Auth status changed: ${authStatus}`);
             this.isLoggedIn = authStatus;
         });
+    }
+
+    // TODO: Add more robust options filters.
+    protected shouldShowNavItem(navBarItemOption: NavBarItemOption): boolean {
+        return this.isLoggedIn;
     }
 
     /** Verify login status by subscribing to the logged in service.
      *
      */
     public verifyLoginStatus(): void {
-        console.log('Verifying login status...');
+        this.log.debug('Verifying login status...');
         this.loggedInService.isLoggedIn().subscribe();
     }
 
