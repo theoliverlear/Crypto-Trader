b5e019e23f04848d83010506a511fdcb4147c667
theoliverlear <sigwartholiver2@gmail.com>
2026-04-25 14:43:16 -0500

    Edit SubscriptionTier.kt to rename `ELITE` tier to `ULTIMATE` for updated subscription labels

--- Crypto-Trader-Library/Api-Library/Api-Models/src/main/kotlin/org/cryptotrader/api/library/entity/user/SubscriptionTier.kt
+++ Crypto-Trader-Library/Api-Library/Api-Models/src/main/kotlin/org/cryptotrader/api/library/entity/user/SubscriptionTier.kt
@@ -3,7 +3,7 @@ package org.cryptotrader.api.library.entity.user
 enum class SubscriptionTier(val label: String, val level: Int, val intervalMs: Long) {
     FREE("Free", 1, 10000L),
     PRO("Pro", 2, 5000L),
-    ELITE("Elite", 3, 1000L);
+    ULTIMATE("Ultimate", 3, 1000L);
 
     companion object {
         fun fromLabel(label: String): SubscriptionTier? {
