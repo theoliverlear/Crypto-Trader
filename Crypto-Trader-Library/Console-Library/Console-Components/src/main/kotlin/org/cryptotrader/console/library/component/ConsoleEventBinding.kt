89543e311709af3e07a05bc9861b04b31cd400c7
theoliverlear <sigwartholiver2@gmail.com>
2026-04-25 13:06:27 -0500

    Add ConsoleEventBinding.kt to define console event bindings in Console-Components module

---
+++ Crypto-Trader-Library/Console-Library/Console-Components/src/main/kotlin/org/cryptotrader/console/library/component/ConsoleEventBinding.kt
@@ -0,0 +1,6 @@
+package org.cryptotrader.console.library.component
+
+enum class ConsoleEventBinding(val bindingName: String) {
+    CONSOLE_REQUESTS("consoleRequests-out-0"),
+    CONSOLE_REPLIES("consoleReplies-out-0");
+}
