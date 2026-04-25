a4f10f5ecb8e38b5cac3a2bbec6ea34cdc069df8
theoliverlear <sigwartholiver2@gmail.com>
2026-04-24 19:53:16 -0500

    Remove redundant model retrieval methods and fix typo in constant naming in model_retriever.py in Crypto-Trader-Analysis

--- Crypto-Trader-Analysis/src/crypto_trader_analysis/apps/learning/models/ai/model_retriever.py
+++ Crypto-Trader-Analysis/src/crypto_trader_analysis/apps/learning/models/ai/model_retriever.py
@@ -17,7 +17,7 @@ BASE_DIRECTORY = 'models/'
 LSTM_MODEL_DIRECTORY = BASE_DIRECTORY + 'lstm_models/'
 COMPLEX_MODEL_DIRECTORY = BASE_DIRECTORY + 'complex_lstm_models/'
 MULTI_LAYER_MODEL_DIRECTORY = BASE_DIRECTORY + 'multi_layer_models/'
-COMPLEX_MUlTI_LAYER_MODEL_DIRECTORY = BASE_DIRECTORY + 'complex_multi_layer_models/'
+COMPLEX_MULTI_LAYER_MODEL_DIRECTORY = BASE_DIRECTORY + 'complex_multi_layer_models/'
 
 T = TypeVar('T')
 
@@ -39,76 +39,6 @@ def get_model(model_type: T,
         return model_type(dimension=historical_prices.shape[2],
                           target_currency=model_target_currency,
                           sequence_length=training_model.sequence_length)
-
-def get_lstm_model(model_target_currency: str,
-                   currency_model_path: str,
-                   historical_prices,
-                   training_model: TrainingModel,
-                   use_previous_model: bool = True) -> LstmModel:
-    if os.path.exists(currency_model_path) and use_previous_model:
-        logging.info(f"Found existing model: {currency_model_path}. Loading and continuing training...")
-        lstm_model = LstmModel(dimension=historical_prices.shape[2],
-                               target_currency=model_target_currency,
-                               sequence_length=training_model.sequence_length)
-        lstm_model.load_model(currency_model_path)
-        return lstm_model
-    else:
-        logging.info(f"No existing model found. Creating a new model for {model_target_currency}...")
-        return LstmModel(dimension=historical_prices.shape[2],
-                         target_currency=model_target_currency)
-
-def get_complex_lstm_model(model_target_currency: str,
-                           currency_model_path: str,
-                           historical_prices,
-                           training_model: TrainingModel,
-                           use_previous_model: bool = True) -> ComplexLstmModel:
-    if os.path.exists(currency_model_path) and use_previous_model:
-        logging.info(f"Found existing model: {currency_model_path}. Loading and continuing training...")
-        complex_lstm_model = ComplexLstmModel(dimension=historical_prices.shape[2],
-                                              target_currency=model_target_currency,
-                                              sequence_length=training_model.sequence_length)
-        complex_lstm_model.load_model(currency_model_path)
-        return complex_lstm_model
-    else:
-        logging.info(f"No existing model found. Creating a new model for {model_target_currency}...")
-        return ComplexLstmModel(dimension=historical_prices.shape[2],
-                                target_currency=model_target_currency)
-
-def get_multi_layer_lstm_model(model_target_currency: str,
-                               currency_model_path: str,
-                               historical_prices,
-                               training_model: TrainingModel,
-                               use_previous_model: bool = True) -> MultiLayerLstmModel:
-    if os.path.exists(currency_model_path) and use_previous_model:
-        logging.info(f"Found existing model: {currency_model_path}. Loading and continuing training...")
-        complex_lstm_model = MultiLayerLstmModel(dimension=historical_prices.shape[2],
-                                                 target_currency=model_target_currency,
-                                                 sequence_length=training_model.sequence_length)
-        complex_lstm_model.load_model(currency_model_path)
-        return complex_lstm_model
-    else:
-        logging.info(f"No existing model found. Creating a new model for {model_target_currency}...")
-        return MultiLayerLstmModel(dimension=historical_prices.shape[2],
-                                   target_currency=model_target_currency,
-                                   sequence_length=training_model.sequence_length)
-
-def get_complex_multi_layer_lstm_model(model_target_currency: str,
-                                       currency_model_path: str,
-                                       historical_prices,
-                                       training_model: TrainingModel,
-                                       use_previous_model: bool = True) -> ComplexMultiLayerLstmModel:
-    if os.path.exists(currency_model_path) and use_previous_model:
-        logging.info(f"Found existing model: {currency_model_path}. Loading and continuing training...")
-        complex_lstm_model = ComplexMultiLayerLstmModel(dimension=historical_prices.shape[2],
-                                              target_currency=model_target_currency,
-                                              sequence_length=training_model.sequence_length)
-        complex_lstm_model.load_model(currency_model_path)
-        return complex_lstm_model
-    else:
-        logging.info(f"No existing model found. Creating a new model for {model_target_currency}...")
-        return ComplexMultiLayerLstmModel(dimension=historical_prices.shape[2],
-                                          target_currency=model_target_currency,
-                                          sequence_length=training_model.sequence_length)
 
 
 def model_exists(target_currency: str, model_type: ModelType = ModelType.LSTM) -> bool:
