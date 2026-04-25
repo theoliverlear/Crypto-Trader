826353d9d7effe07b372ae6f4354fc33c0765a5d
theoliverlear <sigwartholiver2@gmail.com>
2026-04-24 19:35:23 -0500

    Add module-info.java to define module dependencies and exported packages for Health-Models in Crypto-Trader-Library

---
+++ Crypto-Trader-Library/Health-Library/Health-Models/src/main/java/module-info.java
@@ -0,0 +1,12 @@
+open module org.cryptotrader.health.library.models {
+    requires jakarta.persistence;
+    requires static lombok;
+    requires kotlin.stdlib;
+    requires org.apache.httpcomponents.httpclient;
+    requires org.apache.httpcomponents.httpcore;
+    requires org.slf4j;
+    requires org.hibernate.orm.core;
+
+    exports org.cryptotrader.health.library.entity;
+    exports org.cryptotrader.health.library.model;
+}
