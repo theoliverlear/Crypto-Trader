aa33e8d9f510bc3decb97c6002de5f8a68723738
theoliverlear <sigwartholiver2@gmail.com>
2026-04-25 12:28:39 -0500

    Move CryptoTraderLoggingAutoConfig.java to Logging-Config module under Logging-Library to reorganize project structure

--- Crypto-Trader-Library/Logging-Library/Logging-Config/src/main/java/org/cryptotrader/logging/config/CryptoTraderLoggingAutoConfig.java
+++ Crypto-Trader-Library/Logging-Library/Logging-Config/src/main/java/org/cryptotrader/logging/config/CryptoTraderLoggingAutoConfig.java
@@ -3,16 +3,18 @@ package org.cryptotrader.logging.config;
 import org.cryptotrader.logging.properties.CryptoTraderLoggingProperties;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
 @AutoConfiguration
-@ConditionalOnClass(name = "org.qos.logback.classic.Logger")
+@ConditionalOnClass(name = "ch.qos.logback.classic.Logger")
 @EnableConfigurationProperties({CryptoTraderLoggingProperties.class})
 public class CryptoTraderLoggingAutoConfig {
 
     @Bean
+    @ConditionalOnMissingBean(name = "globalExceptionHandler")
     @ConditionalOnProperty(prefix = "cryptotrader.exceptions", name = "enabled", havingValue = "true", matchIfMissing = true)
     public GlobalExceptionHandler globalExceptionHandler() {
         return new GlobalExceptionHandler();
