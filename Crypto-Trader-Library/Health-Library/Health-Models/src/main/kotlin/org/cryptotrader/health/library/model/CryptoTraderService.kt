e00f91a08768f07d1b510ad14b2193bf000c9f56
theoliverlear <sigwartholiver2@gmail.com>
2026-04-25 12:50:08 -0500

    Move CryptoTraderService.kt from Crypto-Trader-Health to Health-Models module in Health-Library and expand supported service types

--- Crypto-Trader-Library/Health-Library/Health-Models/src/main/kotlin/org/cryptotrader/health/library/model/CryptoTraderService.kt
+++ Crypto-Trader-Library/Health-Library/Health-Models/src/main/kotlin/org/cryptotrader/health/library/model/CryptoTraderService.kt
@@ -1,20 +1,30 @@
 @file:JvmName("CryptoTraderService")
-package org.cryptotrader.health.models
+
+package org.cryptotrader.health.library.model
 
 enum class CryptoTraderService(val port: Int) {
     ANALYSIS(8000),
     API(8080),
     DATA(8085),
+    DATABASE(5432),
     ENGINE(8086),
     CONTACT(8087),
     CONSOLE(8088),
+    AGENT(8089),
+    CHAT(8090),
+    LOGGING(8093),
+    HEALTH(8094),
+    SECURITY(8095),
+    ADMIN(9000),
+    VERSION(9001),
     DOCS(443);
+
     companion object {
         const val PREFIX = "Crypto Trader"
     }
-    
+
     override fun toString(): String {
         val normalizedName = this.name.lowercase().replaceFirstChar { it.uppercase() }
-        return "$PREFIX $normalizedName"
+        return "$PREFIX $normalizedName".replace(" ", "-")
     }
 }
