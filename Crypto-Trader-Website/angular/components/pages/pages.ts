4446b3d75e0d37993b000305c0d9f22a9930bd77
theoliverlear <sigwartholiver2@gmail.com>
2026-04-26 11:18:27 -0500

    Remove migrated component imports from pages.ts in Crypto-Trader-Website to simplify and streamline the `pages` array

--- Crypto-Trader-Website/angular/components/pages/pages.ts
+++ Crypto-Trader-Website/angular/components/pages/pages.ts
@@ -1,27 +1,13 @@
 import { AccountComponent } from './account/account.component';
-import { AuthorizeComponent } from './authorize/authorize.component';
-import { ConsoleComponent } from './console/console.component';
-import { CurrenciesComponent } from './currencies/currencies.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
-import { PortfolioComponent } from './portfolio/portfolio.component';
 import { StatisticsComponent } from './statistics/statistics.component';
 import { TermsOfServiceComponent } from './terms-of-service/terms-of-service.component';
-import { TradeComponent } from './trade/trade.component';
-import { TraderComponent } from './trader/trader.component';
 
 export const pages = [
     AccountComponent,
-    AuthorizeComponent,
-    ConsoleComponent,
-    CurrenciesComponent,
-    DashboardComponent,
     HomeComponent,
     NotFoundComponent,
-    PortfolioComponent,
     StatisticsComponent,
     TermsOfServiceComponent,
-    TradeComponent,
-    TraderComponent,
 ];
