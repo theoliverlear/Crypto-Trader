759861cc95d5902041dfc30e353d0e9e3cb63868
theoliverlear <sigwartholiver2@gmail.com>
2026-04-25 21:05:12 -0500

    Refactor loading-wheel.component.ts to add color schemes, size variants, and improve visibility logic in `element-group-system` directory

--- Crypto-Trader-Website/angular/components/elements/element-group-system/loading-wheel/loading-wheel.component.ts
+++ Crypto-Trader-Website/angular/components/elements/element-group-system/loading-wheel/loading-wheel.component.ts
@@ -1,6 +1,10 @@
 // loading-wheel.component.ts
 import { Component, HostBinding, Input } from '@angular/core';
 
+import { ElementSize } from '@theoliverlear/angular-suite';
+
+import { LoadingWheelColorScheme } from './models/LoadingWheelColorScheme';
+
 @Component({
     selector: 'loading-wheel',
     templateUrl: './loading-wheel.component.html',
@@ -8,11 +12,18 @@ import { Component, HostBinding, Input } from '@angular/core';
     standalone: false,
 })
 export class LoadingWheelComponent {
-    @Input() visible: boolean = true;
+    @Input() public visible: boolean = true;
+    @Input() public colorScheme: LoadingWheelColorScheme = LoadingWheelColorScheme.OCEAN;
+    @Input() public size: ElementSize = ElementSize.MEDIUM;
 
     @HostBinding('style.visibility')
-    get visibility() {
+    get visibility(): "visible" | "hidden" {
         return this.visible ? 'visible' : 'hidden';
+    }
+
+    @HostBinding('class')
+    get hostClasses(): string {
+        return `${this.colorScheme} ${this.size}`;
     }
 
     constructor() {}
