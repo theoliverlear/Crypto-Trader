5c52d3656aa0912430ffd9b3d7ca05a5bc20fb0c
theoliverlear <sigwartholiver2@gmail.com>
2026-04-25 21:02:31 -0500

    Refactor tier-promo-stripe.component.ts to simplify component logic by outsourcing to domain components

--- Crypto-Trader-Website/angular/components/elements/element-group-promo/tier-promo-stripe/tier-promo-stripe.component.ts
+++ Crypto-Trader-Website/angular/components/elements/element-group-promo/tier-promo-stripe/tier-promo-stripe.component.ts
@@ -1,11 +1,7 @@
 // tier-promo-stripe.component.ts
-import {
-    AfterViewInit,
-    Component,
-    ElementRef,
-    HostBinding,
-    OnDestroy,
-} from '@angular/core';
+import { Component } from '@angular/core';
+
+import { TagType } from '@theoliverlear/angular-suite';
 
 import { SubscriptionTier } from '../tier-promo/models/SubscriptionTier';
 
@@ -15,39 +11,7 @@ import { SubscriptionTier } from '../tier-promo/models/SubscriptionTier';
     templateUrl: './tier-promo-stripe.component.html',
     styleUrls: ['./tier-promo-stripe.component.scss'],
 })
-export class TierPromoStripeComponent implements AfterViewInit, OnDestroy {
-    @HostBinding('class.in-view') private inView: boolean = false;
-
-    private observer?: IntersectionObserver;
-
-    constructor(private readonly elRef: ElementRef<HTMLElement>) {}
-
-    public ngAfterViewInit(): void {
-        if (typeof window !== 'undefined' && 'IntersectionObserver' in window) {
-            this.observer = new IntersectionObserver(
-                (entries: IntersectionObserverEntry[]): void => {
-                    for (const entry of entries) {
-                        if (entry.isIntersecting) {
-                            this.inView = true;
-                            if (this.observer) {
-                                this.observer.unobserve(entry.target);
-                            }
-                        }
-                    }
-                },
-                { threshold: 0.2 },
-            );
-            this.observer.observe(this.elRef.nativeElement);
-        } else {
-            this.inView = true;
-        }
-    }
-
-    public ngOnDestroy(): void {
-        if (this.observer) {
-            this.observer.disconnect();
-        }
-    }
-
+export class TierPromoStripeComponent {
     protected readonly SubscriptionTier: typeof SubscriptionTier = SubscriptionTier;
+    protected readonly TagType: typeof TagType = TagType;
 }
