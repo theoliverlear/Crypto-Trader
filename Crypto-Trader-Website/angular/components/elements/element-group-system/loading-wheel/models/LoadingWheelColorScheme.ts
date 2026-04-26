74b1f7c3591b4d24985a9f3df950471954ec3a9a
theoliverlear <sigwartholiver2@gmail.com>
2026-04-25 21:05:49 -0500

    Add LoadingWheelColorScheme.ts to define SCSS-based color schemes for loading wheel component in `element-group-system` directory

---
+++ Crypto-Trader-Website/angular/components/elements/element-group-system/loading-wheel/models/LoadingWheelColorScheme.ts
@@ -0,0 +1,10 @@
+/**
+ * The color scheme of the loading wheel by SCSS class.
+ */
+export enum LoadingWheelColorScheme {
+    OCEAN = 'color-ocean',
+    SUNSET = 'color-sunset',
+    GOLDEN = 'color-golden',
+    EMERALD = 'color-emerald',
+    FROST = 'color-frost',
+}
