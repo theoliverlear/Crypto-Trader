877af80ff40427ce18d71291769d825fdc085648
theoliverlear <sigwartholiver2@gmail.com>
2026-04-25 21:14:13 -0500

    Add LogEventsPublisher.kt to publish log batch events with `EventPublisher` in Logging-Events module

---
+++ Crypto-Trader-Library/Logging-Library/Logging-Events/src/main/kotlin/org/cryptotrader/logging/library/events/publisher/LogEventsPublisher.kt
@@ -0,0 +1,23 @@
+package org.cryptotrader.logging.library.events.publisher
+
+import org.cryptotrader.logging.library.events.LogBatchEvent
+import org.cryptotrader.universal.library.component.EventPublisher
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Component
+
+@Component
+class LogEventsPublisher @Autowired constructor(
+    @Autowired(required = false)
+    private val eventPublisher: EventPublisher?
+) {
+    private val log = LoggerFactory.getLogger(LogEventsPublisher::class.java)
+
+    fun publishBatch(bindingName: String, batch: LogBatchEvent) {
+        if (this.eventPublisher != null) {
+            eventPublisher.publish(bindingName, batch)
+        } else {
+            log.debug("EventPublisher unavailable; skipping publishBatch in docs/non-stream context.")
+        }
+    }
+}
