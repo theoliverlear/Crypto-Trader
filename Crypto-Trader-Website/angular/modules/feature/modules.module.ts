19b5814b12c42738e9d3a98559288b16f96924e9
theoliverlear <sigwartholiver2@gmail.com>
2026-04-26 17:03:42 -0500

    Edit modules.module.ts to replace element components with `PromoModule` and update imports in Crypto-Trader-Website

--- Crypto-Trader-Website/angular/modules/feature/modules.module.ts
+++ Crypto-Trader-Website/angular/modules/feature/modules.module.ts
@@ -3,17 +3,12 @@ import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { AngularSuiteModule } from '@theoliverlear/angular-suite';
 
 import { UniversalModule } from '../shared/universal.module';
-import { ModuleArchitectureMapComponent } from '@components/elements/element-group-modules/module-architecture-map/module-architecture-map.component';
-import { ModuleCardComponent } from '@components/elements/element-group-modules/module-card/module-card.component';
-import { ModuleInfraTileComponent } from '@components/elements/element-group-modules/module-infra-tile/module-infra-tile.component';
-import { ModuleInterfaceSelectorComponent } from '@components/elements/element-group-modules/module-interface-selector/module-interface-selector.component';
+import { PromoModule } from './promo.module';
 import { ModulesComponent } from '@components/pages/modules/modules.component';
 
+// TODO: Fix module-promo sharing issue.
+
 const modulesComponents = [
-    ModuleArchitectureMapComponent,
-    ModuleCardComponent,
-    ModuleInfraTileComponent,
-    ModuleInterfaceSelectorComponent,
     ModulesComponent,
 ];
 
@@ -23,6 +18,7 @@ const modulesComponents = [
         CommonModule,
         AngularSuiteModule,
         UniversalModule,
+        PromoModule,
     ],
     exports: [...modulesComponents],
     schemas: [CUSTOM_ELEMENTS_SCHEMA],
