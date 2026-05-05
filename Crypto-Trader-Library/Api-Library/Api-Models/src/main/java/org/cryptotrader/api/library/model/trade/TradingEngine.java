b638cebf5a1e0a28e07ab8e47d4e8e8e66c10e40
theoliverlear <sigwartholiver2@gmail.com>
2026-05-04 19:32:55 -0500

    Add `canTrade()` methods to TradingEngine.java for evaluating trade conditions in Api-Models

--- Crypto-Trader-Library/Api-Library/Api-Models/src/main/java/org/cryptotrader/api/library/model/trade/TradingEngine.java
+++ Crypto-Trader-Library/Api-Library/Api-Models/src/main/java/org/cryptotrader/api/library/model/trade/TradingEngine.java
@@ -5,6 +5,11 @@ import org.cryptotrader.api.library.entity.portfolio.PortfolioAsset;
 public interface TradingEngine {
     boolean trade();
     boolean trade(double currentPrice);
+
+    boolean canTrade();
+    boolean canTrade(double currentPrice);
+
+    // TODO: This should return an enum. Buy, sell, failure.
     void sell(double currentPrice);
     void buy(double currentPrice);
     PortfolioAsset getAsset();
