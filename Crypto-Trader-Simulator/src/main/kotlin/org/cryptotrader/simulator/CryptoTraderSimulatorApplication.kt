8202356e02c792c1a1540ae092d222e46628c570
theoliverlear <sigwartholiver2@gmail.com>
2026-05-04 19:23:27 -0500

    Add CryptoTraderSimulatorApplication.kt to bootstrap the Crypto-Trader-Simulator application with Spring Boot

---
+++ Crypto-Trader-Simulator/src/main/kotlin/org/cryptotrader/simulator/CryptoTraderSimulatorApplication.kt
@@ -0,0 +1,15 @@
+package org.cryptotrader.simulator
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication(scanBasePackages = [
+    "org.cryptotrader.simulator.library.config",
+    "org.cryptotrader.simulator.library.services",
+    "org.cryptotrader.simulator.library.events",
+])
+class CryptoTraderSimulatorApplication
+
+fun main(args: Array<String>) {
+    runApplication<CryptoTraderSimulatorApplication>(*args)
+}
