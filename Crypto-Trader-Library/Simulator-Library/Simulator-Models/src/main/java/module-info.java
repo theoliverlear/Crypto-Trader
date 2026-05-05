097809f895968aa34cc1bca37688e27a88a261a1
theoliverlear <sigwartholiver2@gmail.com>
2026-05-04 19:27:13 -0500

    Add module-info.java to define the Simulator-Models module in Simulator-Library

---
+++ Crypto-Trader-Library/Simulator-Library/Simulator-Models/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+open module org.cryptotrader.simulator.library.models {
+    requires kotlin.stdlib;
+    requires org.cryptotrader.universal.library.models;
+    requires org.cryptotrader.data.library.models;
+    requires org.cryptotrader.api.library.models;
+    requires jakarta.persistence;
+    requires static lombok;
+}
