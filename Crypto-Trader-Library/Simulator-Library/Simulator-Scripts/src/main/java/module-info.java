913fffc19b1e6c13b15ebf5e30d6e4750b82774d
theoliverlear <sigwartholiver2@gmail.com>
2026-05-04 19:27:44 -0500

    Add module-info.java to define the Simulator-Scripts module in Simulator-Library

---
+++ Crypto-Trader-Library/Simulator-Library/Simulator-Scripts/src/main/java/module-info.java
@@ -0,0 +1,5 @@
+module org.cryptotrader.simulator.library.scripts {
+    requires kotlin.stdlib;
+    requires org.cryptotrader.data.library.models;
+    requires org.cryptotrader.api.library.models;
+}
