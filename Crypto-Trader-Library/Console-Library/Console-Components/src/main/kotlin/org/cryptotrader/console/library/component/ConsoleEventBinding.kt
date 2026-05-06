1a37661ef0c1dab6a5eca9582cef0b7ec4bbc213
theoliverlear <sigwartholiver2@gmail.com>
2026-05-05 19:30:41 -0500

    Refactor ConsoleEventBinding.kt to implement `EventBinding` interface and rename `CONSOLE_REPLIES` to `CONSOLE_RESPONSES` in Console-Components

--- Crypto-Trader-Library/Console-Library/Console-Components/src/main/kotlin/org/cryptotrader/console/library/component/ConsoleEventBinding.kt
+++ Crypto-Trader-Library/Console-Library/Console-Components/src/main/kotlin/org/cryptotrader/console/library/component/ConsoleEventBinding.kt
@@ -1,6 +1,8 @@
 package org.cryptotrader.console.library.component
 
-enum class ConsoleEventBinding(val bindingName: String) {
+import org.cryptotrader.universal.library.events.model.EventBinding
+
+enum class ConsoleEventBinding(override val bindingName: String) : EventBinding {
     CONSOLE_REQUESTS("consoleRequests-out-0"),
-    CONSOLE_REPLIES("consoleReplies-out-0");
+    CONSOLE_RESPONSES("consoleResponses-out-0");
 }
