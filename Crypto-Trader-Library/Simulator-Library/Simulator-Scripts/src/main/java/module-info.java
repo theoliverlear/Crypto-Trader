8bf8eaa4f77e808ace1baa5dcd14220c0d1f11bf
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2026-05-09 15:33:36 -0500

    Expand module-info.java to include Universal-Models dependency and export `scripts` package in Simulator-Scripts

--- Crypto-Trader-Library/Simulator-Library/Simulator-Scripts/src/main/java/module-info.java
+++ Crypto-Trader-Library/Simulator-Library/Simulator-Scripts/src/main/java/module-info.java
@@ -1,5 +1,9 @@
 module org.cryptotrader.simulator.library.scripts {
     requires kotlin.stdlib;
+
+    requires org.cryptotrader.universal.library.models;
     requires org.cryptotrader.data.library.models;
     requires org.cryptotrader.api.library.models;
+
+    exports org.cryptotrader.simulator.library.scripts;
 }
