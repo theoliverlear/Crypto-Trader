f9f663909c94ee228f2fba50753cf9a06a68f2fa
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2026-05-09 15:25:33 -0500

    Add module dependencies and export configuration in module-info.java for Simulator-Config

--- Crypto-Trader-Library/Simulator-Library/Simulator-Config/src/main/java/module-info.java
+++ Crypto-Trader-Library/Simulator-Library/Simulator-Config/src/main/java/module-info.java
@@ -1,2 +1,17 @@
 module org.cryptotrader.simulator.library.config {
+    requires kotlin.stdlib;
+    requires com.fasterxml.jackson.databind;
+    requires com.fasterxml.jackson.kotlin;
+    requires spring.beans;
+    requires spring.context;
+    requires spring.boot;
+    requires spring.boot.autoconfigure;
+    requires spring.cloud.stream;
+    requires spring.messaging;
+    requires spring.security.config;
+    requires spring.security.web;
+    requires org.cryptotrader.simulator.library.communication;
+    requires org.cryptotrader.simulator.library.services;
+
+    exports org.cryptotrader.simulator.library.config;
 }
