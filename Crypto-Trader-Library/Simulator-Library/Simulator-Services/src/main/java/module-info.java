26b5cb639b8326096ec698db1c09fd7c756ab51c
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2026-05-09 15:32:08 -0500

    Add module dependencies for Kotlin, Spring, and Crypto-Trader libraries in module-info.java for Simulator-Services

--- Crypto-Trader-Library/Simulator-Library/Simulator-Services/src/main/java/module-info.java
+++ Crypto-Trader-Library/Simulator-Library/Simulator-Services/src/main/java/module-info.java
@@ -1,3 +1,16 @@
 module org.cryptotrader.simulator.library.services {
+    requires kotlin.stdlib;
+    requires java.sql;
 
+    requires org.cryptotrader.universal.library.models;
+    requires org.cryptotrader.api.library.models;
+    requires org.cryptotrader.data.library.models;
+    requires org.cryptotrader.data.library.repositories;
+    requires org.cryptotrader.simulator.library.scripts;
+
+    requires spring.boot.starter.data.jpa;
+    requires spring.beans;
+    requires spring.context;
+
+    exports org.cryptotrader.simulator.library.services;
 }
