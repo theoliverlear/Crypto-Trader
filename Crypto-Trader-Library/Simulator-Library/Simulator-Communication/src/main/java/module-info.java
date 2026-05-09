8d48ae54e2966d9fbb31fd5d974ff301e388bce1
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2026-05-09 15:24:56 -0500

    Add Kotlin standard library and response package export in module-info.java for Simulator-Communication

--- Crypto-Trader-Library/Simulator-Library/Simulator-Communication/src/main/java/module-info.java
+++ Crypto-Trader-Library/Simulator-Library/Simulator-Communication/src/main/java/module-info.java
@@ -1,3 +1,5 @@
 module org.cryptotrader.simulator.library.communication {
+    requires kotlin.stdlib;
     exports org.cryptotrader.simulator.library.communication.request;
+    exports org.cryptotrader.simulator.library.communication.response;
 }
