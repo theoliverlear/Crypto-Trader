9e9bb61817350682ebf1cf2f8b65d1023f526360
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2026-05-09 15:26:49 -0500

    Add module dependencies for Spring, Kotlin, SLF4J, and Crypto-Trader libraries in module-info.java for Simulator-Events

--- Crypto-Trader-Library/Simulator-Library/Simulator-Events/src/main/java/module-info.java
+++ Crypto-Trader-Library/Simulator-Library/Simulator-Events/src/main/java/module-info.java
@@ -1,4 +1,12 @@
 module org.cryptotrader.simulator.library.events {
+    requires kotlin.stdlib;
+    requires spring.context;
+    requires org.slf4j;
+    requires org.cryptotrader.universal.library.components;
+    requires org.cryptotrader.simulator.library.communication;
+    requires org.cryptotrader.simulator.library.services;
+    requires org.cryptotrader.universal.library.events;
+
     exports org.cryptotrader.simulator.library.events;
     exports org.cryptotrader.simulator.library.events.publisher;
 }
