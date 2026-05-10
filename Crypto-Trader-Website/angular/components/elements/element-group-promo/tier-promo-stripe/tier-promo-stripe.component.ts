38786c7dcd4478db518792b0162d3cef9feb2f37
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2026-05-10 11:57:25 -0500

    Expand tier-promo-stripe.component.ts to initialize and fetch subscription tier prices using `SubscriptionTierPricesService`

--- Crypto-Trader-Website/angular/components/elements/element-group-promo/tier-promo-stripe/tier-promo-stripe.component.ts
+++ Crypto-Trader-Website/angular/components/elements/element-group-promo/tier-promo-stripe/tier-promo-stripe.component.ts
@@ -1,17 +1,36 @@
 // tier-promo-stripe.component.ts
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
 
-import { TagType } from '@theoliverlear/angular-suite';
+import { TagType } from '@theoliverlear/angular-suite'
 
-import { SubscriptionTier } from '../tier-promo/models/SubscriptionTier';
+import { SubscriptionTier } from '../tier-promo/models/SubscriptionTier'
+import { SubscriptionTierPricesService } from '@http/subscription-tier/subscription-tier-prices.service'
+import { SubscriptionTierPrices } from '@models/promo/types'
 
+/**
+ * A stripe promoting subscription tiers
+ */
 @Component({
     selector: 'tier-promo-stripe',
     standalone: false,
     templateUrl: './tier-promo-stripe.component.html',
     styleUrls: ['./tier-promo-stripe.component.scss'],
 })
-export class TierPromoStripeComponent {
-    protected readonly SubscriptionTier: typeof SubscriptionTier = SubscriptionTier;
-    protected readonly TagType: typeof TagType = TagType;
+export class TierPromoStripeComponent implements OnInit {
+    protected subscriptionTierPrices: SubscriptionTierPrices
+    constructor(private readonly subscriptionTierPricesService: SubscriptionTierPricesService) {}
+
+    /**
+     * Initializes prices on component initialization.
+     */
+    public ngOnInit(): void {
+        this.subscriptionTierPricesService
+            .getTierPrices()
+            .subscribe((prices: SubscriptionTierPrices): void => {
+                this.subscriptionTierPrices = prices
+            })
+    }
+
+    protected readonly SubscriptionTier: typeof SubscriptionTier = SubscriptionTier
+    protected readonly TagType: typeof TagType = TagType
 }
