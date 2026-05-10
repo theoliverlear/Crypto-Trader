142dc46276acadd3a2d66236e8e8f319fab771ed
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2026-05-10 11:57:57 -0500

    Expand types.ts to define subscription tier pricing and features in Crypto-Trader-Website

--- Crypto-Trader-Website/angular/models/promo/types.ts
+++ Crypto-Trader-Website/angular/models/promo/types.ts
@@ -1,4 +1,20 @@
 export type CodeWindow = {
-    title: string,
+    title: string
     code: string
-};
+}
+
+export type SubscriptionTierPrices = {
+    freeTierCost: number
+    proTierCost: number
+    ultimateTierCost: number
+}
+
+export type PossibleSubscriptionTierPrices = SubscriptionTierPrices | null
+
+/**
+ * A feature of a subscription tier.
+ */
+export interface TierFeature {
+    text: string
+    highlight?: boolean
+}
