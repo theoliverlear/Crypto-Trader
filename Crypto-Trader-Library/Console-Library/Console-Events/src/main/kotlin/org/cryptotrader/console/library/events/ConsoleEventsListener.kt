d99c28aadcecab6e1a575c8bc657861b3c2ba825
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2026-05-13 06:25:40 -0500

    Refactor ConsoleEventsListener.kt to extend `BaseEventConsumer` in Console-Events

--- Crypto-Trader-Library/Console-Library/Console-Events/src/main/kotlin/org/cryptotrader/console/library/events/ConsoleEventsListener.kt
+++ Crypto-Trader-Library/Console-Library/Console-Events/src/main/kotlin/org/cryptotrader/console/library/events/ConsoleEventsListener.kt
@@ -5,59 +5,41 @@ import org.cryptotrader.console.library.communication.response.ConsoleCommandRes
 import org.cryptotrader.console.library.component.ConsoleAuthenticationRunner
 import org.cryptotrader.console.library.component.ConsoleEventBinding
 import org.cryptotrader.console.library.services.ConsoleCommandService
+import org.cryptotrader.universal.library.events.BaseEventConsumer
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.cloud.stream.function.StreamBridge
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.integration.support.MessageBuilder
 import org.springframework.messaging.Message
 import java.util.function.Consumer
 
 @Configuration
 open class ConsoleEventsListener @Autowired constructor(
-    private val streamBridge: StreamBridge,
+    streamBridge: StreamBridge,
     private val consoleCommandService: ConsoleCommandService,
     private val consoleAuthenticationRunner: ConsoleAuthenticationRunner
+) : BaseEventConsumer<ConsoleCommandRequest, ConsoleCommandResponse>(
+    streamBridge,
+    ConsoleEventBinding.CONSOLE_RESPONSES.bindingName
 ) {
-    private val log: Logger = LoggerFactory.getLogger(ConsoleEventsListener::class.java)
+    override val log: Logger = LoggerFactory.getLogger(ConsoleEventsListener::class.java)
 
     @Bean(name = ["consoleRequestsConsumer"])
     open fun consoleRequestsConsumer(): Consumer<Message<ConsoleCommandRequest>> {
-        return Consumer { message ->
-            val command: ConsoleCommandRequest = message.payload
-            log.info("Received command: {}", command)
-            val correlationIdHeader = message.headers["correlationId"]
-            val rawAuth: Any? = message.headers["Authorization"]
-            val authorizationHeader: String? = when (rawAuth) {
-                is String -> rawAuth
-                is ByteArray -> try {
-                    String(rawAuth, Charsets.UTF_8)
-                } catch (e: Exception) { null }
-                else -> null
+        return this.createConsumer { request, authorizationHeader ->
+            log.info("Received console command: {}", request)
+            this.consoleAuthenticationRunner.runWithAuthorizationHeader(authorizationHeader) {
+                this.consoleCommandService.executeCommand(request)
             }
-            if (authorizationHeader != null) {
-                val sample = authorizationHeader.take(16) + "..."
-                log.debug("Decoded Authorization header (type={}): {}", rawAuth?.javaClass?.simpleName, sample)
-            } else if (rawAuth != null) {
-                log.debug("Authorization header present but could not decode (type={})", rawAuth.javaClass.simpleName)
-            }
-            val result: ConsoleCommandResponse = try {
-                this.consoleAuthenticationRunner.runWithAuthorizationHeader(authorizationHeader) {
-                    this.consoleCommandService.executeCommand(command)
-                }
-            } catch (exception: Exception) {
-                ConsoleCommandResponse(
-                    "An error occurred: ${exception.message}",
-                    null
-                )
-            }
-            val replyMessage = MessageBuilder
-                .withPayload(result)
-                .setHeader("correlationId", correlationIdHeader)
-                .build()
-            this.streamBridge.send(ConsoleEventBinding.CONSOLE_RESPONSES.bindingName, replyMessage)
         }
+    }
+
+    override fun handleError(exception: Exception): ConsoleCommandResponse {
+        return ConsoleCommandResponse(
+            "An error occurred: ${exception.message}",
+            null
+        )
     }
 }
