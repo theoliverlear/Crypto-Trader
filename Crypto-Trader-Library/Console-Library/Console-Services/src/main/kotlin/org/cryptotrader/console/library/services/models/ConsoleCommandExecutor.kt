e05862809f9eae3f4385c54df295e2ac08747852
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2026-05-13 06:33:36 -0500

    Expand ConsoleCommandExecutor.kt to include command resolution, matching, and execution methods with overloads for request-based inputs in Console-Services

--- Crypto-Trader-Library/Console-Library/Console-Services/src/main/kotlin/org/cryptotrader/console/library/services/models/ConsoleCommandExecutor.kt
+++ Crypto-Trader-Library/Console-Library/Console-Services/src/main/kotlin/org/cryptotrader/console/library/services/models/ConsoleCommandExecutor.kt
@@ -1,7 +1,33 @@
 package org.cryptotrader.console.library.services.models
 
+import org.cryptotrader.console.library.communication.request.ConsoleCommandRequest
 import org.cryptotrader.console.library.communication.response.ConsoleCommandResponse
+import org.cryptotrader.console.library.model.ConsoleCommandRegex
+import org.cryptotrader.console.library.model.exception.CommandNotSupportedException
+import kotlin.jvm.Throws
 
 interface ConsoleCommandExecutor {
-    fun executeCommand(command: String): ConsoleCommandResponse
+    fun isTopLevelCommand(commandInput: String): Boolean
+    fun isTopLevelCommand(commandInput: ConsoleCommandRequest): Boolean
+
+    fun isCommandMatch(command: ConsoleCommandRegex, commandInput: String): Boolean
+    fun isCommandMatch(command: ConsoleCommandRegex, commandInput: ConsoleCommandRequest): Boolean
+
+    @Throws(CommandNotSupportedException::class)
+    fun resolveCommand(commandInput: String): ConsoleCommandRegex
+
+    @Throws(CommandNotSupportedException::class)
+    fun resolveCommand(commandInput: ConsoleCommandRequest): ConsoleCommandRegex
+
+    @Throws(CommandNotSupportedException::class)
+    fun executeCommand(commandInput: String): ConsoleCommandResponse
+
+    @Throws(CommandNotSupportedException::class)
+    fun executeCommand(commandInput: ConsoleCommandRequest): ConsoleCommandResponse
+
+    @Throws(CommandNotSupportedException::class)
+    fun executeCommand(command: ConsoleCommandRegex, commandInput: String): ConsoleCommandResponse
+
+    @Throws(CommandNotSupportedException::class)
+    fun executeCommand(command: ConsoleCommandRegex, commandInput: ConsoleCommandRequest): ConsoleCommandResponse
 }
