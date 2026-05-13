2468677d009c4830c1f0aa44ddeb585f1d32fc0f
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2026-05-13 06:29:35 -0500

    Add module-info.java to define module structure and exports in Console-Scripts

---
+++ Crypto-Trader-Library/Console-Library/Console-Scripts/src/main/java/module-info.java
@@ -0,0 +1,6 @@
+module org.cryptotrader.console.library.scripts {
+    requires kotlin.stdlib;
+    requires org.cryptotrader.console.library.models;
+
+    exports org.cryptotrader.console.library.scripts;
+}
