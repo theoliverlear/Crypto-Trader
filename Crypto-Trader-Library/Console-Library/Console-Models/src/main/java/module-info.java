78b74164981cbfa2a23e042e7e9c820777246390
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2026-05-13 06:29:03 -0500

    Expand module-info.java to export `command` package in Console-Models

--- Crypto-Trader-Library/Console-Library/Console-Models/src/main/java/module-info.java
+++ Crypto-Trader-Library/Console-Library/Console-Models/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 open module org.cryptotrader.console.library.models {
     requires kotlin.stdlib;
-    
+
     exports org.cryptotrader.console.library.model;
     exports org.cryptotrader.console.library.model.exception;
+    exports org.cryptotrader.console.library.model.command;
 }
