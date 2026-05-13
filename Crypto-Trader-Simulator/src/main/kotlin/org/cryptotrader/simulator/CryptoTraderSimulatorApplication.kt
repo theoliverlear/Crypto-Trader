b12a247ec93a09b11bb52506037aa1a76580b5d3
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2026-05-13 18:56:46 -0500

    Expand CryptoTraderSimulatorApplication.kt to configure JPA repositories, entity scanning, and system script calls for currency management

--- Crypto-Trader-Simulator/src/main/kotlin/org/cryptotrader/simulator/CryptoTraderSimulatorApplication.kt
+++ Crypto-Trader-Simulator/src/main/kotlin/org/cryptotrader/simulator/CryptoTraderSimulatorApplication.kt
@@ -1,17 +1,28 @@
 package org.cryptotrader.simulator
 
+import org.cryptotrader.universal.library.component.SystemScripts
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
-@SpringBootApplication(scanBasePackages = [
+@SpringBootApplication(
+    scanBasePackages = [
     "org.cryptotrader.simulator.library.config",
     "org.cryptotrader.simulator.library.services",
+    "org.cryptotrader.data.library.services",
+    "org.cryptotrader.data.library.component",
     "org.cryptotrader.simulator.library.events",
     "org.cryptotrader.data.library.repository",
-    "org.cryptotrader.universal.library.events"
+    "org.cryptotrader.universal.library.events",
+    "org.cryptotrader.universal.library.config"
 ])
+@EnableJpaRepositories(basePackages = ["org.cryptotrader.data.library.repository"])
+@EntityScan(basePackages = ["org.cryptotrader.data.library.entity"])
 class CryptoTraderSimulatorApplication
 
 fun main(args: Array<String>) {
+    SystemScripts.blockCurrencyLoading()
+    SystemScripts.blockCurrencyHarvesting()
     runApplication<CryptoTraderSimulatorApplication>(*args)
 }
