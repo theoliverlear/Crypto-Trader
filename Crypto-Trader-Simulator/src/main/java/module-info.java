ed8ad644a05783fe5706960bcde0cdfa395e6347
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2026-05-13 19:09:27 -0500

    Expand module-info.java to include dependencies for JPA, YAML, cloud streaming, and universal libraries in Crypto-Trader-Simulator

--- Crypto-Trader-Simulator/src/main/java/module-info.java
+++ Crypto-Trader-Simulator/src/main/java/module-info.java
@@ -7,4 +7,14 @@ open module org.cryptotrader.simulator {
     requires org.cryptotrader.simulator.library.services;
     requires org.cryptotrader.simulator.library.events;
     requires org.cryptotrader.logging.library.config;
+    requires org.cryptotrader.universal.library.config;
+    requires org.cryptotrader.data.library.repositories;
+    requires org.cryptotrader.universal.library.components;
+    requires org.cryptotrader.universal.library.events;
+    requires spring.data.jpa;
+
+    requires org.yaml.snakeyaml;
+    requires spring.web;
+    requires jakarta.xml.bind;
+    requires spring.cloud.stream;
 }
