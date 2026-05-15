bb6ecbf4575e6122c22103e6e8adf0f031e9521a
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2026-05-15 18:46:38 -0500

    Expand module-info.java to include `api-communication`, `simulator-communication`, `data-services`, `data-components`, and `slf4j` modules in Simulator-Services

--- Crypto-Trader-Library/Simulator-Library/Simulator-Services/src/main/java/module-info.java
+++ Crypto-Trader-Library/Simulator-Library/Simulator-Services/src/main/java/module-info.java
@@ -4,13 +4,18 @@ module org.cryptotrader.simulator.library.services {
 
     requires org.cryptotrader.universal.library.models;
     requires org.cryptotrader.api.library.models;
+    requires org.cryptotrader.api.library.communication;
     requires org.cryptotrader.data.library.models;
     requires org.cryptotrader.data.library.repositories;
     requires org.cryptotrader.simulator.library.scripts;
+    requires org.cryptotrader.simulator.library.communication;
+    requires org.cryptotrader.data.library.services;
+    requires org.cryptotrader.data.library.components;
 
     requires spring.boot.starter.data.jpa;
     requires spring.beans;
     requires spring.context;
+    requires org.slf4j;
 
     exports org.cryptotrader.simulator.library.services;
 }
