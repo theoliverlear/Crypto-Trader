9a1729e4ce7b172319b91798f029b4453892e519
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2026-05-15 18:58:47 -0500

    Refactor AssetSimulationRequest.kt to change `numShares` and `numDollars` types from `Int` to `Double` in Simulator-Communication

--- Crypto-Trader-Library/Simulator-Library/Simulator-Communication/src/main/kotlin/org/cryptotrader/simulator/library/communication/request/AssetSimulationRequest.kt
+++ Crypto-Trader-Library/Simulator-Library/Simulator-Communication/src/main/kotlin/org/cryptotrader/simulator/library/communication/request/AssetSimulationRequest.kt
@@ -1,11 +1,7 @@
 package org.cryptotrader.simulator.library.communication.request
 
-import java.time.LocalDate
-
 data class AssetSimulationRequest(
     val currencyCode: String,
-    val startDate: LocalDate,
-    val endDate: LocalDate,
-    val numShares: Int,
-    val numDollars: Int,
+    val numShares: Double,
+    val numDollars: Double,
     )
