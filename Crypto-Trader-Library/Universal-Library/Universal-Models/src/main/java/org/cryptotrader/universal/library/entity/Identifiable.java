f507444f27fa7df07c0654ef37c2025f0370a195
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2026-05-15 18:47:39 -0500

    Refactor Identifiable.java to become an abstract generic class with customizable ID type

--- Crypto-Trader-Library/Universal-Library/Universal-Models/src/main/java/org/cryptotrader/universal/library/entity/Identifiable.java
+++ Crypto-Trader-Library/Universal-Library/Universal-Models/src/main/java/org/cryptotrader/universal/library/entity/Identifiable.java
@@ -1,21 +1,6 @@
 package org.cryptotrader.universal.library.entity;
 
-import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
-@MappedSuperclass
-public class Identifiable {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
-    protected Long id;
-    public Identifiable() {
-        this.id = null;
-    }
-    public Identifiable(Long id) {
-        this.id = id;
-    }
+public abstract class Identifiable<T> {
+    public abstract T getId();
+    public abstract void setId(T id);
 }
