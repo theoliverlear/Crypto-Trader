a697f181a1e415bf15986708dd4a71a77f7196f5
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2026-05-15 18:42:55 -0500

    Expand module-info.java to open `request` and `response` packages in Simulator-Communication

--- Crypto-Trader-Library/Simulator-Library/Simulator-Communication/src/main/java/module-info.java
+++ Crypto-Trader-Library/Simulator-Library/Simulator-Communication/src/main/java/module-info.java
@@ -1,5 +1,9 @@
 module org.cryptotrader.simulator.library.communication {
     requires kotlin.stdlib;
+
     exports org.cryptotrader.simulator.library.communication.request;
     exports org.cryptotrader.simulator.library.communication.response;
+
+    opens org.cryptotrader.simulator.library.communication.request;
+    opens org.cryptotrader.simulator.library.communication.response;
 }
