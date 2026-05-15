b0976cef06c74e4da39bae57c2c415eb7584b9ab
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2026-05-15 18:42:12 -0500

    Expand module-info.java to include `universal-models` module in Logging-Models

--- Crypto-Trader-Library/Logging-Library/Logging-Models/src/main/java/module-info.java
+++ Crypto-Trader-Library/Logging-Library/Logging-Models/src/main/java/module-info.java
@@ -2,5 +2,8 @@ open module org.cryptotrader.logging.library.models {
     requires jakarta.persistence;
     requires org.hibernate.orm.core;
     requires static lombok;
+
+    requires org.cryptotrader.universal.library.models;
+
     exports org.cryptotrader.logging.library.entity;
 }
