a42c7a80b05f59ec0996ee982c3a4110a95e87b7
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2026-05-15 18:49:08 -0500

    Add module-info.java to define module requirements and exports in Universal-Services

---
+++ Crypto-Trader-Library/Universal-Library/Universal-Services/src/main/java/module-info.java
@@ -0,0 +1,7 @@
+open module org.cryptotrader.universal.library.services {
+    requires kotlin.stdlib;
+    requires org.cryptotrader.universal.library.models;
+    requires spring.data.jpa;
+
+    exports org.cryptotrader.universal.library.services;
+}
