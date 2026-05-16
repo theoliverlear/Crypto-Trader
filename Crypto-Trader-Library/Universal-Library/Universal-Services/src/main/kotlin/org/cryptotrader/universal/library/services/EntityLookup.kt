cf264cfffd213463048a496a90930986955d140e
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2026-05-15 19:06:09 -0500

    Refactor EntityLookup.kt to replace `IdentifiableEntity` with `Identifiable` in Universal-Services

--- Crypto-Trader-Library/Universal-Library/Universal-Services/src/main/kotlin/org/cryptotrader/universal/library/services/EntityLookup.kt
+++ Crypto-Trader-Library/Universal-Library/Universal-Services/src/main/kotlin/org/cryptotrader/universal/library/services/EntityLookup.kt
@@ -1,9 +1,9 @@
 package org.cryptotrader.universal.library.services
 
-import org.cryptotrader.universal.library.entity.IdentifiableEntity
+import org.cryptotrader.universal.library.entity.Identifiable
 import java.util.Optional
 
-interface EntityLookup<Entity : IdentifiableEntity<Id>, Id> {
+interface EntityLookup<Entity : Identifiable<Id>, Id : Any> {
     fun findById(id: Id): Optional<Entity>
     fun findByIds(ids: List<Id>): List<Entity>
     fun findAll(): List<Entity>
