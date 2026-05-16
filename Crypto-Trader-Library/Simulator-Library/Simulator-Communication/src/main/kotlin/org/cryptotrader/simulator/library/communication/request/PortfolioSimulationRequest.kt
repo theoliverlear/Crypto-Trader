c5c2e1c3421e1f0b22f03e6f0ec9c44165116697
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2026-05-15 19:14:26 -0500

    Expand PortfolioSimulationRequest.kt to include start date and end date for defining simulation duration in Simulator-Communication

--- Crypto-Trader-Library/Simulator-Library/Simulator-Communication/src/main/kotlin/org/cryptotrader/simulator/library/communication/request/PortfolioSimulationRequest.kt
+++ Crypto-Trader-Library/Simulator-Library/Simulator-Communication/src/main/kotlin/org/cryptotrader/simulator/library/communication/request/PortfolioSimulationRequest.kt
@@ -1,5 +1,9 @@
 package org.cryptotrader.simulator.library.communication.request
 
+import java.time.LocalDateTime
+
 data class PortfolioSimulationRequest(
-    val assetSimulationRequests: List<AssetSimulationRequest>
+    val assetSimulationRequests: List<AssetSimulationRequest>,
+    val startDate: LocalDateTime,
+    val endDate: LocalDateTime,
 )
