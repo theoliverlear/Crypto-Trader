6422be438fe1dba5f31ed9b5ef1a26f9467747c5
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2026-05-15 19:12:24 -0500

    Refactor SimulatorEventBinding.kt to rename event bindings for consistency in Simulator-Events

--- Crypto-Trader-Library/Simulator-Library/Simulator-Events/src/main/kotlin/org/cryptotrader/simulator/library/events/SimulatorEventBinding.kt
+++ Crypto-Trader-Library/Simulator-Library/Simulator-Events/src/main/kotlin/org/cryptotrader/simulator/library/events/SimulatorEventBinding.kt
@@ -3,8 +3,8 @@ package org.cryptotrader.simulator.library.events
 import org.cryptotrader.universal.library.events.model.EventBinding
 
 enum class SimulatorEventBinding(override val bindingName: String) : EventBinding {
-    PORTFOLIO_SIMULATION_REQUEST("portfolioSimulationRequest-out-0"),
-    PORTFOLIO_SIMULATION_REPLY("portfolioSimulationReply-out-0"),
-    ASSET_SIMULATION_REQUEST("assetSimulationRequest-out-0"),
-    ASSET_SIMULATION_REPLY("assetSimulationReply-out-0")
+    PORTFOLIO_SIMULATION_REQUESTS("portfolioSimulationRequests-out-0"),
+    PORTFOLIO_SIMULATION_RESPONSES("portfolioSimulationResponses-out-0"),
+    ASSET_SIMULATION_REQUESTS("assetSimulationRequests-out-0"),
+    ASSET_SIMULATION_RESPONSES("assetSimulationResponses-out-0")
 }
