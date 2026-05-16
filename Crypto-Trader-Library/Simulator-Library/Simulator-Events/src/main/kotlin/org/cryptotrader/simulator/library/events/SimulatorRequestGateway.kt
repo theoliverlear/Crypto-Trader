81d42e714d7ce7fb54ed1598b23d12f2108c9bf4
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2026-05-15 19:11:02 -0500

    Refactor SimulatorRequestGateway.kt to simplify implementation by abstracting to super class in Simulator-Events

--- Crypto-Trader-Library/Simulator-Library/Simulator-Events/src/main/kotlin/org/cryptotrader/simulator/library/events/SimulatorRequestGateway.kt
+++ Crypto-Trader-Library/Simulator-Library/Simulator-Events/src/main/kotlin/org/cryptotrader/simulator/library/events/SimulatorRequestGateway.kt
@@ -2,39 +2,17 @@ package org.cryptotrader.simulator.library.events
 
 import org.cryptotrader.simulator.library.communication.request.PortfolioSimulationRequest
 import org.cryptotrader.simulator.library.communication.response.PortfolioSimulationResponse
-import org.cryptotrader.simulator.library.services.SimulatorService
 import org.cryptotrader.universal.library.events.EventPublisher
 import org.cryptotrader.universal.library.events.RequestGatewayController
 import org.cryptotrader.universal.library.events.model.EventBinding
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 import java.time.Duration
-import java.util.concurrent.CompletableFuture
-import java.util.concurrent.ConcurrentHashMap
-
-
 
 @Component
 class SimulatorRequestGateway @Autowired constructor(
     override val eventPublisher: EventPublisher,
-    private val simulatorService: SimulatorService
-) : RequestGatewayController<PortfolioSimulationRequest, PortfolioSimulationResponse> () {
-    override val pendingReplies: MutableMap<String, CompletableFuture<PortfolioSimulationResponse>> =
-        ConcurrentHashMap<String, CompletableFuture<PortfolioSimulationResponse>>()
+) : RequestGatewayController<PortfolioSimulationRequest, PortfolioSimulationResponse>() {
 
-
-    override fun execute(
-        binding: EventBinding,
-        request: PortfolioSimulationRequest
-    ): PortfolioSimulationResponse {
-        return this.execute(binding, request, Duration.ofSeconds(30))
-    }
-
-    override fun execute(
-        binding: EventBinding,
-        request: PortfolioSimulationRequest,
-        timeout: Duration
-    ): PortfolioSimulationResponse {
-        return this.execute(binding, request, timeout, null)
-    }
+    override val defaultTimeout: Duration = Duration.ofSeconds(120)
 }
