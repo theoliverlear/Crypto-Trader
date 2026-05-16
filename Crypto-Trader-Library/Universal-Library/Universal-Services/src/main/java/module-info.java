18b4f3020f3f0513ec9357d39c75d7ef88ee1814
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2026-05-15 21:12:07 -0500

    Add `org.slf4j` dependency to module-info.java in Universal-Services module

--- Crypto-Trader-Library/Universal-Library/Universal-Services/src/main/java/module-info.java
+++ Crypto-Trader-Library/Universal-Library/Universal-Services/src/main/java/module-info.java
@@ -2,6 +2,7 @@ open module org.cryptotrader.universal.library.services {
     requires kotlin.stdlib;
     requires org.cryptotrader.universal.library.models;
     requires spring.data.jpa;
+    requires org.slf4j;
 
     exports org.cryptotrader.universal.library.services;
 }
