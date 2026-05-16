c5d468ebddfff3a8ea6455f9dd85a88bfe58e730
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2026-05-15 21:08:42 -0500

    Refactor PortfolioPictureService.java to replace repository access with entity service usage for better modularity and maintainability in Api-Services

--- Crypto-Trader-Library/Api-Library/Api-Services/src/main/java/org/cryptotrader/api/library/services/ProfilePictureService.java
+++ Crypto-Trader-Library/Api-Library/Api-Services/src/main/java/org/cryptotrader/api/library/services/ProfilePictureService.java
@@ -3,6 +3,7 @@ package org.cryptotrader.api.library.services;
 
 import org.cryptotrader.api.library.entity.user.ProfilePicture;
 import org.cryptotrader.api.library.repository.ProfilePictureRepository;
+import org.cryptotrader.api.library.services.entity.user.ProfilePictureEntityService;
 import org.cryptotrader.api.library.services.models.ProfilePictureOperations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,15 +14,19 @@ import java.util.Optional;
 @Service
 public class ProfilePictureService implements ProfilePictureOperations {
     //============================-Variables-=================================
-    private ProfilePictureRepository profilePictureRepository;
+    private final ProfilePictureRepository profilePictureRepository;
+    private final ProfilePictureEntityService profilePictureEntityService;
     //===========================-Constructors-===============================
     @Autowired
-    public ProfilePictureService(ProfilePictureRepository profilePictureRepository) {
+    public ProfilePictureService(ProfilePictureRepository profilePictureRepository,
+                                 ProfilePictureEntityService profilePictureEntityService) {
         this.profilePictureRepository = profilePictureRepository;
+        this.profilePictureEntityService = profilePictureEntityService;
     }
     //============================-Methods-===================================
     public void saveProfilePicture(ProfilePicture profilePicture) {
-        this.profilePictureRepository.save(profilePicture);
+//        this.profilePictureRepository.save(profilePicture);
+        this.profilePictureEntityService.save(profilePicture);
     }
     @Transactional
     public Optional<ProfilePicture> findByUserId(Long userId) {
