4f16c8b2d763aa3f77459e4d1312c7e4bf44b5d0
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2026-05-16 20:27:02 -0500

    Add spacing between methods in EntityLookup.kt to improve readability in Universal-Services

--- Crypto-Trader-Library/Universal-Library/Universal-Services/src/main/kotlin/org/cryptotrader/universal/library/services/EntityLookup.kt
+++ Crypto-Trader-Library/Universal-Library/Universal-Services/src/main/kotlin/org/cryptotrader/universal/library/services/EntityLookup.kt
@@ -5,9 +5,14 @@ import java.util.Optional
 
 interface EntityLookup<Entity : Identifiable<Id>, Id : Any> {
     fun findById(id: Id): Optional<Entity>
+
     fun findByIds(ids: List<Id>): List<Entity>
+
     fun findAll(): List<Entity>
+
     fun exists(entity: Entity): Boolean
+
     fun existsById(id: Id): Boolean
+
     fun count(): Long
 }
