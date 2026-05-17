8185e465efd0c290d82d424ff9a8fa5507216136
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2026-05-16 21:49:11 -0500

    Expand module-info.java to include additional Spring dependencies and new service exports in Security-Services

--- Crypto-Trader-Library/Security-Library/Security-Services/src/main/java/module-info.java
+++ Crypto-Trader-Library/Security-Library/Security-Services/src/main/java/module-info.java
@@ -5,6 +5,18 @@ open module org.cryptotrader.security.library.services {
     requires inet.ipaddr;
     requires org.apache.tomcat.embed.core;
     requires spring.context;
+    requires org.cryptotrader.security.library.repositories;
+    requires org.cryptotrader.security.library.models;
+    requires spring.beans;
+    requires spring.tx;
+    requires spring.web;
+    requires java.net.http;
+    requires static lombok;
+    requires org.cryptotrader.universal.library.services;
+    requires spring.aop;
+    requires spring.core;
 
     exports org.cryptotrader.security.library.service;
+    exports org.cryptotrader.security.library.service.model;
+    exports org.cryptotrader.security.library.service.entity;
 }
