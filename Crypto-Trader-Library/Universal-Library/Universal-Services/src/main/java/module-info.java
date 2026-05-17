d222c841c1a1b72fe781290c2948e45947aa0097
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2026-05-16 21:49:36 -0500

    Expand module-info.java to include `spring.tx` in Universal-Services

--- Crypto-Trader-Library/Universal-Library/Universal-Services/src/main/java/module-info.java
+++ Crypto-Trader-Library/Universal-Library/Universal-Services/src/main/java/module-info.java
@@ -3,6 +3,7 @@ open module org.cryptotrader.universal.library.services {
     requires org.cryptotrader.universal.library.models;
     requires spring.data.jpa;
     requires org.slf4j;
+    requires spring.tx;
 
     exports org.cryptotrader.universal.library.services;
 }
