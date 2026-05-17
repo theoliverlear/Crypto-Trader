cb0569f44488d2f71b8cf0633c1f8e9d51d9888a
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2026-05-16 21:19:48 -0500

    Refactor interface name from `BannedIpAddressesRepository` to `BannedIpAddressRepository` in BannedIpAddressRepository.kt for naming consistency

--- Crypto-Trader-Library/Security-Library/Security-Repositories/src/main/kotlin/org/cryptotrader/security/library/repository/BannedIpAddressRepository.kt
+++ Crypto-Trader-Library/Security-Library/Security-Repositories/src/main/kotlin/org/cryptotrader/security/library/repository/BannedIpAddressRepository.kt
@@ -3,4 +3,4 @@ package org.cryptotrader.security.library.repository
 import org.cryptotrader.security.library.entity.ip.BannedIpAddress
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface BannedIpAddressesRepository : JpaRepository<BannedIpAddress, Long>
+interface BannedIpAddressRepository : JpaRepository<BannedIpAddress, Long>
