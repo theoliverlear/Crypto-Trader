61c78d5b29901d6faebdc7acfd787c472017a04c
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2026-05-16 21:08:43 -0500

    Correct UniqueCurrencyRepository.java to use String as the ID type instead of Long in Currency-Repositories

--- Crypto-Trader-Library/Data-Library/Data-Repositories/src/main/java/org/cryptotrader/data/library/repository/UniqueCurrencyRepository.java
+++ Crypto-Trader-Library/Data-Library/Data-Repositories/src/main/java/org/cryptotrader/data/library/repository/UniqueCurrencyRepository.java
@@ -3,6 +3,6 @@ package org.cryptotrader.data.library.repository;
 import org.cryptotrader.data.library.entity.currency.UniqueCurrency;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UniqueCurrencyRepository extends JpaRepository<UniqueCurrency, Long> {
+public interface UniqueCurrencyRepository extends JpaRepository<UniqueCurrency, String> {
     boolean existsByCurrency(String currencyCode);
 }
