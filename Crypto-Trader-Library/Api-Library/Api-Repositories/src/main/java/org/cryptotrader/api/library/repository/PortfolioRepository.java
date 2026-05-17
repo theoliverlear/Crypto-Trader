a5d8d2a7a2e82b602700c6be02f43db0c4969bdf
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2026-05-16 21:13:01 -0500

    Add `EntityGraph` annotation to optimize asset loading in PortfolioRepository.java for Api-Repositories

--- Crypto-Trader-Library/Api-Library/Api-Repositories/src/main/java/org/cryptotrader/api/library/repository/PortfolioRepository.java
+++ Crypto-Trader-Library/Api-Library/Api-Repositories/src/main/java/org/cryptotrader/api/library/repository/PortfolioRepository.java
@@ -2,11 +2,13 @@ package org.cryptotrader.api.library.repository;
 //=================================-Imports-==================================
 
 import org.cryptotrader.api.library.entity.portfolio.Portfolio;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PortfolioRepository extends JpaRepository<Portfolio, Long> {
     //=============================-Methods-==================================
 
     //---------------------Find-Portfolio-By-User-Id--------------------------
+    @EntityGraph(attributePaths = {"assets", "assets.currency"})
     Portfolio findPortfolioByUserId(Long userId);
 }
