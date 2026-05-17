d794ae64b3fbd17fcd885d089ca8b4cdee9b6aab
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2026-05-16 20:57:08 -0500

    Expand module-info.java to include additional ORM dependencies and export new `entity` package in Console-Models

--- Crypto-Trader-Library/Console-Library/Console-Models/src/main/java/module-info.java
+++ Crypto-Trader-Library/Console-Library/Console-Models/src/main/java/module-info.java
@@ -1,7 +1,14 @@
 open module org.cryptotrader.console.library.models {
     requires kotlin.stdlib;
+    requires jakarta.persistence;
+    requires org.cryptotrader.universal.library.models;
+    requires org.cryptotrader.api.library.models;
+    requires jakarta.annotation;
+    requires static lombok;
+    requires org.hibernate.orm.core;
 
     exports org.cryptotrader.console.library.model;
     exports org.cryptotrader.console.library.model.exception;
     exports org.cryptotrader.console.library.model.command;
+    exports org.cryptotrader.console.library.entity;
 }
