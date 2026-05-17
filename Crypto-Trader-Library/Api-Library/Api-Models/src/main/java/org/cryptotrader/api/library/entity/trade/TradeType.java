b3abffae40693593b3544342974a29022ee9b55a
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2026-05-16 20:55:02 -0500

    Add TODO in TradeType.java to address duplication in Api-Models

--- Crypto-Trader-Library/Api-Library/Api-Models/src/main/java/org/cryptotrader/api/library/entity/trade/TradeType.java
+++ Crypto-Trader-Library/Api-Library/Api-Models/src/main/java/org/cryptotrader/api/library/entity/trade/TradeType.java
@@ -1,11 +1,12 @@
 package org.cryptotrader.api.library.entity.trade;
 
+// TODO: There is some duplication. Choose one, or specialize.
 public enum TradeType {
     BUY("Buy"),
     SELL("Sell");
-    
+
     private final String name;
-    
+
     private TradeType(String name) {
         this.name = name;
     }
