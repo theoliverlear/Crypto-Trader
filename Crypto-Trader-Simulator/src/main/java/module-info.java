8ee1532168e77aafacc468047739477cc384ee50
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2026-05-16 21:52:06 -0500

    Expand module-info.java to include `security-config` in Crypto-Trader-Simulator

--- Crypto-Trader-Simulator/src/main/java/module-info.java
+++ Crypto-Trader-Simulator/src/main/java/module-info.java
@@ -8,6 +8,7 @@ open module org.cryptotrader.simulator {
     requires org.cryptotrader.simulator.library.events;
     requires org.cryptotrader.logging.library.config;
     requires org.cryptotrader.universal.library.config;
+    requires org.cryptotrader.security.library.config;
     requires org.cryptotrader.data.library.repositories;
     requires org.cryptotrader.universal.library.components;
     requires org.cryptotrader.universal.library.events;
