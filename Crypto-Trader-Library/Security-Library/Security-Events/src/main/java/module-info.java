2a1062846b2cd934138b977b6fefec40d56139b8
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2026-05-16 21:18:54 -0500

    Change `requires` dependency from `repositories` to `services` in module-info.java for Security-Events

--- Crypto-Trader-Library/Security-Library/Security-Events/src/main/java/module-info.java
+++ Crypto-Trader-Library/Security-Library/Security-Events/src/main/java/module-info.java
@@ -2,7 +2,7 @@ open module org.cryptotrader.security.library.events {
     requires kotlin.stdlib;
     requires org.slf4j;
     requires spring.context;
-    requires org.cryptotrader.security.library.repositories;
+    requires org.cryptotrader.security.library.services;
     requires org.cryptotrader.security.library.models;
     requires org.cryptotrader.universal.library.models;
 
