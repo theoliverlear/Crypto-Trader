d78249c7c16d2e544c9a62d5f571dd36d9792126
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2026-05-16 21:47:21 -0500

    Expand module-info.java to include `org.hibernate.orm.core` and export `org.cryptotrader.security.library.model` in Security-Models

--- Crypto-Trader-Library/Security-Library/Security-Models/src/main/java/module-info.java
+++ Crypto-Trader-Library/Security-Library/Security-Models/src/main/java/module-info.java
@@ -4,8 +4,10 @@ open module org.cryptotrader.security.library.models {
     requires kotlin.stdlib;
     requires transitive org.cryptotrader.api.library.models;
     requires org.cryptotrader.universal.library.models;
+    requires org.hibernate.orm.core;
 
     exports org.cryptotrader.security.library.entity.ip;
     exports org.cryptotrader.security.library.entity.key;
+    exports org.cryptotrader.security.library.model;
     exports org.cryptotrader.security.library.model.key;
 }
